12d505d41860a26c5418c57b45e9188456d726cf
Haridu <haridu.naveen@gmail.com>
2016-02-03 14:24:52 -0600

    export as constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,6 @@
 // export your constants
-const types = {
-  LOGIN_REQUEST: 'LOGIN_REQUEST',
-  LOGIN_SUCCESS: 'LOGIN_SUCCESS',
-  LOGIN_FAILURE: 'LOGIN_FAILURE',
-  LOGOUT_REQUEST: 'LOGOUT_REQUEST',
-  LOGOUT_SUCCESS: 'LOGOUT_SUCCESS'
-}
-
-export default types
+  export const LOGIN_REQUEST = 'LOGIN_REQUEST'
+  export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+  export const LOGIN_FAILURE = 'LOGIN_FAILURE'
+  export const LOGOUT_REQUEST = 'LOGOUT_REQUEST'
+  export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
