1be17a7b7aa7492c26556841b7899488dfec1351
Haridu <haridu.naveen@gmail.com>
2016-02-04 16:57:23 -0600

    renamed the directory

---
+++ src/store/configureStore.dev.js
@@ -0,0 +1,27 @@
+import { createStore, compose } from 'redux'
+import { persistState } from 'redux-devtools'
+import rootReducer from '../reducers'
+import DevTools from '../containers/DevTools'
+
+const enhancer = compose(
+  DevTools.instrument(),
+  persistState(
+    window.location.href.match(
+      /[?&]debug_session=([^&]+)\b/
+    )
+  )
+)
+
+export default function configureStore (initialState) {
+  // todo -> create store with middleware
+
+  const store = createStore(rootReducer, initialState, enhancer)
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () =>
+      store.replaceReducer(require('../reducers').default)
+    )
+  }
+
+  return store
+}
