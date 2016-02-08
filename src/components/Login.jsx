c8530ef6fe6fef31aafc7a3276ff9796907be416
Siddhartha Basu <biosidd@gmail.com>
2016-02-08 15:46:42 +0000

    Added a login component with provider buttons Added bootstrap and social buttons css files

---
+++ src/components/Login.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import 'styles/login.scss'
+
+export default class Login extends Component {
+    displayName = 'third party login component';
+    render() {
+        return (
+            <div className="row">
+                <div className="col-sm-4">
+                    <a className="btn btn-block btn-social btn-github">
+                        <i className="fa fa-github"></i> Sign in with GitHub
+                    </a>
+                    <a className="btn btn-block btn-social btn-google">
+                        <i className="fa fa-github"></i> Sign in with Google
+                    </a>
+                    <a className="btn btn-block btn-social btn-facebook">
+                        <i className="fa fa-github"></i> Sign in with Facebook
+                    </a>
+                    <a className="btn btn-block btn-social btn-linkedin">
+                        <i className="fa fa-github"></i> Sign in with Linkedin
+                    </a>
+                    <a className="btn btn-block btn-social btn-twitter">
+                        <i className="fa fa-github"></i> Sign in with Twitter
+                    </a>
+                </div>
+            </div>
+        )
+    }
+}
+
+
