61f0733ccb45781431b317513f8f8cc57d7bf2ac
Siddhartha Basu <biosidd@gmail.com>
2016-02-08 17:34:28 +0000

    Fixed bunch of eslint errors

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,37 +1,36 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import Navbar from '../components/Navbar'
+import Navbar from 'components/Navbar'
 
 class App extends Component {
-
-  render () {
-    const { dispatch, isAuthenticated, errorMessage } = this.props
-    return (
-      <div>
-        <Navbar
-          isAuthenticated={isAuthenticated}
-          errorMessage={errorMessage}
-          dispatch={dispatch}
-        />
-        <div className='container'>
-          <p>Authenticated: {JSON.stringify(isAuthenticated)}</p>
-        </div>
-      </div>
-    )
-  }
+    displayName = 'the primary app component';
+    static propTypes = {
+        dispatch: PropTypes.func.isRequired,
+        isAuthenticated: PropTypes.bool.isRequired,
+        errorMessage: PropTypes.string
+    };
+    render() {
+        const { dispatch, isAuthenticated, errorMessage } = this.props
+        return (
+          <div>
+            <Navbar
+              isAuthenticated={isAuthenticated}
+              errorMessage={errorMessage}
+              dispatch={dispatch}
+            />
+            <div className="container">
+              <p>Authenticated: {JSON.stringify(isAuthenticated)}</p>
+            </div>
+          </div>
+        )
+    }
 }
 
-App.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  isAuthenticated: PropTypes.bool.isRequired,
-  errorMessage: PropTypes.string
-}
-
-const mapStateToProps = state => {
-  return {
-    isAuthenticated: state.isAuthenticated,
-    errorMessage: state.errorMessage
-  }
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.isAuthenticated,
+        errorMessage: state.errorMessage
+    }
 }
 
 export default connect(mapStateToProps)(App)
