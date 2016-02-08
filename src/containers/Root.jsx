a631f40d5ccc0d65bd46ca9c8126d69a649de65f
Siddhartha Basu <biosidd@gmail.com>
2016-02-08 17:26:51 +0000

    proptype verification became mandatory

--- src/containers/Root.jsx
+++ src/containers/Root.jsx
@@ -1,6 +1,21 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./Root.prod')
-} else {
-  module.exports = require('./Root.dev')
+import React, { Component, PropTypes } from 'react'
+import { Provider } from 'react-redux'
+import App from 'containers/App'
+import DevTools from 'containers/DevTools'
+
+export default class Root extends Component {
+    displayName = 'root component';
+    static propTypes = {
+        store: PropTypes.object.isRequired
+    };
+    render() {
+        const { store } = this.props
+        return (
+          <Provider store={store}>
+              <App />
+              <DevTools />
+          </Provider>
+        )
+    }
 }
 
