f3f26e28a785b2785fb7b3216ab79b71ed606ea8
Siddhartha Basu <biosidd@gmail.com>
2016-02-08 18:07:14 +0000

    Fixed eslint errors about curly braces and newlines Import bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,40 @@
 import React, { Component, PropTypes } from 'react'
-import Login from './Login'
-import Logout from './Logout'
+import Login from 'components/Login'
+import Logout from 'components/Logout'
 import { loginUser, logoutUser } from '../actions/auth'
+import 'styles/vendor/bootstrap.css'
 
 export default class Navbar extends Component {
+    displayName = 'navbar display';
+    static propTypes = {
+        dispatch: PropTypes.func.isRequired,
+        isAuthenticated: PropTypes.bool.isRequired,
+        errorMessage: PropTypes.string
+    };
 
-  render () {
-    const { dispatch, isAuthenticated, errorMessage } = this.props
+    render() {
+        const { dispatch, isAuthenticated, errorMessage } = this.props
 
-    return (
-      <nav className='navbar navbar-default'>
-        <div className='container-fluid'>
-          <a className='navbar-brand' href='#'>Auth App</a>
-           <div className='navbar-form'>
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Auth App</a>
+                <div className="navbar-form">
 
-           {!isAuthenticated &&
-             <Login
-               errorMessage={errorMessage}
-               onLoginClick={ creds => dispatch(loginUser(creds)) }
-             />
-           }
+                { !isAuthenticated &&
+                     <Login
+                       errorMessage={ errorMessage }
+                       onLoginClick={ creds => dispatch(loginUser(creds)) }/>
+                }
 
-           {isAuthenticated &&
-             <Logout onLogoutClick={() => dispatch(logoutUser())} />
-           }
+                { isAuthenticated &&
+                    <Logout onLogoutClick={ () => dispatch(logoutUser()) }/>
+                }
 
-         </div>
-       </div>
-     </nav>
-    )
-  }
-
+             </div>
+           </div>
+         </nav>
+        )
+    }
 }
 
-Navbar.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  isAuthenticated: PropTypes.bool.isRequired,
-  errorMessage: PropTypes.string
-}
