f672f37304adba8f5bd8630f2f9db6282bb484b4
Siddhartha Basu <biosidd@gmail.com>
2016-02-17 02:39:41 +0000

    removed everything from the boilerplate that are no longer needed for this project

--- src/store/configureStore.prod.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux'
-import rootReducer from '../reducers'
-
-export default function configureStore (initialState) {
-	// todo -> create store with middleware
-  return createStore(rootReducer, initialState)
-}
