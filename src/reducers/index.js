08b7932814eb048fe08fed5762b421af0906e874
Haridu <haridu.naveen@gmail.com>
2016-03-14 14:17:02 -0500

    adding form reducer to the application

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import auth from 'reducers/auth'
 import { routeReducer } from 'react-router-redux'
+import {reducer as formReducer} from 'redux-form'
 
 const rootReducer = combineReducers({
     auth,
-    routing: routeReducer
+    routing: routeReducer,
+    form: formReducer
 })
 
 export default rootReducer
