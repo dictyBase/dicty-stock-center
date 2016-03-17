41ef9ffe413597c691c7a9ec374fb9490a7fec4c
Haridu <haridu.naveen@gmail.com>
2016-03-17 17:01:11 -0500

    loading component while form is being submitted

---
+++ src/components/FormSubmit.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import 'styles/core.scss'
+
+export default class FormSubmit extends Component {
+    displayName = 'loading component during submitting the order form';
+    render() {
+        return (
+            <div className="container">
+              <h2> Feel the bern while your information is being submitted........ </h2>
+            </div>
+        )
+    }
+}
