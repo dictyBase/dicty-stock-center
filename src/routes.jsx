868002edfe7d37869b9e449567fc0ad27bb5fbd6
Haridu <haridu.naveen@gmail.com>
2016-03-17 16:58:55 -0500

    added form submit loading component route

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,6 +8,7 @@ import Home from 'components/Home'
 import Profile from 'components/Profile'
 import Error from 'components/Error'
 import OrderForm from 'components/OrderForm'
+import FormSubmit from 'components/FormSubmit'
 
 export default (
     <Route path="/" component={ App }>
@@ -18,6 +19,7 @@ export default (
         <Route path="home/profile" component={ Profile } />
         <Route path="error" component={ Error } />
         <Route path="order/form" component={ OrderForm } />
+        <Route path="order/form/submitting" component={ FormSubmit } />
     </Route>
 )
 
