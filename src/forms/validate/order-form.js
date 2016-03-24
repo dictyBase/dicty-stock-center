ea84437957823aa78f265b968d18af3c52308e45
Haridu <haridu.naveen@gmail.com>
2016-03-24 17:33:58 -0500

    adding sync validation for billing section

--- src/forms/validate/order-form.js
+++ src/forms/validate/order-form.js
@@ -37,6 +37,37 @@ const validate = data => {
     if (!data.shipAccountNum) {
         errors.shipAccountNum = 'Required'
     }
+    if (!data.billFirstName) {
+        errors.billFirstName = 'Required'
+    }
+    if (!data.billLastName) {
+        errors.billLastName = 'Required'
+    }
+    if (!data.billOrg) {
+        errors.billOrg = 'Required'
+    }
+    if (!data.billGroup) {
+        errors.billGroup = 'Required'
+    }
+    if (!data.billAddress) {
+        errors.billAddress = 'Required'
+    }
+    if (!data.billCity) {
+        errors.billCity = 'Required'
+    }
+    if (!data.billZip) {
+        errors.billZip = 'Required'
+    }
+    if (!data.billCountry) {
+        errors.billCountry = 'Required'
+    } else if (data.billCountry === 'select') {
+        errors.billCountry = 'Required'
+    }
+    if (!data.billEmail) {
+        errors.billEmail = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(data.billEmail)) {
+        errors.billEmail = 'Invalid email address'
+    }
     return errors
 }
 
