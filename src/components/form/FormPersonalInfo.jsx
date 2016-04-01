56f1a41b98295ac3da501a0ba22bda8eb7cd350e
Haridu <haridu.naveen@gmail.com>
2016-04-01 14:58:43 -0500

    moved organization info into a seperate component

--- src/components/form/FormPersonalInfo.jsx
+++ src/components/form/FormPersonalInfo.jsx
@@ -6,13 +6,11 @@ export default class FormPersonalInfo extends Component {
 
     static propTypes = {
         firstName: PropTypes.object.isRequired,
-        lastName: PropTypes.object.isRequired,
-        org: PropTypes.object.isRequired,
-        group: PropTypes.object.isRequired
+        lastName: PropTypes.object.isRequired
     }
 
     render() {
-        const { firstName, lastName, org, group } = this.props
+        const { firstName, lastName } = this.props
         return (
             <div>
                 <div className="form-group">
@@ -36,30 +34,6 @@ export default class FormPersonalInfo extends Component {
                         <input type="text" className="form-control" { ...lastName } />
                         { lastName.touched && lastName.error &&
                             <div className="text-danger">{ lastName.error }</div>
-                        }
-                    </div>
-                </div>
-                <div className="form-group">
-                    <label className="col-sm-3 control-label">
-                        <span className="text-danger">* </span>
-                        Organization:
-                    </label>
-                    <div className="col-sm-9">
-                        <input type="text" className="form-control" { ...org } />
-                        { org.touched && org.error &&
-                            <div className="text-danger">{ org.error }</div>
-                        }
-                    </div>
-                </div>
-                <div className="form-group">
-                    <label className="col-sm-3 control-label">
-                        <span className="text-danger">* </span>
-                        Lab/Group:
-                    </label>
-                    <div className="col-sm-9">
-                        <input type="text" className="form-control" { ...group } />
-                        { group.touched && group.error &&
-                            <div className="text-danger">{ group.error }</div>
                         }
                     </div>
                 </div>
