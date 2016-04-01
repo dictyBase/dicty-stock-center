53cb107eb16bd8602409bc15b39252ba1749ef10
Haridu <haridu.naveen@gmail.com>
2016-04-01 16:12:33 -0500

    moved email field into FormPersonalInfo component

--- src/components/form/FormContactInfo.jsx
+++ src/components/form/FormContactInfo.jsx
@@ -5,30 +5,17 @@ export default class FormContactInfo extends Component {
     displayName = 'form contact information';
 
     static propTypes = {
-        phone: PropTypes.object.isRequired,
-        email: PropTypes.object.isRequired
+        phone: PropTypes.object.isRequired
     }
 
     render() {
-        const { phone, email } = this.props
+        const { phone } = this.props
         return (
             <div>
                 <div className="form-group">
-                    <label className="col-sm-3 control-label">Phone:</label>
+                    <label className="col-sm-3 control-label">Phone</label>
                     <div className="col-sm-9">
                         <input type="text" className="form-control" { ...phone } />
-                    </div>
-                </div>
-                <div className="form-group">
-                    <label className="col-sm-3 control-label">
-                        <span className="text-danger">* </span>
-                        Email:
-                    </label>
-                    <div className="col-sm-9">
-                        <input type="email" className="form-control" { ...email } />
-                        { email.touched && email.error &&
-                            <div className="text-danger">{ email.error }</div>
-                        }
                     </div>
                 </div>
             </div>
--- src/components/form/FormPersonalInfo.jsx
+++ src/components/form/FormPersonalInfo.jsx
@@ -6,11 +6,12 @@ export default class FormPersonalInfo extends Component {
 
     static propTypes = {
         firstName: PropTypes.object.isRequired,
-        lastName: PropTypes.object.isRequired
+        lastName: PropTypes.object.isRequired,
+        email: PropTypes.object.isRequired
     }
 
     render() {
-        const { firstName, lastName } = this.props
+        const { firstName, lastName, email } = this.props
         return (
             <div>
                 <div className="form-group">
@@ -34,6 +35,18 @@ export default class FormPersonalInfo extends Component {
                         <input type="text" className="form-control" { ...lastName } />
                         { lastName.touched && lastName.error &&
                             <div className="text-danger">{ lastName.error }</div>
+                        }
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-sm-3 control-label">
+                        <span className="text-danger">* </span>
+                        Email:
+                    </label>
+                    <div className="col-sm-9">
+                        <input type="email" className="form-control" { ...email } />
+                        { email.touched && email.error &&
+                            <div className="text-danger">{ email.error }</div>
                         }
                     </div>
                 </div>
