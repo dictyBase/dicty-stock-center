94af3307d9031cca64c7e07095c712afec4f6439
Haridu <haridu.naveen@gmail.com>
2016-04-04 14:51:08 -0500

    created new component for payment information

---
+++ src/components/form/FormPaymentMethod.jsx
@@ -0,0 +1,56 @@
+import React, { Component, PropTypes } from 'react'
+import 'styles/core.scss'
+
+export default class FormPaymentMethod extends Component {
+    displayName = 'payment method for orders';
+
+    static propTypes = {
+        payMethod: PropTypes.object.isRequired,
+        poNum: PropTypes.object.isRequired
+    }
+
+    renderPoNumber = () => {
+        const { poNum } = this.props
+        return (
+            <div className="form-group">
+                <div className="col-sm-offset-3 col-sm-9">
+                    <input type="text" className="form-control" { ...poNum }
+                        placeholder="PO Number"
+                    />
+                </div>
+            </div>
+        )
+    }
+
+    render() {
+        const { payMethod } = this.props
+        return (
+            <div>
+                <div className="form-group">
+                    <label className="col-sm-3 control-label">Payment Method</label>
+                    <div className="col-sm-9">
+                        <label className="radio-inline">
+                            <input type="radio" { ...payMethod } value="Credit"
+                                checked={ payMethod.value === 'Credit' }
+                            />
+                            Credit
+                        </label>
+                        <label className="radio-inline">
+                            <input type="radio" { ...payMethod } value="Wire"
+                                checked={ payMethod.value === 'Wire' }
+                            />
+                            Wire Transfer
+                        </label>
+                        <label className="radio-inline">
+                            <input type="radio" { ...payMethod } value="PO"
+                                checked={ payMethod.value === 'PO' }
+                            />
+                            Purchase Order (PO)
+                        </label>
+                    </div>
+                </div>
+                { payMethod.value === 'PO' && this.renderPoNumber() }
+            </div>
+        )
+    }
+}
