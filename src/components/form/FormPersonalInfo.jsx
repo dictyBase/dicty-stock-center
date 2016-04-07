1d3f420789c9a47a3e51f820ebf31e1274fe9889
Haridu <haridu.naveen@gmail.com>
2016-04-07 17:24:29 -0500

    using the new FormGroupInput component

--- src/components/form/FormPersonalInfo.jsx
+++ src/components/form/FormPersonalInfo.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react'
+import FormGroupInput from './FormGroupInput'
 import 'styles/core.scss'
 
 export default class FormPersonalInfo extends Component {
@@ -14,42 +15,18 @@ export default class FormPersonalInfo extends Component {
         const { firstName, lastName, email } = this.props
         return (
             <div>
-                <div className="form-group">
-                    <label className="col-sm-3 control-label">
-                        <span className="text-danger">* </span>
-                        First Name:
-                    </label>
-                    <div className="col-sm-9">
-                        <input type="text" className="form-control" {...firstName} />
-                        { firstName.touched && firstName.error &&
-                            <div className="text-danger">{ firstName.error }</div>
-                        }
-                    </div>
-                </div>
-                <div className="form-group">
-                    <label className="col-sm-3 control-label">
-                        <span className="text-danger">* </span>
-                        Last Name:
-                    </label>
-                    <div className="col-sm-9">
-                        <input type="text" className="form-control" { ...lastName } />
-                        { lastName.touched && lastName.error &&
-                            <div className="text-danger">{ lastName.error }</div>
-                        }
-                    </div>
-                </div>
-                <div className="form-group">
-                    <label className="col-sm-3 control-label">
-                        <span className="text-danger">* </span>
-                        Email:
-                    </label>
-                    <div className="col-sm-9">
-                        <input type="email" className="form-control" { ...email } />
-                        { email.touched && email.error &&
-                            <div className="text-danger">{ email.error }</div>
-                        }
-                    </div>
-                </div>
+                <FormGroupInput field={ firstName } >
+                    <span className="text-danger">* </span>
+                    First Name:
+                </FormGroupInput>
+                <FormGroupInput field={ lastName } >
+                    <span className="text-danger">* </span>
+                    Last Name:
+                </FormGroupInput>
+                <FormGroupInput field={ email } >
+                    <span className="text-danger">* </span>
+                    Email:
+                </FormGroupInput>
             </div>
         )
     }
