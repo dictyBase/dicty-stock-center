48d2ec097454cd3ee0efedef309a82d7c408faba
Haridu <haridu.naveen@gmail.com>
2016-04-14 15:44:40 -0500

    fixes #12

--- src/reducers/order-form.js
+++ src/reducers/order-form.js
@@ -2,11 +2,27 @@ import types from '../constants'
 
 const { FORM_SUBMIT_REQUEST, FORM_SUBMIT_SUCCESS, FORM_SUBMIT_FAILURE } = types
 
-const initialState = {
-    submitted: false
+const sameAsCustomer = (state) => {
+    const fields = [['firstName', 'payerFirstName'], ['lastName', 'payerLastName'],
+        ['email', 'payerEmail'], ['org', 'payerOrg'], ['group', 'payerGroup'],
+        ['address', 'payerAddress'], ['address2', 'payerAddress2'], ['city', 'payerCity'],
+        ['state', 'payerState'], ['zip', 'payerZip'], ['country', 'payerCountry'],
+        ['phone', 'payerPhone']
+    ]
+    let newState = {}
+    for (let [customer, payer] of fields) {
+        // only if there is a value in the customer field,
+        // then copy the value into the payer field
+        if (state[customer]) {
+            newState[payer] = {
+                value: state[customer].value
+            }
+        }
+    }
+    return Object.assign({}, state, newState)
 }
 
-const orderFormReducer = (state = initialState, action) => {
+const orderFormReducer = (state, action) => {
     switch (action.type) {
     case FORM_SUBMIT_REQUEST:
         return {
@@ -25,6 +41,13 @@ const orderFormReducer = (state = initialState, action) => {
             submitted: false,
             error: action.error
         }
+    case 'redux-form/CHANGE':
+        if (action.field === 'sameAsCustomer' && action.value) {
+            return sameAsCustomer(state)
+        } else if (state.sameAsCustomer && state.sameAsCustomer.value) {
+            return sameAsCustomer(state)
+        }
+        return state
     default:
         return state
     }
