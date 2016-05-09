96ea4d73f3501bda861558764421f95d95d2ecad
Haridu <haridu.naveen@gmail.com>
2016-05-09 17:24:08 -0500

    added api.js file #15

---
+++ src/utils/api.js
@@ -0,0 +1,59 @@
+export const createUser = (url, values) => {
+    let config = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            data: {
+                type: 'user',
+                attributes: {
+                    first_name: values.firstNae,
+                    last_name: values.lastName,
+                    email: values.email,
+                    organization: values.org,
+                    group: values.group,
+                    address: {first: values.address, second: values.address2},
+                    city: values.city,
+                    state: values.state,
+                    zip: values.zip,
+                    country: values.country,
+                    phone: values.phone
+                }
+            }
+        })
+    }
+    return fetch(`${url}/users`, config)
+}
+export const getUser = (url, id) => {
+    let config = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    }
+    return fetch(`${url}/users/${id}`, config)
+}
+
+export const updateUser = (url, values) => {
+    let config = {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            data: {
+                type: 'user',
+                id: values.email,
+                attributes: {
+                    first_name: values.firstNae,
+                    last_name: values.lastName,
+                    email: values.email,
+                    organization: values.org,
+                    group: values.group,
+                    address: {first: values.address, second: values.address2},
+                    city: values.city,
+                    state: values.state,
+                    zip: values.zip,
+                    country: values.country,
+                    phone: values.phone
+                }
+            }
+        })
+    }
+    return fetch(`${url}/users/${values.email}`, config)
+}
