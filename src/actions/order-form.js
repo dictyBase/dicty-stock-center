17a35e8030040265f1d40a45a1bf1d9763df04f6
Haridu <haridu.naveen@gmail.com>
2016-05-09 17:16:29 -0500

    updated shipping form actions #15

--- src/actions/order-form.js
+++ src/actions/order-form.js
@@ -1,40 +1,22 @@
-// import types from '../constants'
-// import { routeActions } from 'react-router-redux'
-// import simpleStorage from 'simplestorage.js'
+import { routeActions } from 'react-router-redux'
+import { createUser, getUser, updateUser } from 'utils/api'
+import types from '../constants'
 
-// const { FORM_SUBMIT_REQUEST, FORM_SUBMIT_SUCCESS } = types
+const { ADD_SHIPPING_INFO } = types
 
-// const submitRequest = () => {
+const addShippingInfo = (consumer, details) => {
+    return {
+        type: ADD_SHIPPING_INFO,
+        initialized: true,
+        consumer,
+        details
+    }
+}
+
+// const addPaymentInfo = paymentInfo => {
 //     return {
-//         type: FORM_SUBMIT_REQUEST,
-//         submitted: false
-//     }
-// }
-
-// const userExists = data => {
-//     return {
-//         type: FORM_SUBMIT_SUCCESS,
-//         submitted: true,
-//         data
-//     }
-// }
-
-// const submitFailure = error => {
-//     return {
-//         type: FORM_SUBMIT_FAILURE,
-//         submitted: false,
-//         error: error
-//     }
-// }
-
-// const submitOrder = (data) => {
-//     return dispatch => {
-//         setTimeout(() => {
-//             dispatch(submitRequest())
-//             simpleStorage.set('formdata', data)
-//             dispatch(submitSuccess(data))
-//             dispatch(routeActions.push('/home'))
-//         }, 2000)
+//         type: ADD_PAYMENT_INFO,
+//         paymentInfo
 //     }
 // }
 
@@ -50,140 +32,50 @@ const json = response => {
     return response.json()
 }
 
-// const submitShippingaddr = values => {
-//     return dispatch => {
-//         let config = {
-//             method: 'POST',
-//             headers: { 'Content-Type': 'application/json' },
-//             body: JSON.stringify({
-//                 content: values
-//             })
-//         }
-//         fetch('http://localhost:3001/users', config)
-//         .then(status)
-//         .then(json)
-//         .then(data => {
-//             console.log('success with json')
-//         })
-//         .catch(error => {
-//             console.log('catch error', error)
-//         })
-//     }
-// }
 
 let server = SERVER
 if (process.env.SERVER) {
     server = process.env.SERVER
 }
 
-const userExists = (email, reject) => {
-    let config = {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
+export const submitShippingInfo = (values, dispatch) => {
+    let shippingDetails = {
+        shipAccount: values.shipAccount,
+        shipAccountNum: values.shipAccountNum,
+        comments: values.comments
     }
-    fetch(`${server}/users/${email}`, config)
-    .then(response => {
-        if (response.status === 200) {
-            console.log('user exist')
-            return Promise.resolve(response, true)
-        } else if (response.status === 404) {
-            console.log('user does not exist')
-            return Promise.resolve(response, false)
-        }
-        return Promise.reject(new Error('Error'))
-    })
-}
-
-const createUser = values => {
-    // todo: create api.js file in util folder
-    let config = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            data: {
-                type: 'user',
-                attributes: {
-                    first_name: values.firstNae,
-                    last_name: values.lastName,
-                    email: values.email,
-                    organization: values.org,
-                    group: values.group,
-                    address: {first: values.address, second: values.address2},
-                    city: values.city,
-                    state: values.state,
-                    zip: values.zip,
-                    country: values.country,
-                    phone: values.phone
-                }
-            }
-        })
-    }
-    fetch(`${server}/users`, config)
-    .then(status)
-    .then(json)
-    .then(data => {
-        return Promise.resolve(data)
-    })
-}
-
-const updateUser = values => {
-    // todo: create api.js file in util folder
-    let config = {
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            data: {
-                type: 'user',
-                id: values.email,
-                attributes: {
-                    first_name: values.firstNae,
-                    last_name: values.lastName,
-                    email: values.email,
-                    organization: values.org,
-                    group: values.group,
-                    address: {first: values.address, second: values.address2},
-                    city: values.city,
-                    state: values.state,
-                    zip: values.zip,
-                    country: values.country,
-                    phone: values.phone
-                }
-            }
-        })
-    }
-    fetch(`${server}/users/${values.email}`, config)
-    .then(status)
-    .then(json)
-    .then(data => {
-        return Promise.resolve(data)
-    })
-}
-
-export const submitShippingAddress = (values, dispatch) => {
-    const email = values.email
-
     return new Promise((resolve, reject) => {
-        userExists(email)
-        .then((response, isThere) => {
-            if (isThere) {
-                updateUser(values)
-                .then(updateResponse => {
-                    console.log('user updated successfully')
-                    // update reducer with state
-                    // go to next view
+        getUser(server, values.email)
+        .then(response => {
+            if (response.status === 200) {
+                return Promise.resolve(true)
+            } else if (response.status === 404) {
+                return Promise.resolve(false)
+            }
+            return Promise.reject(new Error('Error'))
+        })
+        .then(userExists => {
+            if (userExists) {
+                updateUser(server, values)
+                .then(status)
+                .then(json)
+                .then(consumer => {
                     resolve()
+                    dispatch(addShippingInfo(consumer, shippingDetails))
+                    dispatch(routeActions.push('/order/payment'))
                 })
                 .catch(error => {
                     console.log(error)
                     reject({_error: 'User cannot be updated'})
                 })
             } else {
-                createUser(values)
-                .then(createResponse => {
-                    console.log('user created successfully')
-                    // update the reducer with state
-                    // go to next view
+                createUser(server, values)
+                .then(status)
+                .then(json)
+                .then(consumer => {
                     resolve()
+                    dispatch(addShippingInfo(consumer, shippingDetails))
+                    dispatch(routeActions.push('/order/billing'))
                 })
                 .catch(error => {
                     console.log(error)
@@ -193,71 +85,8 @@ export const submitShippingAddress = (values, dispatch) => {
         })
         .catch(error => {
             console.log(error)
-            reject({_error: 'User cannot be located'})
+            reject({_error: 'Fetching user error!'})
         })
     })
 }
 
-
-// fetch('http://localhost:8080/users', config)
-//     .then(response => {
-//         if (response.status >= 200 && response.status < 300) {
-//             console.log('user created. resolve the promise')
-//             console.log(response)
-//             resolve()
-//         } else {
-//             console.log('bad response. reject promise')
-//             console.log(response)
-//             reject({_error: response.status})
-//         }
-//     }) // POST request. server does not repond with a json, so no response.json()
-//     .catch(error => {
-//         console.log('fetching error', error)
-//         reject({_error: error})
-//     })
-
-
-// // Test
-// export const submitShippingAddress = (values, dispatch) => {
-//     return new Promise((resolve, reject) => {
-//         if (values.phone === '75') {
-//             reject({_error: '75 is error'})
-//         }
-//         resolve()
-//     })
-// }
-
-// let server = SERVER
-// if (process.env.SERVER) {
-//     server = process.env.SERVER
-// }
-
-// const submitShippingAddr = (data) => {
-//     return dispatch => {
-//         let config = {
-//             method: 'POST',
-//             headers: { 'Content-Type': 'application/json' },
-//             body: JSON.stringify({
-//                 content: data
-//             })
-//         }
-//         dispatch(submitRequest())
-//         dispatch(routeActions.push('/order/form/submitting'))
-//         fetch(`${server}/orders`, config)
-//         .then(response => {
-//             if (!(response.status >= 200 && response.status < 300)) {
-//                 dispatch(submitFailure(response))
-//                 return
-//             }
-//             dispatch(submitSuccess(response))
-//             dispatch(routeActions.push('/home'))
-//         })
-//         .catch(err => {
-//             dispatch(submitFailure(err))
-//         })
-//     }
-// }
-
-// export const submitShippingAddress = (data, dispatch) => {
-//     dispatch(submitShippingaddr(data))
-// }
