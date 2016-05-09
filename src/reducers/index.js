81d7bd6193e6633aff4f7008412ded3996ec8dde
Haridu <haridu.naveen@gmail.com>
2016-05-09 17:15:09 -0500

    updated order reducer #15

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,14 +2,13 @@ import { combineReducers } from 'redux'
 import auth from 'reducers/auth'
 import { routeReducer } from 'react-router-redux'
 import {reducer as formReducer} from 'redux-form'
-import orderFormReducer from 'reducers/order-form'
+import orderReducer from 'reducers/order-form'
 
 const rootReducer = combineReducers({
     auth,
     routing: routeReducer,
-    form: formReducer.plugin({
-        orderForm: orderFormReducer
-    })
+    form: formReducer,
+    order: orderReducer
 })
 
 export default rootReducer
--- src/reducers/order-form.js
+++ src/reducers/order-form.js
@@ -1,55 +1,24 @@
 import types from '../constants'
 
-const { FORM_SUBMIT_REQUEST, FORM_SUBMIT_SUCCESS, FORM_SUBMIT_FAILURE } = types
+const { ADD_SHIPPING_INFO } = types
 
-const sameAsCustomer = (state) => {
-    const fields = [['firstName', 'payerFirstName'], ['lastName', 'payerLastName'],
-        ['email', 'payerEmail'], ['org', 'payerOrg'], ['group', 'payerGroup'],
-        ['address', 'payerAddress'], ['address2', 'payerAddress2'], ['city', 'payerCity'],
-        ['state', 'payerState'], ['zip', 'payerZip'], ['country', 'payerCountry'],
-        ['phone', 'payerPhone']
-    ]
-    let newState = {}
-    for (let [customer, payer] of fields) {
-        // only if there is a value in the customer field,
-        // then copy the value into the payer field
-        if (state[customer]) {
-            newState[payer] = {
-                value: state[customer].value
-            }
-        }
-    }
-    return Object.assign({}, state, newState)
+const initialState = {
+    initialized: false
 }
 
-const orderFormReducer = (state, action) => {
+const orderReducer = (state = initialState, action) => {
     switch (action.type) {
-    case FORM_SUBMIT_REQUEST:
+    case ADD_SHIPPING_INFO:
         return {
             ...state,
-            submitted: false
+            initialized: true,
+            shippingInfo: {
+                consumer: action.consumer,
+                details: action.details
+            }
         }
-    case FORM_SUBMIT_SUCCESS:
-        return {
-            ...state,
-            submitted: true,
-            data: action.data
-        }
-    case FORM_SUBMIT_FAILURE:
-        return {
-            ...state,
-            submitted: false,
-            error: action.error
-        }
-    case 'redux-form/CHANGE':
-        if (action.field === 'sameAsCustomer' && action.value) {
-            return sameAsCustomer(state)
-        } else if (state.sameAsCustomer && state.sameAsCustomer.value) {
-            return sameAsCustomer(state)
-        }
-        return state
     default:
         return state
     }
 }
-export default orderFormReducer
+export default orderReducer
