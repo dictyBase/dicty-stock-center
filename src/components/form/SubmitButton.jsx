dbc69455eea2d5e369d7e329e7f304c454938eb1
Haridu <haridu.naveen@gmail.com>
2016-05-12 16:00:03 -0500

    created submit button component

---
+++ src/components/form/SubmitButton.jsx
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react'
+import 'styles/core.scss'
+
+export default class SubmitButton extends Component {
+    displayName = 'form submit button';
+
+    static propTypes = {
+        submitting: PropTypes.bool,
+        name: PropTypes.string,
+        icon: PropTypes.string
+    }
+
+    render() {
+        const { submitting, name, icon } = this.props
+        return (
+            <button type="submit"
+                className="btn btn-primary btn-lg btn-block"
+                disabled={ submitting }> { name }
+                { submitting ? <i className="fa fa-spinner fa-pulse fa-fw margin-bottom"></i>
+                    : <i className={ icon && icon } aria-hidden="true"></i>
+                }
+            </button>
+        )
+    }
+}
