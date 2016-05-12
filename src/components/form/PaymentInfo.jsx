403b80d57b5b6f5b790de481d5fbc94af6c94d59
Haridu <haridu.naveen@gmail.com>
2016-05-12 13:10:42 -0500

    adding payer form

--- src/components/form/PaymentInfo.jsx
+++ src/components/form/PaymentInfo.jsx
@@ -1,8 +1,14 @@
 import React, { Component, PropTypes } from 'react'
+import { reduxForm } from 'redux-form'
 import Consumer from './Consumer'
+import User from './User'
 import 'styles/core.scss'
 
-export default class PaymentInfo extends Component {
+export const fields = [ 'firstName', 'lastName', 'email', 'org', 'group',
+    'address', 'address2', 'city', 'state', 'zip', 'country', 'phone',
+    'shipAccount', 'shipAccountNum', 'comments' ]
+
+class PaymentInfo extends Component {
     displayName = 'payment information'
 
     static propTypes = {
@@ -12,6 +18,10 @@ export default class PaymentInfo extends Component {
     render() {
         const { consumer } = this.props.order
         const { editShipping } = this.props.orderActions
+        const {
+            fields: { firstName, lastName, email, org, group, address, address2, city,
+                state, zip, country, phone }
+        } = this.props
         return (
             <div>
                 <div className="row">
@@ -24,7 +34,37 @@ export default class PaymentInfo extends Component {
                         <Consumer consumer={ consumer } edit={ editShipping } />
                     </div>
                 </div>
+                <hr />
+                <div className="row">
+                    <form onSubmit={ '' } className="form-horizontal">
+                        <div className="col-md-6">
+                            <User title = { 'Payer Address' }
+                                firstName = { firstName }
+                                lastName = { lastName }
+                                email = { email }
+                                org ={ org }
+                                group = { group }
+                                address = { address }
+                                address2 = { address2 }
+                                city = { city }
+                                state = { state }
+                                zip = { zip }
+                                country = { country }
+                                phone = { phone }
+                            />
+                        </div>
+                        <div className="col-md-6">
+                            Payment Method
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
 }
+
+export default reduxForm({
+    form: 'payment',
+    fields
+})(PaymentInfo)
+
