8df895ad94872361ecfb20a9bac762fa8880eeec
Haridu <haridu.naveen@gmail.com>
2016-05-11 22:34:28 -0500

    pass order props to its children

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -2,12 +2,22 @@ import React, { Component } from 'react'
 import 'styles/core.scss'
 
 export default class Order extends Component {
-    displayName = 'order parent component';
+    displayName = 'order parent component'
+
+    renderChildren = () => {
+        const { children, order, orderActions } = this.props
+        return React.Children.map(children, (child) => {
+            return React.cloneElement(child, {
+                order: order,
+                orderActions: orderActions
+            })
+        })
+    }
 
     render() {
         return (
             <div className="container">
-                { this.props.children }
+                { this.renderChildren() }
             </div>
         )
     }
