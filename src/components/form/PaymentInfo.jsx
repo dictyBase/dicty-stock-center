6c97872b7ed5491a9bdca111e3c0b16c58907a76
Haridu <haridu.naveen@gmail.com>
2016-05-11 22:37:41 -0500

    updated the component

--- src/components/form/PaymentInfo.jsx
+++ src/components/form/PaymentInfo.jsx
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import Consumer from './Consumer'
 import 'styles/core.scss'
 
 export default class PaymentInfo extends Component {
-    displayName = 'payment information';
+    displayName = 'payment information'
+
+    static propTypes = {
+        order: PropTypes.object
+    }
 
     render() {
+        const { consumer } = this.props.order
+        const { editShipping } = this.props.orderActions
         return (
             <div>
-              <h2 className="page-header">Please enter payment information</h2>
+                <div className="row">
+                    <div className="col-xs-12">
+                        <h2 className="page-header">Please enter payment information</h2>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-12">
+                        <Consumer consumer={ consumer } edit={ editShipping } />
+                    </div>
+                </div>
             </div>
         )
     }
