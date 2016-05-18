315566f694798d0af2429efc53c19615a8e21698
Haridu <haridu.naveen@gmail.com>
2016-05-18 14:06:35 -0500

    fixes #19

---
+++ src/components/form/Submit.jsx
@@ -0,0 +1,83 @@
+import React, { Component, PropTypes } from 'react'
+import SubmitButton from './SubmitButton'
+import EditPanel from './EditPanel'
+import Items from 'components/Items'
+import Panel from 'dicty-react-components/src/Panel'
+import PanelBody from 'dicty-react-components/src/PanelBody'
+import 'styles/core.scss'
+
+export default class Submit extends Component {
+    displayName = 'submit order'
+
+    static propTypes = {
+        order: PropTypes.object
+    }
+
+    render() {
+        // mocking items. order items must be in the state
+        const items = [
+            {id: 'Strain-DBS0238283', name: 'A+/scrA/dis-GFP'},
+            {id: 'Strain-DBS0236054', name: 'DDB_G0268328-'},
+            {id: 'Plasmid-87', name: 'pACA.URA'}
+        ]
+        const { consumer, payer } = this.props.order
+        const { editShipping, editPayment } = this.props.orderActions
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-xs-12">
+                        <h2 className="page-header">Review Your Order</h2>
+                        <p> Please review your order and click "Complete Order"
+                            button to place your order
+                        </p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-8">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <Panel>
+                                    <PanelBody>
+                                        <EditPanel user={ consumer }
+                                            edit={ editShipping }
+                                            title={ ' Ship to:' }
+                                            icon ={ 'fa fa-truck' }
+                                        />
+                                    </PanelBody>
+                                </Panel>
+                            </div>
+                            <div className="col-md-6">
+                                <Panel>
+                                    <PanelBody>
+                                        <EditPanel user={ payer }
+                                            edit={ editPayment }
+                                            title={ ' Payer:' }
+                                            icon = { 'fa fa-credit-card-alt' }
+                                        />
+                                    </PanelBody>
+                                </Panel>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="row">
+                            <div className="col-xs-12">
+                                <Panel>
+                                    <PanelBody>
+                                        <h4><i className="fa fa-shopping-cart"></i> Items</h4>
+                                        <Items items={ items }/>
+                                    </PanelBody>
+                                </Panel>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <SubmitButton name={ 'Complete Order ' }
+                            submitting={ false }
+                            icon = { 'fa fa-check-circle-o' }
+                        />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
