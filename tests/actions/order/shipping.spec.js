2d5cb3e19134aef287853873bf51e94925a0dacf
Haridu <haridu.naveen@gmail.com>
2016-05-25 14:37:44 -0500

    added additional describe function

--- tests/actions/order/shipping.spec.js
+++ tests/actions/order/shipping.spec.js
@@ -1,56 +1,58 @@
 import * as actions from 'actions/order/shipping'
 import types from 'constants'
 
-describe('actions/order', () => {
-    it('should create an action to add shipping information', () => {
-        const user = {
-            data: {
-                type: 'user',
-                id: '25',
-                attributes: {
-                    first_name: 'John',
-                    last_name: 'Smith',
-                    email: 'john@gmail.com',
-                    organization: 'Northwestern',
-                    group: 'Bio',
-                    address: {
-                        first: 'N Lake Shore', second: ''
-                    },
-                    city: 'Chicago',
-                    state: 'IL',
-                    zip: '60848',
-                    country: 'USA',
-                    phone: ''
+describe('actions', () => {
+    describe('order', () => {
+        it('should create an action to add shipping information', () => {
+            const user = {
+                data: {
+                    type: 'user',
+                    id: '25',
+                    attributes: {
+                        first_name: 'John',
+                        last_name: 'Smith',
+                        email: 'john@gmail.com',
+                        organization: 'Northwestern',
+                        group: 'Bio',
+                        address: {
+                            first: 'N Lake Shore', second: ''
+                        },
+                        city: 'Chicago',
+                        state: 'IL',
+                        zip: '60848',
+                        country: 'USA',
+                        phone: ''
+                    }
                 }
             }
-        }
-        const details = {
-            shipAccount: 'UPS',
-            shipAccountNum: '123456',
-            comments: ''
-        }
-        const consumer = {
-            type: 'user',
-            id: '25',
-            firstName: 'John',
-            lastName: 'Smith',
-            email: 'john@gmail.com',
-            org: 'Northwestern',
-            group: 'Bio',
-            address: 'N Lake Shore',
-            address2: '',
-            city: 'Chicago',
-            state: 'IL',
-            zip: '60848',
-            country: 'USA',
-            phone: ''
-        }
-        const expectedAction = {
-            type: types.ADD_SHIPPING,
-            initialized: true,
-            consumer,
-            details
-        }
-        expect(actions.addShipping(user, details)).to.deep.equal(expectedAction)
+            const details = {
+                shipAccount: 'UPS',
+                shipAccountNum: '123456',
+                comments: ''
+            }
+            const consumer = {
+                type: 'user',
+                id: '25',
+                firstName: 'John',
+                lastName: 'Smith',
+                email: 'john@gmail.com',
+                org: 'Northwestern',
+                group: 'Bio',
+                address: 'N Lake Shore',
+                address2: '',
+                city: 'Chicago',
+                state: 'IL',
+                zip: '60848',
+                country: 'USA',
+                phone: ''
+            }
+            const expectedAction = {
+                type: types.ADD_SHIPPING,
+                initialized: true,
+                consumer,
+                details
+            }
+            expect(actions.addShipping(user, details)).to.deep.equal(expectedAction)
+        })
     })
 })
