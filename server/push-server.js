c51d851676a4ac16da8c53b48afcd9479de8ff7a
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2016-05-27 07:18:55 -0500

    added a node js server for handling html5 push state

---
+++ server/push-server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var express = require('express');
+var fs = require('fs');
+var morgan = require('morgan');
+
+var app = express();
+var staticStats = fs.statSync( '/www' );
+if (staticStats.isDirectory()) {
+    app.use(morgan('combined'));
+    app.use('/assets', express.static( '/www' ));
+    app.get('/*', function(req, res, next) {
+        // Just send the index.html for other files to support HTML5Mode
+        res.sendFile('index.html', { root: '/www' });
+    });
+
+    var server = app.listen(9596);
+    console.log('Express static server listening on http://%s:%d', server.address().address,9596);
+}
+else {
+    console.log('No /www folder, did not start the server');
+}
