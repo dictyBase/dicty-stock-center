0fbdac4b3b9794bbf409b862fc0bd5c83cd689d7
Haridu <haridu.naveen@gmail.com>
2016-05-27 16:19:25 -0500

    unit test for select field #27

---
+++ tests/components/form/FormGroupSelect.spec.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import FormGroupSelect from 'components/form/FormGroupSelect'
+import { mount } from 'enzyme'
+
+describe('components', () => {
+    describe('form/FormGroupSelect', () => {
+        let wrapper = mount(<FormGroupSelect
+            field ={ {value: '', touched: true} }
+            list={ ['USA', 'Japan', 'England'] }
+        />)
+
+        it('should render a select element', () => {
+            expect(wrapper.find('select')).to.exist
+        })
+
+        it('should add has-error class when there is a validation error', () => {
+            expect(wrapper.find('.form-group').hasClass('has-error')).to.equal(false)
+            wrapper.setProps({field: {touched: true, error: 'error'}})
+            expect(wrapper.find('.form-group').hasClass('has-error')).to.equal(true)
+        })
+        it('should render validation error messages properly', () => {
+            expect(wrapper.hasClass('help-block')).to.equal(false)
+
+            const field = {value: '', touched: true, error: 'required'}
+            wrapper.setProps({field})
+            expect(wrapper.contains(
+                <div className="help-block">{ field.error }</div>
+            )).to.equal(true)
+        })
+    })
+})
