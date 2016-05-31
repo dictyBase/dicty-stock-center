279d2d1642b5fa253dba084eeaebc8a255547fd4
Haridu <haridu.naveen@gmail.com>
2016-05-31 14:09:55 -0500

    test for ShippingInfo component #27

---
+++ tests/components/form/ShippingInfo.spec.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import ShippingInfo from 'components/form/ShippingInfo'
+import { shallow } from 'enzyme'
+
+describe('components', () => {
+    describe('form/ShippingInfo', () => {
+        const shipAccount = {value: 'Fedex', touched: true}
+        const shipAccountNum = {value: '', touched: false}
+
+        const wrapper = shallow(<ShippingInfo
+            shipAccount={ shipAccount }
+            shipAccountNum={ shipAccountNum }
+        />)
+
+        it('should not render shipAccountNum textbox when "call in" option is selected', () => {
+            expect(wrapper.containsMatchingElement(
+                  <input type="text" className="form-control" />
+            )).to.equal(true)
+
+            wrapper.setProps({shipAccount: {value: 'WillCall', touched: true}})
+
+            expect(wrapper.containsMatchingElement(
+                  <input type="text" className="form-control" />
+            )).to.equal(false)
+        })
+
+        it('should render validation error messages for shipAccount radio buttons properly', () => {
+            expect(wrapper.hasClass('help-block')).to.equal(false)
+
+            const field = {value: '', touched: true, error: 'required'}
+            wrapper.setProps({shipAccount: field})
+            expect(wrapper.contains(
+                <div className="help-block">{ field.error }</div>
+            )).to.equal(true)
+        })
+
+        it('should render validation error messages for shipAccountNum textbox properly', () => {
+            expect(wrapper.hasClass('help-block')).to.equal(false)
+
+            const field = {value: '', touched: true, error: 'number required'}
+            wrapper.setProps({shipAccountNum: field})
+            expect(wrapper.contains(
+                <div className="help-block">{ field.error }</div>
+            )).to.equal(true)
+        })
+    })
+})
