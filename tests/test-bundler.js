eb6a88f439ce15b241924027bcf221d555b03652
Haridu <haridu.naveen@gmail.com>
2016-06-01 14:51:39 -0500

    turned off isparta coverage reporting
    turned off coverage report for now because it was giving an error since we have bootstrap.js file without jquery

--- tests/test-bundler.js
+++ tests/test-bundler.js
@@ -3,6 +3,6 @@ const testsContext = require.context('./', true, /\.spec\.js$/)
 testsContext.keys().forEach(testsContext)
 
 // require all `src/**/*.js` except for `main.js` (for isparta coverage reporting)
-const componentsContext = require.context('../src/', true, /^((?!main).)*\.js$/)
+// const componentsContext = require.context('../src/', true, /^((?!main).)*\.js$/)
 
-componentsContext.keys().forEach(componentsContext)
+// componentsContext.keys().forEach(componentsContext)
