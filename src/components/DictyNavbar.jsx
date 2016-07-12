35a9ef7cf23bf23e412fe87a542f4bb186d3bed7
Haridu <haridu.naveen@gmail.com>
2016-07-12 14:11:48 -0500

    removed custom styles to have default look

--- src/components/DictyNavbar.jsx
+++ src/components/DictyNavbar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Navbar from 'dicty-react-components/src/Navbar'
-import NavItem from 'dicty-react-components/src/NavItem'
+// import NavItem from 'dicty-react-components/src/NavItem'
 import NavbarHeader from 'dicty-react-components/src/NavbarHeader'
 import NavbarItems from 'dicty-react-components/src/NavbarItems'
 import NavbarDropdown from 'dicty-react-components/src/NavbarDropdown'
@@ -10,68 +10,27 @@ export default class DictyNavbar extends Component {
     displayName = 'navigation bar';
     render() {
         const { genomes, tools, explore, research, community } = this.props
-        const navStyle = {
-            backgroundColor: '#1F4484',
-            border: '1px solid #1F4484'
-        }
-        const itemStyle = {
-            color: '#ffffff',
-            fontSize: '18px',
-            ':hover': {
-                color: '#ffffff',
-                backgroundColor: '#1b3c74'
-            },
-            ':focus': {
-                color: '#ffffff',
-                backgroundColor: '#1b3c74'
-            }
-        }
-        const menuItemStyle = {
-            ':hover': {
-                color: '#ffffff',
-                backgroundColor: '#1F4484'
-            },
-            ':focus': {
-                color: '#ffffff',
-                backgroundColor: '#1F4484'
-            },
-            '@media (max-width: 767px)': {
-                color: '#ffffff',
-
-                ':hover': {
-                    color: '#2574A9'
-                }
-            }
-        }
         return (
-            <Navbar navStyle={ navStyle }>
-                <NavbarHeader href="/" name="dictyBase"/>
+            <Navbar navStyle={ {marginTop: '5px'} }>
+                <NavbarHeader href="/" name="Stock Center" />
                 <NavbarItems>
-                    <NavbarDropdown name="Genomes" itemStyle={ itemStyle }>
-                        <DropdownMenu menuItems={ genomes }
-                            menuItemStyle={ menuItemStyle }
-                        />
+                    <NavbarDropdown name="Genomes">
+                        <DropdownMenu menuItems={ genomes } />
                     </NavbarDropdown>
-                    <NavbarDropdown name="Tools" itemStyle={ itemStyle }>
-                        <DropdownMenu menuItems={ tools }
-                            menuItemStyle={ menuItemStyle }
-                        />
+                    <NavbarDropdown name="Tools">
+                        <DropdownMenu menuItems={ tools } />
                     </NavbarDropdown>
-                    <NavbarDropdown name="Explore" itemStyle={ itemStyle }>
-                        <DropdownMenu menuItems={ explore }
-                            menuItemStyle={ menuItemStyle }
-                        />
+                    <NavbarDropdown name="Explore">
+                        <DropdownMenu menuItems={ explore } />
                     </NavbarDropdown>
-                    <NavbarDropdown name="Research" itemStyle={ itemStyle }>
-                        <DropdownMenu menuItems={ research }
-                            menuItemStyle={ menuItemStyle }
-                        />
+                    <NavbarDropdown name="Research">
+                        <DropdownMenu menuItems={ research } />
                     </NavbarDropdown>
-                    <NavItem link="/" title="Dicty Stock Center" itemStyle={ itemStyle }/>
-                    <NavbarDropdown name="Community" itemStyle={ itemStyle }>
-                        <DropdownMenu menuItems={ community }
-                            menuItemStyle={ menuItemStyle }
-                        />
+                    <NavbarDropdown name="Stock Center">
+                        <DropdownMenu menuItems={ community } />
+                    </NavbarDropdown>
+                    <NavbarDropdown name="Community">
+                        <DropdownMenu menuItems={ community } />
                     </NavbarDropdown>
                 </NavbarItems>
             </Navbar>
