51794163b5e3872c0d213178bd472b2e83df0368
Haridu <haridu.naveen@gmail.com>
2016-07-15 13:46:17 -0500

    updated my dsc layout

--- src/components/MyDsc.jsx
+++ src/components/MyDsc.jsx
@@ -1,24 +1,41 @@
 import React, { Component } from 'react'
+import { Grid, Cell } from 'radium-grid'
+import Panel from 'dicty-react-components/src/Panel'
+import PanelHeader from 'dicty-react-components/src/PanelHeader'
+import PanelTitle from 'dicty-react-components/src/PanelTitle'
+import PanelBody from 'dicty-react-components/src/PanelBody'
 import { Link } from 'react-router'
 import 'styles/core.scss'
 
 export default class MyDsc extends Component {
     displayName = 'user profile';
     render() {
-        const { user } = this.props.auth
+        const { user, provider } = this.props.auth
         return (
             <div className="container">
-                <ol className="breadcrumb">
-                  <li><Link to="/home">Home</Link></li>
-                  <li className="active">My Profile</li>
-                </ol>
-                <h2 className="page-header">
-                    My Profile
-                </h2>
-                <ul>
-                    <li>Name: { user.name } </li>
-                    <li>Email: { user.email } </li>
-                </ul>
+                <Grid>
+                    <Cell width="1">
+                        <ol className="breadcrumb">
+                          <li><Link to="/">Home</Link></li>
+                          <li className="active">My DSC</li>
+                        </ol>
+                    </Cell>
+                    <Cell>
+                        <h1 className="page-header">My DSC</h1>
+                    </Cell>
+                    <Cell width="1">
+                        <Panel>
+                            <PanelHeader>
+                                <PanelTitle>Personal Information</PanelTitle>
+                            </PanelHeader>
+                            <PanelBody>
+                                <h3>Name: { user.name }</h3>
+                                <h3>Email: { user.email }</h3>
+                                <h3>Provider: { provider }</h3>
+                            </PanelBody>
+                        </Panel>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
