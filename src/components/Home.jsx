4d4fdda9e21dfa8ffc3aaf6ef7322fca939a091f
Haridu <haridu.naveen@gmail.com>
2016-07-19 14:02:15 -0500

    seperated the data from the view

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,14 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router'
 import { Grid, Cell } from 'radium-grid'
+import { Link } from 'react-router'
 import 'styles/custom.scss'
 
 export default class Home extends Component {
     displayName = 'homepage component';
-    renderUserInfo = () => {
+    renderGreeting = () => {
         const { user } = this.props.auth
         return (
-            <Grid>
-                <Cell>
-                    Hello, { user.name }
-                </Cell>
-                <Cell>
-                    <Link to="home/profile">My Profile</Link>
-                </Cell>
-            </Grid>
+            <span>Hello, { user.name }</span>
         )
     }
     render() {
@@ -37,17 +30,40 @@ export default class Home extends Component {
         materials. Strains are stored at two different locations in liquid nitrogen, either as
         spores or as vegetative amoebae.
         `
-        const availability = {
-            plasmids: 1917,
-            strains: 709,
-            antibodies: 12,
-            cdna: 1
-        }
-        const {plasmids, strains, antibodies, cdna} = availability
+        const links = [
+            {name: 'Contact the DSC', to: ''},
+            {name: 'DSC FAQ', to: ''},
+            {name: 'Nomenclature Guide', to: ''},
+            {name: 'DSC History', to: ''},
+            {name: 'Other Stock Centers', to: ''}
+        ]
+        const info = [
+            {name: 'Order Information', to: '/page/orderInformation'},
+            {name: 'Payment Information', to: '/page/paymentInformation'},
+            {name: 'Deposit Information', to: '/page/depositInformation'}
+        ]
+        const availability = [
+            {name: 'Strains', amount: 709},
+            {name: 'Plasmids', amount: 1917},
+            {name: 'Antibodies', amount: 12},
+            {name: 'cDNA library', amount: 1}
+        ]
+        const downloads = [
+            {name: 'Phenotype Ontology', to: ''},
+            {name: 'Strain Characteristics', to: ''},
+            {name: 'Mutagenesis Methods', to: ''},
+            {name: 'Plasmid Keywords', to: ''}
+        ]
+        const materials = [
+            {name: 'Strain Catalog', to: ''},
+            {name: 'Plasmid Catalog', to: ''},
+            {name: 'Bacterial Strains', to: ''},
+            {name: 'Other Materials', to: ''}
+        ]
         return (
             <div className="container">
                 <Grid cellWidth="1">
-                    <Cell>{ user && this.renderUserInfo() }</Cell>
+                    <Cell>{ user && this.renderGreeting() }</Cell>
                     <Cell>
                         <h1 className="page-header">
                             Welcome to Dicty Stock Center (DSC)
@@ -63,11 +79,13 @@ export default class Home extends Component {
                             <Cell>{ subIntro }</Cell>
                             <Cell>
                                 <div className="panel-dsc panel-blue">
-                                    <a href="#"><h4>Contact the DSC</h4></a>
-                                    <a href="#"><h4>DSC FAQ</h4></a>
-                                    <a href="#"><h4>Nomenclature Guide</h4></a>
-                                    <a href="#"><h4>DSC History</h4></a>
-                                    <a href="#"><h4>Other Stock Centers</h4></a>
+                                    { links.map((link, index) => {
+                                        return (
+                                            <h4 key={ index }>
+                                                <Link to={ link.to }>{ link.name }</Link>
+                                            </h4>
+                                        )
+                                    }) }
                                 </div>
                             </Cell>
                         </Grid>
@@ -76,27 +94,37 @@ export default class Home extends Component {
                         <Grid cellWidth="1">
                             <Cell>
                                 <div className="panel-dsc panel-blue">
-                                    <a href="#"><h4>Order Info</h4></a>
-                                    <a href="#"><h4>Payment Info</h4></a>
-                                    <a href="#"><h4>Deposit Info</h4></a>
+                                    { info.map((link, index) => {
+                                        return (
+                                            <h4 key={ index }>
+                                                <Link to={ link.to }>{ link.name }</Link>
+                                            </h4>
+                                        )
+                                    }) }
                                 </div>
                             </Cell>
                             <Cell>
                                 <div className="panel-dsc panel-gray">
                                     <h4>Availability</h4>
-                                    <h5><strong>{ strains }</strong> Strains</h5>
-                                    <h5><strong>{ plasmids }</strong> Plasmids</h5>
-                                    <h5><strong>{ antibodies }</strong> Antibodies</h5>
-                                    <h5><strong>{ cdna }</strong> cDNA library</h5>
+                                    { availability.map((item, index) => {
+                                        return (
+                                            <h5 key={ index }>
+                                                <strong>{ item.amount }</strong> { item.name }
+                                            </h5>
+                                        )
+                                    }) }
                                 </div>
                             </Cell>
                             <Cell>
                                 <div className="panel-dsc panel-blue">
                                     <h4>Download / View</h4>
-                                    <a href="#"><h5>Phenotype Ontology</h5></a>
-                                    <a href="#"><h5>Strain Characteristics</h5></a>
-                                    <a href="#"><h5>Mutagenesis Methods</h5></a>
-                                    <a href="#"><h5>Plasmid Keywords</h5></a>
+                                    { downloads.map((link, index) => {
+                                        return (
+                                            <h5 key={ index }>
+                                                <Link to={ link.to }>{ link.name }</Link>
+                                            </h5>
+                                        )
+                                    }) }
                                 </div>
                             </Cell>
                         </Grid>
@@ -117,10 +145,13 @@ export default class Home extends Component {
                             </Cell>
                             <Cell>
                                 <div className="panel-dsc panel-gray">
-                                    <a href="#"><h4>Strain Catalog</h4></a>
-                                    <a href="#"><h4>Plasmid Catalog</h4></a>
-                                    <a href="#"><h4>Bacterial Strains</h4></a>
-                                    <a href="#"><h4>Other Materials</h4></a>
+                                { materials.map((link, index) => {
+                                    return (
+                                        <h4 key={ index }>
+                                            <Link to={ link.to }>{ link.name }</Link>
+                                        </h4>
+                                    )
+                                }) }
                                 </div>
                             </Cell>
                         </Grid>
