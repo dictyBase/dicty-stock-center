0528156378863dce54dfd2c8673ffc343e779437
Haridu <haridu.naveen@gmail.com>
2016-07-20 12:02:07 -0500

    added orcid to the sample client config file

--- src/utils/clientConfig.sample.js
+++ src/utils/clientConfig.sample.js
@@ -2,7 +2,8 @@ const clientConfig = {
     facebook: { clientId: 'xxxxxxxxxxxxxxx' },
     google: { clientId: 'xxxxxxxxxxxxxxxxxx' },
     linkedin: { clientId: 'xxxxxxxxxxxxxxxxxx' },
-    dropbox: { clientId: 'xxxxxxxxxxxxxxxxxx' }
+    dropbox: { clientId: 'xxxxxxxxxxxxxxxxxx' },
+    orcid: { clientId: 'xxxxxxxxxxxxxxxxxx' }
 }
 
 export default clientConfig
