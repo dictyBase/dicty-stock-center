aca4d45e7798cc85fd9b9e6ea2806177552ce53d
Haridu <haridu.naveen@gmail.com>
2016-07-25 17:28:51 -0500

    components for static info pages

---
+++ src/components/InfoPage.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import {Editor, EditorState, convertFromRaw} from 'draft-js'
+import simpleStorage from 'simplestorage.js'
+import { Grid, Cell } from 'radium-grid'
+import infoPages from 'utils/infoPages'
+
+export default class InfoPage extends Component {
+    displayName = 'information page component'
+    constructor(props) {
+        super(props)
+        const page = props.routeProps.params.name
+        this.state = {
+            editorState: simpleStorage.get(page) ? EditorState.createWithContent(
+                convertFromRaw(simpleStorage.get(page))
+            ) : EditorState.createWithContent(
+                convertFromRaw(infoPages[page])
+            )
+        }
+    }
+    onClick = (e) => {
+        const { editorState } = this.state
+        const { pageActions, routeProps } = this.props
+        e.preventDefault()
+        pageActions.editInfoPage(
+            editorState.getCurrentContent(),
+            routeProps.params.name
+        )
+    }
+    render() {
+        const { editorState } = this.state
+        return (
+          <div className="container">
+              <Grid cellWidth="1">
+                  <Cell align="right">
+                      <a href="#" onClick={ this.onClick }>
+                        <i className="fa fa-2x fa-pencil" title="Edit page"></i>
+                      </a>
+                  </Cell>
+                  <Cell>
+                      <Editor
+                        editorState={ editorState }
+                        ref="editor"
+                        readOnly
+                      />
+                  </Cell>
+              </Grid>
+          </div>
+        )
+    }
+}
