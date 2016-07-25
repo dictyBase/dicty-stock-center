90ef0e03e0f21cdb397091ed5aa45b871ed6b047
Haridu <haridu.naveen@gmail.com>
2016-07-25 17:28:02 -0500

    added raw data for static info pages
    temporarily keeping raw data in utils folder until server is implemented

---
+++ src/utils/infoPages.js
@@ -0,0 +1,520 @@
+const infoPages = {
+    orders: {
+        entityMap: {},
+        blocks: [
+            {
+                text: 'Ordering Information',
+                type: 'header-one'
+            },
+            {
+                text: 'Dicty Stock Center Distribution Policy',
+                type: 'header-three'
+            },
+            {
+                text: `The DSC fills any request from bona fide teaching or non-profit research \
+institutions. Users from for-profit institutions need to send an email stating \
+that the materials will be used for research only and no profit will be made from \
+DSC materials.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 53,
+                        length: 10,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 98,
+                        length: 10,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled'
+            },
+            {
+                text: `We also expect the recipient to provide a FedEx, UPS, or a DHL billing \
+number to pay for the shipping. If you do not have any shipping account call \
+the DSC +1 (312) 503-4169 and provide Credit Card information to be added to \
+a Shipper's waybill. The credit card card payment for shipping has nothing to \
+do with DSC charges, and is merely a courtesy to customers who do not have a \
+shipping account with one of the listed providers.`,
+                type: 'unstyled'
+            },
+            {
+                text: '',
+                type: 'unstyled'
+            },
+            {
+                text: `The Dicty Stock Center strives to ship strains on an LPB plate, a \
+GYP plate, or in HL5, within a week. We try to bundle orders into one shipment, however, since \
+strains grow at different rates, to insure the best viability, we may ship large \
+orders in multiple shipments as dictated by growth. Plasmids will be sent as bacterial \
+transformants on an LB drug plate, or as DNA on a piece of filter paper. When strains \
+are received they need to be stored in the lab following one of the procedures \
+described. \New users may also consult our DSC FAQ page.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 444,
+                        length: 6,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled'
+            },
+            {
+                text: 'Ordering Procedures for Strains or Plasmids:',
+                type: 'header-three'
+            },
+            {
+                text: '',
+                type: 'unstyled'
+            },
+            {
+                text: 'Step 1:',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 7,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: `To find the item(s) you want, go to the catalogs, or Search the Dicty Stock \
+Center. You can search for strains (including bacterial strains) or plasmids with \
+various search fields.`,
+                type: 'unstyled'
+            },
+            {
+                text: '',
+                type: 'unstyled'
+            },
+            {
+                text: 'Step 2:',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 7,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: `Following searching, click the Name (in blue color) and you will be \
+directed to a new window containing detailed strain or plasmid information.`,
+                type: 'unstyled'
+            },
+            {
+                text: '',
+                type: 'unstyled'
+            },
+            {
+                text: 'Step 3:',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 7,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: `Click Add to Cart at the bottom of the page and the item will be included \
+in the cart. Click the blue shopping cart symbol in the upper right to see what \
+has been ordered so far. Once everything you need is in the shopping cart, click \
+Check Out at the bottom of the page.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 6,
+                        length: 11,
+                        style: 'ITALIC'
+                    },
+                    {
+                        offset: 235,
+                        length: 9,
+                        style: 'ITALIC'
+                    },
+                    {
+                        offset: 6,
+                        length: 11,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 235,
+                        length: 9,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled'
+            },
+            {
+                text: 'Step 4:',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 7,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: `Fill in the shipping information. Please do not forget the billing number \
+for the \shipping charges and the payer information.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 107,
+                        length: 17,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 107,
+                        length: 17,
+                        style: 'ITALIC'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'Note, this payer may be different from the person who places the order!\n',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 71,
+                        style: 'ITALIC'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'Step 5:',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 7,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'You will receive a confirmation at the email address you provided.',
+                type: 'unstyled'
+            },
+            {
+                text: '',
+                type: 'unstyled'
+            },
+            {
+                text: 'Step 6:',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 7,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: `Receive the item(s) 7-10 working days later and if you are satisfied, the \
+payer will receive notifications and bills from NU Core to pay for the materials.`,
+                type: 'unstyled'
+            },
+            {
+                text: '',
+                type: 'unstyled'
+            },
+            {
+                text: 'If you need additional assistance, please e-mail the Dicty Stock Center',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 71,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 53,
+                        length: 18,
+                        style: 'UNDERLINE'
+                    }
+                ],
+                entityRanges: []
+            }
+        ]
+    },
+    deposits: {
+        entityMap: {},
+        blocks: [
+            {
+                text: 'Procedure for Strain and Plasmid Deposits to the Dicty Stock Center',
+                type: 'header-two',
+                depth: 0
+            },
+            {
+                text: `The Dicty Stock Center can accept Dictyostelium strains, plasmids, and \
+other materials relevant for research using Dictyostelium such as antibodies and cDNA or \
+genomic libraries.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 34,
+                        length: 13,
+                        style: 'ITALIC'
+                    },
+                    {
+                        offset: 115,
+                        length: 13,
+                        style: 'ITALIC'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'Depositing Dictyostelium strains',
+                type: 'header-three',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 11,
+                        length: 13,
+                        style: 'ITALIC'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: `Dictyostelium strains can be sent as axenic cultures, frozen cells, \
+colonies on lawns of bacteria, lyophilized spores, or spores in silica gel. If strains are \
+sent on plates, please identify the medium and the bacterial strain that were used. \
+In the case of knockout mutants, we also would like the parental strain and the knockout plasmid.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 13,
+                        style: 'ITALIC'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: `A Strain Deposit Form must be filled for each plasmid deposited and emailed \
+    to the Stock Center prior to shipping.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 2,
+                        length: 19,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'Depositing plasmids',
+                type: 'header-three',
+                depth: 0
+            },
+            {
+                text: `Plasmids can be deposited as either DNA or as a transformed bacterial \
+culture (preferred).`,
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: `A Plasmid Deposit Form must be filled for each plasmid deposited and \
+emailed to the Stock Center prior to shipping.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 3,
+                        length: 20,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'Depositing other materials',
+                type: 'header-three',
+                depth: 0
+            },
+            {
+                text: `Please contact the Dicty Stock Center if you would like to deposit other \
+materials.`,
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'Mailing address',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 15,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'Deposits should be sent to:',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: `Lina Fouad\nDicty Stock Center, Northwestern University\nLurie 7-250\n \
+303 E. Superior St. `,
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'Chicago IL 60611\nTel. (312) 503-4169',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'The Dicty Stock Center FedEx account can be charged to submit strains.',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'Please email the DSC staff to request the account number.',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'Additional Shipping Information',
+                type: 'header-three',
+                depth: 0
+            },
+            {
+                text: `To comply with postal regulations, submitted items should be triple \
+packaged. We have found that sealing lids with paraffin film, wrapping items in plastic wrap, \
+next wrapping items in a bench pad, and then putting everything into a plastic bag \
+keeps biological material well contained. Bubble wrapping of shipments helps keep \
+items from getting crushed in the mail.`,
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: `Axenic cultures should be sent in a box, rather than in an envelope or \
+FedEx "Pak".`,
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: `For international shipments, we have found that it is important to stress \
+on shipping labels and customs forms that Dictyostelium, the bacteria on which it \
+grows, and the bacterial strains carrying plasmids, are non-pathogenic, harmless \
+research organisms, and that the shipment is triple packaged and contains no animal \
+or plant products. Customs also requires a value for the shipments. We use a $10 value.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 116,
+                        length: 13,
+                        style: 'ITALIC'
+                    },
+                    {
+                        offset: 208,
+                        length: 47,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 282,
+                        length: 15,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 311,
+                        length: 27,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            }
+        ]
+    }
+}
+
+export default infoPages
