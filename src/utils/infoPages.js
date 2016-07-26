f2394dcf329ea7368b2f9b43dc5c2c800efab7b7
Haridu <haridu.naveen@gmail.com>
2016-07-26 13:15:08 -0500

    added raw data for payment infomation page

--- src/utils/infoPages.js
+++ src/utils/infoPages.js
@@ -514,7 +514,522 @@ or plant products. Customs also requires a value for the shipments. We use a $10
                 entityRanges: []
             }
         ]
+    },
+    payments: {
+        entityMap: {},
+        blocks: [
+            {
+                text: 'DSC Payment Information',
+                type: 'header-one',
+                depth: 0
+            },
+            {
+                text: 'Dicty Stock Center Fees and Payment Information',
+                type: 'header-three',
+                depth: 0
+            },
+            {
+                text: `The National Institute of Health mandates that fees are charged for Dicty \
+Stock Center (DSC) services. The fees listed below are being charged starting August 10th, 2015. \
+The DSC fills any request from bona fide teaching or non-profit research institutions.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 224,
+                        length: 10,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: `Users from for-profit institutions need to send an email stating that the \
+materials will be used for research only and no profit will be made from DSC materials.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 11,
+                        length: 10,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'Fees',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 4,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 0,
+                        length: 4,
+                        style: 'UNDERLINE'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'Customers from non-profit organizations: ',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 15,
+                        length: 10,
+                        style: 'UNDERLINE'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'Strain                $30.-',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 22,
+                        length: 5,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'Plasmid             $15.-',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 20,
+                        length: 5,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'Other Material   $40.-',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 17,
+                        length: 5,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'Customers from for-profit organizations: ',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 15,
+                        length: 10,
+                        style: 'UNDERLINE'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'Strain              $100.-',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 20,
+                        length: 6,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'Plasmid           $50.-',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 18,
+                        length: 5,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'Other Material $200.-',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 15,
+                        length: 6,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: `Fees only need to be payed if the obtained biological materials meet \
+the expected quality, e.g. strains are growing.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 116,
+                        style: 'ITALIC'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'Payment Information',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 19,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 0,
+                        length: 19,
+                        style: 'UNDERLINE'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'Payments must be made by',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: `Credit Card: convenient payment for everyone through a secure online form; \
+The link to the secure form will be provided on the invoice. Visa, MasterCard and Discover \
+accepted; incurs a 3.5% service fee (charged by the bank).`,
+                type: 'unordered-list-item',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 12,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 185,
+                        length: 16,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 136,
+                        length: 48,
+                        style: 'ITALIC'
+                    },
+                    {
+                        offset: 185,
+                        length: 16,
+                        style: 'ITALIC'
+                    },
+                    {
+                        offset: 202,
+                        length: 22,
+                        style: 'ITALIC'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: `Purchase Order: checks in US $; not accepted if bureaucratic and \
+time-consuming vendor/supplier information is needed.`,
+                type: 'unordered-list-item',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 15,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 32,
+                        length: 12,
+                        style: 'UNDERLINE'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: `Wire Transfer: for international customers; Northwestern bank information \
+will be sent by request, may incur a bank fee.`,
+                type: 'unordered-list-item',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 14,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: `Each DSC order needs to be billed to one dedictated person (payer) who \
+handles payments for the group or laboratory. We need the following information from you \
+at the TIME YOU ORDER:`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 60,
+                        length: 5,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'The lab/group name',
+                type: 'ordered-list-item',
+                depth: 0
+            },
+            {
+                text: 'The NAME and EMAIL address of the Payer',
+                type: 'ordered-list-item',
+                depth: 0
+            },
+            {
+                text: 'The payment method: Credit Card, Purchase Order, or Wire Transfer',
+                type: 'ordered-list-item',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 20,
+                        length: 11,
+                        style: 'ITALIC'
+                    },
+                    {
+                        offset: 33,
+                        length: 14,
+                        style: 'ITALIC'
+                    },
+                    {
+                        offset: 52,
+                        length: 13,
+                        style: 'ITALIC'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [],
+                entityRanges: []
+            },
+            {
+                text: `We need this complete information to process any order. Soon this \
+information will be automatically reqired when placing an order.`,
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: `The Northwestern billing system is NU Core. The PAYER will receive \
+several emails through the NU Core system:`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 35,
+                        length: 7,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'An email informing that an account has been added with the option to login.',
+                type: 'unordered-list-item',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 27,
+                        length: 7,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: 'A notification to review the charges for the order (optional).',
+                type: 'unordered-list-item',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 18,
+                        length: 18,
+                        style: 'BOLD'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: `The invoice. The PDF file starts with NU-DSC followed by a series of \
+numbers. A link to secure Credit Card payment is included on the invoice.`,
+                type: 'unordered-list-item',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 4,
+                        length: 7,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 88,
+                        length: 26,
+                        style: 'UNDERLINE'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: 'Waiver',
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 0,
+                        length: 6,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 0,
+                        length: 6,
+                        style: 'UNDERLINE'
+                    }
+                ],
+                entityRanges: []
+            },
+            {
+                text: '',
+                type: 'unstyled',
+                depth: 0
+            },
+            {
+                text: `Although all DSC users are expected to pay for stocks, fees for use in \
+academic research may be waived under certain circumstances. If stocks will be used for academic \
+research or teachings and you do not have grant or institutional funds (including start-up) \
+with which to pay the fees, a waiver may be granted. To be considered, submit your DSC request \
+and add the comment that you are applying for a waiver. Then send a brief explanation of \
+your situation to dictystockcs. We still need to add payer information to NU Core, so please \
+supply lab/group and payer name when you order, although the payment may be waived and will \
+show as $0.00-. Instead of payment method please say waiver requested.`,
+                type: 'unstyled',
+                depth: 0,
+                inlineStyleRanges: [
+                    {
+                        offset: 462,
+                        length: 13,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 682,
+                        length: 16,
+                        style: 'BOLD'
+                    },
+                    {
+                        offset: 462,
+                        length: 13,
+                        style: 'UNDERLINE'
+                    },
+                    {
+                        offset: 497,
+                        length: 17,
+                        style: 'ITALIC'
+                    },
+                    {
+                        offset: 656,
+                        length: 14,
+                        style: 'ITALIC'
+                    }
+                ],
+                entityRanges: []
+            }
+        ]
     }
+
 }
 
 export default infoPages
