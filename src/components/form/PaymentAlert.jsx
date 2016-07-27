b69fe7ead6f5c5e8294b7bbb6b010aa19560902b
Haridu <haridu.naveen@gmail.com>
2016-07-27 17:20:56 -0500

    updated the payment alert

--- src/components/form/PaymentAlert.jsx
+++ src/components/form/PaymentAlert.jsx
@@ -17,7 +17,8 @@ export default class PaymentAlert extends Component {
             <div>
                 <div className="alert alert-info">
                     <p><strong>Credit Card: </strong>
-                        Secure payment when billed
+                        Secure payment when billed.
+                        Incurs a <strong>3.5% service fee.</strong>
                     </p>
                     <p><strong>Wire Transfer: </strong>
                         Northwestern bank information will be emailed
