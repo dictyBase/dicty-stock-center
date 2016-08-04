6b869e0a1df8edf0f3dfe549dfd0500d3a2fa6ec
Haridu <haridu.naveen@gmail.com>
2016-08-04 16:24:19 -0500

    removed commented out section

--- src/components/EntityToolbar.jsx
+++ src/components/EntityToolbar.jsx
@@ -1,25 +1,3 @@
-// import React from 'react';
-
-// import StyleButton from './StyleButton';
-
-// export default function EntityControls(props) {
-//   let { entityControls } = props;
-//   var currentStyle = props.editorState.getCurrentInlineStyle();
-//   return (
-//     <div className="RichEditor-controls">
-//       {entityControls.map(type =>
-//         <StyleButton
-//           key={type.label}
-//           active={currentStyle.has(type.style)}
-//           label={type.label}
-//           onToggle={type. action}
-//         />
-//       )}
-//     </div>
-//   );
-// }
-
-
 import React, { Component, PropTypes } from 'react'
 import ToolItem from 'components/ToolItem'
 
