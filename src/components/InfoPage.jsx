12607dc0d91ed08f4f39e9444c06cb21ff58c10f
Haridu <haridu.naveen@gmail.com>
2016-08-08 15:23:33 -0500

    display the previous editor info

--- src/components/InfoPage.jsx
+++ src/components/InfoPage.jsx
@@ -4,6 +4,7 @@ import simpleStorage from 'simplestorage.js'
 import { Grid, Cell } from 'radium-grid'
 import findEntities from 'utils/findEntities'
 import Link from 'components/Link'
+import timeSince from 'utils/timeSince'
 
 // import raw data from utils
 import infoPages from 'utils/infoPages'
@@ -18,7 +19,31 @@ export default class InfoPage extends Component {
                 component: Link
             }
         ])
+
+        // name of the page
         const page = props.routeProps.params.name
+
+        // temp page content json data structure.
+        // must come through props. from the server.
+        this.pageContent = {
+            data: {
+                type: 'page',
+                id: 'orderInfo',
+                attributes: {
+                    content: infoPages[page],
+                    lastEdited: {
+                        author: {
+                            name: 'John Smith',
+                            role: 'curator'
+                        },
+                        time: '2016-08-08T14:30:00'
+                    }
+                }
+            }
+        }
+
+        // if there is raw draftjs data for the specific page in localstorage,
+        // then display that. Otherwise display the default page content.
         this.state = {
             editorState: simpleStorage.get(page) ? EditorState.createWithContent(
                 convertFromRaw(simpleStorage.get(page)), decorator
@@ -38,13 +63,32 @@ export default class InfoPage extends Component {
     }
     render() {
         const { editorState } = this.state
+        const { lastEdited } = this.pageContent.data.attributes
         return (
           <div className="container">
               <Grid cellWidth="1">
-                  <Cell align="right">
-                      <a href="#" onClick={ this.onClick }>
-                        <i className="fa fa-2x fa-pencil" title="Edit page"></i>
-                      </a>
+                  <Cell>
+                      <div className="toolbar-nav">
+                      <Grid cellWidth="1/2">
+                        <Cell align="left">
+                            <span className="text-info">
+                                <strong>
+                                  <i className="fa fa-user"></i> { lastEdited.author.name }
+                                </strong> edited { timeSince(lastEdited.time) } ago
+                            </span>
+                        </Cell>
+                        <Cell align="right">
+                            <div>
+                                <span className="label label-primary ">
+                                  { lastEdited.author.role }
+                                </span> &nbsp; &nbsp;
+                                <a href="#" onClick={ this.onClick }>
+                                  <i className="fa fa-pencil" title="Edit page"></i>
+                                </a>
+                            </div>
+                        </Cell>
+                      </Grid>
+                      </div>
                   </Cell>
                   <Cell>
                       <Editor
