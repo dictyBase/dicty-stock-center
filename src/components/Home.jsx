1832947704bc6ec6f861ad39625db3711aa584ab
Haridu <haridu.naveen@gmail.com>
2016-08-11 10:45:44 -0500

    broken down into components and moved data outside of the component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Grid, Cell } from 'radium-grid'
-import { Link } from 'react-router'
 import InlineEditor from './editor/InlineEditor'
+import LinkList from './LinkList'
 
 import 'styles/custom.scss'
 
@@ -9,6 +9,9 @@ export default class Home extends Component {
     displayName = 'homepage component';
     constructor(props) {
         super(props)
+    }
+    componentDidMount() {
+        this.props.pageActions.fetchHomepage()
     }
     renderGreeting = () => {
         const { user } = this.props.auth
@@ -18,69 +21,18 @@ export default class Home extends Component {
     }
     render() {
         const { user } = this.props.auth
-        const intro = `
-The DSC is a rapidly growing central repository for Dictyostelium discoideum strains \
-and those of related species, plasmids, commonly used food bacteria, and other materials \
-such as antibodies. The DSC is located at Northwestern University in Chicago, IL, USA.`
-        const subIntro = `
-The collection is being built by requesting published strains and plasmids. We encourage \
-and also periodically remind investigators to send new mutants, natural isolates, and \
-plasmids, once they have been published. We do regular quality checks, however, a large \
-component of the quality control program consists of feedback from the recipients of \
-materials. DSC orders are placed through a shopping cart system and are filled in the \
-order they are received.`
-
-        const rawContent = {
-            entityMap: {},
-            blocks: [
-                {
-                    text: subIntro,
-                    type: 'unstyled'
-                }
-            ]
-        }
-        const links = [
-            {name: 'Contact the DSC', to: ''},
-            {name: 'DSC FAQ', to: ''},
-            {name: 'Nomenclature Guide', to: ''},
-            {name: 'DSC History', to: ''},
-            {name: 'Other Stock Centers', to: ''}
-        ]
-        const info = [
-            {name: 'Order Information', to: '/orders/information'},
-            {name: 'Payment Information', to: '/payments/information'},
-            {name: 'Deposit Information', to: '/deposits/information'}
-        ]
-        const availability = [
-            {name: 'Strains', amount: 1927},
-            {name: 'Plasmids', amount: 882},
-            {name: 'Antibodies', amount: 12},
-            {name: 'cDNA library', amount: 1},
-            {name: 'Genomic library', amount: 1}
-        ]
-        const downloads = [
-            {name: 'Phenotype Ontology', to: ''},
-            {name: 'Strain Characteristics', to: ''},
-            {name: 'Mutagenesis Methods', to: ''},
-            {name: 'Plasmid Keywords', to: ''}
-        ]
-        const materials = [
-            {name: 'Strain Catalog', to: ''},
-            {name: 'Plasmid Catalog', to: ''},
-            {name: 'Bacterial Strains', to: ''},
-            {name: 'Other Materials', to: ''}
-        ]
+        const { content, title } = this.props.page
         return (
             <div className="container">
                 <Grid cellWidth="1">
                     <Cell>{ user && this.renderGreeting() }</Cell>
                     <Cell>
                         <h1 className="page-header">
-                            Welcome to Dicty Stock Center (DSC)
+                            { title }
                         </h1>
                     </Cell>
                     <Cell>
-                        <p>{ intro }</p>
+                        <p>{ content.intro }</p>
                     </Cell>
                 </Grid>
                 <Grid smallCellWidth="1">
@@ -89,17 +41,11 @@ order they are received.`
                             <Cell>
                                 <InlineEditor
                                   auth={ this.props.auth }
-                                  rawContent={ rawContent }/>
+                                  rawContent={ content.about }/>
                             </Cell>
                             <Cell>
                                 <div className="panel-dsc panel-blue">
-                                    { links.map((link, index) => {
-                                        return (
-                                            <h4 key={ index }>
-                                                <Link to={ link.to }>{ link.name }</Link>
-                                            </h4>
-                                        )
-                                    }) }
+                                    <LinkList list={ content.links }/>
                                 </div>
                             </Cell>
                         </Grid>
@@ -108,19 +54,13 @@ order they are received.`
                         <Grid cellWidth="1">
                             <Cell>
                                 <div className="panel-dsc panel-blue">
-                                    { info.map((link, index) => {
-                                        return (
-                                            <h4 key={ index }>
-                                                <Link to={ link.to }>{ link.name }</Link>
-                                            </h4>
-                                        )
-                                    }) }
+                                    <LinkList list={ content.info }/>
                                 </div>
                             </Cell>
                             <Cell>
                                 <div className="panel-dsc panel-gray">
                                     <h4>Availability</h4>
-                                    { availability.map((item, index) => {
+                                    { content.availability.map((item, index) => {
                                         return (
                                             <h5 key={ index }>
                                                 <strong>{ item.amount }</strong> { item.name }
@@ -131,14 +71,7 @@ order they are received.`
                             </Cell>
                             <Cell>
                                 <div className="panel-dsc panel-blue">
-                                    <h4>Download / View</h4>
-                                    { downloads.map((link, index) => {
-                                        return (
-                                            <h5 key={ index }>
-                                                <Link to={ link.to }>{ link.name }</Link>
-                                            </h5>
-                                        )
-                                    }) }
+                                    <LinkList list={ content.downloads } title="Download / View"/>
                                 </div>
                             </Cell>
                         </Grid>
@@ -159,13 +92,7 @@ order they are received.`
                             </Cell>
                             <Cell>
                                 <div className="panel-dsc panel-gray">
-                                { materials.map((link, index) => {
-                                    return (
-                                        <h4 key={ index }>
-                                            <Link to={ link.to }>{ link.name }</Link>
-                                        </h4>
-                                    )
-                                }) }
+                                    <LinkList list={ content.materials } />
                                 </div>
                             </Cell>
                         </Grid>
