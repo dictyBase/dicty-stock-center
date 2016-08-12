c753dac01ad4dd91743fbef5c54d5b64c87802c2
Haridu <haridu.naveen@gmail.com>
2016-08-12 16:57:22 -0500

    receive stock center props

--- src/components/Home/Availability.jsx
+++ src/components/Home/Availability.jsx
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import 'styles/custom.scss'
 
 export default class Availability extends Component {
     displayName = 'stock center availability'
+    static propTypes = {
+        stockCenter: PropTypes.array.isRequired,
+        stockCenterActions: PropTypes.object.isRequired
+    }
+    componentDidMount() {
+        console.log('componentDidMount')
+        const { stockCenterActions } = this.props
+        stockCenterActions.fetchAvailability()
+    }
     render() {
-        const availability = [
-            {name: 'Strains', amount: 1927},
-            {name: 'Plasmids', amount: 882},
-            {name: 'Antibodies', amount: 12},
-            {name: 'cDNA library', amount: 1},
-            {name: 'Genomic library', amount: 1}
-        ]
+        // const availability = [
+        //     {name: 'Strains', amount: 1927},
+        //     {name: 'Plasmids', amount: 882},
+        //     {name: 'Antibodies', amount: 12},
+        //     {name: 'cDNA library', amount: 1},
+        //     {name: 'Genomic library', amount: 1}
+        // ]
+        const { availability } = this.props.stockCenter
         return (
             <div className="panel-dsc panel-gray">
                 <h4>Availability</h4>
