5721260b296e29d667bf34239470575d97bd8929
Haridu <haridu.naveen@gmail.com>
2016-08-23 14:26:43 -0500

    actions creators for fetching strain list

--- src/actions/stockCenter.js
+++ src/actions/stockCenter.js
@@ -1,7 +1,8 @@
+import React from 'react'
 import types from 'constants'
 import availability from 'fake-data/availability'
 
-const { AVAILABILITY_FETCH_SUCCESS } = types
+const { AVAILABILITY_FETCH_SUCCESS, STRAINS_FETCH_REQUEST, STRAINS_FETCH_SUCCESS } = types
 
 // const requestAvailability = () => {
 //     return {
@@ -9,12 +10,104 @@ const { AVAILABILITY_FETCH_SUCCESS } = types
 //         isFetching: true
 //     }
 // }
+const strainList = {
+    rows: [
+        {
+            availability: true,
+            strain_descriptor: 'acaA-',
+            strain_names: 'aca-, acaA-(r)',
+            systematic_name: 'DG1108',
+            id: 'DBS0252577',
+            add_to_cart: (
+              <button
+                className="btn btn-primary"
+                onClick={ () => { console.log('clicked') } }>
+                  Add to cart
+              </button>
+            )
+        },
+        {
+            availability: true,
+            strain_descriptor: 'abcB4-',
+            strain_names: 'AK00010',
+            systematic_name: 'DBS0350146',
+            id: 'DBS0350146',
+            add_to_cart: (
+              <button
+                className="btn btn-primary"
+                onClick={ () => { console.log('clicked') } }>
+                  Add to cart
+              </button>
+            )
+        }
+    ],
+    columns: [
+        {
+            property: 'availability',
+            header: {
+                label: 'Availability'
+            }
+        },
+        {
+            property: 'strain_descriptor',
+            header: {
+                label: 'Strain Descriptor'
+            }
+        },
+        {
+            property: 'strain_names',
+            header: {
+                label: 'Strain Names'
+            }
+        },
+        {
+            property: 'systematic_name',
+            header: {
+                label: 'Systematic Name'
+            }
+        },
+        {
+            property: 'id',
+            header: {
+                label: 'Strain ID'
+            }
+        },
+        {
+            property: 'add_to_cart',
+            header: {
+                label: ''
+            }
+        }
+    ]
+}
+
+const requestStrains = () => {
+    return {
+        type: STRAINS_FETCH_REQUEST
+    }
+}
+
+const receiveStrains = (data) => {
+    return {
+        type: STRAINS_FETCH_SUCCESS,
+        data
+    }
+}
 
 const receiveAvailability = (data) => {
     return {
         type: AVAILABILITY_FETCH_SUCCESS,
         isFetching: false,
         availability: data.attributes.availability
+    }
+}
+
+export const fetchStrainList = () => {
+    return (dispatch) => {
+        dispatch(requestStrains())
+        setTimeout(() => {
+            dispatch(receiveStrains(strainList))
+        }, 1000)
     }
 }
 
