38e38bb28a0b4069a551d18a5cb4ffca6bbb5447
Haridu <haridu.naveen@gmail.com>
2016-08-24 16:06:10 -0500

    moved data to 'fake-data' directory

--- src/actions/stockCenter.js
+++ src/actions/stockCenter.js
@@ -1,6 +1,6 @@
-import React from 'react'
 import types from 'constants'
 import availability from 'fake-data/availability'
+import strainList from 'fake-data/strains'
 
 const { AVAILABILITY_FETCH_SUCCESS, STRAINS_FETCH_REQUEST, STRAINS_FETCH_SUCCESS } = types
 
@@ -10,69 +10,6 @@ const { AVAILABILITY_FETCH_SUCCESS, STRAINS_FETCH_REQUEST, STRAINS_FETCH_SUCCESS
 //         isFetching: true
 //     }
 // }
-const strainList = {
-    rows: [
-        {
-            availability: true,
-            strain_descriptor: 'acaA-',
-            strain_names: 'aca-, acaA-(r)',
-            systematic_name: 'DG1108',
-            id: 'DBS0252577'
-        },
-        {
-            availability: true,
-            strain_descriptor: 'abcB4-',
-            strain_names: 'AK00010',
-            systematic_name: 'DBS0350146',
-            id: 'DBS0350146'
-        }
-    ],
-    columns: [
-        {
-            property: 'availability',
-            header: {
-                label: 'Availability'
-            }
-        },
-        {
-            property: 'strain_descriptor',
-            header: {
-                label: 'Strain Descriptor'
-            }
-        },
-        {
-            property: 'strain_names',
-            header: {
-                label: 'Strain Names'
-            }
-        },
-        {
-            property: 'systematic_name',
-            header: {
-                label: 'Systematic Name'
-            }
-        },
-        {
-            property: 'id',
-            header: {
-                label: 'Strain ID'
-            }
-        },
-        {
-            cell: {
-                format: (value, { rowData }) => (
-                  <button
-                    className="btn btn-primary"
-                    onClick={ () => { console.log(`${JSON.stringify(rowData, null, 2)}`) } }>
-                      Add to cart
-                  </button>
-                )
-            },
-            width: 200,
-            visible: true
-        }
-    ]
-}
 
 const requestStrains = () => {
     return {
---
+++ src/fake-data/strains.js
@@ -0,0 +1,39 @@
+const strainList = [
+    {
+        availability: 'Yes',
+        strain_descriptor: 'acaA-',
+        strain_names: 'aca-, acaA-(r)',
+        systematic_name: 'DG1108',
+        id: 'DBS0252577'
+    },
+    {
+        availability: 'Yes',
+        strain_descriptor: 'dfbcdsaB4-',
+        strain_names: 'AK00010',
+        systematic_name: 'DBS0350146',
+        id: 'DBS032350146'
+    },
+    {
+        availability: 'No',
+        strain_descriptor: 'dfdscB4-',
+        strain_names: 'dhjf44',
+        systematic_name: 'SDFJ745',
+        id: 'DBS038350146'
+    },
+    {
+        availability: 'Yes',
+        strain_descriptor: 'sdasda33-',
+        strain_names: 'jfgkhjs3',
+        systematic_name: 'ABFKF84',
+        id: 'DBS03350146'
+    },
+    {
+        availability: 'Yes',
+        strain_descriptor: 'sada2332-',
+        strain_names: 'kjsjks4',
+        systematic_name: 'KFDJR48',
+        id: 'DBS02350146'
+    }
+]
+
+export default strainList
