a44402052726f4e607c3f9c05e479279b6a18e67
Haridu <haridu.naveen@gmail.com>
2016-08-24 17:11:27 -0500

    made property names more simple

--- src/fake-data/strains.js
+++ src/fake-data/strains.js
@@ -1,37 +1,37 @@
 const strainList = [
     {
-        availability: 'Yes',
-        strain_descriptor: 'acaA-',
-        strain_names: 'aca-, acaA-(r)',
-        systematic_name: 'DG1108',
+        available: true,
+        descriptor: 'acaA-',
+        names: 'aca-, acaA-(r)',
+        systematicName: 'DG1108',
         id: 'DBS0252577'
     },
     {
-        availability: 'Yes',
-        strain_descriptor: 'dfbcdsaB4-',
-        strain_names: 'AK00010',
-        systematic_name: 'DBS0350146',
+        available: false,
+        descriptor: 'dfbcdsaB4-',
+        names: 'AK00010',
+        systematicName: 'DBS0350146',
         id: 'DBS032350146'
     },
     {
-        availability: 'No',
-        strain_descriptor: 'dfdscB4-',
-        strain_names: 'dhjf44',
-        systematic_name: 'SDFJ745',
+        available: true,
+        descriptor: 'dfdscB4-',
+        names: 'dhjf44',
+        systematicName: 'SDFJ745',
         id: 'DBS038350146'
     },
     {
-        availability: 'Yes',
-        strain_descriptor: 'sdasda33-',
-        strain_names: 'jfgkhjs3',
-        systematic_name: 'ABFKF84',
+        available: true,
+        descriptor: 'sdasda33-',
+        names: 'jfgkhjs3',
+        systematicName: 'ABFKF84',
         id: 'DBS03350146'
     },
     {
-        availability: 'Yes',
-        strain_descriptor: 'sada2332-',
-        strain_names: 'kjsjks4',
-        systematic_name: 'KFDJR48',
+        available: false,
+        descriptor: 'sada2332-',
+        names: 'kjsjks4',
+        systematicName: 'KFDJR48',
         id: 'DBS02350146'
     }
 ]
