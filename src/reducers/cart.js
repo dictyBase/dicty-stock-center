51feeec4b7f9f0ad1811924c2f260ddff089d023
Haridu <haridu.naveen@gmail.com>
2016-08-24 17:12:31 -0500

    add item to cart test

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -12,8 +12,8 @@ const cartReducer = (state = initialState, action) => {
     switch (action.type) {
     case ADD_TO_CART:
         return {
-            ...state
-            // return state
+            ...state,
+            item: action.item
         }
     default:
         return state
