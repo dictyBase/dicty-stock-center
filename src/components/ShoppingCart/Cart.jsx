c12151875f4985e1b74dfa14aa63681052cc7270
Haridu <haridu.naveen@gmail.com>
2016-08-30 00:04:21 -0500

    updated the design of the shopping cart

--- src/components/ShoppingCart/Cart.jsx
+++ src/components/ShoppingCart/Cart.jsx
@@ -1,39 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { Grid, Cell } from 'radium-grid'
-// import Panel from 'dicty-react-components/src/Panel'
-// import PanelBody from 'dicty-react-components/src/PanelBody'
-// import PanelHeader from 'dicty-react-components/src/PanelHeader'
-// import PanelTitle from 'dicty-react-components/src/PanelTitle'
 import 'styles/core.scss'
 
 export default class Cart extends Component {
-    displayName = 'Shopping cart items';
+    displayName = 'Shopping cart items'
+    static propTypes = {
+        item: PropTypes.object.isRequired,
+        cartActions: PropTypes.object.isRequired
+    }
 
     render() {
+        const { item, cartActions } = this.props
         return (
-                    <div>
-                    <hr />
+                <div className="container">
                     <Grid verticalAlign="middle" >
-                        <Cell width="5/8" smallWidth="1">
-                            <h2>DBS982374</h2>
-                            <h5>strainid</h5>
+                        <Cell width="2/4" smallWidth="1" smallAlign="center">
+                            <h3>{ item.name } <small> { item.id }</small></h3>
                         </Cell>
-                        <Cell width="1/8" smallWidth="1/3" align="right">
-                            <input type="number"
-                              className="form-control"
-                              placeholder="Quantity" />
+                        <Cell width="1/4" smallWidth="1/2" align="right" smallAlign="center">
+                            <h3 className="text-success">{ item.fee }</h3>
                         </Cell>
-                        <Cell width="1/8" smallWidth="1" align="right">
-                            <h3 className="text-success">$30.00</h3>
-                        </Cell>
-                        <Cell width="1/8" smallWidth="1" align="right">
-                            <button type="button" className="btn btn-default">
+                        <Cell width="1/4" smallWidth="1/2" align="right" smallAlign="center">
+                            <button type="button"
+                              className="btn btn-default"
+                              onClick={ cartActions.removeItem }>
                                 <i className="fa fa-trash-o"></i>
                             </button>
                         </Cell>
                     </Grid>
-                    <hr/>
-                    </div>
+                    <hr />
+                </div>
         )
     }
 }
--- src/components/ShoppingCart/index.jsx
+++ src/components/ShoppingCart/index.jsx
@@ -1,26 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import Cart from './Cart'
 import { Grid, Cell } from 'radium-grid'
 import 'styles/core.scss'
 
 export default class ShoppingCart extends Component {
-    displayName = 'Shopping cart';
-
+    displayName = 'Shopping cart'
+    static propTypes = {
+        cart: PropTypes.object,
+        cartActions: PropTypes.object
+    }
+    renderAlert = () => {
+        return (
+            <Cell width="1">
+                <div className="container">
+                    <div className="alert alert-danger">
+                      <strong>
+                            <i className="fa fa-exclamation-circle"></i>
+                      </strong> There are no items in your cart.
+                    </div>
+                </div>
+            </Cell>
+        )
+    }
     render() {
+        const { cart, cartActions } = this.props
         return (
             <div className="container">
-                <Grid cellWidth="1">
-                    <Cell align="center">
+                <Grid>
+                    <Cell align="center" width="1">
                           <h1 className="dicty-header">Shopping Cart</h1>
                     </Cell>
-                    <Cell>
-                        <Cart />
-                    </Cell>
-                    <Cell>
-                        <Cart />
-                    </Cell>
-                    <Cell>
-                        <Cart />
+                    <Cell width="1" >
+                        <Grid>
+                        { cart.addedItems.length > 0 ? (
+                            cart.addedItems.map((item, index) => {
+                                return (
+                                    <Cell key={ index } width="1">
+                                        <Cart
+                                          item={ item }
+                                          cartActions={ cartActions }
+                                        />
+                                    </Cell>
+                                )
+                            })
+                            ) : this.renderAlert()
+                        }
+                        </Grid>
                     </Cell>
                 </Grid>
             </div>
