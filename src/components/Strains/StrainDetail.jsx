e0dfecbba8346435fa92860d1e65769c82300a69
el-komandante <rudydeberry@hotmail.com>
2017-01-02 14:09:00 -0600

    created initial responsive detail table #82

--- src/components/Strains/StrainDetail.jsx
+++ src/components/Strains/StrainDetail.jsx
@@ -4,13 +4,13 @@ import 'styles/custom.scss'
 
 const Row = (props) => {
     return (
-      <div style={ {flexGrow: 1, flexBasis: 0, display: 'flex', width: '100%', borderBottom: '1px solid grey'} }>
-        <div style={ {display: 'flex', flexGrow: 1, flexBasis: 0, width: '50%'} }>
-          <div style={ {padding: '5px 0px 5px 10px', width: '33.3333%', height: '100%', borderRight: '1px solid grey'} }>{ Object.keys(props.left)[0] }</div>
+      <div className="strain-detail-row" style={ {flexGrow: 1, flexBasis: 0, display: 'flex', width: '100%', justifyContent: 'center', alignItems: 'center'} }>
+        <div className="strain-detail-item" style={ {display: 'flex', flexGrow: 1, flexBasis: 0, width: '100%', borderBottom: '1px solid grey', height: '100%'} }>
+          <div style={ {padding: '5px 0px 5px 10px', width: '33.3333%', height: '100%', borderRight: '1px solid grey'} }><b>{ Object.keys(props.left)[0] }</b></div>
           <div style={ {padding: '5px 0px 5px 10px', width: '66.66666%', height: '100%', borderRight: '1px solid grey'} }>{ Object.values(props.left)[0] }</div>
         </div>
-        <div style={ {display: 'flex', flexGrow: 1, flexBasis: 0, width: '50%'} }>
-          <div style={ {padding: '5px 0px 5px 10px', width: '33.3333%', height: '100%', borderRight: '1px solid grey'} }>{ Object.keys(props.right)[0] }</div>
+        <div className="strain-detail-item" style={ {display: 'flex', flexGrow: 1, flexBasis: 0, width: '100%', borderBottom: '1px solid grey', height: '100%'} }>
+          <div style={ {padding: '5px 0px 5px 10px', width: '33.3333%', height: '100%', borderRight: '1px solid grey'} }><b>{ Object.keys(props.right)[0] }</b></div>
           <div style={ {padding: '5px 0px 5px 10px', width: '66.66666%'} }>{ Object.values(props.right)[0] }</div>
         </div>
       </div>
