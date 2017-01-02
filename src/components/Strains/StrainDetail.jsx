c263724e903a14e7bc50e0c8597662d53523551f
el-komandante <rudydeberry@hotmail.com>
2017-01-02 02:54:51 -0600

    refactored page to use custom row component

--- src/components/Strains/StrainDetail.jsx
+++ src/components/Strains/StrainDetail.jsx
@@ -1,53 +1,21 @@
 import React, { Component } from 'react'
 import { Grid, Cell } from 'radium-grid'
-import { AutoSizer, List, Table } from 'react-virtualized'
-import styled from 'styled-components'
 import 'styles/custom.scss'
 
-const StrainGrid = styled(Grid)`
-    max-width: 80%;
-    min-width: 0%;
-    margin: 0 auto;
-`
-const DataLabel = styled(Cell)`
-    margin-top: 2px;
-    margin-bottom: 2px;
-    background: grey;
-`
-const DataCell = styled(Cell)`
-    padding: 10px;
-    margin-top: 2px;
-    margin-bottom: 2px;
-    background: grey;
-`
-
-// const Row = (props) => {
-//     let displayName = 'row'
-//     return (
-//         <div style={ {height: 50, display: 'flex', alignItems: 'center', borderBottom: '1px solid grey', borderLeft: '1px solid grey', borderRight: '1px solid grey'} }>
-//           <div style={ {flexGrow: 0.5, flexBasis: 0, lineHeight: '50px', paddingLeft: 10, borderRight: '1px solid grey', fontSize: '1.25em'} }>
-//             { props.titles[0] }
-//           </div>
-//           <div style={ {flexGrow: 1, flexBasis: 0, lineHeight: '50px', paddingLeft: 10, paddingRight: 10, borderRight: '1px solid grey'} }>
-//             { props.data[0] }
-//           </div>
-//           <div style={ {flexGrow: 0.5, flexBasis: 0, lineHeight: '50px', paddingLeft: 10, borderRight: '1px solid grey', fontSize: '1.25em'} }>
-//             { props.titles[1] }
-//           </div>
-//           <div style={ {flexGrow: 1, flexBasis: 0, lineHeight: '50px', paddingLeft: 10, paddingRight: 10} }>
-//             { props.data[1] }
-//           </div>
-//         </div>
-//     )
-    // return (
-    //   <Grid width="1/2" style={ {background: props.even ? '#f3f3f3' : 'white', borderBottom: '1px solid grey', borderLeft: '1px solid grey', borderRight: '1px solid grey'} }>
-    //     <Cell width="1/6" style={ {paddingLeft: 2, borderRight: '1px solid grey', textAlign: 'center'} }><h4>{ props.titles[0] }</h4></Cell>
-    //     <Cell width="1/3" style={ {padding: 10, borderRight: '1px solid grey', textAlign: 'center'} }>{ props.data[0] }</Cell>
-    //     <Cell width="1/6" style={ {paddingLeft: 2, borderRight: '1px solid grey'} }><h4>{ props.titles[1] }</h4></Cell>
-    //     <Cell width="1/3" style={ {padding: 10} }>{ props.data[1] }</Cell>
-    //   </Grid>
-    // )
-// }
+const Row = (props) => {
+    return (
+      <div style={ {flexGrow: 1, flexBasis: 0, display: 'flex', width: '100%', borderBottom: '1px solid grey'} }>
+        <div style={ {display: 'flex', flexGrow: 1, flexBasis: 0, width: '50%'} }>
+          <div style={ {padding: '5px 0px 5px 10px', width: '33.3333%', height: '100%', borderRight: '1px solid grey'} }>{ Object.keys(props.left)[0] }</div>
+          <div style={ {padding: '5px 0px 5px 10px', width: '66.66666%', height: '100%', borderRight: '1px solid grey'} }>{ Object.values(props.left)[0] }</div>
+        </div>
+        <div style={ {display: 'flex', flexGrow: 1, flexBasis: 0, width: '50%'} }>
+          <div style={ {padding: '5px 0px 5px 10px', width: '33.3333%', height: '100%', borderRight: '1px solid grey'} }>{ Object.keys(props.right)[0] }</div>
+          <div style={ {padding: '5px 0px 5px 10px', width: '66.66666%'} }>{ Object.values(props.right)[0] }</div>
+        </div>
+      </div>
+    )
+}
 
 export default class StrainDetail extends Component {
     displayName = 'strain detail'
@@ -73,111 +41,26 @@ export default class StrainDetail extends Component {
           {'_blank': 'asdf '}
         ]
         return (
-          <div className="container">
-            <Grid cellWidth="1">
+          <div className="strain-details">
+            <Grid cellWidth="1" style={ {width: '85%'} }>
               <Cell align="center">
                     <h1 className="dicty-header">{ this.props.params.id }</h1>
               </Cell>
-              <Cell align="center" style={ {background: '#15317e', color: 'white'} }>
-                <h3>Strain Details</h3>
-              </Cell>
             </Grid>
-            <List
-              height={ 50 * data1.length }
-              rowHeight={ 50 }
-              rowCount={ data1.length }
-              width={ 570 }
-              style={ {border: '.5px solid grey', margin: '0 auto', display: 'inline-block'} }
-              rowRenderer={ ({index, key, style}) => {
-                  const dataKey = Object.keys(data1[index])[0]
-                  let border = {
-                      // borderTop: index === 0 && '1px solid grey',
-                      borderBottom: '1px solid grey'
-                  }
-                  return (
-                    <div key={ key } style={ {lineHeight: '50px', ...style, ...border} }>
-                      <div style={ {fontSize: '1.25em', width: '33.3333333%', display: 'inline-block', borderRight: '1px solid grey', paddingLeft: 5, height: '100%'} }>{ dataKey }</div>
-                      <div style={ {display: 'inline-block', paddingLeft: 10, height: '100%'} }>{ data1[index][dataKey] }</div>
-                    </div>
-                  )
-            } }
-          />
-          <List
-            height={ 50 * data2.length }
-            rowHeight={ 50 }
-            rowCount={ data2.length }
-            width={ 570 }
-            style={ {border: '.5px solid grey', margin: '0 auto', display: 'inline-block'} }
-            rowRenderer={ ({index, key, style}) => {
-                const dataKey = Object.keys(data2[index])[0]
-                let border = {
-                    // borderTop: index === 0 && '1px solid grey',
-                    borderBottom: '1px solid grey'
-                }
-                return (
-                  <div key={ key } style={ {lineHeight: '50px', ...style, ...border} }>
-                    <div style={ {fontSize: '1.25em', width: '33.3333333%', display: 'inline-block', borderRight: '1px solid grey', paddingLeft: 5, height: '100%'} }>{ dataKey }</div>
-                    <div style={ {display: 'inline-block', paddingLeft: 10, height: '100%'} }>{ data2[index][dataKey] }</div>
-                  </div>
-                )
-            } }
-          />
+            <div align="center" style={ {padding: 10, maxWidth: '85%', background: '#15317e', color: 'white', margin: '0 auto', textAlign: 'center'} }>
+              <h3>Strain Details</h3>
+            </div>
+            <div className="detail-container" style={ {maxWidth: '85%', minWidth: '40%', margin: '0 auto', borderTop: '1px solid grey', borderLeft: '1px solid grey', borderRight: '1px solid grey'} }>
+              <Row left={ data1[0] } right={ data2[0] } />
+              <Row left={ data1[1] } right={ data2[1] } />
+              <Row left={ data1[2] } right={ data2[2] } />
+              <Row left={ data1[3] } right={ data2[3] } />
+              <Row left={ data1[4] } right={ data2[4] } />
+              <Row left={ data1[5] } right={ data2[5] } />
+              <Row left={ data1[6] } right={ data2[6] } />
+              <Row left={ data1[7] } right={ data2[7] } />
+            </div>
           </div>
-      )
+        )
     }
-    // render() {
-    //     return (
-    //         <div className="strain-detail-grid" style={ {overflowX: 'scroll', width: '85%', margin: '0 auto'} }>
-    //           <Grid cellWidth="1">
-    //             <Cell align="center">
-    //                   <h1 className="dicty-header">{ this.props.params.id }</h1>
-    //             </Cell>
-    //             <Cell align="center" style={ {background: '#15317e', color: 'white'} }>
-    //               <h3>Strain Details</h3>
-    //             </Cell>
-    //           </Grid>
-    //           <Row
-    //             even={ false }
-    //             titles={ ['Strain Descriptor', 'Strain ID'] }
-    //             data={ ['γS18', this.props.params.id] }
-    //           />
-    //           <Row
-    //             even
-    //             titles={ ['Strain Names', 'Systematic Name'] }
-    //             data={ ['gammaS18, gammaS-18, γS-18', 'γS18'] }
-    //           />
-    //         <Row
-    //           even={ false }
-    //           titles={ ['Strain Summary', 'Genotype'] }
-    //           data={ ['Radiation-sensitive strain', 'radH18'] }
-    //         />
-    //         </div>
-    //     )
-    // }
-    // render() {
-        // return (
-        //   <div>
-        //     <StrainGrid style={ {maxWidth: '80%', minWidth: '0%', margin: '0 auto'} } cellWidth="1">
-        //         <Cell align="center">
-        //               <h1 className="dicty-header">{ this.props.params.id }</h1>
-        //         </Cell>
-        //         <Cell align="center" style={ {background: '#337ab7', color: 'white'} }>
-        //           <h3>Strain Details</h3>
-        //         </Cell>
-        //         <Row
-        //           even={ false }
-        //           titles={ ['Strain Descriptor', 'Strain ID'] }
-        //           data={ ['γS18', this.props.params.id] }
-        //         />
-        //       <Row
-        //         even
-        //         titles={ ['Strain Names', 'Systematic Name'] }
-        //         data={ ['gammaS18, gammaS-18, γS-18', 'γS18'] }
-        //       />
-        //
-        //
-        //     </StrainGrid>
-        //   </div>
-        // )
-    // }
 }
