29b910e872dc6480cf0cf84949922ebcfe07982f
wildlifehexagon <crossthebreeze@gmail.com>
2017-10-28 15:18:49 -0500

    add temporary styled-components page

---
+++ src/styles/EditInfoPage.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const InfoGrid = styled.div`
+  display: flex,
+  flexDirection: row,
+  flexWrap: wrap,
+  justifyContent: space-between,
+  minWidth: 100%,
+`
+
+export const InfoCell = styled.div`
+  display: flex !important;
+  flex-basis: calc(33.33333333333333% - 0px) !important;
+  align-items: flex-start !important;
+  justify-content: flex-start !important;
+  order: initial !important;
+`
