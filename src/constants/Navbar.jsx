6b977b1406b2b7fc6bec45546414495693bd67f0
wildlifehexagon <crossthebreeze@gmail.com>
2017-11-14 10:21:07 -0600

    add new header and rewrite navbar content

---
+++ src/constants/Navbar.jsx
@@ -0,0 +1,142 @@
+export const NavbarLinks = [
+    {
+        dropdown: true,
+        title: 'Genomes',
+        items: [
+            {
+                name: 'Genomes Home',
+                href: '/genomes'
+            }
+        ]
+    },
+    {
+        dropdown: true,
+        title: 'Tools',
+        items: [
+            {
+                name: 'Tools Home',
+                href: '/tools'
+            },
+            {
+                name: 'New Genome Browser',
+                href: '/tools/jbrowse'
+            }
+        ]
+    },
+    {
+        dropdown: true,
+        title: 'Explore',
+        items: [
+            {
+                name: 'Explore Home',
+                href: '/explore'
+            },
+            {
+                name: 'Dicty Art',
+                href: '/explore/art'
+            },
+            {
+                name: 'Gallery',
+                href: '/explore/gallery'
+            },
+            {
+                name: 'Genome Resources',
+                href: '/explore/resources'
+            },
+            {
+                name: 'Genome Statistics',
+                href: '/explore/statistics'
+            },
+            {
+                name: 'Learn About Dicty',
+                href: '/explore/learn'
+            },
+            {
+                name: 'Teaching Protocols',
+                href: '/explore/teach'
+            },
+            {
+                name: 'Useful Links',
+                href: '/explore/links'
+            }
+        ]
+    },
+    {
+        dropdown: true,
+        title: 'Research',
+        items: [
+            {
+                name: 'Research Home',
+                href: '/research'
+            },
+            {
+                name: 'Anatomy Ontology',
+                href: '/research/ontology'
+            },
+            {
+                name: 'Codon Bias Table',
+                href: '/research/codon'
+            },
+            {
+                name: 'Nomenclature Guidelines',
+                href: '/research/nomenclature'
+            },
+            {
+                name: 'Phenotyping',
+                href: '/research/phenotyping'
+            },
+            {
+                name: 'Techniques',
+                href: '/research/techniques'
+            }
+        ]
+    },
+    {
+        dropdown: true,
+        title: 'Dicty Stock Center',
+        items: [
+            {
+                name: 'Stock Center Home',
+                href: '/stockcenter'
+            }
+        ]
+    },
+    {
+        dropdown: true,
+        title: 'Community',
+        items: [
+            {
+                name: 'Community Home',
+                href: '/community'
+            },
+            {
+                name: 'Cite Us',
+                href: '/citation'
+            },
+            {
+                name: 'Dicty Annual Conferences',
+                href: '/community/conference'
+            },
+            {
+                name: 'Dicty Email Forum',
+                href: '/community/listserv'
+            },
+            {
+                name: 'Dicty Labs',
+                href: '/community/labs'
+            },
+            {
+                name: 'History',
+                href: '/community/history'
+            },
+            {
+                name: 'Jobs',
+                href: '/community/jobs'
+            },
+            {
+                name: 'Upcoming Meetings',
+                href: '/community/meetings'
+            }
+        ]
+    }
+]
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react'
-import { Footer } from 'dicty-components-header-footer'
-import DictyNavbar from 'components/DictyNavbar'
-import DictyHeader from 'components/DictyHeader'
+import { Header, Footer } from 'dicty-components-header-footer'
+import { Navbar } from 'dicty-components-navbar'
 import Cart from 'components/Cart'
 import { connect } from 'react-redux'
 import { StyleRoot } from 'radium'
@@ -14,6 +13,8 @@ import * as pageActionCreators from 'actions/page'
 import * as dscActionsCreators from 'actions/stockCenter'
 import * as cartActionCreators from 'actions/cart'
 import { routerActions } from 'react-router-redux'
+import { FooterLinks } from 'constants/Footer'
+import { NavbarLinks } from 'constants/Navbar'
 
 class App extends Component {
     displayName = 'the primary app component';
@@ -30,94 +31,20 @@ class App extends Component {
         })
     };
     render() {
-        const siteMap = [
-            [
-                {
-                    header: {description: 'Genomes Home', link: '/genomes'},
-                    items: []
-                }
-            ],
-            [
-                {
-                    header: {description: 'Tools Home', link: '/tools'},
-                    items: [
-                        {description: 'New Genome Browser', link: '/tools/jbrowse'}
-                    ]
-                }
-            ],
-            [
-                {
-                    header: {description: 'Explore Home', link: '/explore'},
-                    items: [
-                        {description: 'Dicty Art', link: '/explore/art'},
-                        {description: 'Gallery', link: '/explore/gallery'},
-                        {description: 'Genome Resources', link: '/explore/resources'},
-                        {description: 'Genome Statistics', link: '/explore/statistics'},
-                        {description: 'Learn About Dicty', link: '/explore/learn'},
-                        {description: 'Teaching Protocols', link: '/explore/teach'},
-                        {description: 'Useful Links', link: '/explore/links'}
-                    ]
-                }
-            ],
-            [
-                {
-                    header: {description: 'Research Home', link: '/research'},
-                    items: [
-                        {description: 'Anatomy Ontology', link: '/research/ontology'},
-                        {description: 'Codon Bias Table', link: '/research/codon'},
-                        {description: 'Nomenclature Guidelines', link: '/research/nomenclature'},
-                        {description: 'Phenotyping', link: '/research/phenotyping'},
-                        {description: 'Techniques', link: '/research/techniques'}
-                    ]
-                }
-            ],
-            [
-                {
-                    header: {description: 'Stock Center Home', link: '/stockcenter'},
-                    items: []
-                },
-                {
-                    header: {description: 'Community Home', link: '/community'},
-                    items: [
-                        {description: 'Cite Us', link: '/citation'},
-                        {description: 'Dicty Annual Conferences', link: '/community/conference'},
-                        {description: 'Dicty Email Forum', link: '/community/listserv'},
-                        {description: 'Dicty Labs', link: '/community/labs'},
-                        {description: 'History', link: '/community/history'},
-                        {description: 'Jobs', link: '/community/jobs'},
-                        {description: 'Upcoming Meetings', link: '/community/meetings'}
-                    ]
-                }
-            ],
-            [
-                {
-                    header: {description: 'Please Cite:', link: '#'},
-                    items: [
-                        {description: 'dictyBase', link: 'http://www.ncbi.nlm.nih.gov/pubmed/23172289'},
-                        {description: 'Dicty Stock Center', link: 'http://www.ncbi.nlm.nih.gov/pubmed/23494302'}
-                    ]
-                },
-                {
-                    header: {description: 'Supported By:', link: '#'},
-                    items: [
-                        {description: 'NIH', link: 'https://www.nih.gov/'},
-                        {description: 'GMOD', link: 'http://gmod.org/wiki/Main_Page'},
-                        {description: 'Gene Ontology', link: 'http://geneontology.org/'}
-                    ]
-                }
-            ]
-        ]
         return (
             <StyleRoot>
                 <div>
-                    <DictyHeader
+                    <Header
                       auth={ this.props.auth }
                       authActions={ this.props.authActions }
+                      downloads=""
+                      info=""
+                      cite=""
                     />
-                    {/* <DictyNavbar items={ siteMap } /> */}
+                    <Navbar items={ NavbarLinks } />
                     <Cart cart={ this.props.cart }/>
                     { this.renderChildren() }
-                    <Footer items={ siteMap } />
+                    <Footer items={ FooterLinks } />
                 </div>
             </StyleRoot>
         )
