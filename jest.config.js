5714e9ce715c8f2ffba3b9a6c1636341718a47ba
wildlifehexagon <crossthebreeze@gmail.com>
2017-12-18 12:03:41 -0600

    move old tests to backup folder

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     moduleFileExtensions: ['js', 'jsx'],
     moduleNameMapper: {
+        Carousel: '<rootDir>/src/styles/index.js',
         styles: '<rootDir>/src/styles/index.js'
     },
     verbose: true
