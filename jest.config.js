ab294cf4c32345bed3c3a505d418603620640a6a
wildlifehexagon <crossthebreeze@gmail.com>
2017-12-18 11:07:24 -0600

    update dependencies and remove karma

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    moduleFileExtensions: [
-        'js',
-        'jsx'
-    ],
+    moduleFileExtensions: ['js', 'jsx'],
     moduleNameMapper: {
         styles: '<rootDir>/src/styles/index.js'
     },
