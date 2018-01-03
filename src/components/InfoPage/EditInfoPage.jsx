90a8054f273de88860abdd882a4f0cca4ace188c
wildlifehexagon <crossthebreeze@gmail.com>
2018-01-03 15:41:57 -0600

    update travis config + fix linting errors

--- src/components/InfoPage/EditInfoPage.jsx
+++ src/components/InfoPage/EditInfoPage.jsx
@@ -5,20 +5,28 @@ import createUndoPlugin from 'draft-js-undo-plugin'
 import createToolbarPlugin, { Separator } from 'draft-js-static-toolbar-plugin'
 import createToolbarLinkPlugin from 'draft-js-toolbar-link-plugin'
 import {
-    ItalicButton,
-    BoldButton,
-    UnderlineButton,
-    CodeButton,
-    HeadlineOneButton,
-    HeadlineTwoButton,
-    HeadlineThreeButton,
-    UnorderedListButton,
-    OrderedListButton,
-    BlockquoteButton,
-    CodeBlockButton
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton
 } from 'draft-js-buttons'
 import { Flex, Box } from 'rebass'
-import { Container, ToolbarNav, EditorStyle, EditPanel, StaticToolbar, DefaultButton, SuccessButton } from 'styles'
+import {
+  Container,
+  ToolbarNav,
+  EditorStyle,
+  EditPanel,
+  StaticToolbar,
+  DefaultButton,
+  SuccessButton
+} from 'styles'
 
 const undoPlugin = createUndoPlugin()
 const toolbarLinkPlugin = createToolbarLinkPlugin({
@@ -51,71 +59,71 @@ const { Toolbar } = toolbarPlugin
 const plugins = [toolbarPlugin, toolbarLinkPlugin, undoPlugin]
 
 export default class EditInfoPage extends Component {
-    displayName = 'information page editor'
-    constructor(props) {
-        super(props)
+  displayName = 'information page editor'
+  constructor(props) {
+      super(props)
 
-        if (props.page.content) {
-            this.state = {
-                editorState: EditorState.createWithContent(
-                    convertFromRaw(props.page.content)
-                )
-            }
-        }
-    }
-
-    onChange = editorState => this.setState({ editorState })
-    focus = () => this.refs.editor.focus()
-    onSave = () => {
-        const { editorState } = this.state
-        const { match, pageActions } = this.props
-        const rawData = convertToRaw(editorState.getCurrentContent())
-        pageActions.saveEditing(match.params.name, rawData)
-    }
-    onCancel = () => {
-        const { pageActions, match } = this.props
-        pageActions.cancelEditing(match.params.name)
-    }
-    render() {
-        const { editorState } = this.state
-        return (
-            <Container>
-                <EditPanel>
-                    <ToolbarNav>
-                        <StaticToolbar>
-                            <Toolbar />
-                        </StaticToolbar>
-                    </ToolbarNav>
-                    <EditorStyle>
-                        <Editor
-                            editorState={ editorState }
-                            onChange={ this.onChange }
-                            plugins={ plugins }
-                            ref="{(element) => { this.editor = element }}"
-                        />
-                    </EditorStyle>
-                    <Flex justify="space-between">
-                        <Box width="25%" />
-                        <Box width="25%" />
-                        <Box width="25%" mr={ 1 }>
-                            <DefaultButton
-                                type="button"
-                                className={ `block` }
-                                onClick={ this.onCancel }>
-                                Cancel
-                            </DefaultButton>
-                        </Box>
-                        <Box width="25%">
-                            <SuccessButton
-                                type="button"
-                                className={ `block` }
-                                onClick={ this.onSave }>
-                                Save
-                            </SuccessButton>
-                        </Box>
-                    </Flex>
-                </EditPanel>
-            </Container>
+      if (props.page.content) {
+          this.state = {
+              editorState: EditorState.createWithContent(
+          convertFromRaw(props.page.content)
         )
-    }
+          }
+      }
+  }
+
+  onChange = editorState => this.setState({ editorState })
+  focus = () => this.refs.editor.focus()
+  onSave = () => {
+      const { editorState } = this.state
+      const { match, pageActions } = this.props
+      const rawData = convertToRaw(editorState.getCurrentContent())
+      pageActions.saveEditing(match.params.name, rawData)
+  }
+  onCancel = () => {
+      const { pageActions, match } = this.props
+      pageActions.cancelEditing(match.params.name)
+  }
+  render() {
+      const { editorState } = this.state
+      return (
+      <Container>
+        <EditPanel>
+          <ToolbarNav>
+            <StaticToolbar>
+              <Toolbar />
+            </StaticToolbar>
+          </ToolbarNav>
+          <EditorStyle>
+            <Editor
+              editorState={ editorState }
+              onChange={ this.onChange }
+              plugins={ plugins }
+              ref="{(element) => { this.editor = element }}"
+            />
+          </EditorStyle>
+          <Flex justify="space-between">
+            <Box width="25%" />
+            <Box width="25%" />
+            <Box width="25%" mr={ 1 }>
+              <DefaultButton
+                type="button"
+                className={ `block` }
+                onClick={ this.onCancel }>
+                Cancel
+              </DefaultButton>
+            </Box>
+            <Box width="25%">
+              <SuccessButton
+                type="button"
+                className={ `block` }
+                onClick={ this.onSave }>
+                Save
+              </SuccessButton>
+            </Box>
+          </Flex>
+        </EditPanel>
+      </Container>
+    )
+  }
 }
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,45 +1,45 @@
 import styled from 'styled-components'
 import { injectGlobal } from 'styled-components'
 
-injectGlobal`
-    *, :after, :before {
-        box-sizing: border-box;
+injectGlobal([`
+*, :after, :before {
+    box-sizing: border-box;
+}
+
+body {
+    font-family: Helvetica Neue,Helvetica,Arial,sans-serif;
+    font-size: 14px;
+    line-height: 1.42857;
+    color: #333;
+    background-color: #fff;
+    margin: auto;
+
+    h1, h2, h3, h4, h5, h6 {
+        font-family: inherit;
+        font-weight: 500;
+        line-height: 1.1;
+        color: inherit;
     }
 
-    body {
-        font-family: Helvetica Neue,Helvetica,Arial,sans-serif;
+    h4, h5, h6 {
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+
+    h4 {
+        font-size: 18px;
+    }
+
+    h5 {
         font-size: 14px;
-        line-height: 1.42857;
-        color: #333;
-        background-color: #fff;
-        margin: auto;
-
-        h1, h2, h3, h4, h5, h6 {
-            font-family: inherit;
-            font-weight: 500;
-            line-height: 1.1;
-            color: inherit;
-        }
-
-        h4, h5, h6 {
-            margin-top: 10px;
-            margin-bottom: 10px;
-        }
-
-        h4 {
-            font-size: 18px;
-        }
-
-        h5 {
-            font-size: 14px;
-        }
     }
+}
 
-    a {
-        color: #337ab7;
-        text-decoration: none;
-    }
-`
+a {
+    color: #337ab7;
+    text-decoration: none;
+}
+`])
 
 export const Container = styled.div`
     padding-right: 15px;
