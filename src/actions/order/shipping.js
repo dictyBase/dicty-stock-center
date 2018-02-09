cb8acf49ece0cf58a9ad145b42a82436a3124220
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-02-09 15:50:35 +0000

    Fixed all import paths for constant

--- src/actions/order/shipping.js
+++ src/actions/order/shipping.js
@@ -1,97 +1,108 @@
-import { push } from 'react-router-redux'
-import { createUser, getUser, updateUser } from 'utils/api'
-import { status, json } from 'utils/fetch'
-import types from 'constants'
+import { push } from "react-router-redux"
+import { createUser, getUser, updateUser } from "utils/api"
+import { status, json } from "utils/fetch"
+import { dsctypes } from "./../../constants"
 
-const { ADD_SHIPPING } = types
+const { ADD_SHIPPING } = dsctypes
 
 export const addShipping = (user, details) => {
-    // retrieve user info from json api structure and
-    // store in the state of the app
-    const {
-        type, id,
-        attributes: {
-            first_name: firstName,
-            last_name: lastName,
-            email,
-            organization: org,
-            group,
-            address: {
-                first: address, second: address2
-            },
-            city,
-            state,
-            zip,
-            country,
-            phone
-        }
-    } = user.data
+  // retrieve user info from json api structure and
+  // store in the state of the app
+  const {
+    type,
+    id,
+    attributes: {
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      organization: org,
+      group,
+      address: { first: address, second: address2 },
+      city,
+      state,
+      zip,
+      country,
+      phone,
+    },
+  } = user.data
 
-    const consumer = {
-        type, id, firstName, lastName, email, org, group, address,
-        address2, city, state, zip, country, phone
-    }
-    return {
-        type: ADD_SHIPPING,
-        initialized: true,
-        consumer,
-        details
-    }
+  const consumer = {
+    type,
+    id,
+    firstName,
+    lastName,
+    email,
+    org,
+    group,
+    address,
+    address2,
+    city,
+    state,
+    zip,
+    country,
+    phone,
+  }
+  return {
+    type: ADD_SHIPPING,
+    initialized: true,
+    consumer,
+    details,
+  }
 }
 
 let server = process.env.REACT_APP_API_SERVER
 export const submitForm = (values, dispatch) => {
-    let details = {
-        shipAccount: values.shipAccount,
-        shipAccountNum: values.shipAccountNum,
-        comments: values.comments
-    }
-    return new Promise((resolve, reject) => {
-        getUser(server, values.email)
-        .then(response => {
-            if (response.status === 200) {
-                return Promise.resolve(true)
-            } else if (response.status === 404) {
-                return Promise.resolve(false)
-            }
-            return Promise.reject(new Error('Error'))
-        })
-        .then(userExists => {
-            if (userExists) {
-                updateUser(server, values)
-                .then(status)
-                .then(json)
-                .then(user => {
-                    resolve()
-                    dispatch(addShipping(user, details))
-                    dispatch(push('/order/payment'))
-                })
-                .catch(error => {
-                    reject({_error: 'User cannot be updated', error})
-                })
-            } else {
-                createUser(server, values)
-                .then(status)
-                .then(json)
-                .then(user => {
-                    resolve()
-                    dispatch(addShipping(user, details))
-                    dispatch(push('/order/payment'))
-                })
-                .catch(error => {
-                    reject({_error: 'User cannot be created', error})
-                })
-            }
-        })
-        .catch(error => {
-            reject({_error: error.message})
-        })
-    })
+  let details = {
+    shipAccount: values.shipAccount,
+    shipAccountNum: values.shipAccountNum,
+    comments: values.comments,
+  }
+  return new Promise((resolve, reject) => {
+    getUser(server, values.email)
+      .then(response => {
+        if (response.status === 200) {
+          return Promise.resolve(true)
+        } else if (response.status === 404) {
+          return Promise.resolve(false)
+        }
+        return Promise.reject(new Error("Error"))
+      })
+      .then(userExists => {
+        if (userExists) {
+          updateUser(server, values)
+            .then(status)
+            .then(json)
+            .then(user => {
+              resolve()
+              dispatch(addShipping(user, details))
+              dispatch(push("/order/payment"))
+            })
+            .catch(error => {
+              reject({ _error: "User cannot be updated", error })
+            })
+        } else {
+          createUser(server, values)
+            .then(status)
+            .then(json)
+            .then(user => {
+              resolve()
+              dispatch(addShipping(user, details))
+              dispatch(push("/order/payment"))
+            })
+            .catch(error => {
+              reject({ _error: "User cannot be created", error })
+            })
+        }
+      })
+      .catch(error => {
+        reject({ _error: error.message })
+      })
+  })
 }
 
 // direct user to edit shipping information
 export const editShipping = () => {
-    return dispatch => {
-        dispatch(push('/order/shipping/edit'))
-    }
+  return dispatch => {
+    dispatch(push("/order/shipping/edit"))
+  }
 }
--- src/actions/page.js
+++ src/actions/page.js
@@ -1,116 +1,116 @@
-import types from 'constants'
-import { push } from 'react-router-redux'
-import simpleStorage from 'simplestorage.js'
+import { dsctypes } from "./../constants"
+import { push } from "react-router-redux"
+import simpleStorage from "simplestorage.js"
 
 // fake data for "information" pages
-import infoPages from 'fake-data/infoPages'
+import infoPages from "fake-data/infoPages"
 
 const {
-    EDIT_PAGE,
-    SAVE_PAGE_REQUEST,
-    SAVE_PAGE_SUCCESS,
-    FETCH_PAGE_REQUEST,
-    FETCH_PAGE_SUCCESS
-} = types
+  EDIT_PAGE,
+  SAVE_PAGE_REQUEST,
+  SAVE_PAGE_SUCCESS,
+  FETCH_PAGE_REQUEST,
+  FETCH_PAGE_SUCCESS,
+} = dsctypes
 
-const doEdit = (content) => {
-    return {
-        type: EDIT_PAGE,
-        payload: {
-            content: content
-        }
-    }
+const doEdit = content => {
+  return {
+    type: EDIT_PAGE,
+    payload: {
+      content: content,
+    },
+  }
 }
 
 const fetchPageRequest = () => {
-    return {
-        type: FETCH_PAGE_REQUEST,
-        isFetching: true
-    }
+  return {
+    type: FETCH_PAGE_REQUEST,
+    isFetching: true,
+  }
 }
 
-const fetchPageSuccess = (content) => {
-    const { id, attributes } = content.data
-    return {
-        type: FETCH_PAGE_SUCCESS,
-        isFetching: false,
-        page: id,
-        content: attributes.content,
-        lastEdited: attributes.lastEdited
-    }
+const fetchPageSuccess = content => {
+  const { id, attributes } = content.data
+  return {
+    type: FETCH_PAGE_SUCCESS,
+    isFetching: false,
+    page: id,
+    content: attributes.content,
+    lastEdited: attributes.lastEdited,
+  }
 }
 
 const savePageRequest = () => {
-    return {
-        type: SAVE_PAGE_REQUEST,
-        isFetching: true
-    }
+  return {
+    type: SAVE_PAGE_REQUEST,
+    isFetching: true,
+  }
 }
 
 const savePageSuccess = () => {
-    return {
-        type: SAVE_PAGE_SUCCESS,
-        isFetching: false
-    }
+  return {
+    type: SAVE_PAGE_SUCCESS,
+    isFetching: false,
+  }
 }
 
-export const fetchInfoPage = (page) => {
-    return (dispatch) => {
-        dispatch(fetchPageRequest())
+export const fetchInfoPage = page => {
+  return dispatch => {
+    dispatch(fetchPageRequest())
 
-        // if there is raw draftjs data for the specific page in localstorage,
-        // then display that. Otherwise display the default fake page content.
-        let content
-        if (simpleStorage.get(page)) {
-            content = simpleStorage.get(page)
-        } else {
-            content = infoPages[page]
-        }
-        const pageContent = {
-            data: {
-                type: 'page',
-                id: page,
-                attributes: {
-                    content: content,
-                    lastEdited: {
-                        author: {
-                            name: 'John Smith',
-                            role: 'curator'
-                        },
-                        time: '2016-08-08T14:30:00'
-                    }
-                }
-            }
-        }
-        setTimeout(() => {
-            dispatch(fetchPageSuccess(pageContent))
-        }, 1000)
+    // if there is raw draftjs data for the specific page in localstorage,
+    // then display that. Otherwise display the default fake page content.
+    let content
+    if (simpleStorage.get(page)) {
+      content = simpleStorage.get(page)
+    } else {
+      content = infoPages[page]
     }
+    const pageContent = {
+      data: {
+        type: "page",
+        id: page,
+        attributes: {
+          content: content,
+          lastEdited: {
+            author: {
+              name: "John Smith",
+              role: "curator",
+            },
+            time: "2016-08-08T14:30:00",
+          },
+        },
+      },
+    }
+    setTimeout(() => {
+      dispatch(fetchPageSuccess(pageContent))
+    }, 1000)
+  }
 }
 
 export const editPage = (content, name) => {
-    return (dispatch) => {
-        dispatch(doEdit(content))
-        dispatch(push(`/${name}/information/edit`))
-    }
+  return dispatch => {
+    dispatch(doEdit(content))
+    dispatch(push(`/${name}/information/edit`))
+  }
 }
 
 export const saveEditing = (page, data) => {
-    return (dispatch) => {
-        dispatch(savePageRequest())
+  return dispatch => {
+    dispatch(savePageRequest())
 
-        // save data in localstorage.
-        // ex: order/information --> {order: {..raw draftjs data..}}
-        // ex: payment/information --> {payment: {..raw draftjs data..}}
-        simpleStorage.set(page, data)
-        dispatch(savePageSuccess())
+    // save data in localstorage.
+    // ex: order/information --> {order: {..raw draftjs data..}}
+    // ex: payment/information --> {payment: {..raw draftjs data..}}
+    simpleStorage.set(page, data)
+    dispatch(savePageSuccess())
 
-        dispatch(push(`/${page}/information`))
-    }
+    dispatch(push(`/${page}/information`))
+  }
 }
 
-export const cancelEditing = (page) => {
-    return (dispatch) => {
-        dispatch(push(`/${page}/information`))
-    }
+export const cancelEditing = page => {
+  return dispatch => {
+    dispatch(push(`/${page}/information`))
+  }
 }
