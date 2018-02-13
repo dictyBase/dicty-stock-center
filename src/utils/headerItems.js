0c2ef5a0316fb9fd65e473a22407095b8438c1e1
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-02-13 04:19:39 +0000

    Configuration for header items to show

---
+++ src/utils/headerItems.js
@@ -0,0 +1,47 @@
+const headerItems = [
+  {
+    url: "/cite",
+    icon: "plus",
+    text: "Cite Us",
+  },
+  {
+    url: "/downloads",
+    icon: "download",
+    text: "Downloads",
+  },
+  {
+    url: "/info",
+    icon: "info-circle",
+    text: "About dictyBase",
+  },
+  {
+    url: "/login",
+    icon: "sign-in",
+    text: "Login",
+  },
+]
+
+const loggedHeaderItems = [
+  {
+    url: "/cite",
+    icon: "plus",
+    text: "Cite Us",
+  },
+  {
+    url: "/downloads",
+    icon: "download",
+    text: "Downloads",
+  },
+  {
+    url: "/info",
+    icon: "info-circle",
+    text: "About dictyBase",
+  },
+  {
+    url: "/logout",
+    icon: "sign-out",
+    text: "Logout",
+  },
+]
+
+export { headerItems, loggedHeaderItems }
