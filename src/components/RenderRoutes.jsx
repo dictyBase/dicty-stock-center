9fb7f162d96cdd8b91c94ddc9ad6b1d3910a0129
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-02-13 04:10:11 +0000

    Added protected route

--- src/components/RenderRoutes.jsx
+++ src/components/RenderRoutes.jsx
@@ -1,24 +1,31 @@
-import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import routes from '../routes'
+import React, { Component } from "react"
+import { Route, Switch, Redirect } from "react-router-dom"
+import routes from "../routes"
 
 export default class RenderRoutes extends Component {
-    displayName = 'list of routes';
-    render() {
-        return (
-            <Switch>
-                { routes.map((route, i) => (
-                    <Route
-                        exact
-                        key={ i }
-                        path={ route.path }
-                        render={ ({ match, location }) =>
-                        <route.component {...this.props}
-                        match={ match } location={ location } /> }
-                     />
-                )) }
-            </Switch>
-        )
-    }
+  displayName = "list of routes"
+  render() {
+    return (
+      <Switch>
+        {routes.map((route, i) => (
+          <Route
+            exact
+            key={i}
+            path={route.path}
+            render={({ match, location }) =>
+              route.protected && !this.props.auth.isAuthenticated ? (
+                <Redirect to="/login" />
+              ) : (
+                <route.component
+                  {...this.props}
+                  match={match}
+                  location={location}
+                />
+              )
+            }
+          />
+        ))}
+      </Switch>
+    )
+  }
 }
-
--- src/routes.js
+++ src/routes.js
@@ -1,119 +1,121 @@
-import Login from 'components/Login'
-import OauthCallback from 'components/OauthCallback'
-import { AuthLoader, Logout } from 'components/Auth'
-import Home from 'components/Home'
-import MyDsc from 'components/MyDsc'
-import Error from 'components/Error'
-import ShoppingCart from 'components/ShoppingCart'
-import InfoPage from 'components/InfoPage'
-import EditInfoPage from 'components/InfoPage/EditInfoPage'
-import Contact from 'components/Contact'
-import PageNotReady from 'components/PageNotReady'
-import Strains from 'components/Strains'
-import StrainDetail from 'components/Strains/StrainDetail'
-import Plasmids from 'components/Plasmids'
-import PlasmidDetail from 'components/Plasmids/PlasmidDetail'
-import Shipping from 'components/form/Shipping'
-import EditShipping from 'components/form/EditShipping'
-import Payment from 'components/form/Payment'
-import EditPayment from 'components/form/EditPayment'
-import Submit from 'components/form/Submit'
-import SubmitLoader from 'components/form/SubmitLoader'
-import OrderConfirmation from 'components/OrderConfirmation'
+import Login from "components/Login"
+import OauthCallback from "components/OauthCallback"
+import { AuthLoader, Logout } from "components/Auth"
+import Home from "components/Home"
+import MyDsc from "components/MyDsc"
+import Error from "components/Error"
+import ShoppingCart from "components/ShoppingCart"
+import InfoPage from "components/InfoPage"
+import EditInfoPage from "components/InfoPage/EditInfoPage"
+import Contact from "components/Contact"
+import PageNotReady from "components/PageNotReady"
+import Strains from "components/Strains"
+import StrainDetail from "components/Strains/StrainDetail"
+import Plasmids from "components/Plasmids"
+import PlasmidDetail from "components/Plasmids/PlasmidDetail"
+import Shipping from "components/form/Shipping"
+import EditShipping from "components/form/EditShipping"
+import Payment from "components/form/Payment"
+import EditPayment from "components/form/EditPayment"
+import Submit from "components/form/Submit"
+import SubmitLoader from "components/form/SubmitLoader"
+import OrderConfirmation from "components/OrderConfirmation"
 
 const routes = [
-    {
-        path: '/',
-        component: Home
-    },
-    {
-        path: '/:name/information',
-        component: InfoPage
-    },
-    {
-        path: '/:name/information/edit',
-        component: EditInfoPage
-    },
-    {
-        path: '/login',
-        component: Login
-    },
-    {
-        path: '/:provider/callback',
-        component: OauthCallback
-    },
-    {
-        path: '/load/auth',
-        component: AuthLoader
-    },
-    {
-        path: '/strains',
-        component: Strains
-    },
-    {
-        path: '/strains/:id',
-        component: StrainDetail
-    },
-    {
-        path: '/plasmids',
-        component: Plasmids
-    },
-    {
-        path: '/plasmids/:id',
-        component: PlasmidDetail
-    },
-    {
-        path: '/contact',
-        component: Contact
-    },
-    {
-        path: '/logout',
-        component: Logout
-    },
-    {
-        path: '/my-dsc',
-        component: MyDsc
-    },
-    {
-        path: '/error',
-        component: Error
-    },
-    {
-        path: '/cart',
-        component: ShoppingCart
-    },
-    {
-        path: '/order/shipping',
-        component: Shipping
-    },
-    {
-        path: '/order/shipping/edit',
-        component: EditShipping
-    },
-    {
-        path: '/order/payment',
-        component: Payment
-    },
-    {
-        path: '/order/payment/edit',
-        component: EditPayment
-    },
-    {
-        path: '/order/submit',
-        component: Submit
-    },
-    {
-        path: '/order/submitting',
-        component: SubmitLoader
-    },
-    {
-        path: '/order/submitted',
-        component: OrderConfirmation
-    },
-    {
-        path: '*',
-        component: PageNotReady
-    }
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/:name/information",
+    component: InfoPage,
+  },
+  {
+    path: "/:name/information/edit",
+    component: EditInfoPage,
+    protected: true,
+  },
+  {
+    path: "/login",
+    component: Login,
+  },
+  {
+    path: "/:provider/callback",
+    component: OauthCallback,
+  },
+  {
+    path: "/load/auth",
+    component: AuthLoader,
+  },
+  {
+    path: "/strains",
+    component: Strains,
+  },
+  {
+    path: "/strains/:id",
+    component: StrainDetail,
+  },
+  {
+    path: "/plasmids",
+    component: Plasmids,
+  },
+  {
+    path: "/plasmids/:id",
+    component: PlasmidDetail,
+  },
+  {
+    path: "/contact",
+    component: Contact,
+  },
+  {
+    path: "/logout",
+    component: Logout,
+  },
+  {
+    path: "/my-dsc",
+    component: MyDsc,
+    protected: true,
+  },
+  {
+    path: "/error",
+    component: Error,
+  },
+  {
+    path: "/cart",
+    component: ShoppingCart,
+  },
+  {
+    path: "/order/shipping",
+    component: Shipping,
+  },
+  {
+    path: "/order/shipping/edit",
+    component: EditShipping,
+  },
+  {
+    path: "/order/payment",
+    component: Payment,
+  },
+  {
+    path: "/order/payment/edit",
+    component: EditPayment,
+  },
+  {
+    path: "/order/submit",
+    component: Submit,
+  },
+  {
+    path: "/order/submitting",
+    component: SubmitLoader,
+  },
+  {
+    path: "/order/submitted",
+    component: OrderConfirmation,
+  },
+  {
+    path: "*",
+    component: PageNotReady,
+  },
 ]
 
 export default routes
