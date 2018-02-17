63f2d0895a85c92c2ad2c9282fcc2886b8b3443f
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-02-17 13:06:56 +0000

    Integrated the middleware in store

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -2,10 +2,23 @@ import { createStore, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import rootReducer from "../reducers"
 import { routerMiddleware } from "react-router-redux"
+import {
+  tokenStorageMiddleware,
+  userStorageMiddleware,
+} from "middlewares/storage"
+import { dsctypes } from "../constants"
 import history from "utils/routerHistory"
 
-const middleware = routerMiddleware(history)
-const enhancer = applyMiddleware(middleware, thunk)
+const options = {
+  save: dsctypes.LOGIN_SUCCESS,
+  remove: dsctypes.LOGOUT_SUCCESS,
+}
+const enhancer = applyMiddleware(
+  routerMiddleware(history),
+  thunk,
+  tokenStorageMiddleware(options),
+  userStorageMiddleware(options),
+)
 
 export default function configureStore(initialState) {
   const store = createStore(rootReducer, initialState, enhancer)
