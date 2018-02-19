11ffa1f109a11f959fe18a7e479cfccdae487b81
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-02-19 14:07:28 +0000

    Added a module for initializing state Added flow types Added function to loading multiple parts of the state

---
+++ src/utils/hydrateStore.js
@@ -0,0 +1,41 @@
+// @flow
+type hydrateArg = {
+  /** Part of the state tree to be loaded */
+  key?: string,
+  /** Key to retrieve from localStorage */
+  namespace: string,
+}
+
+type hydrateStoreFunc = hydrateArg
+
+type hydrateLoadArg = Array<hydrateStoreFunc>
+
+/**
+ * Load states from localstorage to use in Redux's createStore method
+ */
+let hydrateStore = ({ key, namespace }: hydrateArg) => {
+  try {
+    let loadedState = {}
+    if (key) {
+      loadedState[key] = JSON.parse(window.localStorage.getItem(namespace))
+    } else {
+      loadedState = JSON.parse(window.localStorage.getItem(namespace))
+    }
+    return loadedState
+  } catch (e) {
+    console.error("unable to load state from localStoage %s", JSON.stringify(e))
+  }
+}
+
+/**
+ * Combine multiple load methods to return a single state to use in Redux's createStore method
+ */
+let hydrateAll = (...loaderFuncs: hydrateLoadArg) => {
+  let combinedStates = loaderFuncs.reduce((merged, loader) => {
+    let state = loader
+    return { ...merged, ...state }
+  })
+  return combinedStates
+}
+
+export { hydrateStore, hydrateAll }
