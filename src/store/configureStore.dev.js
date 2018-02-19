3b4fb2a6480862f77ecf8c69acab73fdacf9eef0
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-02-19 20:59:50 +0000

    Added instance of middleware to persist state of shopping cart

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -2,22 +2,27 @@ import { createStore, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import rootReducer from "../reducers"
 import { routerMiddleware } from "react-router-redux"
-import {
-  tokenStorageMiddleware,
-  userStorageMiddleware,
-} from "middlewares/storage"
+import manageStateStorage from "middlewares/storage"
 import { dsctypes } from "../constants"
 import history from "utils/routerHistory"
 
-const options = {
-  save: dsctypes.LOGIN_SUCCESS,
-  remove: dsctypes.LOGOUT_SUCCESS,
+const authArg = {
+  save_action: dsctypes.LOGIN_SUCCESS,
+  remove_action: dsctypes.LOGOUT_SUCCESS,
+  key: "auth",
+  namespace: "auth",
+}
+const cartArg = {
+  save_action: dsctypes.ADD_TO_CART,
+  remove_action: dsctypes.REMOVE_FROM_CART,
+  key: "cart",
+  namespace: "shoppingCart",
 }
 const enhancer = applyMiddleware(
   routerMiddleware(history),
   thunk,
-  tokenStorageMiddleware(options),
-  userStorageMiddleware(options),
+  manageStateStorage(authArg),
+  manageStateStorage(cartArg),
 )
 
 export default function configureStore(initialState) {
