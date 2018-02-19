007f300b86ae6f9bd7f7e27527bd81f40185bfbd
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-02-19 20:59:23 +0000

    Added load method to hydrate store from localstorage

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Root from 'containers/Root'
-import history from 'utils/routerHistory'
-import simpleStorage from 'simplestorage.js'
-import configureStore from 'store'
+import React from "react"
+import { render } from "react-dom"
+import Root from "containers/Root"
+import history from "utils/routerHistory"
+import simpleStorage from "simplestorage.js"
+import configureStore from "store"
+import { hydrateAll, hydrateStore } from "utils/hydrateStore"
 
-// persist shopping cart to local storage
-const initialState = {
-    cart: simpleStorage.get('shoppingCart')
-}
+// load state from localStorage(if any) to set the
+// initial state for the store
+const initialState = hydrateAll(
+  hydrateStore({ key: "auth", namespace: "auth" }),
+  hydrateStore({ key: "cart", namespace: "shoppingCart" }),
+)
 
 const store = configureStore(initialState)
 
 store.subscribe(() => {
-    simpleStorage.set('shoppingCart', store.getState().cart)
+  simpleStorage.set("shoppingCart", store.getState().cart)
 })
 
 // Render the React application to the DOM
 render(
-  <Root store={ store } history={ history }/>,
-  document.getElementById('root')
+  <Root store={store} history={history} />,
+  document.getElementById("root"),
 )
