9659a3752d06667dd601c5e78b63f3f658e66ef4
wildlifehexagon <crossthebreeze@gmail.com>
2018-03-19 10:34:41 -0500

    move slideshow images to more appropriate folder

--- src/components/Home/Slideshow.jsx
+++ src/components/Home/Slideshow.jsx
@@ -1,10 +1,10 @@
 // @flow
 import React from "react"
 import { Carousel } from "react-responsive-carousel"
-import ctr9 from "../images/ctr9-mutant-DG1071.png"
-import gbqA from "../images/gbqA-mutant-DG1120.png"
-import ggtA from "../images/ggtA-mutant-DG1109.png"
-import tipB from "../images/tipB-mutant-DG1036.png"
+import ctr9 from "static/slideshow-images/ctr9-mutant-DG1071.png"
+import gbqA from "static/slideshow-images/gbqA-mutant-DG1120.png"
+import ggtA from "static/slideshow-images/ggtA-mutant-DG1109.png"
+import tipB from "static/slideshow-images/tipB-mutant-DG1036.png"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 /**
