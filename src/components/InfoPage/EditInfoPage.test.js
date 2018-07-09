4cd7d8b5ebe04fdae3410c734df883a92d277b59
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-09 17:23:41 -0500

    add basic unit tests for EditInfoPage (currently broken)

--- src/components/InfoPage/EditInfoPage.test.js
+++ src/components/InfoPage/EditInfoPage.test.js
@@ -3,11 +3,13 @@ import { shallow, mount } from "enzyme"
 import sinon from "sinon"
 import "../../setupTests"
 import { EditInfoPage } from "./EditInfoPage"
+import { Container } from "styles"
+import { EditorState } from "draft-js"
 
 describe("InfoPage/EditInfoPage", () => {
   let props
   let mountedEditInfoPage
-  const infoPage = () => {
+  const editInfoPage = () => {
     if (!mountedEditInfoPage) {
       mountedEditInfoPage = mount(<EditInfoPage {...props} />)
     }
@@ -25,5 +27,30 @@ describe("InfoPage/EditInfoPage", () => {
     mountedEditInfoPage = undefined
   })
 
-  // insert tests here
+  describe("initial render", () => {
+    beforeEach(() => {
+      props = {
+        page: {
+          data: {
+            attributes: {
+              content: "page content",
+              updated_at: "999",
+            },
+          },
+        },
+        match: {
+          params: {
+            name: "order",
+          },
+        },
+      }
+    })
+
+    it("always renders a Container", () => {
+      expect(editInfoPage().find(Container).length).toBeGreaterThan(0)
+    })
+    it("always renders an Editor", () => {
+      expect(editInfoPage().find(Editor).length).toBeGreaterThan(0)
+    })
+  })
 })
