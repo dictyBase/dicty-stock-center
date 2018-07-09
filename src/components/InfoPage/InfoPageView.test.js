7bda2df8f3fe29f86dc1245f6474d39283b7baad
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-09 17:18:31 -0500

    add more unit tests for InfoPageView

--- src/components/InfoPage/InfoPageView.test.js
+++ src/components/InfoPage/InfoPageView.test.js
@@ -3,13 +3,15 @@ import { shallow, mount } from "enzyme"
 import sinon from "sinon"
 import "../../setupTests"
 import { InfoPageView } from "./InfoPageView"
+import { Container } from "styles"
+import { Editor } from "draft-js"
 
 describe("InfoPage/InfoPageView", () => {
   let props
   let mountedInfoPageView
-  const infoPage = () => {
+  const infoPageView = () => {
     if (!mountedInfoPageView) {
-      mountedInfoPageView = mount(<InfoPageView {...props} />)
+      mountedInfoPageView = shallow(<InfoPageView {...props} />)
     }
     return mountedInfoPageView
   }
@@ -27,5 +29,72 @@ describe("InfoPage/InfoPageView", () => {
     mountedInfoPageView = undefined
   })
 
-  // insert tests here
+  describe("initial render", () => {
+    beforeEach(() => {
+      props = {
+        page: {
+          data: {
+            attributes: {
+              content: "page content",
+              updated_at: "999",
+            },
+          },
+        },
+        match: {
+          params: {
+            name: "order",
+          },
+        },
+        fetchUserInfo: () => {},
+      }
+    })
+
+    it("always renders a Container", () => {
+      expect(infoPageView().find(Container).length).toBeGreaterThan(0)
+    })
+    it("always renders an Editor", () => {
+      expect(infoPageView().find(Editor).length).toBeGreaterThan(0)
+    })
+    it("Editor is readOnly", () => {
+      const editor = infoPageView().find(Editor)
+      expect(editor)
+        .props()
+        .readOnly.toBe(true)
+    })
+  })
+
+  describe("user is authenticated with no other permissions", () => {
+    beforeEach(() => {
+      props = {
+        page: {
+          data: {
+            attributes: {
+              content: "page content",
+              updated_at: "999",
+            },
+          },
+        },
+        match: {
+          params: {
+            name: "order",
+          },
+        },
+        fetchUserInfo: () => {},
+        isAuthenticated: true,
+      }
+    })
+    it("calls componentDidMount", () => {
+      sinon.spy(InfoPageView.prototype, "componentDidMount")
+      infoPageView()
+      expect(InfoPageView.prototype.componentDidMount.calledOnce).toEqual(true)
+    })
+  })
+
+  describe("user is authenticated with editing privileges and valid JWT", () => {
+    // add tests here
+  })
+
+  describe("user is authenticated with editing privileges but JWT is invalid", () => {
+    // add tests here
+  })
 })
