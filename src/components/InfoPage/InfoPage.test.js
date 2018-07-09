7ff676a9e1b33f548a31f2b740f4c3841a536dd4
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-09 14:41:21 -0500

    reconfigure testing template for InfoPage and add more tests

--- src/components/InfoPage/InfoPage.test.js
+++ src/components/InfoPage/InfoPage.test.js
@@ -4,27 +4,81 @@ import sinon from "sinon"
 import "../../setupTests"
 import { InfoPage } from "./InfoPage"
 import { InfoPageView } from "./InfoPageView"
+import Loader from "components/Loader"
 
 describe("InfoPage/InfoPage", () => {
-  const props = {
-    page: {
-      data: {
-        attributes: {},
-      },
-    },
-    match: {
-      params: {
-        name: "Name",
-      },
-    },
-    fetchInfoPage: () => {},
-    isFetching: false,
+  let props
+  let mountedInfoPage
+  const infoPage = () => {
+    if (!mountedInfoPage) {
+      mountedInfoPage = mount(<InfoPage {...props} />)
+    }
+    return mountedInfoPage
   }
-  const slugName = "dsc-order"
 
-  it("calls componentDidMount", () => {
-    sinon.spy(InfoPage.prototype, "componentDidMount")
-    const wrapper = mount(<InfoPage {...props} />)
-    expect(InfoPage.prototype.componentDidMount.calledOnce).toEqual(true)
+  beforeEach(() => {
+    props = {
+      page: undefined,
+      match: undefined,
+      fetchInfoPage: undefined,
+      isFetching: undefined,
+    }
+    mountedInfoPage = undefined
+  })
+
+  describe("initial render", () => {
+    beforeEach(() => {
+      props = {
+        page: {
+          data: {
+            attributes: {
+              content: "page content",
+            },
+          },
+        },
+        match: {
+          params: {
+            name: "order",
+          },
+        },
+        fetchInfoPage: () => {},
+        isFetching: true,
+      }
+    })
+
+    it("always renders Loader", () => {
+      expect(infoPage().find(Loader).length).toBe(1)
+    })
+    it("calls componentDidMount", () => {
+      sinon.spy(InfoPage.prototype, "componentDidMount")
+      infoPage()
+      expect(InfoPage.prototype.componentDidMount.calledOnce).toEqual(true)
+    })
+  })
+
+  describe("after content is fetched", () => {
+    beforeEach(() => {
+      props = {
+        page: {
+          data: {
+            attributes: {
+              content: "page content",
+            },
+          },
+        },
+        match: {
+          params: {
+            name: "order",
+          },
+        },
+        fetchInfoPage: () => {},
+        isFetching: false,
+      }
+    })
+
+    it("renders InfoPageView", () => {
+      const Shallow = shallow(<InfoPage {...props} />)
+      expect(Shallow.find(InfoPageView).length).toBe(1)
+    })
   })
 })
