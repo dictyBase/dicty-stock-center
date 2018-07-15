17036cc933c15a2c91f66f82287ea714e03740fd
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-15 11:47:00 -0500

    add unit tests for InlineEditor methods

--- src/components/InlineEditor.test.js
+++ src/components/InlineEditor.test.js
@@ -62,42 +62,50 @@ describe("InlineEditor", () => {
     it("always renders an EditPanel component", () => {
       expect(inlineEditor().find(EditPanel).length).toBe(1)
     })
+
     it("always renders an Editor", () => {
       expect(inlineEditor().find(Editor).length).toBe(1)
     })
+
     it("renders the Authorization component", () => {
       expect(inlineEditor().find(Authorization).length).toBe(1)
     })
+
     it("renders ToolbarNav if editor isn't read only", () => {
       inlineEditor().setState({
         readOnly: false,
       })
       expect(inlineEditor().find(ToolbarNav).length).toBe(1)
     })
+
     it("does not render ToolbarNav if editor is read only", () => {
       inlineEditor().setState({
         readOnly: true,
       })
       expect(inlineEditor().find(ToolbarNav).length).toBe(0)
     })
+
     it("renders DefaultBlockButton if editor isn't read only", () => {
       inlineEditor().setState({
         readOnly: false,
       })
       expect(inlineEditor().find(DefaultBlockButton).length).toBe(1)
     })
+
     it("does not render DefaultBlockButton if editor is read only", () => {
       inlineEditor().setState({
         readOnly: true,
       })
       expect(inlineEditor().find(DefaultBlockButton).length).toBe(0)
     })
+
     it("renders SuccessBlockButton if editor isn't read only", () => {
       inlineEditor().setState({
         readOnly: false,
       })
       expect(inlineEditor().find(SuccessBlockButton).length).toBe(1)
     })
+
     it("does not render SuccessBlockButton if editor is read only", () => {
       inlineEditor().setState({
         readOnly: true,
@@ -117,6 +125,7 @@ describe("InlineEditor", () => {
             },
           },
         },
+        saveInlineEditing: () => {},
       }
     })
 
@@ -129,6 +138,7 @@ describe("InlineEditor", () => {
       button.find("button").simulate("click")
       expect(mockCallBack.mock.calls.length).toEqual(1)
     })
+
     it("should set state on SuccessBlockButton click", () => {
       const mockCallBack = jest.fn()
 
@@ -138,6 +148,7 @@ describe("InlineEditor", () => {
       button.find("button").simulate("click")
       expect(inlineEditor().state("readOnly")).toEqual(true)
     })
+
     it("DefaultBlockButton click should only be registered once", () => {
       const mockCallBack = jest.fn()
 
@@ -147,6 +158,7 @@ describe("InlineEditor", () => {
       button.find("button").simulate("click")
       expect(mockCallBack.mock.calls.length).toEqual(1)
     })
+
     it("should set state on DefaultBlockButton click", () => {
       const mockCallBack = jest.fn()
 
@@ -155,6 +167,43 @@ describe("InlineEditor", () => {
       )
       button.find("button").simulate("click")
       expect(inlineEditor().state("readOnly")).toEqual(true)
+    })
+  })
+
+  describe("InlineEditor methods", () => {
+    beforeEach(() => {
+      props = {
+        page: {
+          data: {
+            attributes: {
+              content: content,
+              updated_at: "999",
+            },
+          },
+        },
+        saveInlineEditing: () => {},
+        editInline: () => {},
+      }
+    })
+
+    const preventDefault = jest.fn()
+
+    it("should be read only after using onSave", () => {
+      const instance = inlineEditor().instance()
+      instance.onSave(true)
+      expect(inlineEditor().state("readOnly")).toEqual(true)
+    })
+
+    it("should be read only after using onCancel", () => {
+      const instance = inlineEditor().instance()
+      instance.onCancel(true)
+      expect(inlineEditor().state("readOnly")).toEqual(true)
+    })
+
+    it("should not be read only after using onEdit", () => {
+      const instance = inlineEditor().instance()
+      instance.onEdit({ preventDefault })
+      expect(inlineEditor().state("readOnly")).toEqual(false)
     })
   })
 })
