17822fba6128687605b0281defea9dcf40458491
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-16 20:13:04 -0500

    add more unit tests for api classes

--- src/utils/apiClasses.test.js
+++ src/utils/apiClasses.test.js
@@ -1,40 +1,38 @@
-import { JsonAPI, AuthAPI } from "./apiClasses"
-
-const authenticatedUser = {
-  token: "faketoken",
-  isAuthenticated: true,
-  data: {
-    id: "9",
-    attributes: {
-      first_name: "John",
-      last_name: "Doe",
-      email: "johndoe@fakeemail.com",
-      organization: "Northwestern University",
-      group_name: "n/a",
-      first_address: "750 N Lake Shore Drive",
-      second_address: "#11",
-      city: "Chicago",
-      state: "IL",
-      zipcode: "60611",
-      country: "USA",
-      phone: "n/a",
-      is_active: true,
-      created_at: "2018-07-16T22:58:10.407Z",
-      updated_at: "2018-07-16T22:58:10.407Z",
-    },
-    relationships: {
-      roles: {
-        links: {
-          self: "string",
-        },
-      },
-    },
-  },
-}
+import { JsonAPI, AuthAPI, AuthenticatedUser, ContentAPI } from "./apiClasses"
 
 describe("API Classes", () => {
   describe("JsonAPI class", () => {
-    let instance = new JsonAPI(authenticatedUser)
+    const userJson = {
+      data: {
+        id: "9",
+        attributes: {
+          first_name: "John",
+          last_name: "Doe",
+          email: "johndoe@fakeemail.com",
+          organization: "Northwestern University",
+          group_name: "n/a",
+          first_address: "750 N Lake Shore Drive",
+          second_address: "#11",
+          city: "Chicago",
+          state: "IL",
+          zipcode: "60611",
+          country: "USA",
+          phone: "n/a",
+          is_active: true,
+          created_at: "2018-07-16T22:58:10.407Z",
+          updated_at: "2018-07-16T22:58:10.407Z",
+        },
+        relationships: {
+          roles: {
+            links: {
+              self: "string",
+            },
+          },
+        },
+      },
+    }
+
+    let instance = new JsonAPI(userJson)
 
     it("creates a new JsonAPI instance", () => {
       expect(typeof instance, "object")
@@ -42,21 +40,56 @@ describe("API Classes", () => {
 
     it("can get attributes", () => {
       const attr = instance.getAttributes()
-      expect(attr).toBe(authenticatedUser.data.attributes)
+      expect(attr).toBe(userJson.data.attributes)
     })
 
     it("can get id", () => {
       const id = instance.getId()
-      expect(id).toBe(authenticatedUser.data.id)
+      expect(id).toBe("9")
     })
 
     it("can get relationships", () => {
       const relationships = instance.getRelationships()
-      expect(relationships).toBe(authenticatedUser.data.relationships)
+      expect(relationships).toBe(userJson.data.relationships)
     })
   })
 
   describe("AuthAPI class", () => {
+    const authenticatedUser = {
+      token: "faketoken",
+      isAuthenticated: true,
+      provider: "google",
+      user: {
+        data: {
+          id: "9",
+          attributes: {
+            first_name: "John",
+            last_name: "Doe",
+            email: "johndoe@fakeemail.com",
+            organization: "Northwestern University",
+            group_name: "n/a",
+            first_address: "750 N Lake Shore Drive",
+            second_address: "#11",
+            city: "Chicago",
+            state: "IL",
+            zipcode: "60611",
+            country: "USA",
+            phone: "n/a",
+            is_active: true,
+            created_at: "2018-07-16T22:58:10.407Z",
+            updated_at: "2018-07-16T22:58:10.407Z",
+          },
+          relationships: {
+            roles: {
+              links: {
+                self: "string",
+              },
+            },
+          },
+        },
+      },
+    }
+
     let instance = new AuthAPI(authenticatedUser)
 
     it("creates a new AuthAPI instance", () => {
@@ -65,7 +98,7 @@ describe("API Classes", () => {
 
     it("checks if authenticated", () => {
       const authenticated = instance.isAuthenticated()
-      expect(authenticated).toBe(authenticatedUser.isAuthenticated)
+      expect(authenticated).toBe(true)
     })
 
     it("can get token", () => {
@@ -75,7 +108,7 @@ describe("API Classes", () => {
 
     it("can get provider", () => {
       const provider = instance.getProvider()
-      expect(provider).toBe(authenticatedUser.provider)
+      expect(provider).toBe("google")
     })
 
     it("can get user", () => {
@@ -85,18 +118,123 @@ describe("API Classes", () => {
   })
 
   describe("AuthenticatedUser class", () => {
-    // tests go here
+    const superuser = {
+      token: "faketoken",
+      isAuthenticated: true,
+      provider: "google",
+      data: {
+        id: "9",
+        attributes: {
+          first_name: "John",
+          last_name: "Doe",
+          email: "johndoe@fakeemail.com",
+          organization: "Northwestern University",
+          group_name: "n/a",
+          first_address: "750 N Lake Shore Drive",
+          second_address: "#11",
+          city: "Chicago",
+          state: "IL",
+          zipcode: "60611",
+          country: "USA",
+          phone: "n/a",
+          is_active: true,
+          created_at: "2018-07-16T22:58:10.407Z",
+          updated_at: "2018-07-16T22:58:10.407Z",
+        },
+        relationships: {
+          roles: {
+            links: {
+              self: "string",
+            },
+          },
+        },
+      },
+      roles: {
+        type: "roles",
+        id: "99",
+        attributes: {
+          role: "superuser",
+          description: "total power",
+          created_at: "2018-07-17T00:57:07.502Z",
+          updated_at: "2018-07-17T00:57:07.502Z",
+        },
+      },
+    }
+
+    let instance = new AuthenticatedUser(superuser)
+
+    it("creates a new AuthenticatedUser instance", () => {
+      expect(typeof instance, "object")
+    })
+
+    it("can get user's full name", () => {
+      const name = instance.getFullName()
+      expect(name).toBe("John Doe")
+    })
+
+    // need to list roles as array above
+
+    // it("can get roles", () => {
+    //   const roles = instance.getRoles()
+    //   expect(roles).toBe("Superuser")
+    // })
+
+    // it("can overwrite as superuser", () => {
+    //   const endUser = instance.canOverwrite("99")
+    //   expect(endUser).toBe(true)
+    // })
   })
 
   describe("PermissionAPI class", () => {
-    // tests go here
+    it("can get resources", () => {
+      // test goes here
+    })
+
+    it("can get permissions", () => {
+      // tests go here
+    })
+
+    it("can verify permissions", () => {
+      // tests go here
+    })
   })
 
   describe("RoleAPI class", () => {
-    // tests go here
+    it("can get roles", () => {
+      // test goes here
+    })
+
+    it("can check roles", () => {
+      // tests go here
+    })
   })
 
   describe("ContentAPI class", () => {
-    // tests go here
+    const content = {
+      data: {
+        id: "9",
+        attributes: {
+          name: "content",
+          slug: "information",
+          created_by: "99",
+          updated_by: "99",
+          created_at: "2018-07-17T01:07:20.898Z",
+          updated_at: "2018-07-17T01:07:20.898Z",
+          content: "fake content",
+          namespace: "dictybase",
+        },
+      },
+    }
+
+    let instance = new ContentAPI(content)
+
+    it("creates a new ContentAPI instance", () => {
+      expect(typeof instance, "object")
+    })
+
+    it("can get user's ID", () => {
+      const name = instance.getUser()
+      expect(name).toBe("99")
+    })
   })
 })
