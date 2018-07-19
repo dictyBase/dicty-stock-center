bfb6213268a7466e5d5b7c41bf123b24d42b7c3b
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-19 10:01:50 -0500

    refactor function to fix issue with tests failing

--- src/utils/oauthConfig.js
+++ src/utils/oauthConfig.js
@@ -4,19 +4,13 @@ import clientConfig from "utils/clientConfig"
 // helper function to set redirect URL with basename if included
 const redirectUrlGenerator = provider => {
   let url
-  if (
-    process.env.REACT_APP_BASENAME === "" ||
-    process.env.REACT_APP_BASENAME === "/"
-  ) {
+  const basename = process.env.REACT_APP_BASENAME || "/"
+  if (basename === "" || basename === "/") {
     url = `${window.location.origin}/${provider}/callback`
-  } else if (process.env.REACT_APP_BASENAME.charAt(0) === "/") {
-    url = `${window.location.origin}${
-      process.env.REACT_APP_BASENAME
-    }/${provider}/callback`
+  } else if (basename.charAt(0) === "/") {
+    url = `${window.location.origin}${basename}/${provider}/callback`
   } else {
-    url = `${window.location.origin}/${
-      process.env.REACT_APP_BASENAME
-    }/${provider}/callback`
+    url = `${window.location.origin}/${basename}/${provider}/callback`
   }
   return url
 }
