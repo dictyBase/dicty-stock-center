9194439a941dd609057bf00293c288e8f89e7d1d
wildlifehexagon <crossthebreeze@gmail.com>
2019-04-05 11:52:16 -0500

    feat: add ErrorPage component to display user-friendly error messages
    fix #165

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -47,256 +47,226 @@ const makeOauthConfig = ({ query, provider, url }: oauthArg) => {
   return { config, endpoint }
 }
 
-export const requestLogin = (provider: string) => {
-  return {
-    type: LOGIN_REQUEST,
-    payload: {
-      isFetching: true,
-      provider: provider,
-    },
-  }
-}
+export const requestLogin = (provider: string) => ({
+  type: LOGIN_REQUEST,
+  payload: {
+    isFetching: true,
+    provider: provider,
+  },
+})
 
-export const receiveLogin = ({ user, token }: receiveLoginArg) => {
-  return {
-    type: LOGIN_SUCCESS,
-    payload: {
-      isFetching: false,
-      token: token,
-      user: user,
-    },
-  }
-}
+export const receiveLogin = ({ user, token }: receiveLoginArg) => ({
+  type: LOGIN_SUCCESS,
+  payload: {
+    isFetching: false,
+    token: token,
+    user: user,
+  },
+})
 
-export const loginError = (error: string) => {
-  return {
-    type: LOGIN_FAILURE,
-    payload: {
-      isFetching: false,
-      error: error,
-    },
-  }
-}
+export const loginError = (error: Object) => ({
+  type: LOGIN_FAILURE,
+  payload: {
+    isFetching: false,
+    error: error,
+  },
+})
 
-const receiveLogout = () => {
-  return {
-    type: LOGOUT_SUCCESS,
-    payload: {
-      isFetching: false,
-    },
-  }
-}
+const receiveLogout = () => ({
+  type: LOGOUT_SUCCESS,
+  payload: {
+    isFetching: false,
+  },
+})
 
-export const fetchUserRequest = () => {
-  return {
-    type: FETCH_USER_REQUEST,
-    payload: {
-      isFetching: true,
-    },
-  }
-}
+export const fetchUserRequest = () => ({
+  type: FETCH_USER_REQUEST,
+  payload: {
+    isFetching: true,
+  },
+})
 
-export const fetchUserSuccess = (json: Object) => {
-  return {
-    type: FETCH_USER_SUCCESS,
-    payload: {
-      isFetching: false,
-      json,
-    },
-  }
-}
+export const fetchUserSuccess = (json: Object) => ({
+  type: FETCH_USER_SUCCESS,
+  payload: {
+    isFetching: false,
+    json,
+  },
+})
 
-export const fetchUserFailure = (error: string) => {
-  return {
-    type: FETCH_USER_FAILURE,
-    payload: {
-      error: error,
-    },
-  }
-}
+export const fetchUserFailure = (error: Object) => ({
+  type: FETCH_USER_FAILURE,
+  payload: {
+    error: error,
+  },
+})
 
-export const fetchRoleRequest = () => {
-  return {
-    type: FETCH_ROLE_REQUEST,
-    payload: {
-      isFetching: true,
-    },
-  }
-}
+export const fetchRoleRequest = () => ({
+  type: FETCH_ROLE_REQUEST,
+  payload: {
+    isFetching: true,
+  },
+})
 
-export const fetchRoleSuccess = (json: Object) => {
-  return {
-    type: FETCH_ROLE_SUCCESS,
-    payload: {
-      isFetching: false,
-      json,
-    },
-  }
-}
+export const fetchRoleSuccess = (json: Object) => ({
+  type: FETCH_ROLE_SUCCESS,
+  payload: {
+    isFetching: false,
+    json,
+  },
+})
 
-export const fetchRoleFailure = (error: string) => {
-  return {
-    type: FETCH_ROLE_FAILURE,
-    payload: {
-      error: error,
-    },
-  }
-}
+export const fetchRoleFailure = (error: Object) => ({
+  type: FETCH_ROLE_FAILURE,
+  payload: {
+    error: error,
+  },
+})
 
 // Calls the API to get a token and
 // dispatch actions along the way
-export const oAuthLogin = ({ query, provider, url }: oauthArg) => {
-  return async (dispatch: Function) => {
-    const { config, endpoint } = makeOauthConfig({ query, provider, url })
-    try {
-      dispatch(requestLogin(provider))
-      dispatch(push("/load/auth"))
-      const res = await fetch(endpoint, config)
-      const contentType = res.headers.get("content-type")
-      if (contentType && contentType.includes("application/vnd.api+json")) {
-        if (res.ok) {
-          const json = await res.json()
-          dispatch(receiveLogin(json))
-          await dispatch(fetchRoleInfo(json.user.data.id))
-          dispatch(push("/mydsc"))
-          // history.go(-3)
-        } else if (res.status === 401) {
-          // user has invalid credentials, redirect with notification
-          dispatch(
-            loginError(
-              `You are not an authorized user of dictyBase.
+export const oAuthLogin = ({ query, provider, url }: oauthArg) => async (
+  dispatch: Function,
+) => {
+  const { config, endpoint } = makeOauthConfig({ query, provider, url })
+  try {
+    dispatch(requestLogin(provider))
+    dispatch(push("/load/auth"))
+    const res = await fetch(endpoint, config)
+    const contentType = res.headers.get("content-type")
+    if (contentType && contentType.includes("application/vnd.api+json")) {
+      if (res.ok) {
+        const json = await res.json()
+        dispatch(receiveLogin(json))
+        await dispatch(fetchRoleInfo(json.user.data.id))
+        dispatch(push("/mydsc"))
+        // history.go(-3)
+      } else if (res.status === 401) {
+        // user has invalid credentials, redirect with notification
+        dispatch(
+          loginError(
+            `You are not an authorized user of dictyBase.
               Please sign in with proper credentials or sign up with our user registration form when it is available.`,
-            ),
-          )
-          dispatch(push("/login"))
-        } else {
-          dispatch(loginError(res.statusText))
-          dispatch(push("/error"))
-        }
+          ),
+        )
+        dispatch(push("/login"))
       } else {
-        if (process.env.NODE_ENV !== "production") {
-          console.error(res.statusText)
-        }
-        dispatch(loginError(res.statusText))
+        dispatch(loginError(res.body))
         dispatch(push("/error"))
       }
-    } catch (error) {
-      dispatch(loginError(error.toString()))
+    } else {
+      if (process.env.NODE_ENV !== "production") {
+        console.error(res.body)
+      }
+      dispatch(loginError(res.body))
       dispatch(push("/error"))
     }
+  } catch (error) {
+    dispatch(loginError(error))
+    dispatch(push("/error"))
   }
 }
 
 // Logs the user out
-export const logoutUser = () => {
-  return (dispatch: Function) => {
-    dispatch(receiveLogout())
-  }
+export const logoutUser = () => (dispatch: Function) => {
+  dispatch(receiveLogout())
 }
 
 // fetch user function that fetches data using async/await
 // checks if header is correct, then either grabs data or displays error
 // this is used to get a non-authenticated user's information
-export const fetchUserInfo = (userId: string) => {
-  return async (dispatch: Function) => {
-    try {
-      dispatch(fetchUserRequest())
-      const res = await fetch(
-        `${fetchUserByIdResource}/${userId}`,
-        fetchHeaderConfig,
-      )
-      const contentType = res.headers.get("content-type")
-      if (contentType && contentType.includes("application/vnd.api+json")) {
-        const json = await res.json()
-        if (res.ok) {
-          dispatch(fetchUserSuccess(json))
-          await dispatch(fetchNonAuthRoleInfo(json.data.id))
-        } else {
-          if (process.env.NODE_ENV !== "production") {
-            printError(res, json)
-          }
-          dispatch(fetchUserFailure(json.errors[0].title))
-          dispatch(push("/error"))
-        }
+export const fetchUserInfo = (userId: string) => async (dispatch: Function) => {
+  try {
+    dispatch(fetchUserRequest())
+    const res = await fetch(
+      `${fetchUserByIdResource}/${userId}`,
+      fetchHeaderConfig,
+    )
+    const contentType = res.headers.get("content-type")
+    if (contentType && contentType.includes("application/vnd.api+json")) {
+      const json = await res.json()
+      if (res.ok) {
+        dispatch(fetchUserSuccess(json))
+        await dispatch(fetchNonAuthRoleInfo(json.data.id))
       } else {
         if (process.env.NODE_ENV !== "production") {
-          console.error(res.statusText)
+          printError(res, json)
         }
-        dispatch(fetchUserFailure(res.statusText))
+        dispatch(fetchUserFailure(json.errors[0]))
         dispatch(push("/error"))
       }
-    } catch (error) {
-      dispatch(fetchUserFailure(error.toString()))
-      dispatch(push("/error"))
+    } else {
       if (process.env.NODE_ENV !== "production") {
-        console.error(`Network error: ${error.message}`)
+        console.error(res.statusText)
       }
+      dispatch(fetchUserFailure(res.body))
+      dispatch(push("/error"))
+    }
+  } catch (error) {
+    dispatch(fetchUserFailure(error))
+    dispatch(push("/error"))
+    if (process.env.NODE_ENV !== "production") {
+      console.error(`Network error: ${error.message}`)
     }
   }
 }
 
 // fetch roles function that fetches data using async/await
 // checks if header is correct, then either grabs data or displays error
-export const fetchRoleInfo = (userId: string) => {
-  return async (dispatch: Function) => {
-    try {
-      dispatch(fetchRoleRequest())
-      const res = await fetch(
-        `${fetchUserByIdResource}/${userId}/roles`,
-        fetchHeaderConfig,
-      )
-      const contentType = res.headers.get("content-type")
-      if (contentType && contentType.includes("application/vnd.api+json")) {
-        const json = await res.json()
-        if (res.ok) {
-          if (json.data) {
-            dispatch(fetchRoleSuccess(json))
-            await dispatch(fetchPermissionInfo(json.data[0].id))
-          }
-        } else {
-          if (process.env.NODE_ENV !== "production") {
-            printError(res, json)
-          }
-          dispatch(fetchRoleFailure(json.errors[0].title))
+export const fetchRoleInfo = (userId: string) => async (dispatch: Function) => {
+  try {
+    dispatch(fetchRoleRequest())
+    const res = await fetch(
+      `${fetchUserByIdResource}/${userId}/roles`,
+      fetchHeaderConfig,
+    )
+    const contentType = res.headers.get("content-type")
+    if (contentType && contentType.includes("application/vnd.api+json")) {
+      const json = await res.json()
+      if (res.ok) {
+        if (json.data) {
+          dispatch(fetchRoleSuccess(json))
+          await dispatch(fetchPermissionInfo(json.data[0].id))
         }
       } else {
         if (process.env.NODE_ENV !== "production") {
-          console.error(res.statusText)
+          printError(res, json)
         }
-        dispatch(fetchRoleFailure(res.statusText))
+        dispatch(fetchRoleFailure(json.errors[0].title))
       }
-    } catch (error) {
-      dispatch(fetchUserFailure(error.toString()))
+    } else {
       if (process.env.NODE_ENV !== "production") {
-        console.error(`Network error: ${error.message}`)
+        console.error(res.statusText)
       }
+      dispatch(fetchRoleFailure(res.statusText))
+    }
+  } catch (error) {
+    dispatch(fetchUserFailure(error.toString()))
+    if (process.env.NODE_ENV !== "production") {
+      console.error(`Network error: ${error.message}`)
     }
   }
 }
 
-export const fetchNonAuthRoleInfo = (userId: string) => {
-  return {
-    types: [
-      FETCH_NON_AUTH_ROLE_REQUEST,
-      FETCH_NON_AUTH_ROLE_SUCCESS,
-      FETCH_NON_AUTH_ROLE_FAILURE,
-    ],
-    url: `${fetchUserByIdResource}/${userId}/roles`,
-    config: fetchHeaderConfig,
-  }
-}
+export const fetchNonAuthRoleInfo = (userId: string) => ({
+  types: [
+    FETCH_NON_AUTH_ROLE_REQUEST,
+    FETCH_NON_AUTH_ROLE_SUCCESS,
+    FETCH_NON_AUTH_ROLE_FAILURE,
+  ],
+  url: `${fetchUserByIdResource}/${userId}/roles`,
+  config: fetchHeaderConfig,
+})
 
-export const fetchPermissionInfo = (roleId: string) => {
-  return {
-    types: [
-      FETCH_PERMISSION_REQUEST,
-      FETCH_PERMISSION_SUCCESS,
-      FETCH_PERMISSION_FAILURE,
-    ],
-    url: `${fetchRoleByIdResource}/${roleId}/permissions`,
-    config: fetchHeaderConfig,
-  }
-}
+export const fetchPermissionInfo = (roleId: string) => ({
+  types: [
+    FETCH_PERMISSION_REQUEST,
+    FETCH_PERMISSION_SUCCESS,
+    FETCH_PERMISSION_FAILURE,
+  ],
+  url: `${fetchRoleByIdResource}/${roleId}/permissions`,
+  config: fetchHeaderConfig,
+})
 
 // helper function to print HTTP errors to console
 // responses are structured in JSONAPI format
---
+++ src/components/ErrorPage.js
@@ -0,0 +1,199 @@
+// @flow
+import React from "react"
+import { connect } from "react-redux"
+import { withStyles } from "@material-ui/core/styles"
+import Grid from "@material-ui/core/Grid"
+import Button from "@material-ui/core/Button"
+import FontAwesome from "react-fontawesome"
+import sadDicty from "static/sad-dicty.png"
+
+const styles = theme => ({
+  error400: {
+    backgroundColor: "#eff8fb",
+    textAlign: "center",
+    paddingTop: 30,
+    paddingBottom: 30,
+    marginBottom: 30,
+    borderRadius: 5,
+  },
+  error500: {
+    backgroundColor: "#a63232",
+    textAlign: "center",
+    paddingTop: 40,
+    paddingBottom: 40,
+    marginBottom: 30,
+    borderRadius: 5,
+    color: "#e3e3e3",
+  },
+  backButton: {
+    width: "25%",
+    padding: "20px",
+    textTransform: "none",
+    backgroundColor: "#15317e",
+    color: "#e3e3e3",
+    "&:hover": {
+      backgroundColor: "#1a3d9e",
+    },
+  },
+  mainGrid: {
+    marginTop: "40px",
+  },
+  paragraph: {
+    padding: "10px",
+  },
+  link: {
+    color: "#428bca",
+    textDecoration: "none",
+  },
+  link500: {
+    color: "#e0e0e0",
+    textDecoration: "none",
+  },
+  list: {
+    margin: "0 auto",
+    display: "table",
+  },
+})
+
+type Props = {
+  /** Material-UI styling */
+  classes: Object,
+  /** the object that contains auth data from current state */
+  auth: Object,
+  /** the object that contains order data from current state */
+  order: Object,
+  /** the object that contains page data from current state */
+  page: Object,
+}
+
+/**
+ * General error handling page. It displays different messages based on HTTP status code.
+ */
+
+export const ErrorPage = (props: Props) => {
+  const { auth, order, page, classes } = props
+
+  let errorStatus = 0
+  let errorMsg
+
+  if (auth.error) {
+    errorStatus = auth.error.status
+    errorMsg = auth.error.title
+  }
+
+  if (order.error) {
+    errorStatus = order.error.status
+    errorMsg = order.error.title
+  }
+
+  if (page.error) {
+    console.log(page.error)
+    errorStatus = page.error.status
+    errorMsg = page.error.title
+  }
+
+  if (errorStatus >= 500) {
+    return (
+      <Grid container className={classes.mainGrid} justify="center">
+        <Grid item xs={10} md={8}>
+          <div className={classes.error500}>
+            <h2>Sorry! There was a server error.</h2>
+            <p>
+              If the problem persists, please email us at{" "}
+              <a
+                className={classes.link500}
+                href="mailto:dictybase@northwestern.edu">
+                dictybase@northwestern.edu
+              </a>
+              .
+            </p>
+            <a href="/">
+              <Button
+                className={classes.backButton}
+                size="small"
+                variant="contained"
+                color="default">
+                Back to homepage
+              </Button>
+            </a>
+          </div>
+        </Grid>
+      </Grid>
+    )
+  }
+
+  if (errorStatus === 404) {
+    return (
+      <Grid container className={classes.mainGrid} justify="center">
+        <Grid item xs={10} md={8}>
+          <div className={classes.error400}>
+            <img src={sadDicty} alt="Sad Dicty -- Gene Not Found" />
+            <h3>Gene Not Found</h3>
+            <div className={classes.list}>
+              <ul>
+                <li>This is probably an invalid ID. Try a different one.</li>
+                <li>You might be coming here from an outdated link.</li>
+              </ul>
+            </div>
+            <p>
+              {" "}
+              If problems persist, email us at{" "}
+              <a
+                className={classes.link}
+                href="mailto:dictybase@northwestern.edu">
+                dictybase@northwestern.edu
+              </a>
+              .
+            </p>
+            <a href="/">
+              <Button
+                className={classes.backButton}
+                size="small"
+                variant="contained"
+                color="primary">
+                Back to homepage
+              </Button>
+            </a>
+          </div>
+        </Grid>
+      </Grid>
+    )
+  }
+
+  return (
+    <Grid container className={classes.mainGrid} justify="center">
+      <Grid item xs={10} md={8}>
+        <div className={classes.error400}>
+          <img src={sadDicty} alt="Sad Dicty -- HTTP Error" />
+          <h1>
+            <FontAwesome name="exclamation-circle" /> {errorStatus} Error
+          </h1>
+          <h3>{errorMsg}</h3>
+          <p>
+            If the problem persists, please email us at{" "}
+            <a
+              className={classes.link}
+              href="mailto:dictybase@northwestern.edu">
+              dictybase@northwestern.edu
+            </a>
+            .
+          </p>
+          <a href="/">
+            <Button
+              className={classes.backButton}
+              size="small"
+              variant="contained"
+              color="primary">
+              Back to Homepage
+            </Button>
+          </a>
+        </div>
+      </Grid>
+    </Grid>
+  )
+}
+
+const mapStateToProps = ({ auth, order, page }) => ({ auth, order, page })
+
+// $FlowFixMe
+export default connect(mapStateToProps)(withStyles(styles)(ErrorPage))
