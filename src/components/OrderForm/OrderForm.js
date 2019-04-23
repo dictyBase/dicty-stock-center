e44197667f33dd630d0b039b13333f800bc953eb
wildlifehexagon <crossthebreeze@gmail.com>
2019-04-23 17:44:53 -0500

    feat: connect order form submission to apollo client mutation
    #190

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,7 +1,10 @@
 /* eslint-disable react/jsx-no-bind */
 // @flow
 import React, { useState } from "react"
+import { connect } from "react-redux"
 import { Form, Formik } from "formik"
+import gql from "graphql-tag"
+import { Mutation } from "react-apollo"
 import { Helmet } from "react-helmet"
 import { withStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
@@ -11,8 +14,17 @@ import SubmitPage from "./Submit/SubmitPage"
 import initialValues from "./initialValues"
 import validationSchema from "./validationSchema"
 import styles from "./formStyles"
+import type { MapStateToProps } from "react-redux"
 
 const pages = [ShippingPage, PaymentPage, SubmitPage]
+
+const POST_ORDER = gql`
+  mutation CreateOrder($input: CreateOrderInput!) {
+    createOrder(input: $input) {
+      id
+    }
+  }
+`
 
 type Props = {
   /** Material-UI styling */
@@ -25,15 +37,8 @@ type Props = {
 
 const OrderForm = (props: Props) => {
   const [pageNum, setPageNum] = useState(0)
-  const { classes } = props
+  const { classes, items } = props
   const PageComponent = pages[pageNum]
-
-  const handleSubmit = (values, actions) => {
-    actions.setSubmitting(false)
-    alert(JSON.stringify(values, null, 2))
-    const { history } = props
-    history.push("/order/submitted")
-  }
 
   return (
     <Grid container spacing={16} className={classes.layout}>
@@ -42,19 +47,47 @@ const OrderForm = (props: Props) => {
         <meta name="description" content="Order form for Dicty Stock Center" />
       </Helmet>
       <Grid item xs={12}>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={handleSubmit}
-          render={props => (
-            <Form>
-              <PageComponent {...props} page={[pageNum, setPageNum]} />
-            </Form>
+        <Mutation mutation={POST_ORDER}>
+          {(createOrder, { loading, error }) => (
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={async (values, actions) => {
+                actions.setSubmitting(false)
+                await createOrder({
+                  variables: {
+                    input: {
+                      courier: values.shippingAccount,
+                      courier_account: values.shippingAccountNumber,
+                      comments: values.comments,
+                      payment: values.paymentMethod,
+                      purchase_order_num: values.purchaseOrderNum,
+                      status: "IN_PREPARATION",
+                      consumer: values.email,
+                      payer: values.payerEmail,
+                      purchaser: values.email,
+                      items: items.map(item => item.id),
+                    },
+                  },
+                })
+                const { history } = props
+                history.push("/order/submitted")
+              }}
+              render={props => (
+                <Form>
+                  <PageComponent {...props} page={[pageNum, setPageNum]} />
+                </Form>
+              )}
+            />
           )}
-        />
+        </Mutation>
       </Grid>
     </Grid>
   )
 }
 
-export default withStyles(styles)(OrderForm)
+const mapStateToProps: MapStateToProps<*, *, *> = state => ({
+  items: state.cart.addedItems,
+})
+
+export default connect(mapStateToProps)(withStyles(styles)(OrderForm))
