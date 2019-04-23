715e9c908dd2b3c6da7a986810e2d03a3cc15b88
wildlifehexagon <crossthebreeze@gmail.com>
2019-04-23 14:55:40 -0500

    docs: update styleguide build
    #190

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,7 +2,13 @@ const path = require("path")
 module.exports = {
   styleguideDir: "docs",
   webpackConfig: require("react-scripts/config/webpack.config")("development"),
-  ignore: ["**/*.test.{js,jsx,ts,tsx}", "**/*Styles.js", "**/mock*.js"],
+  ignore: [
+    "**/*.test.{js,jsx,ts,tsx}",
+    "**/*Styles.js",
+    "**/mock*.js",
+    "src/components/OrderForm/initialValues.js",
+    "src/components/OrderForm/validationSchema.js",
+  ],
   styleguideComponents: {
     Wrapper: path.join(__dirname, "src/styleguide/Wrapper"),
   },
