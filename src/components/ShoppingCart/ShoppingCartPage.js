70aea94252844295f30ecea6cdddf5ad92c80b91
wildlifehexagon <crossthebreeze@gmail.com>
2019-04-25 17:51:43 -0500

    refactor: convert ShoppingCartPage to functional component

--- src/components/ShoppingCart/ShoppingCartPage.js
+++ src/components/ShoppingCart/ShoppingCartPage.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from "react"
+import React from "react"
 import { connect } from "react-redux"
 import { Helmet } from "react-helmet"
 import { Link } from "react-router-dom"
@@ -66,84 +66,82 @@ type Props = {
 
 /** ShoppingCartPage displays the items in the cart and provides buttons to checkout or continue browsing. */
 
-export class ShoppingCartPage extends Component<Props> {
-  render() {
-    const { items, classes } = this.props
+export const ShoppingCartPage = (props: Props) => {
+  const { items, classes } = props
 
-    return (
-      <div className={classes.container}>
-        <Helmet>
-          <title>Shopping Cart - Dicty Stock Center</title>
-          <meta
-            name="description"
-            content="Shopping cart page for Dicty Stock Center"
-          />
-        </Helmet>
-        <Grid container justify="center">
-          <Grid item>
-            <h1 className={classes.header}>Shopping Cart</h1>
+  return (
+    <div className={classes.container}>
+      <Helmet>
+        <title>Shopping Cart - Dicty Stock Center</title>
+        <meta
+          name="description"
+          content="Shopping cart page for Dicty Stock Center"
+        />
+      </Helmet>
+      <Grid container justify="center">
+        <Grid item>
+          <h1 className={classes.header}>Shopping Cart</h1>
+        </Grid>
+      </Grid>
+      {items.length > 0 ? (
+        <>
+          <Grid container justify="center" wrap="wrap">
+            <Grid item xs={12}>
+              <ShoppingCartItems />
+            </Grid>
+          </Grid>
+          <Grid container wrap="wrap" justify="center">
+            <Grid item xs={3} />
+            <Grid item xs={12} sm={6} className={classes.btnGrid}>
+              <Button
+                component={Link}
+                to="/strains"
+                color="primary"
+                variant="contained"
+                size="large"
+                className={classes.continueBtn}>
+                <FontAwesomeIcon icon="share" /> &nbsp; Continue Shopping
+              </Button>
+              <Button
+                component={Link}
+                to="/order/checkout"
+                color="primary"
+                variant="contained"
+                size="large"
+                className={classes.checkoutBtn}>
+                <FontAwesomeIcon icon="shopping-cart" /> &nbsp; Checkout
+              </Button>
+            </Grid>
+            <Grid item xs={3} />
+          </Grid>
+        </>
+      ) : (
+        <Grid container wrap="wrap" justify="center">
+          <Grid item xs={11} sm={7}>
+            <div className={classes.alertBox}>
+              <strong>
+                <FontAwesomeIcon icon="exclamation-circle" />
+              </strong>{" "}
+              There are no items in your cart.
+            </div>
+            <Grid container justify="center">
+              <Grid item>
+                <h2>Browse our catalogs</h2>
+                <ul>
+                  <li>
+                    <Link to="/strains">Strains Catalog</Link>
+                  </li>
+                  <li>
+                    <Link to="/plasmids">Plasmids Catalog</Link>
+                  </li>
+                </ul>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
-        {items.length > 0 ? (
-          <>
-            <Grid container justify="center" wrap="wrap">
-              <Grid item xs={12}>
-                <ShoppingCartItems />
-              </Grid>
-            </Grid>
-            <Grid container wrap="wrap" justify="center">
-              <Grid item xs={3} />
-              <Grid item xs={12} sm={6} className={classes.btnGrid}>
-                <Button
-                  component={Link}
-                  to="/strains"
-                  color="primary"
-                  variant="contained"
-                  size="large"
-                  className={classes.continueBtn}>
-                  <FontAwesomeIcon icon="share" /> &nbsp; Continue Shopping
-                </Button>
-                <Button
-                  component={Link}
-                  to="/order/checkout"
-                  color="primary"
-                  variant="contained"
-                  size="large"
-                  className={classes.checkoutBtn}>
-                  <FontAwesomeIcon icon="shopping-cart" /> &nbsp; Checkout
-                </Button>
-              </Grid>
-              <Grid item xs={3} />
-            </Grid>
-          </>
-        ) : (
-          <Grid container wrap="wrap" justify="center">
-            <Grid item xs={11} sm={7}>
-              <div className={classes.alertBox}>
-                <strong>
-                  <FontAwesomeIcon icon="exclamation-circle" />
-                </strong>{" "}
-                There are no items in your cart.
-              </div>
-              <Grid container justify="center">
-                <Grid item>
-                  <h2>Browse our catalogs</h2>
-                  <ul>
-                    <li>
-                      <Link to="/strains">Strains Catalog</Link>
-                    </li>
-                    <li>
-                      <Link to="/plasmids">Plasmids Catalog</Link>
-                    </li>
-                  </ul>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        )}
-      </div>
-    )
-  }
+      )}
+    </div>
+  )
 }
 
 const mapStateToProps: MapStateToProps<*, *, *> = state => ({
