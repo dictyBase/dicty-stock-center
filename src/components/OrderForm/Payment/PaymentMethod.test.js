730de2db26ec4e5bfe57476d3b18d7234cc11d82
wildlifehexagon <crossthebreeze@gmail.com>
2019-04-25 16:21:04 -0500

    test: add tests for payment and shipping methods
    #195

--- src/components/OrderForm/Payment/PaymentMethod.test.js
+++ src/components/OrderForm/Payment/PaymentMethod.test.js
@@ -1,17 +1,21 @@
 import React from "react"
 import { shallow } from "enzyme"
+import sinon from "sinon"
 import PaymentMethod from "./PaymentMethod"
 import Grid from "@material-ui/core/Grid"
 import RadioGroup from "@material-ui/core/RadioGroup"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
-import TextField from "../TextField"
 
 describe("OrderForm/Payment/PaymentMethod", () => {
+  const handleChangeSpy = sinon.spy()
+  const setFieldValueSpy = sinon.spy()
   const props = {
     classes: {
       requiredText: "requiredText",
       innerForm: "innerForm",
     },
+    handleChange: handleChangeSpy,
+    setFieldValue: setFieldValueSpy,
   }
   const wrapper = shallow(<PaymentMethod {...props} />)
   describe("initial render", () => {
@@ -22,6 +26,35 @@ describe("OrderForm/Payment/PaymentMethod", () => {
       expect(wrapper.dive().find(Grid)).toHaveLength(3)
       expect(wrapper.dive().find(RadioGroup)).toHaveLength(1)
       expect(wrapper.dive().find(FormControlLabel)).toHaveLength(3)
+    })
+  })
+  describe("radio button interactions", () => {
+    it("sets field value when clicking first two radio buttons", () => {
+      // click credit card button
+      const cc = wrapper
+        .dive()
+        .find(FormControlLabel)
+        .first()
+      cc.simulate("change")
+      expect(setFieldValueSpy.calledOnce).toBe(true)
+      // click wire transfer button
+      const wire = wrapper
+        .dive()
+        .find(FormControlLabel)
+        .at(1)
+      wire.simulate("change")
+      expect(setFieldValueSpy.calledTwice).toBe(true)
+    })
+    it("does not set field value when clicking Purchase Order radio button", () => {
+      // reset spy
+      setFieldValueSpy.resetHistory()
+      // click wire transfer button
+      const po = wrapper
+        .dive()
+        .find(FormControlLabel)
+        .at(2)
+      po.simulate("change")
+      expect(setFieldValueSpy.notCalled).toBe(true)
     })
   })
 })
--- src/components/OrderForm/Shipping/ShippingMethod.test.js
+++ src/components/OrderForm/Shipping/ShippingMethod.test.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { shallow } from "enzyme"
+import sinon from "sinon"
 import ShippingMethod from "./ShippingMethod"
 import Grid from "@material-ui/core/Grid"
 import RadioGroup from "@material-ui/core/RadioGroup"
@@ -7,11 +8,15 @@ import FormControlLabel from "@material-ui/core/FormControlLabel"
 import TextField from "../TextField"
 
 describe("OrderForm/Shipping/ShippingMethod", () => {
+  const handleChangeSpy = sinon.spy()
+  const setFieldValueSpy = sinon.spy()
   const props = {
     classes: {
       requiredText: "requiredText",
       innerForm: "innerForm",
     },
+    handleChange: handleChangeSpy,
+    setFieldValue: setFieldValueSpy,
   }
   const wrapper = shallow(<ShippingMethod {...props} />)
   describe("initial render", () => {
@@ -23,6 +28,42 @@ describe("OrderForm/Shipping/ShippingMethod", () => {
       expect(wrapper.dive().find(TextField)).toHaveLength(1)
       expect(wrapper.dive().find(RadioGroup)).toHaveLength(1)
       expect(wrapper.dive().find(FormControlLabel)).toHaveLength(4)
+    })
+  })
+  describe("radio button interactions", () => {
+    it("sets field value when clicking prepaid shipping label", () => {
+      // click prepaid label button
+      const label = wrapper
+        .dive()
+        .find(FormControlLabel)
+        .last()
+      label.simulate("change")
+      expect(setFieldValueSpy.calledOnce).toBe(true)
+    })
+    it("does not set field value when clicking others", () => {
+      // reset spy
+      setFieldValueSpy.resetHistory()
+      // click fedex button
+      const fedex = wrapper
+        .dive()
+        .find(FormControlLabel)
+        .first()
+      fedex.simulate("change")
+      expect(setFieldValueSpy.notCalled).toBe(true)
+      // click UPS button
+      const ups = wrapper
+        .dive()
+        .find(FormControlLabel)
+        .at(1)
+      ups.simulate("change")
+      expect(setFieldValueSpy.notCalled).toBe(true)
+      // click DHL button
+      const dhl = wrapper
+        .dive()
+        .find(FormControlLabel)
+        .at(2)
+      dhl.simulate("change")
+      expect(setFieldValueSpy.notCalled).toBe(true)
     })
   })
 })
