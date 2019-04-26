7f61cdc0cbbdbf3374a10e1198bbc36e0019b51a
wildlifehexagon <crossthebreeze@gmail.com>
2019-04-26 16:47:02 -0500

    test: add unit tests for individual order form pages
    #195

--- src/components/OrderForm/Shipping/ShippingPage.test.js
+++ src/components/OrderForm/Shipping/ShippingPage.test.js
@@ -1,23 +1,52 @@
 import React from "react"
 import { shallow } from "enzyme"
+import sinon from "sinon"
 import ShippingPage from "./ShippingPage"
 import Grid from "@material-ui/core/Grid"
-import TextField from "../TextField"
-
-// needs coverage for 57,61,88,89,95
+import Button from "@material-ui/core/Button"
+import PanelWrapper from "components/common/PanelWrapper"
+import ShippingAddress from "./ShippingAddress"
+import ShippingMethod from "./ShippingMethod"
+import AdditionalInformation from "./AdditionalInformation"
 
 describe("OrderForm/Shipping/ShippingPage", () => {
+  const setPageNumSpy = sinon.spy()
   const props = {
     classes: {},
+    values: {
+      firstName: "john",
+      lastName: "doe",
+      email: "johndoe@test.com",
+      organization: "northwestern",
+      lab: "dictybase",
+      address1: "123 fake st",
+      city: "chicago",
+      zip: "60601",
+      country: "usa",
+      phone: "1234567890",
+      shippingAccountNumber: "999",
+    },
+    setPageNum: setPageNumSpy,
   }
   const wrapper = shallow(<ShippingPage {...props} />)
   describe("initial render", () => {
     it("renders without crashing", () => {
       expect(wrapper).toHaveLength(1)
     })
-    // it("always renders initial components", () => {
-    //   expect(wrapper.find(Grid)).toHaveLength(6)
-    //   expect(wrapper.find(TextField)).toHaveLength(3)
-    // })
+    it("always renders initial components", () => {
+      expect(wrapper.dive().find(Grid)).toHaveLength(8)
+      expect(wrapper.dive().find(PanelWrapper)).toHaveLength(3)
+      expect(wrapper.dive().find(ShippingAddress)).toHaveLength(1)
+      expect(wrapper.dive().find(ShippingMethod)).toHaveLength(1)
+      expect(wrapper.dive().find(AdditionalInformation)).toHaveLength(1)
+      expect(wrapper.dive().find(Button)).toHaveLength(1)
+    })
+  })
+  describe("clicking continue button", () => {
+    it("should update page number on click", () => {
+      const btn = wrapper.dive().find(Button)
+      btn.simulate("click")
+      expect(setPageNumSpy.calledOnce).toBe(true)
+    })
   })
 })
---
+++ src/components/OrderForm/Submit/SubmitPage.test.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { shallow } from "enzyme"
+import sinon from "sinon"
+import { SubmitPage } from "./SubmitPage"
+import { data } from "./mockSubmitData"
+import Grid from "@material-ui/core/Grid"
+import Button from "@material-ui/core/Button"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import PanelWrapper from "components/common/PanelWrapper"
+
+describe("OrderForm/Submit/SubmitPage", () => {
+  const setPageNumSpy = sinon.spy()
+  const props = {
+    classes: {},
+    items: data,
+    setPageNum: setPageNumSpy,
+  }
+  const wrapper = shallow(<SubmitPage {...props} />)
+  describe("initial render", () => {
+    it("renders without crashing", () => {
+      expect(wrapper).toHaveLength(1)
+    })
+    it("always renders initial components", () => {
+      expect(wrapper.find(Grid)).toHaveLength(21)
+      expect(wrapper.find(PanelWrapper)).toHaveLength(1)
+      expect(wrapper.find(Button)).toHaveLength(2)
+      expect(wrapper.find(FontAwesomeIcon)).toHaveLength(3)
+    })
+  })
+  describe("clicking previous button", () => {
+    it("should update page number on click", () => {
+      // reset spy
+      setPageNumSpy.resetHistory()
+      const btn = wrapper
+        .dive()
+        .find(Button)
+        .first()
+      btn.simulate("click")
+      expect(setPageNumSpy.calledOnce).toBe(true)
+    })
+  })
+})
