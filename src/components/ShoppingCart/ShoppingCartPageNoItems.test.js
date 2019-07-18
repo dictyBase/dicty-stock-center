eb9e9b4cee527872bc330a4f79a298ab69e1cbb1
wildlifehexagon <crossthebreeze@gmail.com>
2019-07-18 14:59:14 -0500

    test: add unit tests for new cart components
    #195

---
+++ src/components/ShoppingCart/ShoppingCartPageNoItems.test.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { shallow } from "enzyme"
+import ShoppingCartPageNoItems from "./ShoppingCartPageNoItems"
+import Grid from "@material-ui/core/Grid"
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+describe("ShoppingCart/ShoppingCartPageNoItems", () => {
+  describe("initial render", () => {
+    const props = {
+      classes: {},
+    }
+    const wrapper = shallow(<ShoppingCartPageNoItems {...props} />).dive()
+    it("renders without crashing", () => {
+      expect(wrapper).toHaveLength(1)
+    })
+    it("always renders initial components", () => {
+      expect(wrapper.find(Grid)).toHaveLength(4)
+      expect(wrapper.find(Link)).toHaveLength(2)
+      expect(wrapper.find(FontAwesomeIcon)).toHaveLength(1)
+    })
+  })
+})
