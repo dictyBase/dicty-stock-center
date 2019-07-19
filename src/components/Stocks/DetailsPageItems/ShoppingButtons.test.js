1abe92646ecea292694680992009ac0805ad4f21
wildlifehexagon <crossthebreeze@gmail.com>
2019-07-19 13:56:54 -0500

    test: add more ShoppingButtons tests #195

--- src/components/Stocks/DetailsPageItems/ShoppingButtons.test.js
+++ src/components/Stocks/DetailsPageItems/ShoppingButtons.test.js
@@ -1,16 +1,18 @@
 import React from "react"
 import { shallow } from "enzyme"
+import sinon from "sinon"
 import { ShoppingButtons } from "./ShoppingButtons"
 import Button from "@material-ui/core/Button"
 import Snackbar from "@material-ui/core/Snackbar"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 describe("Stock/ShoppingButtons", () => {
+  const addToCartSpy = sinon.spy()
   const props = {
     classes: {
       container: "",
     },
-    addToCart: () => {},
+    addToCart: addToCartSpy,
     type: "strain",
     id: "DBS123456",
     name: "test1",
@@ -27,6 +29,10 @@ describe("Stock/ShoppingButtons", () => {
       expect(wrapper.find(Snackbar)).toHaveLength(1)
       expect(wrapper.find(FontAwesomeIcon)).toHaveLength(2)
     })
+    it("should not render Snackbar by default", () => {
+      const snackbar = wrapper.find(Snackbar)
+      expect(snackbar.prop("open")).toBe(false)
+    })
   })
   describe("the rendered div", () => {
     it("contains everything else that gets rendered", () => {
@@ -37,22 +43,19 @@ describe("Stock/ShoppingButtons", () => {
       expect(wrappingDiv.children()).toEqual(wrapper.children())
     })
   })
-  describe("Snackbar", () => {
-    it("should not be visible by default", () => {
+  describe("when first Button is clicked", () => {
+    it("should call addToCart function on click", () => {
+      const firstButton = wrapper.find(Button).first()
+      firstButton.simulate("click")
+      expect(addToCartSpy.calledOnce).toBe(true)
+    })
+    it("should open Snackbar on click", () => {
+      const firstButton = wrapper.find(Button).first()
       const snackbar = wrapper.find(Snackbar)
-      expect(snackbar.props().open).toBe(false)
+
+      firstButton.simulate("click")
+      wrapper.update()
+      expect(snackbar.prop("open")).toBe(true)
     })
   })
-  // describe("when first Button is clicked", () => {
-  //   it("should toggle Snackbar's open state when clicking Add to Cart button", () => {
-  //     const firstButton = wrapper.find(Button).first()
-  //     const snackbar = wrapper.find(Snackbar)
-
-  //     firstButton.simulate("click")
-  //     expect(snackbar.props().open).toBe(true)
-
-  //     firstButton.simulate("click")
-  //     expect(snackbar.props().open).toBe(false)
-  //   })
-  // })
 })
