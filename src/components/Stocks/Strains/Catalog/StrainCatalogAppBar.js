653ae5a75df9ac097d3e5d9a8ffe0836ead43d4c
wildlifehexagon <crossthebreeze@gmail.com>
2019-08-21 15:42:37 -0500

    refactor: move app bar left menu into its own component

--- src/components/Stocks/Strains/Catalog/StrainCatalogAppBar.js
+++ src/components/Stocks/Strains/Catalog/StrainCatalogAppBar.js
@@ -1,58 +1,16 @@
 // @flow
 import React from "react"
-import { makeStyles, fade } from "@material-ui/core/styles"
+import { makeStyles } from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Grid from "@material-ui/core/Grid"
-import IconButton from "@material-ui/core/IconButton"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import StrainCatalogAppBarRightMenu from "components/Stocks/Strains/Catalog/StrainCatalogAppBarRightMenu"
+import StrainCatalogAppBarLeftMenu from "components/Stocks/Strains/Catalog/StrainCatalogAppBarLeftMenu"
 import StrainCatalogAppBarSearch from "components/Stocks/Strains/Catalog/StrainCatalogAppBarSearch"
+import StrainCatalogAppBarRightMenu from "components/Stocks/Strains/Catalog/StrainCatalogAppBarRightMenu"
 
 const useStyles = makeStyles(theme => ({
   container: {
     backgroundColor: "#0059b3",
-  },
-  icon: {
-    color: "#fff",
-    paddingRight: "5px",
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto",
-    },
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  inputRoot: {
-    color: "inherit",
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: 120,
-      "&:focus": {
-        width: 200,
-      },
-    },
   },
 }))
 
@@ -63,9 +21,7 @@ const StrainCatalogAppBar = props => {
     <AppBar position="static" className={classes.container}>
       <Toolbar>
         <Grid container justify="flex-start">
-          <IconButton size="small" className={classes.icon}>
-            <FontAwesomeIcon icon="bars" />
-          </IconButton>
+          <StrainCatalogAppBarLeftMenu />
         </Grid>
         <Grid container justify="center">
           <StrainCatalogAppBarSearch />
---
+++ src/components/Stocks/Strains/Catalog/StrainCatalogAppBarLeftMenu.js
@@ -0,0 +1,26 @@
+// @flow
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import IconButton from "@material-ui/core/IconButton"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    color: "#fff",
+    paddingRight: "5px",
+  },
+}))
+
+const StrainCatalogAppBarLeftMenu = props => {
+  const classes = useStyles()
+
+  return (
+    <>
+      <IconButton size="small" className={classes.icon}>
+        <FontAwesomeIcon icon="bars" />
+      </IconButton>
+    </>
+  )
+}
+
+export default StrainCatalogAppBarLeftMenu
