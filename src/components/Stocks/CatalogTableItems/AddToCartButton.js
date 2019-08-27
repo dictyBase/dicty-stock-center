357ce6e6646aa8e110d9df68a9a2e19cfcdbb7ed
wildlifehexagon <crossthebreeze@gmail.com>
2019-08-27 14:58:12 -0500

    feat: add dialog box when user adds item to cart #236

--- src/components/Stocks/CatalogTableItems/AddToCartButton.js
+++ src/components/Stocks/CatalogTableItems/AddToCartButton.js
@@ -1,18 +1,49 @@
 // @flow
 import React, { useState } from "react"
 import { connect } from "react-redux"
-import { withStyles } from "@material-ui/core/styles"
+import { Link, withRouter } from "react-router-dom"
+import { makeStyles } from "@material-ui/styles"
 import IconButton from "@material-ui/core/IconButton"
-import Snackbar from "@material-ui/core/Snackbar"
+import Button from "@material-ui/core/Button"
+import DialogTitle from "@material-ui/core/DialogTitle"
+import Dialog from "@material-ui/core/Dialog"
+import DialogActions from "@material-ui/core/DialogActions"
+import DialogContent from "@material-ui/core/DialogContent"
+import DialogContentText from "@material-ui/core/DialogContentText"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { addToCart } from "actions/cart"
-import styles from "components/Stocks/Plasmids/plasmidStyles"
+
+const useStyles = makeStyles(theme => ({
+  cartButton: {
+    color: "#004080",
+  },
+  dialogTitle: {
+    backgroundColor: "#0059b3",
+    color: "#fff",
+    margin: 0,
+    padding: "16px",
+  },
+  closeButton: {
+    position: "absolute",
+    right: "8px",
+    top: "8px",
+    color: "#fff",
+  },
+  link: {
+    color: "#004080",
+    textDecoration: "none",
+  },
+  cartDialogButton: {
+    backgroundColor: "#004080",
+    color: "#fff",
+  },
+}))
 
 type Props = {
   /** Action for adding an item to the shopping cart */
   addToCart: Function,
-  /** Material-UI styling */
-  classes: Object,
+  /** React Router object */
+  history: Object,
   /** Strain ID number */
   id: string,
   /** Strain label (name) */
@@ -27,13 +58,14 @@ type Props = {
  */
 
 export const AddToCartButton = ({
-  classes,
   id,
   label,
   addToCart,
   rowHeight,
+  history,
 }: Props) => {
-  const [snackbarOpen, setSnackbarOpen] = useState(false)
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const classes = useStyles()
 
   const handleClick = (id: string, label: string) => {
     addToCart({
@@ -41,11 +73,15 @@ export const AddToCartButton = ({
       id: id,
       name: label,
     })
-    setSnackbarOpen(true)
+    setDialogOpen(true)
   }
 
   const handleClose = () => {
-    setSnackbarOpen(false)
+    setDialogOpen(false)
+  }
+
+  const handleViewCart = () => {
+    history.push("/cart")
   }
 
   return (
@@ -62,20 +98,44 @@ export const AddToCartButton = ({
           <FontAwesomeIcon icon="cart-plus" />
         </IconButton>
       </strong>
-      <Snackbar
-        autoHideDuration={2500}
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-        open={snackbarOpen}
+      <Dialog
         onClose={handleClose}
-        ContentProps={{
-          "aria-describedby": "cart-id",
-        }}
-        message={
-          <span id="cart-id">
-            <FontAwesomeIcon icon="check-circle" /> &nbsp; Item added to cart
-          </span>
-        }
-      />
+        fullWidth
+        aria-labelledby="cart-dialog-title"
+        open={dialogOpen}>
+        <DialogTitle className={classes.dialogTitle} id="cart-dialog-title">
+          Added to Cart
+          <IconButton
+            aria-label="close-dialog"
+            className={classes.closeButton}
+            onClick={handleClose}>
+            <FontAwesomeIcon icon="times" />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <strong>
+              <Link className={classes.link} to={`/strains/${id}`}>
+                {label}
+              </Link>
+            </strong>
+            <br />
+            {id}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} variant="outlined" color="default">
+            Continue Shopping
+          </Button>
+          <Button
+            className={classes.cartDialogButton}
+            onClick={handleViewCart}
+            variant="contained"
+            color="primary">
+            View Cart
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   )
 }
@@ -83,4 +143,4 @@ export const AddToCartButton = ({
 export default connect<*, *, *, *, *, *>(
   null,
   { addToCart },
-)(withStyles(styles)(AddToCartButton))
+)(withRouter(AddToCartButton))
--- src/components/Stocks/CatalogTableItems/AddToCartButton.test.js
+++ src/components/Stocks/CatalogTableItems/AddToCartButton.test.js
@@ -3,7 +3,6 @@ import { shallow } from "enzyme"
 import sinon from "sinon"
 import { AddToCartButton } from "./AddToCartButton"
 import IconButton from "@material-ui/core/IconButton"
-import Snackbar from "@material-ui/core/Snackbar"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 describe("Stocks/CatalogTableItems/AddToCartButton", () => {
@@ -24,20 +23,13 @@ describe("Stocks/CatalogTableItems/AddToCartButton", () => {
       expect(wrapper).toHaveLength(1)
     })
     it("renders expected initial components", () => {
-      expect(wrapper.find(IconButton)).toHaveLength(1)
-      expect(wrapper.find(Snackbar)).toHaveLength(1)
-      expect(wrapper.find(FontAwesomeIcon)).toHaveLength(1)
-    })
-  })
-  describe("Snackbar", () => {
-    it("should not be visible by default", () => {
-      const snackbar = wrapper.find(Snackbar)
-      expect(snackbar.props().open).toBe(false)
+      expect(wrapper.find(IconButton)).toHaveLength(2)
+      expect(wrapper.find(FontAwesomeIcon)).toHaveLength(2)
     })
   })
   describe("clicking add to cart button", () => {
     it("should call addToCart function on click", () => {
-      const btn = wrapper.find(IconButton)
+      const btn = wrapper.find(IconButton).first()
       btn.simulate("click")
       expect(addToCartSpy.calledOnce).toBe(true)
     })
