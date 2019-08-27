8fe44b554c404973fb402f9c83691677d188c2b1
wildlifehexagon <crossthebreeze@gmail.com>
2019-08-27 13:17:53 -0500

    refactor: use new icon for catalog left menu, add dropdown items #220

--- src/components/Stocks/Strains/Catalog/StrainCatalogAppBarLeftMenu.js
+++ src/components/Stocks/Strains/Catalog/StrainCatalogAppBarLeftMenu.js
@@ -1,13 +1,18 @@
 // @flow
-import React from "react"
+import React, { useState } from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import IconButton from "@material-ui/core/IconButton"
+import Menu from "@material-ui/core/Menu"
+import MenuItem from "@material-ui/core/MenuItem"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const useStyles = makeStyles(theme => ({
   icon: {
     color: "#fff",
     paddingRight: "5px",
+  },
+  menuItem: {
+    fontSize: "0.8rem",
   },
 }))
 
@@ -17,13 +22,40 @@ const useStyles = makeStyles(theme => ({
  */
 
 const StrainCatalogAppBarLeftMenu = () => {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
   const classes = useStyles()
+
+  const handleIconClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleItemClick = () => {
+    setAnchorEl(null)
+  }
 
   return (
     <>
-      <IconButton size="small" className={classes.icon}>
-        <FontAwesomeIcon icon="bars" />
+      <IconButton
+        size="small"
+        className={classes.icon}
+        onClick={handleIconClick}>
+        <FontAwesomeIcon icon="cog" />
       </IconButton>
+      <Menu id="menu" anchorEl={anchorEl} open={open} onClose={handleItemClick}>
+        <MenuItem onClick={handleItemClick} className={classes.menuItem}>
+          All Strains &nbsp; <FontAwesomeIcon icon="check-circle" size="sm" />
+        </MenuItem>
+        <MenuItem onClick={handleItemClick} className={classes.menuItem}>
+          GWDI Strains
+        </MenuItem>
+        <MenuItem onClick={handleItemClick} className={classes.menuItem}>
+          Available Strains
+        </MenuItem>
+        <MenuItem onClick={handleItemClick} className={classes.menuItem}>
+          Unavailable Strains
+        </MenuItem>
+      </Menu>
     </>
   )
 }
