337a5b9ce2079dd91701a53e34a797e1f4158e91
wildlifehexagon <crossthebreeze@gmail.com>
2019-09-04 11:18:01 -0500

    refactor: use white search box for catalog page

--- src/components/Stocks/Strains/Catalog/StrainCatalogAppBarSearch.js
+++ src/components/Stocks/Strains/Catalog/StrainCatalogAppBarSearch.js
@@ -1,7 +1,8 @@
 // @flow
 import React from "react"
 import gql from "graphql-tag"
-import { makeStyles, fade } from "@material-ui/core/styles"
+import { makeStyles } from "@material-ui/core/styles"
+import Paper from "@material-ui/core/Paper"
 import InputBase from "@material-ui/core/InputBase"
 import IconButton from "@material-ui/core/IconButton"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -22,46 +23,18 @@ export const GET_STRAINS_FILTER = gql`
 `
 
 const useStyles = makeStyles(theme => ({
-  icon: {
-    color: "#fff",
-    paddingRight: "5px",
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto",
-    },
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
+  root: {
+    padding: "2px 4px",
     display: "flex",
     alignItems: "center",
-    justifyContent: "center",
+    width: 400,
   },
-  inputRoot: {
-    color: "inherit",
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
   },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: 120,
-      "&:focus": {
-        width: 450,
-      },
-    },
+  iconButton: {
+    padding: 10,
   },
 }))
 
@@ -91,23 +64,18 @@ const StrainCatalogAppBarSearch = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className={classes.search}>
-        <div className={classes.searchIcon}>
-          <IconButton size="small" className={classes.icon}>
-            <FontAwesomeIcon icon="search" />
-          </IconButton>
-        </div>
+      <Paper className={classes.root}>
+        <IconButton className={classes.iconButton} aria-label="menu">
+          <FontAwesomeIcon icon="search" />
+        </IconButton>
         <InputBase
-          placeholder="Search…"
-          classes={{
-            root: classes.inputRoot,
-            input: classes.inputInput,
-          }}
+          className={classes.input}
+          placeholder="Search..."
           inputProps={{ "aria-label": "search" }}
           onChange={handleChange}
           value={searchValue}
         />
-      </div>
+      </Paper>
     </form>
   )
 }
