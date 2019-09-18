4f44b97d5725729fbc09fd6fdc5b539c2258fce3
wildlifehexagon <crossthebreeze@gmail.com>
2019-09-18 10:17:53 -0500

    feat: add help dialog content for catalog page, fixes #247

--- src/components/Stocks/CatalogPageItems/HelpDialog.js
+++ src/components/Stocks/CatalogPageItems/HelpDialog.js
@@ -62,11 +62,32 @@ export const HelpDialog = ({ helpDialogOpen, setHelpDialogOpen }: Props) => {
         </IconButton>
       </DialogTitle>
       <DialogContent>
-        <DialogContentText>help content goes here...</DialogContentText>
+        <DialogContentText>
+          The catalog page data is provided as as infinite scroll. As you move
+          down the page, more data is continuously loaded, creating a seamless
+          experience.
+        </DialogContentText>
+        <DialogContentText>
+          The catalog content can be filtered by either selecting an option from
+          the left dropdown menu (coming soon), or by performing a search. The
+          search box provides a dropdown of available fields. Once you select a
+          field, type in your query and hit enter to receive the filtered
+          results.
+        </DialogContentText>
+        <DialogContentText>
+          The ellipsis icon on the right side of the app bar provides options to
+          download the shown data in either PDF or CSV format (coming soon).
+        </DialogContentText>
+        <DialogContentText>
+          Each row of data contains checkboxes, which if toggled will produce
+          new icons to add the items to cart or download a PDF containing those
+          selections. Hovering over an item in the list will also show an "add
+          to cart" button if the item is available in the DSC.
+        </DialogContentText>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose} variant="outlined" color="default">
-          Back to Catalog
+          Close
         </Button>
       </DialogActions>
     </Dialog>
