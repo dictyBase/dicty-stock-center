cc42259821cbd3bb3436c94ce546c5c3288d3b31
wildlifehexagon <crossthebreeze@gmail.com>
2019-10-18 17:11:40 -0500

    test: separate nightwatch functions

--- nightwatch/tests.js
+++ nightwatch/tests.js
@@ -19,48 +19,30 @@ const viewports = [
 
 const date = new Date().toJSON().slice(0, 10)
 
+const test = (browser, url, page) => {
+  return browser.url(url).waitForElementVisible("body", 1000)
+  viewports.forEach(item => {
+    browser
+      .resizeWindow(item.width, 2000)
+      .saveScreenshot(
+        `./nightwatch/reports/dsc-${page}-${item.name}-chrome-${date}.png`,
+      )
+  })
+  browser.end()
+}
+
 module.exports = {
   "inspect homepage": browser => {
-    browser
-      .url("https://eric.dictybase.dev/stockcenter")
-      .waitForElementVisible("body", 1000)
-      .assert.title("Dicty Stock Center")
-      .getText("h1", function(res) {
-        this.assert.equal(res.value, "Welcome to Dicty Stock Center (DSC)")
-      })
-    viewports.forEach(item => {
-      browser
-        .resizeWindow(item.width, 2000)
-        .saveScreenshot(
-          `./nightwatch/reports/dsc-homepage-${item.name}-chrome-${date}.png`,
-        )
-    })
-    browser.end()
+    test(browser, "https://eric.dictybase.dev/stockcenter", "homepage")
   },
   "inspect information page": browser => {
-    browser
-      .url("https://eric.dictybase.dev/stockcenter/information/order")
-      .waitForElementVisible("body", 1000)
-    viewports.forEach(item => {
-      browser
-        .resizeWindow(item.width, 2000)
-        .saveScreenshot(
-          `./nightwatch/reports/dsc-infopage-${item.name}-chrome-${date}.png`,
-        )
-    })
-    browser.end()
+    test(
+      browser,
+      "https://eric.dictybase.dev/stockcenter/information/order",
+      "infopage",
+    )
   },
   "inspect login page": browser => {
-    browser
-      .url("https://eric.dictybase.dev/stockcenter/login")
-      .waitForElementVisible("body", 1000)
-    viewports.forEach(item => {
-      browser
-        .resizeWindow(item.width, 2000)
-        .saveScreenshot(
-          `./nightwatch/reports/dsc-login-${item.name}-chrome-${date}.png`,
-        )
-    })
-    browser.end()
+    test(browser, "https://eric.dictybase.dev/stockcenter/login", "login")
   },
 }
