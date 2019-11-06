c5b2cf0bca5a39acad8972e98e3ff99316721a6b
wildlifehexagon <crossthebreeze@gmail.com>
2019-11-06 15:02:34 -0600

    refactor: move error styles into own file

--- src/components/Stocks/Catalogs/common/OtherError.js
+++ src/components/Stocks/Catalogs/common/OtherError.js
@@ -1,27 +1,9 @@
 // @flow
 import React from "react"
-import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 import Paper from "@material-ui/core/Paper"
 import ErrorMessage from "components/Errors/ErrorMessage"
-
-const useStyles = makeStyles({
-  mainGrid: {
-    marginBottom: "20px",
-  },
-  body: {
-    paddingTop: "50px",
-    textAlign: "center",
-  },
-  link: {
-    color: "#428bca",
-    textDecoration: "none",
-  },
-  catalogPaper: {
-    height: 300,
-    width: "100%",
-  },
-})
+import useStyles from "./errorStyles"
 
 /**
  * OtherError is the UI display when an item was not found.
---
+++ src/components/Stocks/Catalogs/common/errorStyles.js
@@ -0,0 +1,21 @@
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles({
+  mainGrid: {
+    marginBottom: "20px",
+  },
+  body: {
+    paddingTop: "50px",
+    textAlign: "center",
+  },
+  link: {
+    color: "#428bca",
+    textDecoration: "none",
+  },
+  catalogPaper: {
+    height: 300,
+    width: "100%",
+  },
+})
+
+export default useStyles
