2cd63fbebb39365f855fd245208053c631547ce3
wildlifehexagon <crossthebreeze@gmail.com>
2019-11-13 15:59:48 -0600

    test: add more percy snapshot tests

--- cypress/integration/snapshots_spec.js
+++ cypress/integration/snapshots_spec.js
@@ -2,12 +2,38 @@ describe("DSC snapshots", () => {
   it("visits the DSC homepage", () => {
     cy.visit("/")
     cy.contains("Strain & Plasmid Availability")
-    cy.percySnapshot("DSC homepage", { widths: [300, 600, 1000] })
+    cy.percySnapshot("DSC homepage", { widths: [360, 768, 1024] })
   })
-  // it("visits the DSC order information page", function() {
-  //   cy.visit("https://eric.dictybase.dev/stockcenter/information/order")
-  // })
-  // it("visits the DSC login page", function() {
-  //   cy.visit("https://eric.dictybase.dev/stockcenter/login")
-  // })
+  it("visits the order information page", () => {
+    cy.visit("/information/order")
+    cy.percySnapshot("order information page", { widths: [360, 768, 1024] })
+  })
+  it("visits the strain catalog", () => {
+    cy.visit("/strains")
+    cy.contains("DBS0351367")
+    cy.percySnapshot("strain catalog", {
+      widths: [360, 768, 1024],
+    })
+  })
+  it("visits a strain details page", () => {
+    cy.visit("/strains/DBS0351367")
+    cy.contains("Extrachromosomal")
+    cy.percySnapshot("strain details", {
+      widths: [360, 768, 1024],
+    })
+  })
+  it("visits the plasmid catalog", () => {
+    cy.visit("/plasmids")
+    cy.contains("DBP0001070")
+    cy.percySnapshot("plasmid catalog", {
+      widths: [360, 768, 1024],
+    })
+  })
+  it("visits a plasmid details page", () => {
+    cy.visit("/plasmids/DBP0001070")
+    cy.contains("overexpression")
+    cy.percySnapshot("plasmid details", {
+      widths: [360, 768, 1024],
+    })
+  })
 })
