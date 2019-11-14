97b5d886e07a9156b3792d41c4c8b632d2d7522a
wildlifehexagon <crossthebreeze@gmail.com>
2019-11-14 09:55:49 -0600

    test: add more percy snapshot sizes

--- cypress/integration/snapshots_spec.js
+++ cypress/integration/snapshots_spec.js
@@ -2,38 +2,40 @@ describe("DSC snapshots", () => {
   it("visits the DSC homepage", () => {
     cy.visit("/")
     cy.contains("Strain & Plasmid Availability")
-    cy.percySnapshot("DSC homepage", { widths: [360, 768, 1024] })
+    cy.percySnapshot("DSC homepage", { widths: [360, 768, 1000, 1280] })
   })
   it("visits the order information page", () => {
     cy.visit("/information/order")
-    cy.percySnapshot("order information page", { widths: [360, 768, 1024] })
+    cy.percySnapshot("order information page", {
+      widths: [360, 768, 1000, 1280],
+    })
   })
   it("visits the strain catalog", () => {
     cy.visit("/strains")
     cy.contains("HL206")
     cy.percySnapshot("strain catalog", {
-      widths: [360, 768, 1024],
+      widths: [360, 768, 1000, 1280],
     })
   })
   it("visits a strain details page", () => {
     cy.visit("/strains/DBS0351367")
     cy.contains("Extrachromosomal")
     cy.percySnapshot("strain details", {
-      widths: [360, 768, 1024],
+      widths: [360, 768, 1000, 1280],
     })
   })
   it("visits the plasmid catalog", () => {
     cy.visit("/plasmids")
     cy.contains("pSigK")
     cy.percySnapshot("plasmid catalog", {
-      widths: [360, 768, 1024],
+      widths: [360, 768, 1000, 1280],
     })
   })
   it("visits a plasmid details page", () => {
     cy.visit("/plasmids/DBP0001070")
     cy.contains("overexpression")
     cy.percySnapshot("plasmid details", {
-      widths: [360, 768, 1024],
+      widths: [360, 768, 1000, 1280],
     })
   })
 })
