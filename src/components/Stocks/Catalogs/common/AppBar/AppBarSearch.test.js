ca15f37b3c96dbe7ee51a1ca58c62b396a446ca7
wildlifehexagon <crossthebreeze@gmail.com>
2019-11-19 16:13:54 -0600

    test: add more tests for AppBarSearch

--- src/components/Stocks/Catalogs/common/AppBar/AppBarSearch.test.js
+++ src/components/Stocks/Catalogs/common/AppBar/AppBarSearch.test.js
@@ -5,14 +5,15 @@ import AppBarDropdown from "./AppBarDropdown"
 import IconButton from "@material-ui/core/IconButton"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { AppBarProvider } from "./AppBarContext"
+import InputBase from "@material-ui/core/InputBase"
+import sinon from "sinon"
 
 describe("Stocks/Strains/Catalog/AppBarSearch", () => {
   describe("initial render", () => {
     const props = {
       dropdownItems: [],
       query: "test query",
-      setQuery: jest.fn(),
-      setQueryVariables: jest.fn(),
+      catalogDispatch: jest.fn(),
     }
     const wrapper = mount(
       <AppBarProvider>
@@ -23,6 +24,48 @@ describe("Stocks/Strains/Catalog/AppBarSearch", () => {
       expect(wrapper.find(IconButton)).toHaveLength(2)
       expect(wrapper.find(FontAwesomeIcon)).toHaveLength(2)
       expect(wrapper.find(AppBarDropdown)).toHaveLength(1)
+    })
+  })
+
+  describe("clicking buttons", () => {
+    const catalogDispatchSpy = sinon.spy()
+    const props = {
+      dropdownItems: [],
+      query: "test query",
+      catalogDispatch: catalogDispatchSpy,
+    }
+    const wrapper = mount(
+      <AppBarProvider>
+        <AppBarSearch {...props} />
+      </AppBarProvider>,
+    )
+    it("should use dispatch when clear button is clicked", () => {
+      catalogDispatchSpy.resetHistory()
+      const clearBtn = wrapper.find(IconButton).at(1)
+      clearBtn.simulate("click")
+      expect(catalogDispatchSpy.calledTwice).toBe(true)
+    })
+  })
+
+  describe("form submit", () => {
+    const catalogDispatchSpy = sinon.spy()
+    const props = {
+      dropdownItems: [],
+      query: "test query",
+      catalogDispatch: catalogDispatchSpy,
+    }
+    const wrapper = mount(
+      <AppBarProvider>
+        <AppBarSearch {...props} />
+      </AppBarProvider>,
+    )
+    it("should use dispatch when form is submitted", () => {
+      catalogDispatchSpy.resetHistory()
+      const form = wrapper.find("form")
+      form.simulate("submit", {
+        preventDefault: jest.fn(),
+      })
+      expect(catalogDispatchSpy.calledTwice).toBe(true)
     })
   })
 })
