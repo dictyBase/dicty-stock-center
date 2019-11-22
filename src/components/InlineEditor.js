a475256a2695bb708118ed6aed835bde8f1898e3
wildlifehexagon <crossthebreeze@gmail.com>
2019-11-22 11:33:19 -0600

    refactor: move inline editor styles into their own file

--- src/components/InlineEditor.js
+++ src/components/InlineEditor.js
@@ -27,56 +27,8 @@ import {
 import Authorization from "components/authentication/Authorization"
 import { editInline, saveInlineEditing, fetchInfoPage } from "actions/page"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import styles from "./inlineEditorStyles"
 import "draft-js-static-toolbar-plugin/lib/plugin.css"
-
-const styles = theme => ({
-  inlineLink: {
-    cursor: "pointer",
-  },
-  toolbar: {
-    backgroundColor: "#fafafa",
-    borderRadius: "2px",
-    border: "1px solid #ddd",
-    padding: "5px",
-    width: "100%",
-    display: "inline-block",
-  },
-  editorGrid: {
-    marginTop: "4px",
-
-    "& a": {
-      color: "#004080",
-      textDecoration: "none",
-    },
-  },
-  editButton: {
-    fontSize: "0.9em",
-    color: "#337ab7",
-    "&:hover": {
-      color: "#337ab7",
-      backgroundColor: "transparent",
-    },
-  },
-  saveButton: {
-    width: "100%",
-    backgroundColor: "#15317e",
-  },
-  cancelButton: {
-    width: "100%",
-  },
-  cancelButtonGrid: {
-    marginRight: "4px",
-    marginTop: "4px",
-  },
-  saveButtonGrid: {
-    marginTop: "4px",
-  },
-  container: {
-    "[contenteditable='true']:focus": {
-      outline: "none",
-    },
-  },
-})
 
 type Props = {
   /** the object that contains page data from current state */
