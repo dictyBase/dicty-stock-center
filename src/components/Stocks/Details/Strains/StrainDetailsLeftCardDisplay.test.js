4403f74d27dfeac636d1a33bce5e52fe881b7637
wildlifehexagon <crossthebreeze@gmail.com>
2019-12-04 09:45:15 -0600

    test: add new unit tests for strain details components

---
+++ src/components/Stocks/Details/Strains/StrainDetailsLeftCardDisplay.test.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { shallow } from "enzyme"
+import StrainDetailsLeftCardDisplay from "./StrainDetailsLeftCardDisplay"
+import Grid from "@material-ui/core/Grid"
+import Card from "@material-ui/core/Card"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import StrainDetailsLeftCardHeader from "components/Stocks/Details/Strains/StrainDetailsLeftCardHeader"
+import DetailsListItem from "components/Stocks/Details/common/DetailsListItem"
+import PhenotypeList from "./Phenotypes/PhenotypeList"
+import TabPanel from "components/common/TabPanel"
+
+describe("Stock/Details/Strains/StrainDetailsLeftCardDisplay", () => {
+  const props = {
+    species: "Dictyostelium discoideum",
+    rows: [
+      {
+        id: 0,
+        title: "Descriptor",
+        content: "Test descriptor",
+      },
+    ],
+  }
+  const wrapper = shallow(<StrainDetailsLeftCardDisplay {...props} />)
+  describe("initial render", () => {
+    it("always renders initial components", () => {
+      expect(wrapper.find(Grid).exists()).toBe(true)
+      expect(wrapper.find(Card)).toHaveLength(1)
+      expect(wrapper.find(List)).toHaveLength(1)
+      expect(wrapper.find(ListItem)).toHaveLength(1)
+      expect(wrapper.find(StrainDetailsLeftCardHeader)).toHaveLength(1)
+      expect(wrapper.find(TabPanel)).toHaveLength(2)
+      expect(wrapper.find(PhenotypeList)).toHaveLength(1)
+      expect(wrapper.find(DetailsListItem)).toHaveLength(1)
+    })
+  })
+})
