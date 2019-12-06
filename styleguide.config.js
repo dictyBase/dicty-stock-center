5be19dbe2f50d35369b200e4e3ef88df8f16f954
wildlifehexagon <crossthebreeze@gmail.com>
2019-12-06 14:33:48 -0600

    docs: customize styleguide, add styles, split up sections #370

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,8 @@
 const path = require("path")
 module.exports = {
   styleguideDir: "docs",
+  pagePerSection: true,
+  title: "Dicty Stock Center",
   webpackConfig: require("react-scripts/config/webpack.config")("development"),
   ignore: [
     "**/*.test.{js,jsx,ts,tsx}",
@@ -15,15 +17,68 @@ module.exports = {
   ],
   sections: [
     {
+      sectionDepth: 1,
       name: "Catalog Pages",
-      components: "src/components/Stocks/Catalogs/**/*.js",
+      sections: [
+        {
+          name: "Common",
+          components: "src/components/Stocks/Catalogs/common/**/*.js",
+        },
+        {
+          name: "Strains",
+          components: "src/components/Stocks/Catalogs/Strains/*.js",
+        },
+        {
+          name: "Plasmids",
+          components: "src/components/Stocks/Catalogs/Plasmids/*.js",
+        },
+      ],
     },
     {
+      sectionDepth: 1,
       name: "Details Pages",
-      components: "src/components/Stocks/Details/**/*.js",
+      sections: [
+        {
+          name: "Common",
+          components: "src/components/Stocks/Details/common/**/*.js",
+        },
+        {
+          name: "Strains",
+          components: "src/components/Stocks/Details/Strains/*.js",
+        },
+        {
+          name: "Plasmids",
+          components: "src/components/Stocks/Details/Plasmids/*.js",
+        },
+      ],
     },
   ],
   styleguideComponents: {
     Wrapper: path.join(__dirname, "src/styleguide/Wrapper"),
+  },
+  template: {
+    favicon: "./public/favicon.ico",
+    head: {
+      links: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Roboto",
+        },
+      ],
+    },
+  },
+  theme: {
+    maxWidth: "100%",
+    color: {
+      codeBackground: "#F5F5F5;",
+      ribbonBackground: "#004080",
+    },
+    fontFamily: {
+      base: "Roboto, sans-serif",
+    },
+  },
+  ribbon: {
+    url: "http://www.github.com/dictyBase/Dicty-Stock-Center",
+    text: "View on GitHub",
   },
 }
