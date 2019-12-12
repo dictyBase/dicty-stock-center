5699f4bc8a51816f5467194304d3676c17aa78c2
wildlifehexagon <crossthebreeze@gmail.com>
2019-12-12 13:29:07 -0600

    test: add more tests for useCheckboxes

--- src/hooks/useCheckboxes.test.js
+++ src/hooks/useCheckboxes.test.js
@@ -34,7 +34,6 @@ describe("hooks/useCheckboxes", () => {
       const [state, dispatch] = React.useReducer(catalogReducer, {
         checkedItems: [cartData],
       })
-
       return (
         <CatalogContext.Provider value={[state, dispatch]}>
           {children}
@@ -45,9 +44,30 @@ describe("hooks/useCheckboxes", () => {
     const {
       result: { current },
     } = renderHook(() => useCheckboxes(cartData), { wrapper: MockProvider })
-    expect(current.itemIsChecked).toBe(true)
 
+    expect(current.itemIsChecked).toBe(true)
     act(() => current.handleCheckboxChange())
     expect(current.itemIsChecked).toBe(true)
+  })
+
+  it("resets all checked items correctly", () => {
+    const MockProvider = ({ children }) => {
+      const [state, dispatch] = React.useReducer(catalogReducer, {
+        checkedItems: [cartData],
+      })
+      return (
+        <CatalogContext.Provider value={[state, dispatch]}>
+          {children}
+        </CatalogContext.Provider>
+      )
+    }
+
+    const { result } = renderHook(() => useCheckboxes(cartData), {
+      wrapper: MockProvider,
+    })
+
+    expect(result.current.itemIsChecked).toBe(true)
+    act(() => result.current.handleCheckAllChange())
+    expect(result.current.itemIsChecked).toBe(false)
   })
 })
