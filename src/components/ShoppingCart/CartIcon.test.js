d8ebd4fc0324f6855fc22b09d8019a233be05e90
wildlifehexagon <crossthebreeze@gmail.com>
2019-12-17 14:37:46 -0600

    test: add tests for CartIcon

---
+++ src/components/ShoppingCart/CartIcon.test.js
@@ -0,0 +1,53 @@
+import React from "react"
+import { mount } from "enzyme"
+import { BrowserRouter } from "react-router-dom"
+import CartIcon from "./CartIcon"
+import Grid from "@material-ui/core/Grid"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link } from "react-router-dom"
+import { CartContext, cartReducer } from "./CartStore"
+
+describe("ShoppingCart/CartIcon", () => {
+  const MockComponent = ({ items }) => {
+    const [state, dispatch] = React.useReducer(cartReducer, {
+      addedItems: items,
+    })
+    return (
+      <CartContext.Provider value={[state, dispatch]}>
+        <BrowserRouter>
+          <CartIcon />
+        </BrowserRouter>
+      </CartContext.Provider>
+    )
+  }
+  describe("initial render with items", () => {
+    const cartItems = [
+      {
+        id: "DBS123",
+        name: "test1",
+        fee: "$30.00",
+      },
+      {
+        id: "DBP456",
+        name: "test2",
+        fee: "$15.00",
+      },
+    ]
+    const wrapper = mount(<MockComponent items={cartItems} />)
+    it("correctly indicates items in cart", () => {
+      expect(wrapper.find(Grid).exists()).toBe(true)
+      expect(wrapper.find(Link)).toHaveLength(1)
+      expect(wrapper.find(FontAwesomeIcon)).toHaveLength(1)
+      expect(wrapper.text()).toContain("(2)")
+    })
+  })
+  describe("initial render with no items in cart", () => {
+    const wrapper = mount(<MockComponent items={[]} />)
+    it("correctly indicates no items in cart", () => {
+      expect(wrapper.find(Grid).exists()).toBe(true)
+      expect(wrapper.find(Link)).toHaveLength(1)
+      expect(wrapper.find(FontAwesomeIcon)).toHaveLength(1)
+      expect(wrapper.text()).toContain("(0)")
+    })
+  })
+})
