c36172904da80057eb48f340beb8b90ce56e0901
wildlifehexagon <crossthebreeze@gmail.com>
2019-12-17 14:27:40 -0600

    test: add tests for cart reducer

---
+++ src/components/ShoppingCart/CartStore.test.js
@@ -0,0 +1,63 @@
+import { cartReducer } from "./CartStore"
+import { cartTypes } from "constants/cart"
+
+describe("cartReducer", () => {
+  const newItem = {
+    id: "DBS1234567",
+    name: "Test Strain",
+    summary: "this is just a test summary",
+  }
+  it("should add a new item to cart", () => {
+    const state = {
+      addedItems: [],
+      showCartDialog: false,
+    }
+    expect(
+      cartReducer(state, {
+        type: cartTypes.ADD_TO_CART,
+        payload: {
+          item: newItem,
+          fee: "30.00",
+        },
+      }),
+    ).toStrictEqual({
+      addedItems: [
+        {
+          ...newItem,
+          fee: "30.00",
+        },
+      ],
+      showCartDialog: true,
+    })
+  })
+  it("should remove an item from cart", () => {
+    const state = {
+      addedItems: [newItem],
+      showCartDialog: false,
+    }
+    expect(
+      cartReducer(state, {
+        type: cartTypes.REMOVE_FROM_CART,
+        payload: {
+          id: newItem.id,
+        },
+      }),
+    ).toStrictEqual({
+      addedItems: [],
+    })
+  })
+  it("should hide cart dialog", () => {
+    const state = {
+      addedItems: [],
+      showCartDialog: true,
+    }
+    expect(
+      cartReducer(state, {
+        type: cartTypes.HIDE_CART_DIALOG,
+      }),
+    ).toStrictEqual({
+      addedItems: [],
+      showCartDialog: false,
+    })
+  })
+})
