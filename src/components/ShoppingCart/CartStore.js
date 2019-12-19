07591cff6d4bd59c4aa8fc04116e93add35c297a
wildlifehexagon <crossthebreeze@gmail.com>
2019-12-19 12:27:13 -0600

    refactor: put maxItemsInCart in localstorage

--- src/components/ShoppingCart/CartStore.js
+++ src/components/ShoppingCart/CartStore.js
@@ -3,13 +3,14 @@ import React, { createContext, useContext, useReducer } from "react"
 import { cartTypes } from "constants/cart"
 
 const storageKey = "dscCart"
+const maxKey = "dscMaxItems"
 
 const CartContext: Object = createContext()
 
 const initialState = {
   addedItems: JSON.parse(localStorage.getItem(storageKey) || "[]"),
   showCartDialog: false,
-  maxItemsInCart: false,
+  maxItemsInCart: JSON.parse(localStorage.getItem(maxKey) || "false"),
 }
 
 type CartItem = {
@@ -32,32 +33,41 @@ const cartReducer = (
 ) => {
   switch (action.type) {
     case cartTypes.ADD_TO_CART:
-      const newItems = state.addedItems.concat({
-        id: action.payload.id,
-        name: action.payload.name,
-        summary: action.payload.summary,
-        fee: action.payload.fee,
-      })
-      if (newItems.length > 12) {
+      const newItems = state.addedItems
+        .concat({
+          id: action.payload.id,
+          name: action.payload.name,
+          summary: action.payload.summary,
+          fee: action.payload.fee,
+        })
+        .slice(0, 12)
+      if (newItems.length === 12) {
+        localStorage.setItem(storageKey, JSON.stringify(newItems))
+        localStorage.setItem(maxKey, JSON.stringify(true))
         return {
-          ...state,
+          addedItems: newItems,
+          showCartDialog: true,
           maxItemsInCart: true,
         }
       }
       localStorage.setItem(storageKey, JSON.stringify(newItems))
+      localStorage.setItem(maxKey, JSON.stringify(false))
       return {
         addedItems: newItems,
         showCartDialog: true,
         maxItemsInCart: false,
       }
+
     case cartTypes.REMOVE_FROM_CART:
       const updatedItems = [
         ...state.addedItems.slice(0, action.payload.removeIndex),
         ...state.addedItems.slice(action.payload.removeIndex + 1),
       ]
       localStorage.setItem(storageKey, JSON.stringify(updatedItems))
+      localStorage.setItem(maxKey, JSON.stringify(false))
       return {
         addedItems: updatedItems,
+        maxItemsInCart: false,
       }
     case cartTypes.HIDE_CART_DIALOG:
       return {
