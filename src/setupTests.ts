447382ca897cfa78ca1f650c1728cd3e288a7445
wildlifehexagon <crossthebreeze@gmail.com>
2020-02-19 17:19:38 -0600

    fix: add corejs as dev dep to fix jest issues with array flat method

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,6 +2,7 @@ import { configure } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
 import "jest-enzyme"
 import { sprintf } from "sprintf-js"
+import "core-js"
 
 let globalAny = global as any
 
