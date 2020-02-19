4dbc2b71741f703d15ad9c234fd9684add90dad1
wildlifehexagon <crossthebreeze@gmail.com>
2020-02-19 15:22:57 -0600

    test: add unit tests for authReducer

---
+++ src/components/authentication/AuthStore.test.tsx
@@ -0,0 +1,101 @@
+import React from "react"
+import { renderHook } from "react-hooks-testing-library"
+import { authReducer, useAuthStore, ActionType } from "./AuthStore"
+
+const initialState = {
+  isAuthenticated: false,
+  token: "",
+  user: {},
+  provider: "",
+}
+
+const mockToken = "tiubakjdgnjka"
+const mockProvider = "google"
+const mockUser = {
+  id: "999",
+  email: "forrest@macneil.org",
+  first_name: "forrest",
+  last_name: "macneil",
+  roles: [],
+}
+
+describe("authReducer", () => {
+  it("should update state on login", () => {
+    expect(
+      authReducer(initialState, {
+        type: ActionType.LOGIN,
+        payload: {
+          token: mockToken,
+          provider: mockProvider,
+          user: mockUser,
+        },
+      }),
+    ).toStrictEqual({
+      isAuthenticated: true,
+      token: mockToken,
+      provider: mockProvider,
+      user: mockUser,
+    })
+  })
+  it("should return initial state on logout", () => {
+    const state = {
+      isAuthenticated: true,
+      token: "jwtxyz",
+      user: {
+        id: 999,
+        email: "forrest@macneil.org",
+      },
+      provider: "google",
+    }
+    expect(
+      authReducer(state, {
+        type: ActionType.LOGOUT,
+      }),
+    ).toStrictEqual(initialState)
+  })
+  it("should return full updated state when token updated", () => {
+    const state = {
+      isAuthenticated: true,
+      token: mockToken,
+      provider: mockProvider,
+      user: mockUser,
+    }
+    const newToken = "wthjiowvnfskjkdfsbnkjadb"
+    expect(
+      authReducer(state, {
+        type: ActionType.UPDATE_TOKEN,
+        payload: {
+          token: newToken,
+          provider: mockProvider,
+          user: mockUser,
+        },
+      }),
+    ).toStrictEqual({
+      isAuthenticated: true,
+      token: newToken,
+      provider: mockProvider,
+      user: mockUser,
+    })
+  })
+  it("should return state if not defined action type", () => {
+    const state = {
+      isAuthenticated: true,
+      token: mockToken,
+      provider: mockProvider,
+      user: mockUser,
+    }
+    expect(
+      authReducer(state, {
+        type: "not a real type",
+      }),
+    ).toStrictEqual(state)
+  })
+})
+
+// describe("useAuthStore", () => {
+//   it("should throw error if not used in Provider", () => {
+//     const wrapper = ({ children }) => <div>{children}</div>
+//     const { result } = renderHook(() => useAuthStore(), { wrapper })
+//     expect(() => result.current).toThrow()
+//   })
+// })
