3cbff2f58f3b10219be7332688ac4f869cc0af32
wildlifehexagon <crossthebreeze@gmail.com>
2020-02-19 17:19:57 -0600

    test: add unit tests for new auth hooks

--- src/utils/testing.tsx
+++ src/utils/testing.tsx
@@ -6,17 +6,42 @@ import { AuthContext, authReducer } from "components/authentication/AuthStore"
 type Props = {
   children: any
   mocks: ReadonlyArray<MockedResponse>
+  user?: {
+    id: number
+    first_name: string
+    last_name: string
+    email: string
+    roles: Array<{
+      id: number
+      role: string
+      permissions?: Array<{
+        id: number
+        permission: string
+        resource: string
+      }>
+    }>
+  }
 }
 
-const MockAuthProvider = ({ children, mocks }: Props) => {
+const MockSuperuser = {
+  id: 999,
+  first_name: "Art",
+  last_name: "Vandelay",
+  email: "george@vandelayindustries.com",
+  roles: [
+    {
+      id: 1,
+      role: "superuser",
+      permissions: [{ id: 1, permission: "test", resource: "testresource" }],
+    },
+  ],
+}
+
+const MockAuthProvider = ({ children, mocks, user = MockSuperuser }: Props) => {
   const [state, dispatch] = React.useReducer(authReducer, {
     token:
       "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.POstGetfAytaZS82wHcjoTyoqhMyxXiWdR7Nn7A29DNSl0EiXLdwJ6xC6AfgZWF1bOsS_TuYI3OG85AmiExREkrS6tDfTQ2B3WXlrr-wp5AokiRbz3_oB4OxG-W9KcEEbDRcZc0nH3L7LzYptiy1PtAylQGxHTWZXtGz4ht0bAecBgmpdgXMguEIcoqPJ1n3pIWk_dUZegpqx0Lka21H6XxUTxiy8OcaarA8zdnPUnV6AmNP3ecFawIFYdvJB_cm-GvpCSbr8G8y_Mllj8f4x9nBH8pQux89_6gUY618iYv7tuPWBFfEbLxtF2pZS6YC1aSfLQxeNe8djT9YjpvRZA",
-    user: {
-      first_name: "Art",
-      last_name: "Vandelay",
-      email: "george@vandelayindustries.com",
-    },
+    user: user,
     provider: "google",
     isAuthenticated: true,
   })
@@ -29,4 +54,4 @@ const MockAuthProvider = ({ children, mocks }: Props) => {
   )
 }
 
-export { MockAuthProvider }
+export { MockSuperuser, MockAuthProvider }
