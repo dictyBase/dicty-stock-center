01d6e4a4d91b2d329d6568cdd1260502e4ce09be
wildlifehexagon <crossthebreeze@gmail.com>
2020-02-20 15:04:49 -0600

    chore: remove unused envs

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,11 +4,8 @@ import "typescript"
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      REACT_APP_DEBUG: boolean
       NODE_ENV: "development" | "production"
       REACT_APP_BASENAME: string
-      REACT_APP_AUTH_SERVER: string
-      REACT_APP_API_SERVER: string
       REACT_APP_GA_TRACKING_ID: string
       REACT_APP_NAVBAR_JSON: string
       REACT_APP_FOOTER_JSON: string
