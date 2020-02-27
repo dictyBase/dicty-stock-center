0e56691f954b1f96aa3a11b1692af56938481fc4
wildlifehexagon <crossthebreeze@gmail.com>
2020-02-27 10:43:05 -0600

    chore: remove no longer needed sprintf-js

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,21 +1,6 @@
 import { configure } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
 import "jest-enzyme"
-import { sprintf } from "sprintf-js"
 import "core-js"
-
-let globalAny = global as any
-
-globalAny.console = {
-  log: (msg: string, args: any) => {
-    if (typeof msg !== "string") {
-      return
-    }
-    const str = sprintf(msg, args)
-    process.stderr.write(str + "\n")
-  },
-  error: jest.fn(),
-  warn: jest.fn(),
-}
 
 configure({ adapter: new Adapter() })
