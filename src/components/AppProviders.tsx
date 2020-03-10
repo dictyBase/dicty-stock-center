f5328aba894d14fef650258b7f6cf2ae07ddea27
wildlifehexagon <crossthebreeze@gmail.com>
2020-03-10 10:00:29 -0500

    refactor: disable APQ until all data ready

--- src/components/AppProviders.tsx
+++ src/components/AppProviders.tsx
@@ -4,8 +4,8 @@ import { ApolloClient } from "apollo-client"
 import { InMemoryCache } from "apollo-cache-inmemory"
 import { createHttpLink } from "apollo-link-http"
 import { setContext } from "apollo-link-context"
-import { persistCache } from "apollo-cache-persist"
-import { createPersistedQueryLink } from "apollo-link-persisted-queries"
+// import { persistCache } from "apollo-cache-persist"
+// import { createPersistedQueryLink } from "apollo-link-persisted-queries"
 import { BrowserRouter } from "react-router-dom"
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
 import { useAuthStore } from "components/authentication/AuthStore"
@@ -18,21 +18,19 @@ const createClient = async (token: string) => {
       authorization: token ? `Bearer ${token}` : "",
     },
   }))
-  const link = createPersistedQueryLink({
-    useGETForHashedQueries: true,
-  }).concat(
-    authLink.concat(
-      createHttpLink({
-        uri: `${process.env.REACT_APP_GRAPHQL_SERVER}/graphql`,
-        credentials: "include",
-      }),
-    ),
+  // const link = createPersistedQueryLink().concat(
+  const link = authLink.concat(
+    createHttpLink({
+      uri: `${process.env.REACT_APP_GRAPHQL_SERVER}/graphql`,
+      credentials: "include",
+    }),
   )
+  // )
   const cache = new InMemoryCache()
-  await persistCache({
-    cache,
-    storage: window.localStorage as any,
-  })
+  // await persistCache({
+  //   cache,
+  //   storage: window.localStorage as any,
+  // })
   return new ApolloClient({
     cache,
     link,
