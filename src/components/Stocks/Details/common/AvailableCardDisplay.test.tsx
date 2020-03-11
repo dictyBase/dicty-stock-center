474f7b016fed9c7a965fdd6f725e595263385111
wildlifehexagon <crossthebreeze@gmail.com>
2020-03-11 18:32:11 -0500

    test: fix import paths in tests

--- src/components/Stocks/Details/common/AvailableCardDisplay.test.tsx
+++ src/components/Stocks/Details/common/AvailableCardDisplay.test.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { mount } from "enzyme"
-import AvailableCardDisplay from "./AvailableCardDisplayInProgess"
+import AvailableCardDisplay from "./AvailableCardDisplay"
 import { BrowserRouter } from "react-router-dom"
 import Typography from "@material-ui/core/Typography"
 import Divider from "@material-ui/core/Divider"
