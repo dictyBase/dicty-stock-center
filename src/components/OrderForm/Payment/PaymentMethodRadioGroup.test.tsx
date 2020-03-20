654deb8a3ef2c3f2410e40fc67eea9aa80ae5f63
wildlifehexagon <crossthebreeze@gmail.com>
2020-03-20 16:17:59 -0500

    test: update mocks

--- src/components/OrderForm/Payment/PaymentMethodRadioGroup.test.tsx
+++ src/components/OrderForm/Payment/PaymentMethodRadioGroup.test.tsx
@@ -1,20 +1,25 @@
 import React from "react"
 import { mount } from "enzyme"
-import { OrderFormWrapper } from "utils/testing"
 import PaymentMethodRadioGroup from "./PaymentMethodRadioGroup"
 import RadioGroup from "@material-ui/core/RadioGroup"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
+
+const mockSetFieldValue = jest.fn()
+
+jest.mock("formik", () => ({
+  ...jest.requireActual("formik"),
+  useFormikContext: () => ({
+    setFieldValue: mockSetFieldValue,
+    handleChange: jest.fn(),
+  }),
+}))
 
 describe("OrderForm/Payment/PaymentMethodRadioGroup", () => {
   const setPurchaseOrderNumSpy = jest.fn()
   const props = {
     setPurchaseOrderNum: setPurchaseOrderNumSpy,
   }
-  const wrapper = mount(
-    <OrderFormWrapper>
-      <PaymentMethodRadioGroup {...props} />
-    </OrderFormWrapper>,
-  )
+  const wrapper = mount(<PaymentMethodRadioGroup {...props} />)
   describe("initial render", () => {
     it("always renders initial components", () => {
       expect(wrapper.find(RadioGroup)).toHaveLength(1)
@@ -23,22 +28,22 @@ describe("OrderForm/Payment/PaymentMethodRadioGroup", () => {
   })
   // describe("radio button interactions", () => {
   //   it("sets field value when clicking first two radio buttons", () => {
-  //     // click credit card button
-  //     const cc = wrapper.find(FormControlLabel).first()
-  //     cc.simulate("change")
-  //     expect(setPurchaseOrderNumSpy).toBeCalledWith(false)
-  //     // click wire transfer button
-  //     const wire = wrapper.find(FormControlLabel).at(1)
-  //     wire.simulate("change")
-  //     wrapper.update()
-  //     expect(setPurchaseOrderNumSpy).toBeCalledTimes(2)
-  //   })
-  //   it("does not set field value when clicking Purchase Order radio button", () => {
-  //     // click wire transfer button
-  //     const po = wrapper.find(FormControlLabel).at(2)
-  //     po.simulate("change")
-  //     wrapper.update()
-  //     expect(setPurchaseOrderNumSpy).toBeCalledWith(true)
-  //   })
+  // click credit card button
+  // const cc = wrapper.find(FormControlLabel).at(0)
+  // cc.simulate("change")
+  // expect(mockSetFieldValue).toBeCalled()
+  // expect(setPurchaseOrderNumSpy).toBeCalled()
+  // click wire transfer button
+  // const wire = wrapper.find(FormControlLabel).at(1)
+  // wire.simulate("change")
+  // expect(setPurchaseOrderNumSpy).toBeCalledTimes(2)
+  // })
+  // it("does not set field value when clicking Purchase Order radio button", () => {
+  //   // click wire transfer button
+  //   const po = wrapper.find(FormControlLabel).at(2)
+  //   po.simulate("change")
+  //   wrapper.update()
+  //   expect(setPurchaseOrderNumSpy).toBeCalledWith(true)
+  // })
   // })
 })
