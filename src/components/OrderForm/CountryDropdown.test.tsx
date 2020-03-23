bf54943d25bedd0926c24878d9e91c1d4bf21219
wildlifehexagon <crossthebreeze@gmail.com>
2020-03-23 11:26:53 -0500

    test: add tests for countryToFlag function

--- src/components/OrderForm/CountryDropdown.test.tsx
+++ src/components/OrderForm/CountryDropdown.test.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { mount } from "enzyme"
 import { OrderFormWrapper } from "utils/testing"
-import CountryDropdown from "./CountryDropdown"
+import CountryDropdown, { countryToFlag } from "./CountryDropdown"
 import Autocomplete from "@material-ui/lab/Autocomplete"
 
 describe("OrderForm/CountryDropdown", () => {
@@ -14,5 +14,16 @@ describe("OrderForm/CountryDropdown", () => {
     it("always renders initial components", () => {
       expect(wrapper.find(Autocomplete)).toHaveLength(1)
     })
+  })
+})
+
+describe("OrderForm/countryToFlag", () => {
+  it("should return expected string", () => {
+    expect(countryToFlag("IS")).toBe("🇮🇸")
+  })
+  it("should return isoCode if String.fromCodePoint is invalid", () => {
+    // @ts-ignore
+    global.String.fromCodePoint = undefined
+    expect(countryToFlag("IS")).toBe("IS")
   })
 })
--- src/components/OrderForm/CountryDropdown.tsx
+++ src/components/OrderForm/CountryDropdown.tsx
@@ -58,4 +58,5 @@ const CountryDropdown = ({ name }: Props) => {
   )
 }
 
+export { countryToFlag }
 export default CountryDropdown
