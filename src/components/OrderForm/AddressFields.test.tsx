4f1c32509419fea9246388f58b37354a51a43033
wildlifehexagon <crossthebreeze@gmail.com>
2020-03-24 13:45:57 -0500

    test: add more tests for AddressFields

--- src/components/OrderForm/AddressFields.test.tsx
+++ src/components/OrderForm/AddressFields.test.tsx
@@ -1,32 +1,67 @@
 import React from "react"
 import { shallow } from "enzyme"
-import AddressFields from "./AddressFields"
+import AddressFields, { checkIfCountry } from "./AddressFields"
 import CountryDropdown from "./CountryDropdown"
 import TextField from "./TextField"
 
 describe("OrderForm/AddressFields", () => {
-  const props = {
-    fields: [
-      {
-        name: "firstName",
-        field: "First Name",
-        required: true,
-      },
-      {
-        name: "lastName",
-        field: "Last Name",
-        required: true,
-      },
-    ],
-    setFieldValue: jest.fn(),
-    countryValue: "country",
-    countryName: "country",
-  }
-  const wrapper = shallow(<AddressFields {...props} />)
-  describe("initial render", () => {
+  describe("initial render with country", () => {
+    const props = {
+      fields: [
+        {
+          name: "firstName",
+          field: "First Name",
+          required: true,
+        },
+        {
+          name: "lastName",
+          field: "Last Name",
+          required: true,
+        },
+        {
+          name: "country",
+          field: "Country",
+          required: true,
+        },
+      ],
+      countryName: "country",
+    }
+    const wrapper = shallow(<AddressFields {...props} />)
     it("always renders initial components", () => {
       expect(wrapper.find(CountryDropdown)).toHaveLength(1)
       expect(wrapper.find(TextField)).toHaveLength(2)
     })
+  })
+  describe("initial render without country", () => {
+    const props = {
+      fields: [
+        {
+          name: "firstName",
+          field: "First Name",
+          required: true,
+        },
+        {
+          name: "lastName",
+          field: "Last Name",
+          required: true,
+        },
+      ],
+      countryName: "country",
+    }
+    const wrapper = shallow(<AddressFields {...props} />)
+    it("always renders initial components", () => {
+      expect(wrapper.find(CountryDropdown)).toHaveLength(0)
+      expect(wrapper.find(TextField)).toHaveLength(2)
+    })
+  })
+})
+
+describe("checkIfCountry", () => {
+  it("should return true for expected country fields", () => {
+    expect(checkIfCountry("country")).toBeTruthy()
+    expect(checkIfCountry("payerCountry")).toBeTruthy()
+  })
+  it("should return false for wrong field", () => {
+    expect(checkIfCountry("firstName")).toBeFalsy()
   })
 })
