29ed8bb9d6ef3f833a3c96bd1d4331662083f64e
wildlifehexagon <crossthebreeze@gmail.com>
2020-03-23 22:03:17 -0500

    test: add more tests for cart reducer

--- src/components/ShoppingCart/CartStore.test.tsx
+++ src/components/ShoppingCart/CartStore.test.tsx
@@ -2,13 +2,20 @@ import React from "react"
 import { renderHook } from "@testing-library/react-hooks"
 import { cartReducer, useCartStore, CartActionType } from "./CartStore"
 
+const storageKey = "dscCart"
+const maxKey = "dscMaxItems"
+
 describe("cartReducer", () => {
+  jest.spyOn(window.localStorage.__proto__, "setItem")
+  localStorage.setItem = jest.fn()
+
   const newItem = {
     id: "DBS1234567",
     name: "Test Strain",
     summary: "this is just a test summary",
     type: "strain",
   }
+
   it("should add a new item to cart", () => {
     const state = {
       addedItems: [],
@@ -52,6 +59,26 @@ describe("cartReducer", () => {
       maxItemsInCart: false,
       showCartDialog: false,
     })
+    expect(localStorage.setItem).toBeCalledWith(storageKey, "[]")
+    expect(localStorage.setItem).toBeCalledWith(maxKey, "false")
+  })
+  it("should empty entire cart", () => {
+    const state = {
+      addedItems: [newItem, newItem, newItem],
+      showCartDialog: false,
+      maxItemsInCart: false,
+    }
+    expect(
+      cartReducer(state, {
+        type: CartActionType.EMPTY_CART,
+      }),
+    ).toStrictEqual({
+      addedItems: [],
+      maxItemsInCart: false,
+      showCartDialog: false,
+    })
+    expect(localStorage.setItem).toBeCalledWith(storageKey, "[]")
+    expect(localStorage.setItem).toBeCalledWith(maxKey, "false")
   })
   it("should hide cart dialog", () => {
     const state = {
