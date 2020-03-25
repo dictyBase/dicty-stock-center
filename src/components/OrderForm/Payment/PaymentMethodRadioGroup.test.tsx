60d1287ea58a59d10aeab2d796d5ccfb77eb6265
wildlifehexagon <crossthebreeze@gmail.com>
2020-03-24 20:31:16 -0500

    test: add more tests for radio groups

--- src/components/OrderForm/Payment/PaymentMethodRadioGroup.test.tsx
+++ src/components/OrderForm/Payment/PaymentMethodRadioGroup.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { mount } from "enzyme"
+import { shallow } from "enzyme"
 import PaymentMethodRadioGroup from "./PaymentMethodRadioGroup"
 import RadioGroup from "@material-ui/core/RadioGroup"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
@@ -22,31 +22,36 @@ describe("OrderForm/Payment/PaymentMethodRadioGroup", () => {
   const props = {
     setPurchaseOrderNum: setPurchaseOrderNumSpy,
   }
-  const wrapper = mount(<PaymentMethodRadioGroup {...props} />)
+  const wrapper = shallow(<PaymentMethodRadioGroup {...props} />)
   describe("initial render", () => {
     it("always renders initial components", () => {
       expect(wrapper.find(RadioGroup)).toHaveLength(1)
       expect(wrapper.find(FormControlLabel)).toHaveLength(3)
     })
   })
-  // describe("radio button interactions", () => {
-  //   it("sets field value when clicking first two radio buttons", () => {
-  // click credit card button
-  // const cc = wrapper.find(FormControlLabel).at(0)
-  // cc.simulate("change")
-  // expect(mockSetFieldValue).toBeCalled()
-  // expect(setPurchaseOrderNumSpy).toBeCalled()
-  // click wire transfer button
-  // const wire = wrapper.find(FormControlLabel).at(1)
-  // wire.simulate("change")
-  // expect(setPurchaseOrderNumSpy).toBeCalledTimes(2)
-  // })
-  // it("does not set field value when clicking Purchase Order radio button", () => {
-  //   // click wire transfer button
-  //   const po = wrapper.find(FormControlLabel).at(2)
-  //   po.simulate("change")
-  //   wrapper.update()
-  //   expect(setPurchaseOrderNumSpy).toBeCalledWith(true)
-  // })
-  // })
+  describe("radio button interactions", () => {
+    it("sets field value when clicking first two radio buttons", () => {
+      // click credit card button
+      const cc = wrapper.find(FormControlLabel).first()
+      cc.simulate("change")
+      expect(mockSetFieldValue).toBeCalledTimes(1)
+      expect(mockSetFieldValue).toBeCalledWith("purchaseOrderNum", "N/A")
+      expect(setPurchaseOrderNumSpy).toBeCalledTimes(1)
+      expect(setPurchaseOrderNumSpy).toBeCalledWith(false)
+      // click wire transfer button
+      const wire = wrapper.find(FormControlLabel).at(1)
+      wire.simulate("change")
+      expect(setPurchaseOrderNumSpy).toBeCalledTimes(2)
+      expect(setPurchaseOrderNumSpy).toBeCalledWith(false)
+      expect(mockSetFieldValue).toBeCalledWith("purchaseOrderNum", "N/A")
+      expect(mockSetFieldValue).toBeCalledTimes(2)
+    })
+    it("does not set field value when clicking Purchase Order radio button", () => {
+      // click purchase order button
+      const po = wrapper.find(FormControlLabel).at(2)
+      po.simulate("change")
+      expect(setPurchaseOrderNumSpy).toBeCalledWith(true)
+      expect(mockSetFieldValue).toBeCalledWith("purchaseOrderNum", "")
+    })
+  })
 })
--- src/components/OrderForm/Shipping/ShippingMethodRadioGroup.test.tsx
+++ src/components/OrderForm/Shipping/ShippingMethodRadioGroup.test.tsx
@@ -1,48 +1,85 @@
 import React from "react"
-import { mount } from "enzyme"
-import { OrderFormWrapper } from "utils/testing"
+import { shallow } from "enzyme"
 import ShippingMethodRadioGroup from "./ShippingMethodRadioGroup"
 import RadioGroup from "@material-ui/core/RadioGroup"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
 
+const mockSetFieldValue = jest.fn()
+
+jest.mock("formik", () => ({
+  ...jest.requireActual("formik"),
+  useFormikContext: () => ({
+    setFieldValue: mockSetFieldValue,
+    handleChange: jest.fn(),
+    values: {
+      shippingAccount: "fedex",
+    },
+  }),
+}))
+
 describe("OrderForm/Shipping/ShippingMethodRadioGroup", () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  const setShipAccountNumSpy = jest.fn()
+  const setPrepaidNoticeSpy = jest.fn()
+
   const props = {
-    setShipAccountNum: jest.fn(),
-    setPrepaidNotice: jest.fn(),
+    setShipAccountNum: setShipAccountNumSpy,
+    setPrepaidNotice: setPrepaidNoticeSpy,
   }
-  const wrapper = mount(
-    <OrderFormWrapper>
-      <ShippingMethodRadioGroup {...props} />
-    </OrderFormWrapper>,
-  )
+
+  const wrapper = shallow(<ShippingMethodRadioGroup {...props} />)
   describe("initial render", () => {
     it("always renders initial components", () => {
       expect(wrapper.find(RadioGroup)).toHaveLength(1)
       expect(wrapper.find(FormControlLabel)).toHaveLength(4)
     })
   })
-  // describe("radio button interactions", () => {
-  //   it("sets field value when clicking prepaid shipping label", () => {
-  //     // click prepaid label button
-  //     const label = wrapper.find(FormControlLabel).last()
-  //     label.simulate("change")
-  //     expect(setFieldValueSpy.calledOnce).toBe(true)
-  //   })
-  //   it("does not set field value when clicking others", () => {
-  //     // reset spy
-  //     setFieldValueSpy.resetHistory()
-  //     // click fedex button
-  //     const fedex = wrapper.find(FormControlLabel).first()
-  //     fedex.simulate("change")
-  //     expect(setFieldValueSpy.notCalled).toBe(true)
-  //     // click UPS button
-  //     const ups = wrapper.find(FormControlLabel).at(1)
-  //     ups.simulate("change")
-  //     expect(setFieldValueSpy.notCalled).toBe(true)
-  //     // click DHL button
-  //     const dhl = wrapper.find(FormControlLabel).at(2)
-  //     dhl.simulate("change")
-  //     expect(setFieldValueSpy.notCalled).toBe(true)
-  //   })
-  // })
+  describe("radio button interactions", () => {
+    it("sets field value when clicking prepaid shipping label", () => {
+      // click prepaid label button
+      const label = wrapper.find(FormControlLabel).last()
+      label.simulate("change")
+      expect(mockSetFieldValue).toBeCalledTimes(1)
+      expect(mockSetFieldValue).toBeCalledWith(
+        "shippingAccountNumber",
+        "sending prepaid shipping label",
+      )
+      expect(setShipAccountNumSpy).toBeCalledTimes(1)
+      expect(setShipAccountNumSpy).toBeCalledWith(false)
+      expect(setPrepaidNoticeSpy).toBeCalledTimes(1)
+      expect(setPrepaidNoticeSpy).toBeCalledWith(true)
+    })
+    it("does not set field value when clicking others", () => {
+      // click fedex button
+      const fedex = wrapper.find(FormControlLabel).first()
+      fedex.simulate("change")
+      expect(mockSetFieldValue).toBeCalledTimes(1)
+      expect(mockSetFieldValue).toBeCalledWith("shippingAccountNumber", "")
+      expect(setShipAccountNumSpy).toBeCalledTimes(1)
+      expect(setShipAccountNumSpy).toBeCalledWith(true)
+      expect(setPrepaidNoticeSpy).toBeCalledTimes(1)
+      expect(setPrepaidNoticeSpy).toBeCalledWith(false)
+      // click UPS button
+      const ups = wrapper.find(FormControlLabel).at(1)
+      ups.simulate("change")
+      expect(mockSetFieldValue).toBeCalledTimes(2)
+      expect(mockSetFieldValue).toBeCalledWith("shippingAccountNumber", "")
+      expect(setShipAccountNumSpy).toBeCalledTimes(2)
+      expect(setShipAccountNumSpy).toBeCalledWith(true)
+      expect(setPrepaidNoticeSpy).toBeCalledTimes(2)
+      expect(setPrepaidNoticeSpy).toBeCalledWith(false)
+      // click DHL button
+      const dhl = wrapper.find(FormControlLabel).at(2)
+      dhl.simulate("change")
+      expect(mockSetFieldValue).toBeCalledTimes(3)
+      expect(mockSetFieldValue).toBeCalledWith("shippingAccountNumber", "")
+      expect(setShipAccountNumSpy).toBeCalledTimes(3)
+      expect(setShipAccountNumSpy).toBeCalledWith(true)
+      expect(setPrepaidNoticeSpy).toBeCalledTimes(3)
+      expect(setPrepaidNoticeSpy).toBeCalledWith(false)
+    })
+  })
 })
