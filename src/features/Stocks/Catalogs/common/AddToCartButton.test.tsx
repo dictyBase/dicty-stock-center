c75729dd0f47cf16fee8dca1c8b3bd08dbe3e7d3
wildlifehexagon <crossthebreeze@gmail.com>
2020-05-07 15:20:59 -0500

    test: add test for unavailable button displays

--- src/features/Stocks/Catalogs/common/AddToCartButton.test.tsx
+++ src/features/Stocks/Catalogs/common/AddToCartButton.test.tsx
@@ -4,46 +4,56 @@ import { BrowserRouter } from "react-router-dom"
 import AddToCartButton from "./AddToCartButton"
 import IconButton from "@material-ui/core/IconButton"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Tooltip from "@material-ui/core/Tooltip"
 import {
   CartProvider,
   CartContext,
   cartReducer,
 } from "features/ShoppingCart/CartStore"
 import AddToCartDialog from "./AddToCartDialog"
+import UnavailableButton from "./UnavailableButton"
+
+const inStockProps = {
+  data: [
+    {
+      id: "DBS123456",
+      name: "test1",
+      summary: "this is a test summary",
+    },
+  ],
+  inStock: true,
+  setCheckedItems: jest.fn(),
+}
+
+const unavailableProps = {
+  data: [
+    {
+      id: "DBS123456",
+      name: "test1",
+      summary: "this is a test summary",
+    },
+  ],
+  inStock: false,
+}
 
 describe("Stocks/Catalogs/common/AddToCartButton", () => {
-  const props = {
-    data: [
-      {
-        id: "DBS123456",
-        name: "test1",
-        summary: "this is a test summary",
-      },
-    ],
-    inStock: true,
-    setCheckedItems: jest.fn(),
-  }
-  const wrapper = mount(
-    <CartProvider>
-      <BrowserRouter>
-        <AddToCartButton {...props} />
-      </BrowserRouter>
-    </CartProvider>,
-  )
-  describe("initial render", () => {
+  describe("button clicking", () => {
+    const wrapper = mount(
+      <CartProvider>
+        <BrowserRouter>
+          <AddToCartButton {...inStockProps} />
+        </BrowserRouter>
+      </CartProvider>,
+    )
     it("renders expected initial components", () => {
       expect(wrapper.find(IconButton)).toHaveLength(1)
       expect(wrapper.find(FontAwesomeIcon)).toHaveLength(1)
     })
-  })
-  describe("button clicking", () => {
     it("should display AddToCartDialog on click", () => {
       wrapper.find(IconButton).simulate("click")
       expect(wrapper.find(AddToCartDialog)).toHaveLength(1)
     })
   })
-  describe("alternate button display", () => {
+  describe("button display when cart is full", () => {
     const MockedComponent = () => {
       const [state, dispatch] = React.useReducer(cartReducer, {
         addedItems: [],
@@ -54,18 +64,29 @@ describe("Stocks/Catalogs/common/AddToCartButton", () => {
       return (
         <CartProvider>
           <CartContext.Provider value={[state, dispatch]}>
-            <AddToCartButton {...props} />
+            <AddToCartButton {...inStockProps} />
           </CartContext.Provider>
         </CartProvider>
       )
     }
 
-    const newWrapper = mount(<MockedComponent />)
+    const wrapper = mount(<MockedComponent />)
 
     it("should display expected components if cart is full", () => {
-      expect(newWrapper.find(Tooltip)).toHaveLength(1)
-      expect(newWrapper.find(IconButton)).toHaveLength(1)
-      expect(newWrapper.find(FontAwesomeIcon)).toHaveLength(2)
+      expect(wrapper.find(UnavailableButton)).toHaveLength(1)
+    })
+  })
+
+  describe("button display when item is unavailable", () => {
+    const wrapper = mount(
+      <CartProvider>
+        <BrowserRouter>
+          <AddToCartButton {...unavailableProps} />
+        </BrowserRouter>
+      </CartProvider>,
+    )
+    it("should display unavailable button if item unavailable", () => {
+      expect(wrapper.find(UnavailableButton)).toHaveLength(1)
     })
   })
 })
