22bcdd2b0c803e0f297c493b0e236ea2d10fa96e
wildlifehexagon <crossthebreeze@gmail.com>
2020-05-28 15:33:02 -0500

    refactor: disable related stocks cards for now

--- src/features/Stocks/Details/Plasmids/PlasmidDetailsRightColumn.tsx
+++ src/features/Stocks/Details/Plasmids/PlasmidDetailsRightColumn.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import Grid from "@material-ui/core/Grid"
 import AvailabilityCard from "features/Stocks/Details/common/AvailabilityCard"
-import RelatedPlasmidsCard from "./RelatedPlasmidsCard"
+// import RelatedPlasmidsCard from "./RelatedPlasmidsCard"
 import { PlasmidDetailsProps } from "features/Stocks/Details/types/props"
 
 const PlasmidDetailsRightColumn = ({ data }: PlasmidDetailsProps) => (
   <Grid item xs={12} md={3} lg={2}>
     <AvailabilityCard data={data} stockType="plasmid" />
-    <RelatedPlasmidsCard genes={data.genes} />
+    {/* <RelatedPlasmidsCard genes={data.genes} /> */}
   </Grid>
 )
 
