31cdfa3c372da228fcffc1c63b514903b7abd8d7
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-02 11:35:38 -0500

    feat: add link to phenotype search results route #730

--- src/features/Stocks/Details/Strains/Phenotypes/PhenotypeListItem.tsx
+++ src/features/Stocks/Details/Strains/Phenotypes/PhenotypeListItem.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
 import ListItem from "@material-ui/core/ListItem"
@@ -9,6 +10,8 @@ import { PhenotypeData } from "features/Stocks/Details/types/props"
 type Props = {
   data: PhenotypeData
 }
+
+const replaceSpaces = (phenotype: string) => phenotype.split(" ").join("+")
 
 /**
  * PhenotypeListItem handles the display of an individual
@@ -22,7 +25,11 @@ const PhenotypeListItem = ({ data }: Props) => {
     <ListItem className={classes.row}>
       <Grid container spacing={0} alignItems="center">
         <Grid item xs={3} className={classes.item}>
-          <Typography variant="body2">{data.phenotype}</Typography>
+          <Typography variant="body2">
+            <Link to={`/phenotypes/${replaceSpaces(data.phenotype)}`}>
+              {data.phenotype}
+            </Link>
+          </Typography>
         </Grid>
         <Grid item xs={3} className={classes.item}>
           <Typography variant="body2">{data.note}</Typography>
