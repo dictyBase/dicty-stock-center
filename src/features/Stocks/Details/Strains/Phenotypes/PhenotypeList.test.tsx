ff9fce2994b4c80e37dcfcb0cec2d085489135b7
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-03 10:37:36 -0500

    test: use shallow rendering

--- src/features/Stocks/Details/Strains/Phenotypes/PhenotypeList.test.tsx
+++ src/features/Stocks/Details/Strains/Phenotypes/PhenotypeList.test.tsx
@@ -1,12 +1,12 @@
 import React from "react"
-import { mount } from "enzyme"
+import { shallow } from "enzyme"
 import PhenotypeList from "./PhenotypeList"
 import PhenotypeListListHeader from "./PhenotypeListHeader"
 import PhenotypeListItem from "./PhenotypeListItem"
 import { data } from "./mockPhenotypeData"
 
 describe("Stocks/Details/Strains/Phenotypes/PhenotypeList", () => {
-  const wrapper = mount(<PhenotypeList phenotypes={data} />)
+  const wrapper = shallow(<PhenotypeList phenotypes={data} />)
   describe("initial render", () => {
     it("always renders initial components", () => {
       expect(wrapper.find(PhenotypeListListHeader)).toHaveLength(1)
