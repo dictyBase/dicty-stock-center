c314ccbf023195a8db4847bd01f96d4fee5c5c92
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-03 16:01:33 -0500

    refactor: rename to PhenotypesContainer

--- src/features/Stocks/SearchResults/PhenotypesContainer.tsx
+++ src/features/Stocks/SearchResults/PhenotypesContainer.tsx
@@ -5,36 +5,43 @@ import { useQuery } from "@apollo/react-hooks"
 import DetailsLoader from "features/Stocks/Details/common/DetailsLoader"
 import GraphQLErrorPage from "features/Errors/GraphQLErrorPage"
 import { GET_STRAIN_LIST_WITH_PHENOTYPE } from "common/graphql/queries"
+import PhenotypeList from "./PhenotypeList"
 
 const cleanQuery = (phenotype: string) => phenotype.split("+").join(" ")
 
 /**
- * Phenotypes is used to fetch a list of strains with a given phenotype.
+ * PhenotypesContainer is used to fetch a list of strains with a given phenotype.
  */
 
-const Phenotypes = () => {
+const PhenotypesContainer = () => {
   const { name } = useParams()
   const phenotype = cleanQuery(name)
-  const { loading, error, data } = useQuery(GET_STRAIN_LIST_WITH_PHENOTYPE, {
-    variables: { cursor: 0, limit: 1000, phenotype },
-  })
+  const { loading, error, data, fetchMore } = useQuery(
+    GET_STRAIN_LIST_WITH_PHENOTYPE,
+    {
+      variables: { cursor: 0, limit: 10, phenotype },
+    },
+  )
 
   if (loading) return <DetailsLoader />
   if (error) return <GraphQLErrorPage error={error} />
-  console.log(data)
+
   return (
     <div>
       <Helmet>
-        <title>
-          Phenotype Search Results for {phenotype} - Dicty Stock Center
-        </title>
+        <title>{phenotype} - Dicty Stock Center</title>
         <meta
           name="description"
           content={`Dicty Stock Center phenotype search results for ${phenotype}`}
         />
       </Helmet>
+      <PhenotypeList
+        data={data.listStrainsWithPhenotype.strains}
+        fetchMore={fetchMore}
+        cursor={data.listStrainsWithPhenotype.nextCursor}
+      />
     </div>
   )
 }
 
-export default Phenotypes
+export default PhenotypesContainer
