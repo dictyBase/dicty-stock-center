2e988ff3d2dc27c4ac2043297bd6ebc4bb6e0eb2
wildlifehexagon <crossthebreeze@gmail.com>
2020-10-12 16:08:03 -0500

    test: add tests for getting items from storage and verifying max items in cart

--- src/features/ShoppingCart/CartStore.test.tsx
+++ src/features/ShoppingCart/CartStore.test.tsx
@@ -8,6 +8,8 @@ const maxKey = "dscMaxItems"
 describe("cartReducer", () => {
   jest.spyOn(window.localStorage.__proto__, "setItem")
   localStorage.setItem = jest.fn()
+  jest.spyOn(window.localStorage.__proto__, "getItem")
+  localStorage.getItem = jest.fn()
 
   const newItem = {
     id: "DBS1234567",
@@ -41,6 +43,30 @@ describe("cartReducer", () => {
       maxItemsInCart: false,
     })
   })
+
+  it("should not add a new item to cart when full", () => {
+    const items = new Array(12).fill(newItem)
+    const state = {
+      addedItems: items,
+      showCartDialog: false,
+      maxItemsInCart: false,
+    }
+    expect(
+      cartReducer(state, {
+        type: CartActionType.ADD_TO_CART,
+        payload: {
+          ...newItem,
+          fee: "30.00",
+        },
+      }),
+    ).toStrictEqual({
+      addedItems: items,
+      showCartDialog: true,
+      maxItemsInCart: true,
+    })
+    expect(localStorage.setItem).toBeCalledWith(maxKey, "true")
+  })
+
   it("should remove an item from cart", () => {
     const state = {
       addedItems: [
@@ -67,6 +93,7 @@ describe("cartReducer", () => {
     expect(localStorage.setItem).toBeCalledWith(storageKey, "[]")
     expect(localStorage.setItem).toBeCalledWith(maxKey, "false")
   })
+
   it("should empty entire cart", () => {
     const state = {
       addedItems: [
@@ -98,6 +125,7 @@ describe("cartReducer", () => {
     expect(localStorage.setItem).toBeCalledWith(storageKey, "[]")
     expect(localStorage.setItem).toBeCalledWith(maxKey, "false")
   })
+
   it("should hide cart dialog", () => {
     const state = {
       addedItems: [],
@@ -113,6 +141,25 @@ describe("cartReducer", () => {
       showCartDialog: false,
       maxItemsInCart: false,
     })
+  })
+
+  it("should get items from local storage", () => {
+    const state = {
+      addedItems: [],
+      showCartDialog: false,
+      maxItemsInCart: false,
+    }
+    expect(
+      cartReducer(state, {
+        type: CartActionType.GET_ITEMS_FROM_STORAGE,
+      }),
+    ).toStrictEqual({
+      addedItems: [],
+      showCartDialog: false,
+      maxItemsInCart: false,
+    })
+    expect(localStorage.getItem).toBeCalledWith(storageKey)
+    expect(localStorage.setItem).toBeCalledWith(maxKey, "false")
   })
 })
 
