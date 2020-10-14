28a4161e5ac1bf34f309e7fe0517504bef91b681
wildlifehexagon <crossthebreeze@gmail.com>
2020-10-14 10:23:09 -0500

    refactor: use custom hook for apollo client

--- src/app/layout/AppProviders.tsx
+++ src/app/layout/AppProviders.tsx
@@ -1,54 +1,14 @@
-import React, { useEffect, useState, ReactNode } from "react"
+import React, { ReactNode } from "react"
 import { ApolloProvider } from "@apollo/react-hooks"
 import { ApolloClient } from "apollo-client"
 import { InMemoryCache } from "apollo-cache-inmemory"
 import { createHttpLink } from "apollo-link-http"
 import { setContext } from "apollo-link-context"
-// import { persistCache } from "apollo-cache-persist"
-// import { createPersistedQueryLink } from "apollo-link-persisted-queries"
 import { BrowserRouter } from "react-router-dom"
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
 import { useAuthStore } from "features/Authentication/AuthStore"
 import { CartProvider } from "features/ShoppingCart/CartStore"
 import { mutationList } from "common/graphql/mutations"
-
-const isMutation = (value: string) => {
-  if (mutationList.includes(value)) {
-    return true
-  }
-  return false
-}
-
-const createClient = async (token: string) => {
-  const authLink = setContext((request, { headers }) => {
-    const mutation = isMutation(request.operationName || "")
-    return {
-      headers: {
-        ...headers,
-        Authorization: token ? `Bearer ${token}` : "",
-        "X-GraphQL-Method": mutation ? "Mutation" : "Query",
-      },
-    }
-  })
-  // const link = createPersistedQueryLink().concat(
-  const link = authLink.concat(
-    createHttpLink({
-      uri: `${process.env.REACT_APP_GRAPHQL_SERVER}/graphql`,
-      credentials: "include",
-    }),
-  )
-  // )
-  const cache = new InMemoryCache()
-  // await persistCache({
-  //   cache,
-  //   storage: window.localStorage as any,
-  // })
-  return new ApolloClient({
-    cache,
-    link,
-  })
-}
-
 const theme = createMuiTheme({
   palette: {
     primary: {
@@ -78,18 +38,47 @@ const theme = createMuiTheme({
   },
 })
 
-const AppProviders = ({ children }: { children: ReactNode }) => {
-  const [client, setClient] = useState<ApolloClient<any> | undefined>(undefined)
-  const [{ token }] = useAuthStore()
-  useEffect(() => {
-    createClient(token).then((apollo) => setClient(apollo))
-    return () => {}
-  }, [token])
+const isMutation = (value: string) => {
+  if (mutationList.includes(value)) {
+    return true
+  }
+  return false
+}
 
-  if (client === undefined) return <div /> // maybe we could replace with animated dicty logo someday
+const useApolloClient = () => {
+  const [{ token }] = useAuthStore()
+
+  const authLink = setContext((request, { headers }) => {
+    const mutation = isMutation(request.operationName || "")
+    return {
+      headers: {
+        ...headers,
+        Authorization: token ? `Bearer ${token}` : "",
+        "X-GraphQL-Method": mutation ? "Mutation" : "Query",
+      },
+    }
+  })
+
+  const link = authLink.concat(
+    createHttpLink({
+      uri: `${process.env.REACT_APP_GRAPHQL_SERVER}/graphql`,
+      credentials: "include",
+    }),
+  )
+
+  const cache = new InMemoryCache()
+
+  return new ApolloClient({
+    cache,
+    link,
+  })
+}
+
+const AppProviders = ({ children }: { children: ReactNode }) => {
+  const apolloClient = useApolloClient()
 
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apolloClient}>
       <MuiThemeProvider theme={theme}>
         <BrowserRouter basename={process.env.REACT_APP_BASENAME}>
           <CartProvider>{children}</CartProvider>
