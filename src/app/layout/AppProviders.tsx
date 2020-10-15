e9d2082198bf532722d370b740387b8a7779cb82
wildlifehexagon <crossthebreeze@gmail.com>
2020-10-15 10:37:36 -0500

    refactor: move apollo client hook to its own file

--- src/app/layout/AppProviders.tsx
+++ src/app/layout/AppProviders.tsx
@@ -1,13 +1,9 @@
 import React, { ReactNode } from "react"
 import { ApolloProvider } from "@apollo/react-hooks"
-import { ApolloClient } from "apollo-client"
-import { InMemoryCache } from "apollo-cache-inmemory"
-import { createHttpLink } from "apollo-link-http"
-import { setContext } from "apollo-link-context"
 import { BrowserRouter } from "react-router-dom"
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
 import { CartProvider } from "features/ShoppingCart/CartStore"
-import { mutationList } from "common/graphql/mutations"
+import useApolloClient from "common/hooks/useApolloClient"
 
 const theme = createMuiTheme({
   palette: {
@@ -38,39 +34,6 @@ const theme = createMuiTheme({
   },
 })
 
-const isMutation = (value: string) => {
-  if (mutationList.includes(value)) {
-    return true
-  }
-  return false
-}
-
-const useApolloClient = () => {
-  const authLink = setContext((request, { headers }) => {
-    const mutation = isMutation(request.operationName || "")
-    return {
-      headers: {
-        ...headers,
-        "X-GraphQL-Method": mutation ? "Mutation" : "Query",
-      },
-    }
-  })
-
-  const link = authLink.concat(
-    createHttpLink({
-      uri: `${process.env.REACT_APP_GRAPHQL_SERVER}/graphql`,
-      credentials: "include",
-    }),
-  )
-
-  const cache = new InMemoryCache()
-
-  return new ApolloClient({
-    cache,
-    link,
-  })
-}
-
 const AppProviders = ({ children }: { children: ReactNode }) => {
   const apolloClient = useApolloClient()
 
@@ -85,5 +48,4 @@ const AppProviders = ({ children }: { children: ReactNode }) => {
   )
 }
 
-export { isMutation }
 export default AppProviders
