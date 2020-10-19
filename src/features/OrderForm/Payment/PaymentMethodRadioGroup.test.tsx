06d4366aa02b0367cf80014449701f5a65e87842
wildlifehexagon <crossthebreeze@gmail.com>
2020-10-19 17:44:45 -0500

    test: fix broken tests

--- src/features/OrderForm/Payment/PaymentMethodRadioGroup.test.tsx
+++ src/features/OrderForm/Payment/PaymentMethodRadioGroup.test.tsx
@@ -23,28 +23,38 @@ jest.mock("formik", () => {
 
 describe("OrderForm/Payment/PaymentMethodRadioGroup", () => {
   const setPurchaseOrderNumSpy = jest.fn()
+  const setWaiverRequestedSpy = jest.fn()
   const props = {
     setPurchaseOrderNum: setPurchaseOrderNumSpy,
+    setWaiverRequested: setWaiverRequestedSpy,
   }
   const wrapper = shallow(<PaymentMethodRadioGroup {...props} />)
   describe("initial render", () => {
     it("always renders initial components", () => {
       expect(wrapper.find(RadioGroup)).toHaveLength(1)
-      expect(wrapper.find(FormControlLabel)).toHaveLength(3)
+      expect(wrapper.find(FormControlLabel)).toHaveLength(4)
     })
   })
   describe("radio button interactions", () => {
     it("sets field value when clicking first two radio buttons", () => {
       // click credit card button
       const cc = wrapper.find(FormControlLabel).first()
-      cc.simulate("change")
+      cc.simulate("change", {
+        target: {
+          value: "credit",
+        },
+      })
       expect(mockSetFieldValue).toBeCalledTimes(1)
       expect(mockSetFieldValue).toBeCalledWith("purchaseOrderNum", "N/A")
       expect(setPurchaseOrderNumSpy).toBeCalledTimes(1)
       expect(setPurchaseOrderNumSpy).toBeCalledWith(false)
       // click wire transfer button
       const wire = wrapper.find(FormControlLabel).at(1)
-      wire.simulate("change")
+      wire.simulate("change", {
+        target: {
+          value: "wire",
+        },
+      })
       expect(setPurchaseOrderNumSpy).toBeCalledTimes(2)
       expect(setPurchaseOrderNumSpy).toBeCalledWith(false)
       expect(mockSetFieldValue).toBeCalledWith("purchaseOrderNum", "N/A")
@@ -53,7 +63,11 @@ describe("OrderForm/Payment/PaymentMethodRadioGroup", () => {
     it("does not set field value when clicking Purchase Order radio button", () => {
       // click purchase order button
       const po = wrapper.find(FormControlLabel).at(2)
-      po.simulate("change")
+      po.simulate("change", {
+        target: {
+          value: "purchaseOrder",
+        },
+      })
       expect(setPurchaseOrderNumSpy).toBeCalledWith(true)
       expect(mockSetFieldValue).toBeCalledWith("purchaseOrderNum", "")
     })
