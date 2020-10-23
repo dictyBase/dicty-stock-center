933c3462051bdb30c99b97b9ff73b89fff9655c8
wildlifehexagon <crossthebreeze@gmail.com>
2020-10-23 13:05:13 -0500

    refactor: open obo links in new tab, fixes #95

--- src/features/Home/LinkList.tsx
+++ src/features/Home/LinkList.tsx
@@ -2,15 +2,36 @@ import React from "react"
 import { Link } from "react-router-dom"
 import useStyles from "./homeStyles"
 
+type LinkItem = {
+  /** Name of link */
+  name: string
+  /** URL of link */
+  to: string
+  /** Indicates if link should use React Router */
+  routerAware: boolean
+  /** Indicates if link should open in new window */
+  newWindow: boolean
+}
+
 type Props = {
   /** List of links in array form */
-  list: Array<{
-    name: string
-    to: string
-    routerAware: boolean
-  }>
+  list: Array<LinkItem>
   /** Color of panel background, either gray or blue */
   bgColor: string
+}
+
+const getLinkType = (link: LinkItem) => {
+  if (link.routerAware) {
+    return <Link to={link.to}>{link.name}</Link>
+  }
+  if (link.newWindow) {
+    return (
+      <a href={link.to} target="_blank" rel="noopener noreferrer">
+        {link.name}
+      </a>
+    )
+  }
+  return <a href={link.to}>{link.name}</a>
 }
 
 /**
@@ -30,13 +51,7 @@ const LinkList = ({ list, bgColor }: Props) => {
       {isDownloadPanel && <h3>Download / View</h3>}
       <ul className={classes.list}>
         {list.map((link, index) => (
-          <li key={index}>
-            {link.routerAware ? (
-              <Link to={link.to}>{link.name}</Link>
-            ) : (
-              <a href={link.to}>{link.name}</a>
-            )}
-          </li>
+          <li key={index}>{getLinkType(link)}</li>
         ))}
       </ul>
     </div>
