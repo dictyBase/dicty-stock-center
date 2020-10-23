81434a2843940c7a5ed511e3f7d4a5b84d356c33
wildlifehexagon <crossthebreeze@gmail.com>
2020-10-23 13:05:13 -0500

    refactor: open obo links in new tab, fixes #95

--- src/common/constants/linkLists.ts
+++ src/common/constants/linkLists.ts
@@ -4,64 +4,98 @@ const downloadLinks = [
     to:
       "https://raw.githubusercontent.com/dictyBase/migration-data/master/ontologies/dicty_phenotypes.obo",
     routerAware: false,
+    newWindow: true,
   },
   {
     name: "Strain Characteristics",
     to:
       "https://raw.githubusercontent.com/dictyBase/migration-data/master/ontologies/dicty_strain_characteristics.obo",
     routerAware: false,
+    newWindow: true,
   },
   {
     name: "Mutagenesis Methods",
     to:
       "https://raw.githubusercontent.com/dictyBase/migration-data/master/ontologies/dicty_mutagenesis_method.obo",
     routerAware: false,
+    newWindow: true,
   },
   {
     name: "Plasmid Keywords",
     to:
       "https://raw.githubusercontent.com/dictyBase/migration-data/master/ontologies/dicty_plasmid_keywords.obo",
     routerAware: false,
+    newWindow: true,
   },
 ]
 
 const infoLinks = [
-  { name: "Order Information", to: "/information/order", routerAware: true },
+  {
+    name: "Order Information",
+    to: "/information/order",
+    routerAware: true,
+    newWindow: false,
+  },
   {
     name: "Payment Information",
     to: "/information/payment",
     routerAware: true,
+    newWindow: false,
   },
   {
     name: "Deposit Information",
     to: "/information/deposit",
     routerAware: true,
+    newWindow: false,
   },
 ]
 
 const materialsLinks = [
-  { name: "Strain Catalog", to: "/strains", routerAware: true },
-  { name: "Plasmid Catalog", to: "/plasmids", routerAware: true },
-  { name: "Bacterial Strains", to: "", routerAware: true },
+  {
+    name: "Strain Catalog",
+    to: "/strains",
+    routerAware: true,
+    newWindow: false,
+  },
+  {
+    name: "Plasmid Catalog",
+    to: "/plasmids",
+    routerAware: true,
+    newWindow: false,
+  },
+  { name: "Bacterial Strains", to: "", routerAware: true, newWindow: false },
   {
     name: "Additional Materials",
     to: "/information/additional-materials",
     routerAware: true,
+    newWindow: false,
   },
 ]
 
 const miscLinks = [
-  { name: "Contact the DSC", to: "/contact", routerAware: true },
-  { name: "DSC FAQ", to: "/information/faq", routerAware: true },
+  {
+    name: "Contact the DSC",
+    to: "/contact",
+    routerAware: true,
+    newWindow: false,
+  },
+  {
+    name: "DSC FAQ",
+    to: "/information/faq",
+    routerAware: true,
+    newWindow: false,
+  },
   {
     name: "Nomenclature Guide",
     to: "/information/nomenclature-guidelines",
     routerAware: true,
+    newWindow: false,
   },
   {
     name: "Other Stock Centers",
     to: "/information/other-stock-centers",
     routerAware: true,
+    newWindow: false,
   },
 ]
 
