fdf1d57de71d1267d3bb629fbf303bc4cb62e301
wildlifehexagon <crossthebreeze@gmail.com>
2020-10-23 10:08:19 -0500

    refactor: remove padding from list display

--- src/features/Stocks/SearchResults/PhenotypeList.tsx
+++ src/features/Stocks/SearchResults/PhenotypeList.tsx
@@ -1,31 +1,39 @@
 import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
 import Paper from "@material-ui/core/Paper"
 import List from "@material-ui/core/List"
 import PhenotypeListHeader from "./PhenotypeListHeader"
 import PhenotypeListItem from "./PhenotypeListItem"
 import { StrainWithPhenotype } from "../Details/types/props"
 
+const useStyles = makeStyles({
+  list: {
+    paddingTop: "0px",
+    paddingBottom: "0px",
+  },
+})
+
 type Props = {
   data: Array<StrainWithPhenotype>
-  /** Total number of results for this search */
-  totalCount: number
 }
 
 /**
  * PhenotypeList is used to display a list of phenotypes.
  */
 
-const PhenotypeList = ({ data }: Props) => (
-  // const classes = useStyles()
+const PhenotypeList = ({ data }: Props) => {
+  const classes = useStyles()
 
-  <Paper>
-    <PhenotypeListHeader />
-    <List dense>
-      {data.map((item: StrainWithPhenotype, index: number) => (
-        <PhenotypeListItem key={index} strain={item} />
-      ))}
-    </List>
-  </Paper>
-)
+  return (
+    <Paper>
+      <PhenotypeListHeader />
+      <List className={classes.list} dense>
+        {data.map((item: StrainWithPhenotype, index: number) => (
+          <PhenotypeListItem key={index} strain={item} />
+        ))}
+      </List>
+    </Paper>
+  )
+}
 
 export default PhenotypeList
