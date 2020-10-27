b6cc88557b1317fb7c8403f69e1c26c8714a2c81
wildlifehexagon <crossthebreeze@gmail.com>
2020-10-27 15:31:24 -0500

    test: add tests for fetching more data

--- src/features/Stocks/SearchResults/PhenotypeContainer.test.tsx
+++ src/features/Stocks/SearchResults/PhenotypeContainer.test.tsx
@@ -1,17 +1,10 @@
 import React from "react"
-import {
-  render,
-  screen,
-  fireEvent,
-  queryByTestId,
-  getAllByRole,
-  waitFor,
-} from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { MockedProvider } from "@apollo/client/testing"
 import { BrowserRouter } from "react-router-dom"
 import PhenotypeContainer from "./PhenotypeContainer"
 import { GET_STRAIN_LIST_WITH_PHENOTYPE } from "common/graphql/queries"
-import { first50 } from "./mockData"
+import { first50, second50, lastItems } from "./mockData"
 
 const mockParams = "abolished+protein+phosphorylation"
 
@@ -30,19 +23,19 @@ describe("Stocks/SearchResults/PhenotypeContainer", () => {
   const window = global as any
   let mockObserve: jest.Mock
   let mockUnobserve: jest.Mock
+  beforeEach(() => {
+    mockObserve = jest.fn()
+    mockUnobserve = jest.fn()
+  })
+  beforeAll(() => {
+    window.IntersectionObserver = jest.fn((callback, options) => ({
+      observe: mockObserve,
+      unobserve: mockUnobserve,
+    }))
+    jest.setTimeout(30000)
+  })
 
   describe("initial render with small data set", () => {
-    beforeEach(() => {
-      mockObserve = jest.fn()
-      mockUnobserve = jest.fn()
-    })
-    beforeAll(() => {
-      window.IntersectionObserver = jest.fn((callback, options) => ({
-        observe: mockObserve,
-        unobserve: mockUnobserve,
-      }))
-    })
-
     const mocks = [
       {
         request: {
@@ -76,17 +69,134 @@ describe("Stocks/SearchResults/PhenotypeContainer", () => {
       expect(screen.getByTestId("skeleton-loader")).toBeInTheDocument()
 
       // wait for data to load...
-      const firstRow = await screen.findByText("Dd5P4-/[act15]:Hs-OCRL(D422A)")
+      const firstRow = await screen.findByText(first50[0].label)
       expect(firstRow).toBeInTheDocument()
-      const lastRow = await screen.findByText("[act15]:YFP:iqgC")
+      const lastRow = await screen.findByText(first50[9].label)
       expect(lastRow).toBeInTheDocument()
 
-      const row11 = screen.queryByText("sadA-")
+      const row11 = screen.queryByText(first50[10].label)
       expect(row11).toBeFalsy()
 
       const listItems = await screen.findAllByRole("listitem")
       // should have 11 list items -> 10 rows of data + list header
       expect(listItems).toHaveLength(11)
+    })
+  })
+
+  describe("initial render with large data set", () => {
+    const mocks = [
+      {
+        request: {
+          query: GET_STRAIN_LIST_WITH_PHENOTYPE,
+          variables: {
+            cursor: 0,
+            limit: 50,
+            phenotype: "abolished protein phosphorylation",
+          },
+        },
+        result: {
+          data: {
+            listStrainsWithPhenotype: {
+              totalCount: 50,
+              nextCursor: 123456,
+              strains: first50,
+            },
+          },
+        },
+      },
+      {
+        request: {
+          query: GET_STRAIN_LIST_WITH_PHENOTYPE,
+          variables: {
+            cursor: 123456,
+            limit: 50,
+            phenotype: "abolished protein phosphorylation",
+          },
+        },
+        result: {
+          data: {
+            listStrainsWithPhenotype: {
+              totalCount: 50,
+              nextCursor: 987654,
+              strains: second50,
+            },
+          },
+        },
+      },
+      {
+        request: {
+          query: GET_STRAIN_LIST_WITH_PHENOTYPE,
+          variables: {
+            cursor: 987654,
+            limit: 50,
+            phenotype: "abolished protein phosphorylation",
+          },
+        },
+        result: {
+          data: {
+            listStrainsWithPhenotype: {
+              totalCount: 3,
+              nextCursor: 0,
+              strains: lastItems,
+            },
+          },
+        },
+      },
+    ]
+    it("should render first 50 results", async () => {
+      render(
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <BrowserRouter>
+            <PhenotypeContainer />
+          </BrowserRouter>
+        </MockedProvider>,
+      )
+      // displays loading skeleton first
+      expect(screen.getByTestId("skeleton-loader")).toBeInTheDocument()
+
+      // wait for data to load...
+      const firstRow = await screen.findByText(first50[0].label)
+      expect(firstRow).toBeInTheDocument()
+      const lastRow = await screen.findByText(first50[49].label)
+      expect(lastRow).toBeInTheDocument()
+
+      const row51 = screen.queryByText(second50[0].label)
+      expect(row51).toBeFalsy()
+
+      const listItems = await screen.findAllByRole("listitem")
+      // should have 51 list items -> 50 rows of data + list header
+      expect(listItems).toHaveLength(51)
+    })
+
+    it("should render next 50 results after scrolling", async () => {
+      window.IntersectionObserver = jest.fn((callback, options) => {
+        callback([{ isIntersecting: true }])
+        return {
+          observe: mockObserve,
+          unobserve: mockUnobserve,
+        }
+      })
+      render(
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <BrowserRouter>
+            <PhenotypeContainer />
+          </BrowserRouter>
+        </MockedProvider>,
+      )
+      // displays loading skeleton first
+      expect(screen.getByTestId("skeleton-loader")).toBeInTheDocument()
+
+      // wait for data to load...
+      const firstRow = await screen.findByText(first50[0].label)
+      expect(firstRow).toBeInTheDocument()
+      // next 50 results should be included since isIntersecting is true
+      const firstRowSecondSet = await screen.findByText(second50[0].label)
+      expect(firstRowSecondSet).toBeInTheDocument()
+      const finalItem = await screen.findByText(lastItems[2].label)
+      expect(finalItem).toBeInTheDocument()
+
+      const listItems = await screen.findAllByRole("listitem")
+      expect(listItems).toHaveLength(104) // 103 items + 1 header row
     })
   })
 
