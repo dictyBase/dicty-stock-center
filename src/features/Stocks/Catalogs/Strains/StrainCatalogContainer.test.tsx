20c259865885cffb67d82509d1ffee5511ecf16a
wildlifehexagon <crossthebreeze@gmail.com>
2020-11-03 09:59:08 -0600

    test: use testing-library for catalog containers

--- src/features/Stocks/Catalogs/Strains/StrainCatalogContainer.test.tsx
+++ src/features/Stocks/Catalogs/Strains/StrainCatalogContainer.test.tsx
@@ -1,15 +1,16 @@
 import React from "react"
-import { mount } from "enzyme"
+import { render, screen } from "@testing-library/react"
 import { MockedProvider } from "@apollo/client/testing"
-import wait from "waait"
+import { BrowserRouter } from "react-router-dom"
 import StrainCatalogContainer from "./StrainCatalogContainer"
-import StrainCatalogList from "./StrainCatalogList"
-import CatalogHeader from "features/Stocks/Catalogs/common/CatalogHeader"
-import DetailsLoader from "features/Stocks/Details/common/DetailsLoader"
-import CatalogErrorMessage from "features/Stocks/Catalogs/common/CatalogErrorMessage"
 import { GET_STRAIN_LIST } from "common/graphql/queries"
 import { CatalogProvider } from "features/Stocks/Catalogs/common/CatalogContext"
 import { CartProvider } from "features/ShoppingCart/CartStore"
+import { lastFiveStrainCatalogItems } from "./mockData"
+
+jest.mock("react-virtualized-auto-sizer", () => ({ children }: any) =>
+  children({ height: 535, width: 600 }),
+)
 
 describe("Stocks/Strains/StrainCatalogContainer", () => {
   describe("initial render", () => {
@@ -25,42 +26,41 @@ describe("Stocks/Strains/StrainCatalogContainer", () => {
         },
         result: {
           data: {
-            listStrains: {
-              totalCount: 1,
-              nextCursor: 123456,
-              strains: [
-                {
-                  id: "DBS0238532",
-                  label: "γS13",
-                  summary: "Radiation-sensitive mutant.",
-                  in_stock: true,
-                },
-              ],
-            },
+            listStrains: lastFiveStrainCatalogItems,
           },
         },
       },
     ]
-    const wrapper = mount(
-      <CartProvider>
-        <CatalogProvider>
-          <MockedProvider mocks={mocks} addTypename={false}>
-            <StrainCatalogContainer />
-          </MockedProvider>
-        </CatalogProvider>
-      </CartProvider>,
-    )
-    it("renders Loading component first", () => {
-      expect(wrapper.find(DetailsLoader)).toHaveLength(1)
-    })
-    it("renders expected components after receiving data", async () => {
-      await wait()
-      wrapper.update()
+    it("should render fetched data", async () => {
+      render(
+        <CartProvider>
+          <CatalogProvider>
+            <MockedProvider mocks={mocks} addTypename={false}>
+              <BrowserRouter>
+                <StrainCatalogContainer />
+              </BrowserRouter>
+            </MockedProvider>
+          </CatalogProvider>
+        </CartProvider>,
+      )
+      // displays loading skeleton first
+      expect(screen.getByTestId("skeleton-loader")).toBeInTheDocument()
+      // wait for data to load...
+      const firstRow = await screen.findByText(
+        lastFiveStrainCatalogItems.strains[0].label,
+      )
+      expect(firstRow).toBeInTheDocument()
+      const lastRow = await screen.findByText(
+        lastFiveStrainCatalogItems.strains[4].label,
+      )
+      expect(lastRow).toBeInTheDocument()
 
-      expect(wrapper.find(CatalogHeader)).toHaveLength(1)
-      expect(wrapper.find(StrainCatalogList)).toHaveLength(1)
+      const listItems = await screen.findAllByRole("listitem")
+      // should have 51 list items -> 50 rows of data + list header
+      expect(listItems).toHaveLength(6)
     })
   })
+
   describe("error handling", () => {
     const mocks = [
       {
@@ -75,7 +75,7 @@ describe("Stocks/Strains/StrainCatalogContainer", () => {
         result: {
           errors: [
             {
-              message: "Strains not found",
+              message: "No strains found",
               path: [],
               extensions: { code: "NotFound" },
               locations: undefined,
@@ -89,19 +89,24 @@ describe("Stocks/Strains/StrainCatalogContainer", () => {
         },
       },
     ]
-    const wrapper = mount(
-      <CartProvider>
-        <CatalogProvider>
-          <MockedProvider mocks={mocks} addTypename={false}>
-            <StrainCatalogContainer />
-          </MockedProvider>
-        </CatalogProvider>
-      </CartProvider>,
-    )
-    it("handles errors as expected", async () => {
-      await wait()
-      wrapper.update()
-      expect(wrapper.find(CatalogErrorMessage)).toHaveLength(1)
+    it("displays error message", async () => {
+      render(
+        <CartProvider>
+          <CatalogProvider>
+            <MockedProvider mocks={mocks} addTypename={false}>
+              <BrowserRouter>
+                <StrainCatalogContainer />
+              </BrowserRouter>
+            </MockedProvider>
+          </CatalogProvider>
+        </CartProvider>,
+      )
+      // displays loading skeleton first
+      expect(screen.getByTestId("skeleton-loader")).toBeInTheDocument()
+
+      // wait for error message to load...
+      const errorMsg = await screen.findByText(/No strains found/)
+      expect(errorMsg).toBeInTheDocument()
     })
   })
 })
---
+++ src/features/Stocks/Catalogs/Strains/mockData.ts
@@ -0,0 +1,215 @@
+const firstTenStrainCatalogItems = {
+  nextCursor: 1563868285000,
+  totalCount: 11,
+  strains: [
+    {
+      id: "DBS0351367",
+      label: "myoB-/[act15]:myoB(S332A)",
+      summary: "myoB with S332A substitution expressed in myoB null",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351366",
+      label: "HL206/XP99",
+      summary:
+        "heterozygote diploid tester strain; Parents: HL206 and XP99(DBS0237086)",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351365",
+      label: "HL501/X55",
+      summary: "heterozygote diploid tester strain; Parents: HL501 and X55",
+      in_stock: true,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351364",
+      label: "HL23/HL106",
+      summary:
+        "heterozygote diploid tester strain; Parents: HL23(DBS0236216) and HL106(DBS0236209)",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351363",
+      label: "HL84/XM101",
+      summary:
+        "heterozygote diploid tester strain; Parents: HL84(DBS0351330) and XM101(DBS0237065)",
+      in_stock: true,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351362",
+      label: "HL16/HL106",
+      summary:
+        "heterozygote diploid tester strain; Parents: HL16 and HL106 (DBS0236209)",
+      in_stock: true,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351361",
+      label: "HL206/NP377",
+      summary:
+        "heterozygote diploid tester strain; Parents: HL206 and NP377 (DBS0236648)",
+      in_stock: true,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351360",
+      label: "HL206/XP99",
+      summary:
+        "heterozygote diploid tester strain; Parents: HL206 and XP99(DBS0237086)",
+      in_stock: true,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351359",
+      label: "[act15]:myoB",
+      summary: "myoB overexpressed in AX3",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351358",
+      label: "HL3",
+      summary: "tester strain",
+      in_stock: true,
+      __typename: "Strain",
+    },
+  ],
+  __typename: "StrainListWithCursor",
+}
+
+const nextTenStrainCatalogItems = {
+  nextCursor: 1562662513000,
+  totalCount: 11,
+  strains: [
+    {
+      id: "DBS0351357",
+      label: "aph1-",
+      summary: "aph1 null mutant in AX2",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351356",
+      label: "ppk1-/[act15]:GFP:hsp48",
+      summary:
+        "N-terminal GFP-tagged hsp48 under control of act15 promotor expressed in ppk1 nul",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351355",
+      label: "[TRE-Pmin]:cdk1",
+      summary:
+        "cdk1 overexpressed in AX2 under control of the inducible TRE-Pmin promoter; inducible with doxycycline",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351354",
+      label: "[TRE-Pmin]:cdk1(T14A)",
+      summary:
+        "cdk1 with a point mutation (T14A) overexpressed in AX2 under control of the inducible TRE-Pmin promoter; inducible with doxycycline",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351353",
+      label: "[TRE-Pmin]:cdk1(Y15F)",
+      summary:
+        "cdk1 with a point mutation (Y15F) overexpressed in AX2 under control of the inducible TRE-Pmin promoter; inducible with doxycycline",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351352",
+      label: "[TRE-Pmin]:cdk1(T14A/Y15F)",
+      summary:
+        "cdk1 with mutations (T14A and Y15F) overexpressed in AX2 under control of the inducible TRE-Pmin promoter; inducible with doxycycline",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351351",
+      label: "racE-/[act15]:GFP:rasC(Q62L)",
+      summary: "GFP-tagged rasC with Q62L substitution expressed in racE null",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351350",
+      label: "racE-/[act15]:GFP:racE(S192D)",
+      summary: "GFP-tagged racE with S192D expressed in racE null",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351349",
+      label: "racE-/[act15]:GFP:racE(G23V)",
+      summary: "GFP-tagged racE with G23V expressed in racE null",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351348",
+      label: "racE-/[act15]:GFP:racE(G20V)",
+      summary: "GFP-tagged racE with G20V substitution expressed in racE null",
+      in_stock: false,
+      __typename: "Strain",
+    },
+  ],
+  __typename: "StrainListWithCursor",
+}
+
+const lastFiveStrainCatalogItems = {
+  nextCursor: 0,
+  totalCount: 6,
+  strains: [
+    {
+      id: "DBS0351347",
+      label: "racE-/[act15]:GFP:racE(T25N)",
+      summary: "GFP-tagged racE with T25N substitution expressed in racE null",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351346",
+      label: "tert-",
+      summary: "tert null mutant in AX2",
+      in_stock: false,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351345",
+      label: "HM1524",
+      summary: "fails to form macrocysts in sexual crosses",
+      in_stock: true,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351344",
+      label: "far1-",
+      summary: "",
+      in_stock: true,
+      __typename: "Strain",
+    },
+    {
+      id: "DBS0351342",
+      label: "HL12",
+      summary: "tester strain",
+      in_stock: false,
+      __typename: "Strain",
+    },
+  ],
+  __typename: "StrainListWithCursor",
+}
+
+export {
+  firstTenStrainCatalogItems,
+  nextTenStrainCatalogItems,
+  lastFiveStrainCatalogItems,
+}
