ef23a4abbe31e7b5ccb2d0736f458a806bb37fb3
wildlifehexagon <crossthebreeze@gmail.com>
2020-11-03 15:41:10 -0600

    test: add phenotype search snapshot, remove broken catalog shots

--- cypress/integration/snapshots_spec.ts
+++ cypress/integration/snapshots_spec.ts
@@ -1,42 +1,51 @@
+const screenWidths = [375, 768, 1280]
+
 describe("DSC snapshots", () => {
   it("visits the DSC homepage", () => {
     cy.visit("/")
     cy.contains("Strain & Plasmid Availability")
-    cy.percySnapshot("DSC homepage", { widths: [360, 768, 1000, 1300] })
+    cy.percySnapshot("DSC homepage", { widths: screenWidths })
   })
   it("visits the order information page", () => {
     cy.visit("/information/order")
     cy.contains("Ordering Information")
     cy.percySnapshot("order information page", {
-      widths: [360, 768, 1000, 1300],
+      widths: screenWidths,
     })
   })
-  it("visits the strain catalog", () => {
-    cy.visit("/strains")
-    cy.contains("HL206")
-    cy.percySnapshot("strain catalog", {
-      widths: [360, 768, 1000, 1300],
-    })
-  })
+  // it("visits the strain catalog", () => {
+  //   cy.visit("/strains")
+  //   cy.contains("HL206")
+  //   cy.percySnapshot("strain catalog", {
+  //     widths: screenWidths,
+  //   })
+  // })
   it("visits a strain details page", () => {
     cy.visit("/strains/DBS0351367")
     cy.contains("Extrachromosomal")
     cy.percySnapshot("strain details", {
-      widths: [360, 768, 1000, 1300],
+      widths: screenWidths,
     })
   })
-  it("visits the plasmid catalog", () => {
-    cy.visit("/plasmids")
-    cy.contains("pSigK")
-    cy.percySnapshot("plasmid catalog", {
-      widths: [360, 768, 1000, 1300],
-    })
-  })
+  // it("visits the plasmid catalog", () => {
+  //   cy.visit("/plasmids")
+  //   cy.contains("pSigK")
+  //   cy.percySnapshot("plasmid catalog", {
+  //     widths: screenWidths,
+  //   })
+  // })
   it("visits a plasmid details page", () => {
     cy.visit("/plasmids/DBP0001070")
     cy.contains("overexpression")
     cy.percySnapshot("plasmid details", {
-      widths: [360, 768, 1000, 1300],
+      widths: screenWidths,
+    })
+  })
+  it("visits a phenotype search results page", () => {
+    cy.visit("/phenotypes/abolished+protein+phosphorylation")
+    cy.contains("gskA-")
+    cy.percySnapshot("phenotype search results", {
+      widths: screenWidths,
     })
   })
 })
