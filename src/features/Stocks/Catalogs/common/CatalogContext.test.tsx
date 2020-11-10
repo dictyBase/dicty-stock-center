d33d750b72f827dc508e080f6c41c1528431517e
wildlifehexagon <crossthebreeze@gmail.com>
2020-11-10 13:57:06 -0600

    test: add tests for all catalog actions

--- src/features/Stocks/Catalogs/common/CatalogContext.test.tsx
+++ src/features/Stocks/Catalogs/common/CatalogContext.test.tsx
@@ -1,60 +1,121 @@
-// import React from "react"
-// import { renderHook } from "@testing-library/react-hooks"
 import { catalogReducer, CatalogActionType } from "./CatalogContext"
+import { GET_STRAIN_LIST } from "common/graphql/queries"
 
 describe("Stocks/Catalogs/common/CatalogContext", () => {
   describe("catalogReducer", () => {
     const initialState = {
-      queryVariables: { cursor: 0, filter: "" },
+      queryVariables: { cursor: 0, limit: 10, filter: "" },
       checkedItems: [],
+      leftDropdownValue: "all",
+      searchValue: "",
+      helpDialogOpen: false,
+      query: GET_STRAIN_LIST,
+      searchBoxDropdownValue: "label",
     }
     it("returns initial state if no changes", () => {
-      // @ts-ignore
-      expect(catalogReducer(initialState, {})).toEqual(initialState)
+      expect(
+        catalogReducer(initialState, {
+          type: CatalogActionType.SET_HELP_DIALOG_OPEN,
+          payload: false,
+        }),
+      ).toEqual(initialState)
     })
 
     it("should handle SET_QUERY_VARIABLES", () => {
-      const action = {
-        type: CatalogActionType.SET_QUERY_VARIABLES,
-        payload: {
-          cursor: 0,
-          filter: "",
-          limit: 10,
-        },
+      const type = CatalogActionType.SET_QUERY_VARIABLES
+      const payload = {
+        cursor: 0,
+        filter: "label~=GWDI",
+        limit: 10,
       }
       const expectedState = {
-        queryVariables: action.payload,
-        checkedItems: [],
+        ...initialState,
+        queryVariables: payload,
       }
-      // @ts-ignore
-      expect(catalogReducer(initialState, action)).toEqual(expectedState)
+      expect(
+        catalogReducer(initialState, {
+          type,
+          payload,
+        }),
+      ).toEqual(expectedState)
     })
 
     it("should handle SET_CHECKED_ITEMS", () => {
-      const action = {
-        type: CatalogActionType.SET_CHECKED_ITEMS,
-        payload: [
-          {
-            id: "DBS123456",
-            label: "test1",
-            summary: "this is a test summary",
-          },
-        ],
-      }
+      const type = CatalogActionType.SET_CHECKED_ITEMS
+      const payload = [
+        {
+          id: "DBS123456",
+          label: "test1",
+          summary: "this is a test summary",
+        },
+      ]
       const expectedState = {
-        queryVariables: { cursor: 0, filter: "" },
-        checkedItems: action.payload,
+        ...initialState,
+        checkedItems: payload,
       }
-      // @ts-ignore
-      expect(catalogReducer(initialState, action)).toEqual(expectedState)
+      expect(catalogReducer(initialState, { type, payload })).toEqual(
+        expectedState,
+      )
+    })
+
+    it("should handle SET_SEARCHBOX_DROPDOWN_VALUE", () => {
+      const type = CatalogActionType.SET_SEARCHBOX_DROPDOWN_VALUE
+      const payload = "label"
+      const expectedState = {
+        ...initialState,
+        searchBoxDropdownValue: payload,
+      }
+      expect(
+        catalogReducer(initialState, {
+          type,
+          payload,
+        }),
+      ).toEqual(expectedState)
+    })
+
+    it("should handle SET_SEARCH_VALUE", () => {
+      const type = CatalogActionType.SET_SEARCH_VALUE
+      const payload = "tester"
+      const expectedState = {
+        ...initialState,
+        searchValue: payload,
+      }
+      expect(
+        catalogReducer(initialState, {
+          type,
+          payload,
+        }),
+      ).toEqual(expectedState)
+    })
+
+    it("should handle SET_LEFT_DROPDOWN_VALUE", () => {
+      const type = CatalogActionType.SET_LEFT_DROPDOWN_VALUE
+      const payload = "gwdi"
+      const expectedState = {
+        ...initialState,
+        leftDropdownValue: payload,
+      }
+      expect(
+        catalogReducer(initialState, {
+          type,
+          payload,
+        }),
+      ).toEqual(expectedState)
+    })
+
+    it("should handle SET_HELP_DIALOG_OPEN", () => {
+      const type = CatalogActionType.SET_HELP_DIALOG_OPEN
+      const payload = true
+      const expectedState = {
+        ...initialState,
+        helpDialogOpen: true,
+      }
+      expect(
+        catalogReducer(initialState, {
+          type,
+          payload,
+        }),
+      ).toEqual(expectedState)
     })
   })
 })
-
-// describe("useCatalogStore", () => {
-//   it("should throw error if not used in Provider", () => {
-//     const wrapper = ({ children }) => <div>{children}</div>
-//     const { result } = renderHook(() => useCatalogStore(), { wrapper })
-//     expect(() => result.current).toThrow()
-//   })
-// })
