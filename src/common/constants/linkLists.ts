34013abf04191f86b7fe50c0d5baa1b3bdb1243a
wildlifehexagon <crossthebreeze@gmail.com>
2020-11-11 12:17:26 -0600

    fix: use correct link for bacterial strains

--- src/common/constants/linkLists.ts
+++ src/common/constants/linkLists.ts
@@ -65,7 +65,7 @@ const materialsLinks = [
   },
   {
     name: "Bacterial Strains",
-    to: "/strains?search=bacterial",
+    to: "/strains?filter=bacterial",
     routerAware: true,
     newWindow: false,
   },
