fbffa1cbc840f032624cf6651851cd1f807ffebf
wildlifehexagon <crossthebreeze@gmail.com>
2020-11-13 10:06:06 -0600

    test: fix broken test

--- src/features/Stocks/Catalogs/common/CatalogHeader.test.tsx
+++ src/features/Stocks/Catalogs/common/CatalogHeader.test.tsx
@@ -10,10 +10,15 @@ describe("Stocks/CatalogPageItems/CatalogHeader", () => {
   const wrapper = mount(<CatalogHeader {...props} />)
   describe("initial render", () => {
     it("always renders one Typography component", () => {
-      expect(wrapper.find(Typography)).toHaveLength(1)
+      expect(wrapper.find(Typography)).toHaveLength(2)
     })
     it("matches title prop value", () => {
       expect(wrapper.prop("title")).toEqual(props.title)
+    })
+    it("has expected text for ordering", () => {
+      expect(wrapper.text()).toContain(
+        "PLEASE DO NOT ORDER FROM THIS TEST SITE. THIS IS FOR INTERNAL TESTING ONLY. THANKS!",
+      )
     })
   })
 })
