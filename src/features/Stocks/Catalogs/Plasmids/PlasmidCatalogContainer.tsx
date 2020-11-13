8eb1e94e12a9d3e699ed1a22de19fdad295f93ab
wildlifehexagon <crossthebreeze@gmail.com>
2020-11-13 13:46:25 -0600

    refactor: move catalog display logic into separate component

--- src/features/Stocks/Catalogs/Plasmids/PlasmidCatalogContainer.tsx
+++ src/features/Stocks/Catalogs/Plasmids/PlasmidCatalogContainer.tsx
@@ -1,13 +1,9 @@
 import React from "react"
 import { useQuery } from "@apollo/client"
-import Grid from "@material-ui/core/Grid"
-import CircularProgress from "@material-ui/core/CircularProgress"
-import CatalogHeader from "features/Stocks/Catalogs/common/CatalogHeader"
+import CatalogDisplay from "features/Stocks/Catalogs/common/CatalogDisplay"
 import CatalogErrorMessage from "features/Stocks/Catalogs/common/CatalogErrorMessage"
-import CatalogAppBar from "features/Stocks/Catalogs/common/CatalogAppBar"
 import PlasmidCatalogList from "./PlasmidCatalogList"
 import useCatalogStore from "features/Stocks/Catalogs/context/useCatalogStore"
-import useStyles from "features/Stocks/Catalogs/styles"
 
 const leftDropdownItems = [
   {
@@ -57,7 +53,6 @@ const PlasmidCatalogContainer = ({ filter }: Props) => {
   const { loading, error, data, fetchMore } = useQuery(query, {
     variables: queryVariables,
   })
-  const classes = useStyles()
 
   let content = <div />
 
@@ -92,25 +87,13 @@ const PlasmidCatalogContainer = ({ filter }: Props) => {
   }
 
   return (
-    <Grid container spacing={0} className={classes.layout}>
-      <Grid item xs={12}>
-        <CatalogHeader title="Plasmid Catalog" />
-      </Grid>
-      <Grid item xs={12}>
-        <CatalogAppBar
-          leftDropdownItems={leftDropdownItems}
-          rightDropdownItems={rightDropdownItems}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        {loading && (
-          <div className={classes.spinner}>
-            <CircularProgress data-testid="catalog-spinner" size={100} />
-          </div>
-        )}
-        {content}
-      </Grid>
-    </Grid>
+    <CatalogDisplay
+      stockType="Plasmid"
+      leftDropdownItems={leftDropdownItems}
+      rightDropdownItems={rightDropdownItems}
+      loading={loading}>
+      {content}
+    </CatalogDisplay>
   )
 }
 
