b4b897ae4e49bf4367fd7ede22eb9bb93be866f0
wildlifehexagon <crossthebreeze@gmail.com>
2020-11-20 16:18:11 -0600

    test: convert GenesDisplay to testing-library

--- src/features/Stocks/Details/common/GenesDisplay.test.tsx
+++ src/features/Stocks/Details/common/GenesDisplay.test.tsx
@@ -1,21 +1,25 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { render, screen } from "@testing-library/react"
 import GenesDisplay, { Gene } from "./GenesDisplay"
-import LinkTag from "common/components/LinkTag"
 
 describe("Stocks/Details/common/GenesDisplay", () => {
   describe("initial render with genes", () => {
     const genes = [{ name: "sadA" }, { name: "gflB" }]
-    const wrapper = shallow(<GenesDisplay genes={genes} />)
     it("renders one LinkTag for every gene", () => {
-      expect(wrapper.find(LinkTag)).toHaveLength(2)
+      render(<GenesDisplay genes={genes} />)
+      const links = screen.getAllByRole("link")
+      expect(links).toHaveLength(2)
+      expect(links[0]).toHaveTextContent(/sadA/)
+      expect(links[1]).toHaveTextContent(/gflB/)
     })
   })
+
   describe("initial render with empty gene array", () => {
     const genes = [] as Gene[]
-    const wrapper = shallow(<GenesDisplay genes={genes} />)
     it("renders no LinkTags if gene list is empty", () => {
-      expect(wrapper.find(LinkTag)).toHaveLength(0)
+      render(<GenesDisplay genes={genes} />)
+      const links = screen.queryAllByRole("link")
+      expect(links).toHaveLength(0)
     })
   })
 })
