97406e26fe10a2b6d397a8345454e4d0b75e6c0f
wildlifehexagon <crossthebreeze@gmail.com>
2020-11-24 16:23:57 -0600

    refactor: add aria-label

--- src/app/layout/HeaderRow.tsx
+++ src/app/layout/HeaderRow.tsx
@@ -1,8 +1,6 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import CartIcon from "features/ShoppingCart/CartIcon"
 
 const useStyles = makeStyles({
