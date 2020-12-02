aed7d169838b393b66ebb23810984806b886d9be
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-02 09:00:56 -0600

    refactor: add types for mock data

--- src/features/Stocks/Details/Plasmids/mockPlasmidData.ts
+++ src/features/Stocks/Details/Plasmids/mockPlasmidData.ts
@@ -1,4 +1,7 @@
-const data = {
+import { PlasmidDetails } from "features/Stocks/Details/types/props"
+
+const availablePlasmid = {
+  type: "plasmid",
   summary: "Knockout plasmid for acgA",
   id: "DBP0000385",
   name: "pACG-KO",
@@ -7,10 +10,10 @@ const data = {
   genes: [{ name: "acgA" }],
   image_map: "http://dictybase.org/data/plasmid/images/385.jpg",
   publications: [{ id: "17267449", doi: "10.1242/dev.02775" }],
-  in_stock: true,
   sequence: "",
   keywords: [],
   genbank_accession: "",
-}
+  in_stock: true,
+} as PlasmidDetails
 
-export { data }
+export { availablePlasmid }
