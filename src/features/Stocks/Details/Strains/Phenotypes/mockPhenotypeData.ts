fba61b7895fb26369db24c6e3fa062fe022100f1
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-02 15:49:52 -0600

    test: fix mock data

--- src/features/Stocks/Details/Strains/Phenotypes/mockPhenotypeData.ts
+++ src/features/Stocks/Details/Strains/Phenotypes/mockPhenotypeData.ts
@@ -5,8 +5,27 @@ export const data = [
       "not localized in a polarized fashion to the stalk side of the tip cells",
     assay: "confocal microscopy",
     publication: {
-      id: "21393547",
-      doi: "10.1126/science.1199633",
+      id: "11084033",
+      pub_date: "2000-11-17T00:00:00.000Z",
+      title:
+        "Myosin I phosphorylation is increased by chemotactic stimulation.",
+      journal: "The Journal of biological chemistry",
+      volume: "276",
+      pages: "5235-5239",
+      authors: [
+        {
+          last_name: "Gliksman",
+        },
+        {
+          last_name: "Santoyo",
+        },
+        {
+          last_name: "Novak",
+        },
+        {
+          last_name: "Titus",
+        },
+      ],
     },
   },
   {
@@ -15,16 +34,54 @@ export const data = [
     assay: "Northern blot",
     environment: "during development",
     publication: {
-      id: "11130075",
-      doi: "10.1038/35047099",
+      id: "11084033",
+      pub_date: "2000-11-17T00:00:00.000Z",
+      title:
+        "Myosin I phosphorylation is increased by chemotactic stimulation.",
+      journal: "The Journal of biological chemistry",
+      volume: "276",
+      pages: "5235-5239",
+      authors: [
+        {
+          last_name: "Gliksman",
+        },
+        {
+          last_name: "Santoyo",
+        },
+        {
+          last_name: "Novak",
+        },
+        {
+          last_name: "Titus",
+        },
+      ],
     },
   },
   {
     phenotype: "decreased sporulation",
     note: "",
     publication: {
-      id: "11130075",
-      doi: "10.1038/35047099",
+      id: "11084033",
+      pub_date: "2000-11-17T00:00:00.000Z",
+      title:
+        "Myosin I phosphorylation is increased by chemotactic stimulation.",
+      journal: "The Journal of biological chemistry",
+      volume: "276",
+      pages: "5235-5239",
+      authors: [
+        {
+          last_name: "Gliksman",
+        },
+        {
+          last_name: "Santoyo",
+        },
+        {
+          last_name: "Novak",
+        },
+        {
+          last_name: "Titus",
+        },
+      ],
     },
   },
 ]
