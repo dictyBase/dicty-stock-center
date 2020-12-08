06558084c58ff069b08e06a8c246d6ceecf44b02
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-07 21:17:28 -0600

    refactor: remove unused styles and center align header row

--- src/app/layout/HeaderRow.tsx
+++ src/app/layout/HeaderRow.tsx
@@ -1,34 +1,22 @@
 import React from "react"
-import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 import Breadcrumbs from "common/components/Breadcrumbs"
 import CartIcon from "features/ShoppingCart/CartIcon"
-
-const useStyles = makeStyles({
-  container: {
-    // margin: "auto",
-    // width: "95%",
-  },
-})
 
 /**
  * This is the cart component that displays between the Navbar and body content on every page.
  * It has a cart icon with the current number of added items next to it.
  */
 
-const HeaderRow = () => {
-  const classes = useStyles()
-
-  return (
-    <Grid container justify="space-between" className={classes.container}>
-      <Grid item>
-        <Breadcrumbs />
-      </Grid>
-      <Grid item>
-        <CartIcon />
-      </Grid>
+const HeaderRow = () => (
+  <Grid container justify="space-between" alignItems="center">
+    <Grid item>
+      <Breadcrumbs />
     </Grid>
-  )
-}
+    <Grid item>
+      <CartIcon />
+    </Grid>
+  </Grid>
+)
 
 export default HeaderRow
