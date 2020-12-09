333aea567aaf927d2ba405e12bcef0bb6b42d6c9
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-09 08:59:51 -0600

    style: add comment describing function

--- src/common/utils/stringCapitalizations.ts
+++ src/common/utils/stringCapitalizations.ts
@@ -4,6 +4,9 @@
 const capitalizeFirstCharacter = (str: string) =>
   str.charAt(0).toUpperCase() + str.slice(1)
 
+/**
+ * capitalizeEveryWordInString takes a string with spaces (i.e. "other stock centers" and capitalizes each word.
+ */
 const capitalizeEveryWordInString = (str: string) =>
   str
     .split(" ")
