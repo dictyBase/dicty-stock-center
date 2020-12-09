e8a3636a76a8e14805bc20fa0bf5ee509bcaaa48
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-09 12:43:18 -0600

    refactor: add test id for panel loader

--- src/features/Home/PanelLoader.tsx
+++ src/features/Home/PanelLoader.tsx
@@ -3,6 +3,7 @@ import Grid from "@material-ui/core/Grid"
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
 
 type Props = {
+  /** Number of lines to show in skeleton */
   skeletonCount: number
 }
 
@@ -12,7 +13,7 @@ type Props = {
  */
 
 const PanelLoader = ({ skeletonCount }: Props) => (
-  <Grid container justify="center">
+  <Grid container justify="center" data-testid="panel-loader">
     <Grid item xs={12}>
       <SkeletonTheme color="#D3D3D3	" highlightColor="#DCDCDC">
         <Skeleton count={skeletonCount} />
