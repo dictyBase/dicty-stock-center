bd7bad0bcbf53879ac1f591b9a226e8d212854b2
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-15 15:07:35 -0600

    test: add tests for add page button

--- src/features/EditablePages/Information.test.tsx
+++ src/features/EditablePages/Information.test.tsx
@@ -2,16 +2,45 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import Information, { informationLinks } from "./Information"
 import { BrowserRouter as Router } from "react-router-dom"
+import { MockAuthProvider } from "common/utils/testing"
 
 describe("features/EditablePages/Information", () => {
   test("displays list of links", () => {
     render(
-      <Router>
-        <Information />
-      </Router>,
+      <MockAuthProvider mocks={[]} validToken>
+        <Router>
+          <Information />
+        </Router>
+      </MockAuthProvider>,
     )
     informationLinks.forEach((item) =>
-      expect(screen.getByText(item.name)).toBeInTheDocument(),
+      expect(screen.getByRole("link", { name: item.name })).toBeInTheDocument(),
     )
+  })
+
+  test("displays add page button for authorized user", () => {
+    render(
+      <MockAuthProvider mocks={[]} validToken>
+        <Router>
+          <Information />
+        </Router>
+      </MockAuthProvider>,
+    )
+    expect(
+      screen.getByRole("button", { name: /Add new page/ }),
+    ).toBeInTheDocument()
+  })
+
+  test("does not display add page button for unauthorized users", () => {
+    render(
+      <MockAuthProvider mocks={[]} validToken={false}>
+        <Router>
+          <Information />
+        </Router>
+      </MockAuthProvider>,
+    )
+    expect(
+      screen.queryByRole("button", { name: /Add new page/ }),
+    ).not.toBeInTheDocument()
   })
 })
--- src/features/Errors/NotFoundError.test.tsx
+++ src/features/Errors/NotFoundError.test.tsx
@@ -2,48 +2,17 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import { BrowserRouter } from "react-router-dom"
 import NotFoundError from "./NotFoundError"
-import { MockAuthProvider } from "common/utils/testing"
 
-// https://stackoverflow.com/questions/58117890/how-to-test-components-using-new-react-router-hooks
-jest.mock("react-router-dom", () => {
-  const originalModule = jest.requireActual("react-router-dom")
-  return {
-    ...originalModule,
-    useParams: () => ({
-      name: "order",
-    }),
-    useLocation: () => ({
-      pathname: "/information/order",
-    }),
-  }
-})
+describe("features/Errors/NotFoundError", () => {
+  test("displays not found header", () => {
+    render(
+      <BrowserRouter>
+        <NotFoundError error="Page Not Found" />
+      </BrowserRouter>,
+    )
 
-describe("Errors/NotFoundError", () => {
-  describe("initial render", () => {
-    it("renders add page button when authorized", () => {
-      render(
-        <MockAuthProvider mocks={[]} validToken>
-          <BrowserRouter>
-            <NotFoundError error="Page Not Found" />
-          </BrowserRouter>
-        </MockAuthProvider>,
-      )
-      expect(screen.getByText(/Page Not Found/)).toBeInTheDocument()
-      expect(screen.getByText(/Add a page to this route/)).toBeInTheDocument()
-    })
-
-    it("does not show add page button when unauthorized", () => {
-      render(
-        <MockAuthProvider mocks={[]} validToken={false}>
-          <BrowserRouter>
-            <NotFoundError error="Page Not Found" />
-          </BrowserRouter>
-        </MockAuthProvider>,
-      )
-      expect(screen.getByText(/Page Not Found/)).toBeInTheDocument()
-      expect(
-        screen.queryByText(/Add a page to this route/),
-      ).not.toBeInTheDocument()
-    })
+    expect(
+      screen.getByRole("heading", { name: "Not Found" }),
+    ).toBeInTheDocument()
   })
 })
