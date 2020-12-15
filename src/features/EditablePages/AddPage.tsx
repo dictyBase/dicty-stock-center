9557147417a4763682fe5f6ea7c83d44f29ec835
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-15 14:23:17 -0600

    feat: add option to create new route using textfield, fixes #217

--- src/features/EditablePages/AddPage.tsx
+++ src/features/EditablePages/AddPage.tsx
@@ -2,40 +2,52 @@ import React from "react"
 import { useMutation } from "@apollo/client"
 import { useHistory } from "react-router-dom"
 import Grid from "@material-ui/core/Grid"
-import { makeStyles } from "@material-ui/core/styles"
+import TextField from "@material-ui/core/TextField"
+import {
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme,
+} from "@material-ui/core/styles"
 import { PageEditor } from "dicty-components-page-editor"
 import ErrorNotification from "features/Authentication/ErrorNotification"
 import { useAuthStore } from "features/Authentication/AuthStore"
 import useAuthorization from "common/hooks/useAuthorization"
 import { CREATE_CONTENT } from "common/graphql/mutations"
 import NAMESPACE from "common/constants/namespace"
+import { theme } from "app/layout/AppProviders"
 
 const useStyles = makeStyles(() => ({
   banner: {
     minHeight: "45px",
     textAlign: "center",
-    padding: "40px 20px 20px 20px",
     marginBottom: "20px",
+  },
+  route: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
 }))
 
+const newTheme = createMuiTheme({
+  ...theme,
+  overrides: {
+    MuiOutlinedInput: {
+      input: {
+        padding: "7px",
+      },
+    },
+  },
+})
+
 const error =
   "Your login token is expired. Please log out and then log back in to regain full user access."
-
-type Props = {
-  location: {
-    state: {
-      name: string
-      url: string
-    }
-  }
-}
 
 /**
  * This is the view component so an authorized user can add a new page.
  */
 
-const AddPage = ({ location }: Props) => {
+const AddPage = () => {
   const [{ token }] = useAuthStore()
   const { user, canEditPages, verifiedToken } = useAuthorization()
   const history = useHistory()
@@ -47,14 +59,13 @@ const AddPage = ({ location }: Props) => {
       },
     },
   })
-
-  const prevURL = location.state.url
+  const [textValue, setTextValue] = React.useState("")
 
   const onSave = (value: any) => {
     createContent({
       variables: {
         input: {
-          name: location.state.name,
+          name: textValue,
           created_by: user.id,
           content: JSON.stringify(value.toJSON()),
           namespace: NAMESPACE,
@@ -62,30 +73,38 @@ const AddPage = ({ location }: Props) => {
       },
     })
     setTimeout(() => {
-      history.push(prevURL)
+      history.push(`/information/${textValue}`)
     }, 800)
   }
 
   const onCancel = () => {
-    history.push(prevURL)
+    history.push("/information")
   }
 
   return (
-    <div>
+    <ThemeProvider theme={newTheme}>
       {canEditPages && !verifiedToken && <ErrorNotification error={error} />}
       <Grid container wrap="wrap" justify="center">
         <Grid item xs={12}>
           <div className={classes.banner}>
             <h2>Add Editable Page for Route:</h2>
-            <h3>{location.state.url}</h3>
+            <h3 className={classes.route}>
+              /information/
+              <TextField
+                id="add-page-route"
+                variant="outlined"
+                value={textValue}
+                onChange={(event) => setTextValue(event.target.value)}
+              />
+            </h3>
           </div>
         </Grid>
         <br />
-        <Grid item xs={9}>
+        <Grid item xs={12}>
           <PageEditor onCancel={onCancel} onSave={onSave} newPage={true} />
         </Grid>
       </Grid>
-    </div>
+    </ThemeProvider>
   )
 }
 
