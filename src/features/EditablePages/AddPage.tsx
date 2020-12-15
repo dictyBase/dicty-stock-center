2fcdb85b49a7b853c2e177ae83425b7a630d29a2
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-15 14:54:08 -0600

    feat: add notes for route requirements

--- src/features/EditablePages/AddPage.tsx
+++ src/features/EditablePages/AddPage.tsx
@@ -1,13 +1,14 @@
 import React from "react"
 import { useMutation } from "@apollo/client"
 import { useHistory } from "react-router-dom"
-import Grid from "@material-ui/core/Grid"
-import TextField from "@material-ui/core/TextField"
 import {
   makeStyles,
   ThemeProvider,
   createMuiTheme,
 } from "@material-ui/core/styles"
+import Grid from "@material-ui/core/Grid"
+import TextField from "@material-ui/core/TextField"
+import Typography from "@material-ui/core/Typography"
 import { PageEditor } from "dicty-components-page-editor"
 import ErrorNotification from "features/Authentication/ErrorNotification"
 import { useAuthStore } from "features/Authentication/AuthStore"
@@ -63,8 +64,13 @@ const AddPage = () => {
     },
   })
   const [textValue, setTextValue] = React.useState("")
+  const [textValueError, setTextValueError] = React.useState(false)
 
   const onSave = (value: any) => {
+    if (textValue === "") {
+      setTextValueError(true)
+      return
+    }
     createContent({
       variables: {
         input: {
@@ -84,6 +90,13 @@ const AddPage = () => {
     history.push("/information")
   }
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTextValue(filterText(event.target.value))
+    if (textValue !== "") {
+      setTextValueError(false)
+    }
+  }
+
   return (
     <ThemeProvider theme={newTheme}>
       {canEditPages && !verifiedToken && <ErrorNotification error={error} />}
@@ -98,11 +111,20 @@ const AddPage = () => {
                 variant="outlined"
                 value={textValue}
                 autoFocus
-                onChange={(event) =>
-                  setTextValue(filterText(event.target.value))
-                }
+                onChange={handleChange}
               />
             </h3>
+            <Typography variant="body2" color="inherit">
+              <em>
+                Only lowercase letters, numbers and hyphens are allowed for
+                routes
+              </em>
+            </Typography>
+            {textValueError && (
+              <Typography variant="body1" color="error">
+                Please enter a route before saving
+              </Typography>
+            )}
           </div>
         </Grid>
         <br />
