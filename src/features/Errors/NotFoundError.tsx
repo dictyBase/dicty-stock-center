921bba2f8fde7a21020507614f0ee88a850d7b11
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-15 14:13:40 -0600

    refactor: remove add page button from not found page

--- src/features/Errors/NotFoundError.tsx
+++ src/features/Errors/NotFoundError.tsx
@@ -1,9 +1,6 @@
 import React from "react"
-import { useLocation, useParams } from "react-router-dom"
 import Grid from "@material-ui/core/Grid"
 import { Link } from "react-router-dom"
-import AddPageButton from "features/EditablePages/AddPageButton"
-import useAuthorization from "common/hooks/useAuthorization"
 import sadDicty from "common/assets/sad-dicty.png"
 import useStyles from "./errorStyles"
 
@@ -24,12 +21,7 @@ type Props = {
  */
 
 const NotFoundError = ({ error }: Props) => {
-  const { name, subname } = useParams<Params>()
-  const location = useLocation()
-  const { canEditPages, verifiedToken } = useAuthorization()
   const classes = useStyles()
-
-  const authorizedUser = canEditPages && verifiedToken
 
   return (
     <Grid container className={classes.mainGrid} justify="center">
@@ -44,13 +36,6 @@ const NotFoundError = ({ error }: Props) => {
             Try a different link or navigate back to the{" "}
             <Link to={"/"}>DSC homepage</Link>.
           </p>
-          {authorizedUser && location.pathname.includes("information") && (
-            <AddPageButton
-              name={name}
-              subname={subname}
-              url={location.pathname}
-            />
-          )}
         </div>
       </Grid>
     </Grid>
