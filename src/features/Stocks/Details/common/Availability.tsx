f149503abf7d2560f72b04384fe3700030b55a9f
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-15 09:22:55 -0600

    feat: use alert component for unavailable status

--- src/features/Stocks/Details/common/Availability.tsx
+++ src/features/Stocks/Details/common/Availability.tsx
@@ -1,23 +1,41 @@
 import React from "react"
-import Typography from "@material-ui/core/Typography"
+import { makeStyles } from "@material-ui/core/styles"
+import Alert from "@material-ui/lab/Alert"
 import AvailableDisplay from "./AvailableDisplay"
 import { CartItem } from "common/types"
+
+const useStyles = makeStyles((theme) => ({
+  message: {
+    padding: "0px",
+  },
+}))
 
 type Props = {
   cartData: CartItem
   inStock: boolean
 }
 
-const Availability = ({ cartData, inStock }: Props) => (
-  <React.Fragment>
-    {inStock ? (
-      <AvailableDisplay cartData={cartData} />
-    ) : (
-      <Typography variant="body2" color="error">
-        Currently unavailable at the DSC
-      </Typography>
-    )}
-  </React.Fragment>
-)
+/**
+ * Availability handles the display to indicate a stock's current
+ * inventory status.
+ */
 
+const Availability = ({ cartData, inStock }: Props) => {
+  const classes = useStyles()
+
+  return (
+    <React.Fragment>
+      {inStock ? (
+        <AvailableDisplay cartData={cartData} />
+      ) : (
+        <Alert
+          classes={{ message: classes.message }}
+          icon={false}
+          severity="error">
+          Currently unavailable at the DSC
+        </Alert>
+      )}
+    </React.Fragment>
+  )
+}
 export default Availability
