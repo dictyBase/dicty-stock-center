7a15888ac475dc3f66cd8c8d35df0bdda19cf805
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-16 16:03:17 -0600

    test: use more appropriate methods

--- src/common/utils/itemIsInCart.test.ts
+++ src/common/utils/itemIsInCart.test.ts
@@ -3,9 +3,9 @@ import itemIsInCart from "./itemIsInCart"
 describe("utils/itemIsInCart", () => {
   const addedItems = [{ id: "sein123", name: "jerry", summary: "comedian" }]
   it("should return true for matching item", () => {
-    expect(itemIsInCart(addedItems, "sein123")).toBe(true)
+    expect(itemIsInCart(addedItems, "sein123")).toBeTruthy()
   })
   it("should return false for missing item", () => {
-    expect(itemIsInCart(addedItems, "kramer")).toBe(false)
+    expect(itemIsInCart(addedItems, "kramer")).toBeFalsy()
   })
 })
