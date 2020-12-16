542e2ac8fb8e1e85f858d8a55377ec9822d9915b
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-16 15:24:52 -0600

    chore: remove jest-enzyme and sinon

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom/extend-expect"
 import { configure } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
-import "jest-enzyme"
 import "core-js"
 
 let globalAny = global as any
