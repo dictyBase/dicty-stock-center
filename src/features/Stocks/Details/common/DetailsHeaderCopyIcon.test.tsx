225eee2c89bd97d9f0bf0d1b560fe9e8c7c839e9
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-16 11:31:35 -0600

    test: convert copy icon test from enzyme

--- src/features/Stocks/Details/common/DetailsHeaderCopyIcon.test.tsx
+++ src/features/Stocks/Details/common/DetailsHeaderCopyIcon.test.tsx
@@ -1,33 +1,31 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import DetailsHeaderCopyIcon from "./DetailsHeaderCopyIcon"
-import IconButton from "@material-ui/core/IconButton"
-import DetailsHeaderSnackbar from "./DetailsHeaderSnackbar"
 
 const globalAny = global as any
 
-describe("Stocks/Details/common/DetailsHeaderCopyIcon", () => {
+describe("features/Stocks/Details/common/DetailsHeaderCopyIcon", () => {
   const props = {
     id: "DBS0351367",
   }
-  const wrapper = shallow(<DetailsHeaderCopyIcon {...props} />)
   describe("initial render", () => {
-    it("always renders initial components", () => {
-      expect(wrapper.find(IconButton)).toHaveLength(1)
-      // should not display snackbar initially
-      expect(wrapper.find(DetailsHeaderSnackbar)).toHaveLength(0)
+    it("renders copy icon", () => {
+      render(<DetailsHeaderCopyIcon {...props} />)
+      const button = screen.getByRole("button", { name: "copy icon" })
+      expect(button).toBeInTheDocument()
     })
   })
 
   describe("button clicking", () => {
-    // set up mocks
     globalAny.navigator.clipboard = {
       writeText: jest.fn(() => Promise.resolve()),
     }
     globalAny.window.setTimeout = jest.fn()
     it("should write to clipboard on click", () => {
-      wrapper.find(IconButton).simulate("click")
-      wrapper.update()
+      render(<DetailsHeaderCopyIcon {...props} />)
+      const button = screen.getByRole("button", { name: "copy icon" })
+      userEvent.click(button)
       expect(globalAny.navigator.clipboard.writeText).toHaveBeenCalled()
       expect(globalAny.window.setTimeout).toHaveBeenCalled()
     })
