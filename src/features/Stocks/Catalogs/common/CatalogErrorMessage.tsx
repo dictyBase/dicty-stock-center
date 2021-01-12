3675afcb47b871c537f32679fe2d3618e7b60f98
wildlifehexagon <crossthebreeze@gmail.com>
2021-01-12 16:09:19 -0600

    fix: fix import issues

--- src/features/Stocks/Catalogs/common/CatalogErrorMessage.tsx
+++ src/features/Stocks/Catalogs/common/CatalogErrorMessage.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import NotFoundError from "./NotFoundError"
 import OtherError from "./OtherError"
-import { ApolloError } from "apollo-client"
+import { ApolloError } from "@apollo/client"
 
 type Props = {
   /** GraphQL error object */
