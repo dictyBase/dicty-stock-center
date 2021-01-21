2afbc1ee0a53e8aaad757edf7f42a07507dbf9c2
wildlifehexagon <crossthebreeze@gmail.com>
2021-01-21 15:55:42 -0600

    test: add tests for removing items

--- src/features/ShoppingCart/ShoppingCartItemList.test.tsx
+++ src/features/ShoppingCart/ShoppingCartItemList.test.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import ShoppingCartItemList from "./ShoppingCartItemList"
 import { MockCartProvider } from "common/utils/testing"
 import { fees } from "common/constants/fees"
@@ -41,6 +42,32 @@ describe("features/ShoppingCart/ShoppingCartItemList", () => {
       // three strains ($30 each) + one plasmid ($15) = $75
       const total = screen.getByText("$105.00")
       expect(total).toBeInTheDocument()
+    })
+  })
+
+  describe("button clicking", () => {
+    it("updates quantity on trash button click", () => {
+      render(
+        <MockCartProvider mocks={[]} addedItems={addedItems}>
+          <ShoppingCartItemList />
+        </MockCartProvider>,
+      )
+      const strainQuantity = screen.getAllByTestId("cart-quantity")[0]
+      expect(strainQuantity).toHaveTextContent(/Qty3/)
+      const trashButtons = screen.getAllByRole("button", {
+        name: "Remove Item",
+      })
+      // should have two buttons - one for each item row
+      expect(trashButtons).toHaveLength(2)
+      // click trash button on row with multiple of strain
+      userEvent.click(trashButtons[0])
+      // now all of those items from that row will be removed
+      // leaving one trash button
+      expect(
+        screen.getByRole("button", {
+          name: "Remove Item",
+        }),
+      ).toBeInTheDocument()
     })
   })
 })
