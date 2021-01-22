f5784ffe70a94b0e22931d0a726200342e6298cc
wildlifehexagon <crossthebreeze@gmail.com>
2021-01-22 08:48:10 -0600

    fix: remove unsupported browser warning

--- src/features/Home/Homepage.test.tsx
+++ src/features/Home/Homepage.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, screen, waitFor } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import Homepage from "./Homepage"
 import { MockAuthProvider } from "common/utils/testing"
 
@@ -12,26 +12,6 @@ describe("features/Home/Homepage", () => {
         </MockAuthProvider>,
       )
       expect(screen.getByText(/Hello, Art Vandelay!/)).toBeInTheDocument()
-    })
-  })
-  describe("browser warning", () => {
-    Object.defineProperty(window.navigator, "userAgent", {
-      value: "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/5.0)",
-      configurable: true,
-    })
-    it("should display BrowserWarning for IE 10", async () => {
-      render(
-        <MockAuthProvider mocks={[]}>
-          <Homepage />
-        </MockAuthProvider>,
-      )
-      await waitFor(() => {
-        expect(
-          screen.getByText(
-            /Dicty Stock Center may not work as expected on your browser./,
-          ),
-        ).toBeInTheDocument()
-      })
     })
   })
 })
