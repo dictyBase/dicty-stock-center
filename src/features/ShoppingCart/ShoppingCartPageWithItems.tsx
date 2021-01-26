3c5695adef2f2c6496d0ce8db0db4578972c8aba
wildlifehexagon <crossthebreeze@gmail.com>
2021-01-26 17:05:28 -0600

    refactor: remove shared styles

--- src/features/ShoppingCart/ShoppingCartPageWithItems.tsx
+++ src/features/ShoppingCart/ShoppingCartPageWithItems.tsx
@@ -1,39 +1,49 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 import Button from "@material-ui/core/Button"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import ShoppingCartItemList from "./ShoppingCartItemList"
 import { useCartStore } from "./CartStore"
-import useStyles from "./shoppingCartStyles"
 
+const useStyles = makeStyles((theme) => ({
+  continueBtn: {
+    fontWeight: 1000,
+    margin: theme.spacing(2),
+  },
+}))
+
+/**
+ * ShoppingCartPageWithItems is the display for the cart page when there are
+ * items in the cart.
+ */
 const ShoppingCartPageWithItems = () => {
-  const { state } = useCartStore()
+  const {
+    state: { maxItemsInCart },
+  } = useCartStore()
   const classes = useStyles()
 
   return (
-    <>
-      <Grid container justify="center">
-        <Grid item xs={12}>
-          <ShoppingCartItemList />
-        </Grid>
+    <Grid container justify="center">
+      <Grid item xs={12}>
+        <ShoppingCartItemList />
       </Grid>
-      <Grid container justify="center">
-        <Grid item className={classes.btnGrid}>
-          {!state.maxItemsInCart && (
-            <Button
-              component={Link}
-              to="/strains"
-              color="default"
-              variant="contained"
-              size="large"
-              className={classes.continueBtn}>
-              <FontAwesomeIcon icon="share" /> &nbsp; Continue Shopping
-            </Button>
-          )}
-        </Grid>
+      <Grid item>
+        {!maxItemsInCart && (
+          <Button
+            component={Link}
+            to="/strains"
+            color="default"
+            variant="contained"
+            size="large"
+            startIcon={<FontAwesomeIcon icon="share" />}
+            className={classes.continueBtn}>
+            Continue Shopping
+          </Button>
+        )}
       </Grid>
-    </>
+    </Grid>
   )
 }
 
