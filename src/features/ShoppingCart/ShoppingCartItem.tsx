9ae9593c9ae9e84bae1bef0c52fa54ac16e7319d
wildlifehexagon <crossthebreeze@gmail.com>
2021-01-27 09:45:59 -0600

    refactor: extract cart quantity logic into custom hook

--- src/features/ShoppingCart/ShoppingCartItem.tsx
+++ src/features/ShoppingCart/ShoppingCartItem.tsx
@@ -11,10 +11,9 @@ import IconButton from "@material-ui/core/IconButton"
 import Typography from "@material-ui/core/Typography"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import OutlinedDropdown from "common/components/OutlinedDropdown"
-import { useCartStore } from "features/ShoppingCart/CartStore"
 import useCartItems from "common/hooks/useCartItems"
+import useCartQuantity from "common/hooks/useCartQuantity"
 import strainOrPlasmid from "common/utils/strainOrPlasmid"
-import { maxItemsInCart } from "common/constants/cart"
 import { CartItemWithQuantity } from "common/types"
 
 const useStyles = makeStyles((theme) => ({
@@ -33,33 +32,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-/**
- * getDropdownValues generates a list of possible numbers based on how
- * many items are in the cart.
- */
-const getDropdownValues = (numItemsInCart: number, currentQuantity: number) => {
-  /**
-   * Process for generating values:
-   * 1. Create array of values from 1 to the current number of selected item in
-   *    cart (i.e. 1-4)
-   * 2. Create array of values from current quantity number (i.e. 4) to the
-   *    maximum allowed in cart (12 - 4, which would create an array of 4-8)
-   * 3. Merge these arrays
-   */
-  const arr = []
-  for (let i = 1; i < currentQuantity; i++) {
-    arr.push(i)
-  }
-
-  const secondArr = []
-  const availableToAdd = maxItemsInCart - numItemsInCart
-  for (let i = currentQuantity; i <= currentQuantity + availableToAdd; i++) {
-    secondArr.push(i)
-  }
-
-  return arr.concat(secondArr)
-}
-
 type Props = {
   /** Individual cart item with given quantity */
   item: CartItemWithQuantity
@@ -69,36 +41,9 @@ type Props = {
  * ShoppingCartItem is an individual item displayed in ShoppingCartList.
  */
 const ShoppingCartItem = ({ item }: Props) => {
-  const {
-    state: { addedItems },
-  } = useCartStore()
-  // get array of all items with given ID
-  const matchingItems = addedItems.filter((val) => val.id === item.id)
-  // generate values to display in quantity dropdown
-  const values = getDropdownValues(addedItems.length, matchingItems.length)
-  const { addToCart, removeFromCart } = useCartItems()
+  const { removeFromCart } = useCartItems()
+  const { handleChange, matchingItems, values } = useCartQuantity(item.id)
   const classes = useStyles()
-
-  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    // get quantity from dropdown selection
-    const qtyNum = Number(event.target.value)
-    // calculate the difference between how many of this item are in the cart and
-    // the quantity now selected in the dropdown
-    const qtyDiff = qtyNum - matchingItems.length
-
-    // if there's no change in quantity then do nothing
-    if (qtyDiff === 0) return
-    // negative quantity difference means the user is removing items
-    if (qtyDiff < 0) {
-      const removableItems = matchingItems.splice(0, Math.abs(qtyDiff))
-      removeFromCart(removableItems)
-    } else {
-      // if positive, generate an array of the same item with new quantity
-      // and add those items to the cart
-      const addableItems = Array(qtyDiff).fill(matchingItems[0])
-      addToCart(addableItems)
-    }
-  }
 
   const stock = strainOrPlasmid(item.id)
 
@@ -158,5 +103,4 @@ const ShoppingCartItem = ({ item }: Props) => {
   )
 }
 
-export { getDropdownValues }
 export default ShoppingCartItem
