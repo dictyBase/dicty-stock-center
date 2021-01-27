406578f167e40d616ae24d2fe5bd65bce34406c5
wildlifehexagon <crossthebreeze@gmail.com>
2021-01-27 10:37:58 -0600

    feat: add divider below cart header

--- src/features/ShoppingCart/ShoppingCartPage.tsx
+++ src/features/ShoppingCart/ShoppingCartPage.tsx
@@ -1,21 +1,27 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { makeStyles } from "@material-ui/core/styles"
 import Box from "@material-ui/core/Box"
 import Typography from "@material-ui/core/Typography"
+import Divider from "@material-ui/core/Divider"
 import ShoppingCartPageWithItems from "./ShoppingCartPageWithItems"
 import EmptyCartPage from "./EmptyCartPage"
 import { useCartStore } from "./CartStore"
 
+const useStyles = makeStyles((theme) => ({
+  divider: {
+    marginTop: theme.spacing(1),
+  },
+}))
+
 /** ShoppingCartPage displays different UIs based on whether
  *  there are currently items in the cart. **/
-
 const ShoppingCartPage = () => {
-  const {
-    state: { addedItems },
-  } = useCartStore()
+  const { state } = useCartStore()
+  const classes = useStyles()
 
   let content = <EmptyCartPage />
-  if (addedItems.length > 0) {
+  if (state.addedItems.length > 0) {
     content = <ShoppingCartPageWithItems />
   }
 
@@ -30,6 +36,7 @@ const ShoppingCartPage = () => {
       </Helmet>
       <Box marginTop={3} marginBottom={3}>
         <Typography variant="h1">Your Shopping Cart</Typography>
+        <Divider className={classes.divider} />
       </Box>
       {content}
     </React.Fragment>
