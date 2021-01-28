8d73bc2ca190b92204431b3f2cccb9e52cd3a1af
wildlifehexagon <crossthebreeze@gmail.com>
2021-01-28 17:21:27 -0600

    feat: redesign order confirmation page

--- src/features/OrderForm/OrderConfirmation.tsx
+++ src/features/OrderForm/OrderConfirmation.tsx
@@ -1,17 +1,35 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, Redirect } from "react-router-dom"
+import { makeStyles } from "@material-ui/core/styles"
+import Box from "@material-ui/core/Box"
 import Grid from "@material-ui/core/Grid"
 import Button from "@material-ui/core/Button"
-import Alert from "@material-ui/lab/Alert"
-import AlertTitle from "@material-ui/lab/AlertTitle"
 import Typography from "@material-ui/core/Typography"
+import { grey } from "@material-ui/core/colors"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import useStyles from "./formStyles"
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: grey[200],
+    margin: theme.spacing(2, 0, 2, 0),
+    minHeight: "320px",
+  },
+  button: {
+    "&:hover": {
+      color: "#fff",
+    },
+  },
+  confirmation: {
+    fontSize: "1.2em",
+  },
+}))
 
 type LocationProps = {
   location: {
+    /** URL Pathname */
     pathname: string
     state?: {
+      /** Submitted order ID */
       orderID: string
     }
   }
@@ -24,54 +42,46 @@ type LocationProps = {
 const OrderConfirmation = ({ location }: LocationProps) => {
   const classes = useStyles()
 
-  let content = (
-    <Alert className={classes.warningBox} severity="warning">
-      <AlertTitle>Unavailable</AlertTitle>
-      <p>This page is reserved for successful order submission.</p>
-      <p>
-        If you would like to place an order, please add items to your cart and
-        go through the checkout process.
-      </p>
-    </Alert>
-  )
-
-  if (location.state !== undefined) {
-    content = (
-      <div className={classes.alertBox}>
-        <Typography variant="h1">Success!</Typography>
-        <FontAwesomeIcon icon="check-circle" size="4x" />
-        <p className={classes.confirmation}>
-          <strong>Order ID: {location.state.orderID}</strong>
-        </p>
-        <p>We have sent you a confirmation email.</p>
-        <p>
-          The <strong>Payer</strong> will soon receive emails through the{" "}
-          <strong>NU Core</strong> (Northwestern University) system to complete
-          payment.
-        </p>
-      </div>
-    )
+  if (location.state === undefined) {
+    return <Redirect to="/" />
   }
 
   return (
-    <Grid container wrap="wrap" justify="center">
-      <Grid item xs={8}>
-        <Grid container wrap="wrap" justify="center">
-          <Grid item xs={12}>
-            {content}
-          </Grid>
-          <Grid item>
-            <Button
-              component={Link}
-              to="/"
-              color="primary"
-              variant="contained"
-              size="large"
-              className={classes.btn}>
-              <FontAwesomeIcon icon="home" /> &nbsp; Stock Center Home
-            </Button>
-          </Grid>
-        </Grid>
+    <Grid container alignItems="center" className={classes.container}>
+      <Grid item xs={12}>
+        <Box margin={2}>
+          <Typography gutterBottom variant="h1">
+            <FontAwesomeIcon icon="check-circle" /> Thank you for your order
+          </Typography>
+          <Box mt={3} mb={3}>
+            <Typography
+              gutterBottom
+              component="p"
+              className={classes.confirmation}>
+              <strong>Order ID: {location.state.orderID}</strong>
+            </Typography>
+          </Box>
+          <Box mb={3}>
+            <Typography gutterBottom component="p">
+              We have sent you a confirmation email.
+            </Typography>
+            <Typography gutterBottom component="p">
+              The <strong>Payer</strong> will soon receive emails through the{" "}
+              <strong>NU Core</strong> (Northwestern University) system to
+              complete payment.
+            </Typography>
+          </Box>
+          <Button
+            component={Link}
+            to="/"
+            color="primary"
+            variant="contained"
+            size="large"
+            className={classes.button}
+            startIcon={<FontAwesomeIcon icon="home" />}>
+            DSC Home
+          </Button>
+        </Box>
       </Grid>
     </Grid>
   )
