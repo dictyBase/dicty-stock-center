06a33e25a3bf4a15784dfe5d762f152941f30c8b
wildlifehexagon <crossthebreeze@gmail.com>
2021-01-29 11:19:25 -0600

    refactor: remove grids from payment method

--- src/features/OrderForm/Payment/PaymentMethod.tsx
+++ src/features/OrderForm/Payment/PaymentMethod.tsx
@@ -1,9 +1,21 @@
 import React, { useState } from "react"
-import { Grid } from "@material-ui/core"
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import { grey } from "@material-ui/core/colors"
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
 import TextField from "../TextField"
 import RequiredTextLabel from "../RequiredTextLabel"
-import useStyles from "../formStyles"
 import PaymentMethodRadioGroup from "./PaymentMethodRadioGroup"
+
+const useStyles = makeStyles((theme: Theme) => ({
+  waiver: {
+    backgroundColor: grey[200],
+    border: "1px solid #e6f2ff",
+    borderRadius: theme.spacing(1),
+    margin: theme.spacing(2),
+    padding: theme.spacing(3),
+  },
+}))
 
 /**
  * PaymentMethod contains radio buttons and a text field for listing payment method information.
@@ -15,31 +27,26 @@ const PaymentMethod = () => {
   const classes = useStyles()
 
   return (
-    <Grid
-      container
-      justify="center"
-      alignItems="center"
-      className={classes.innerForm}>
-      <RequiredTextLabel title="Payment Account" />
-      <Grid item xs={12} md={8}>
-        <PaymentMethodRadioGroup
-          setPurchaseOrderNum={setPurchaseOrderNum}
-          setWaiverRequested={setWaiverRequested}
-        />
-        {purchaseOrderNum && (
-          <TextField name="purchaseOrderNum" placeholder="PO Number" />
-        )}
-        {waiverRequested && (
-          <div className={classes.panelBlue}>
-            Please send an email to{" "}
-            <a href="mailto:dictystocks@northwestern.edu" target="_top">
-              dictystocks@northwestern.edu
-            </a>{" "}
-            describing why you need a waiver.
-          </div>
-        )}
-      </Grid>
-    </Grid>
+    <Box mt={1} mb={2} p={2}>
+      <RequiredTextLabel title="Payment Account" variant="h3" />
+      <Box mt={1} />
+      <PaymentMethodRadioGroup
+        setPurchaseOrderNum={setPurchaseOrderNum}
+        setWaiverRequested={setWaiverRequested}
+      />
+      {purchaseOrderNum && (
+        <TextField name="purchaseOrderNum" placeholder="PO Number" />
+      )}
+      {waiverRequested && (
+        <Typography component="p" className={classes.waiver}>
+          Please send an email to{" "}
+          <a href="mailto:dictystocks@northwestern.edu" target="_top">
+            dictystocks@northwestern.edu
+          </a>{" "}
+          describing why you need a waiver.
+        </Typography>
+      )}
+    </Box>
   )
 }
 
