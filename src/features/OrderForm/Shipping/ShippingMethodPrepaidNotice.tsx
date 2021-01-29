a513d4ba299d295ccc88132d3c7ff04c0f3e3a9a
wildlifehexagon <crossthebreeze@gmail.com>
2021-01-29 11:34:12 -0600

    feat: use grey background for prepaid notice

--- src/features/OrderForm/Shipping/ShippingMethodPrepaidNotice.tsx
+++ src/features/OrderForm/Shipping/ShippingMethodPrepaidNotice.tsx
@@ -1,5 +1,16 @@
 import React from "react"
-import useStyles from "../formStyles"
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import { grey } from "@material-ui/core/colors"
+import Typography from "@material-ui/core/Typography"
+
+const useStyles = makeStyles((theme: Theme) => ({
+  prepaid: {
+    backgroundColor: grey[200],
+    borderRadius: theme.spacing(1),
+    margin: theme.spacing(2),
+    padding: theme.spacing(3),
+  },
+}))
 
 /**
  * ShippingMethodPrepaidNotice contains the notice for sending a prepaid
@@ -10,14 +21,12 @@ const ShippingMethodPrepaidNotice = () => {
   const classes = useStyles()
 
   return (
-    <div className={classes.panelBlue}>
+    <Typography className={classes.prepaid}>
       If using a prepaid shipping label, please send ASAP to{" "}
-      <u>
-        <a href="mailto:dictystocks@northwestern.edu" target="_top">
-          dictystocks@northwestern.edu
-        </a>
-      </u>
-    </div>
+      <a href="mailto:dictystocks@northwestern.edu" target="_top">
+        dictystocks@northwestern.edu
+      </a>
+    </Typography>
   )
 }
 
