5050dafbb017f2592164a832d2d60cf2d0f65817
wildlifehexagon <crossthebreeze@gmail.com>
2021-01-29 10:43:36 -0600

    refactor: remove grids from comment box

--- src/features/OrderForm/Shipping/AdditionalInformation.tsx
+++ src/features/OrderForm/Shipping/AdditionalInformation.tsx
@@ -1,33 +1,23 @@
 import React from "react"
-import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
 import TextField from "../TextField"
-import useStyles from "../formStyles"
 
 /**
  * AdditionalInformation contains a text area for entering any optional comments.
  */
 
 const AdditionalInformation = () => {
-  const classes = useStyles()
-
   return (
-    <Grid
-      container
-      justify="center"
-      alignItems="center"
-      className={classes.innerForm}>
-      <Grid item xs={12} md={3}>
-        Comments:
-      </Grid>
-      <Grid item xs={12} md={8}>
-        <TextField
-          name="comments"
-          multiline
-          rows="5"
-          placeholder="Please enter any comments or special instructions here"
-        />
-      </Grid>
-    </Grid>
+    <Box mt={1} mb={2} p={2}>
+      <Typography variant="h3">Comments:</Typography>
+      <TextField
+        name="comments"
+        multiline
+        rows="5"
+        placeholder="Please enter any comments or special instructions here"
+      />
+    </Box>
   )
 }
 
