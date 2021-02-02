1ce636e16710a8d0fa0dd31a93d3d9ddf73a0953
wildlifehexagon <crossthebreeze@gmail.com>
2021-02-02 13:16:33 -0600

    refactor: move card into separate component

--- src/features/ShoppingCart/ShoppingCartPageWithItems.tsx
+++ src/features/ShoppingCart/ShoppingCartPageWithItems.tsx
@@ -1,32 +1,16 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import { makeStyles, Theme } from "@material-ui/core/styles"
-import { grey } from "@material-ui/core/colors"
 import Grid from "@material-ui/core/Grid"
 import List from "@material-ui/core/List"
-import Button from "@material-ui/core/Button"
-import Card from "@material-ui/core/Card"
-import Typography from "@material-ui/core/Typography"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import ShoppingCartItem from "./ShoppingCartItem"
 import ShoppingCartTotalCard from "./ShoppingCartTotalCard"
+import ContinueShoppingCard from "./ContinueShoppingCard"
 import { useCartStore } from "./CartStore"
 import { CartItem, CartItemWithQuantity } from "common/types"
 
 const useStyles = makeStyles((theme: Theme) => ({
   list: {
     paddingTop: "0px",
-  },
-  continueBtn: {
-    fontWeight: 600,
-    marginTop: theme.spacing(2),
-    "&:hover": {
-      color: grey[900],
-    },
-  },
-  card: {
-    padding: theme.spacing(2),
-    marginTop: theme.spacing(2),
   },
 }))
 
@@ -72,24 +56,7 @@ const ShoppingCartPageWithItems = () => {
       </Grid>
       <Grid item xs={3}>
         <ShoppingCartTotalCard />
-        {!maxItemsInCart && (
-          <Card className={classes.card}>
-            <Typography variant="h3" align="center">
-              Need something else?
-            </Typography>
-            <Button
-              component={Link}
-              to="/strains"
-              color="default"
-              variant="contained"
-              size="large"
-              fullWidth
-              startIcon={<FontAwesomeIcon icon="share" />}
-              className={classes.continueBtn}>
-              Continue Shopping
-            </Button>
-          </Card>
-        )}
+        {!maxItemsInCart && <ContinueShoppingCard />}
       </Grid>
     </Grid>
   )
