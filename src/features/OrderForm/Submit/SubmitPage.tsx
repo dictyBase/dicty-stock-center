fc44a05ab5e0bd566c270328c7e06784047d71e1
wildlifehexagon <crossthebreeze@gmail.com>
2021-02-03 11:41:34 -0600

    refactor: remove extra types file

--- src/features/OrderForm/Submit/SubmitPage.tsx
+++ src/features/OrderForm/Submit/SubmitPage.tsx
@@ -3,12 +3,21 @@ import Box from "@material-ui/core/Box"
 import OrderSummary from "./OrderSummary"
 import BackButton from "../BackButton"
 import SubmitButton from "./SubmitButton"
-import { SubmitProps } from "./submitTypes"
+import { FormikValues } from "../utils/initialValues"
+
+type Props = {
+  /** Full object of form data (shipping and payment) */
+  formData: FormikValues
+  /** Function to move to previous step */
+  prevStep: () => void
+  /** Function to set a submit error (bool) */
+  setSubmitError: (arg0: boolean) => void
+}
 
 /**
  * SubmitPage is the final page the user sees before submitting the order.
  */
-const SubmitPage = ({ formData, prevStep, setSubmitError }: SubmitProps) => {
+const SubmitPage = ({ formData, prevStep, setSubmitError }: Props) => {
   return (
     <Box mt={1} mb={2} p={2}>
       <OrderSummary formData={formData} />
