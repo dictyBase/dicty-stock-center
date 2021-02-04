df310f1a75fa5b4c0e763b4d0f58c4dbf56a5981
wildlifehexagon <crossthebreeze@gmail.com>
2021-02-04 16:53:09 -0600

    chore: add mock for refresh token

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,4 +1,6 @@
-import { graphql } from "msw"
+import { graphql, context } from "msw"
+import mockUser from "./mockUser"
+import { activeToken } from "./mockTokens"
 
 export const handlers = [
   graphql.query("StockList", (req, res, ctx) => {
@@ -15,4 +17,45 @@ export const handlers = [
       }),
     )
   }),
+  graphql.query("GetRefreshToken", (req, res, ctx) => {
+    if (!req.body?.variables.token) {
+      return res(
+        ctx.errors([
+          {
+            message: "refresh token does not exist",
+            extensions: {
+              code: "NotFound",
+              timestamp: "xyz",
+            },
+          },
+        ]),
+      )
+    }
+
+    return res(
+      ctx.data({
+        getRefreshToken: {
+          token: activeToken,
+          user: mockUser,
+          identity: {
+            provider: "orcid",
+          },
+        },
+      }),
+    )
+  }),
+  // graphql.mutation("Login", (req, res, ctx) => {
+  //   return res(
+  //     context.cookie("refresh-token", activeToken),
+  //     ctx.data({
+  //       login: {
+  //         token: activeToken,
+  //         user: mockUser,
+  //         identity: {
+  //           provider: "orcid",
+  //         },
+  //       },
+  //     }),
+  //   )
+  // }),
 ]
