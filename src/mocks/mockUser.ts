28fc3352012673b1c0fd8f2fdeae4bcfe7351814
wildlifehexagon <crossthebreeze@gmail.com>
2021-02-04 16:40:49 -0600

    chore: add mock user and token

---
+++ src/mocks/mockUser.ts
@@ -0,0 +1,17 @@
+const mockUser = {
+  id: 999,
+  email: "art@vandelayindustries.com",
+  first_name: "Art",
+  last_name: "Vandelay",
+  roles: [
+    {
+      role: "superuser",
+      permissions: {
+        permission: "admin",
+        resource: "dictybase",
+      },
+    },
+  ],
+}
+
+export default mockUser
