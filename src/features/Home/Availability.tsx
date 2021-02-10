a4cf99f977e6ac747565ac0a6a6e1480e5083b16
wildlifehexagon <crossthebreeze@gmail.com>
2021-02-10 11:28:43 -0600

    fix: remove invalid stock list query

--- src/features/Home/Availability.tsx
+++ src/features/Home/Availability.tsx
@@ -1,7 +1,8 @@
 import React from "react"
-import { useQuery } from "@apollo/client"
-import PanelLoader from "./PanelLoader"
-import { GET_STOCK_TOTALS } from "common/graphql/queries/stocks/lists"
+// import { useQuery } from "@apollo/client"
+// import PanelLoader from "./PanelLoader"
+// import { GET_STOCK_TOTALS } from "common/graphql/queries/stocks/lists"
+import Typography from "@material-ui/core/Typography"
 import useStyles from "./homeStyles"
 
 /**
@@ -10,31 +11,34 @@ import useStyles from "./homeStyles"
 
 const Availability = () => {
   const classes = useStyles()
-  const { loading, error, data } = useQuery(GET_STOCK_TOTALS, {
-    variables: {
-      limit: 100000,
-    },
-    fetchPolicy: "network-only",
-  })
+  // const { loading, error, data } = useQuery(GET_STOCK_TOTALS, {
+  //   variables: {
+  //     limit: 20000,
+  //   },
+  //   fetchPolicy: "network-only",
+  // })
 
-  if (loading) return <PanelLoader skeletonCount={5} />
+  // if (loading) return <PanelLoader skeletonCount={5} />
 
-  if (error)
-    return (
-      <div className={classes.panelGray}>
-        Error fetching number of strains and plasmids
-      </div>
-    )
+  // if (error)
+  //   return (
+  //     <div className={classes.panelGray}>
+  //       Error fetching number of strains and plasmids
+  //     </div>
+  //   )
 
   return (
     <div className={classes.panelGray}>
-      <h4>Strain & Plasmid Availability</h4>
-      <h5>
-        <strong>{data.listStrains.totalCount}</strong> Strains
-      </h5>
-      <h5>
-        <strong>{data.listPlasmids.totalCount}</strong> Plasmids
-      </h5>
+      <Typography variant="h4" gutterBottom>
+        Strain & Plasmid Availability
+      </Typography>
+      <Typography variant="body2">Total counts coming soon</Typography>
+      {/* <Typography variant="body2">
+        <strong>20,000</strong> Strains
+      </Typography>
+      <Typography variant="body2">
+        <strong>900</strong> Plasmids
+      </Typography> */}
     </div>
   )
 }
