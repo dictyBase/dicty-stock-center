25b7f101c8add80c1e4530d00c0922638350ca8a
wildlifehexagon <crossthebreeze@gmail.com>
2021-02-25 14:39:26 -0600

    feat: clean up error displays, fixes #337

--- src/features/Errors/ErrorMessage.tsx
+++ src/features/Errors/ErrorMessage.tsx
@@ -1,25 +1,13 @@
 import React from "react"
-import { makeStyles } from "@material-ui/core/styles"
 import Typography from "@material-ui/core/Typography"
-
-const useStyles = makeStyles((theme) => ({
-  link: {
-    color: "#fff",
-  },
-}))
-
 /**
  * ErrorMessage is the "contact us" message displayed on most error pages.
  */
 const ErrorMessage = () => {
-  const classes = useStyles()
-
   return (
     <Typography variant="body1" gutterBottom>
       If this problem persists, please email us at{" "}
-      <a className={classes.link} href="mailto:dictybase@northwestern.edu">
-        dictybase@northwestern.edu
-      </a>
+      <a href="mailto:dictybase@northwestern.edu">dictybase@northwestern.edu</a>
       .
     </Typography>
   )
