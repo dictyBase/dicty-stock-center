eabf832207ef84e8512a86587093d7bb99a527dc
wildlifehexagon <hartline.eric@gmail.com>
2021-04-14 13:00:24 -0500

    refactor: add real links to footer

---
+++ src/common/utils/footerItems.ts
@@ -0,0 +1,41 @@
+const footerItems = [
+  {
+    url: "/research/techniques",
+    description: "Techniques",
+  },
+  {
+    url: "/research/teach",
+    description: "Teaching Protocols",
+  },
+  {
+    url: "/stockcenter",
+    description: "Dicty Stock Center",
+  },
+  {
+    url:
+      "http://dictybase.org/tools/jbrowse/?data=data%2Fjbrowse%2Fdiscoideum&loc=6%3A1..50022&tracks=reference%2Cgene%2Ctranscript&highlight=",
+    description: "Genome Browser",
+  },
+  {
+    url: "/dictyaccess",
+    description: "DictyAccess",
+  },
+  {
+    url: "/community/conference",
+    description: "Conference",
+  },
+  {
+    url: "/community/labs",
+    description: "Labs",
+  },
+  {
+    url: "/about",
+    description: "About",
+  },
+  {
+    url: "/stockcenter/contact",
+    description: "Contact",
+  },
+]
+
+export default footerItems
