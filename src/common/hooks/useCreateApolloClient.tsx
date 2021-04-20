43d45c216bd4dbe212949d7dd68720bdfbf0ec83
wildlifehexagon <hartline.eric@gmail.com>
2021-04-20 10:28:51 -0500

    feat: add key to purge cache if schema changes

--- src/common/hooks/useCreateApolloClient.tsx
+++ src/common/hooks/useCreateApolloClient.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client"
 import { setContext } from "@apollo/client/link/context"
-import { persistCache, LocalStorageWrapper } from "apollo3-cache-persist"
+import { CachePersistor, LocalStorageWrapper } from "apollo3-cache-persist"
 import localForage from "localforage"
 import { mutationList } from "common/graphql/mutations"
 import {
@@ -10,6 +10,12 @@ import {
   listStrainsPagination,
   listPlasmidsPagination,
 } from "common/graphql/pagination"
+
+// SCHEMA_VERSION needs to be manually updated when there is a breaking schema change.
+// "1" is linked to https://github.com/dictyBase/graphql-schema/tree/17f3fda1bffe1c8348690ee9b9e4be6fc653d4f4
+const SCHEMA_VERSION = "1" // Must be a string.
+const SCHEMA_VERSION_KEY = "dsc-apollo-schema-version"
+const DSC_CACHE_KEY = "dsc-apollo-cache-persist"
 
 const isMutation = (value: string) => {
   if (mutationList.includes(value)) {
@@ -48,29 +54,40 @@ const authLink = setContext((request, { headers }) => {
   }
 })
 
+const server = getGraphQLServer(
+  process.env.REACT_APP_GRAPHQL_SERVER,
+  process.env.DEPLOY_ENV,
+  window.location.origin,
+)
+
+const link = authLink.concat(
+  createHttpLink({
+    uri: `${server}/graphql`,
+    credentials: "include",
+  }),
+)
+
 const useCreateApolloClient = () => {
   const [cacheInitializing, setCacheInitializing] = React.useState(true)
 
-  const server = getGraphQLServer(
-    process.env.REACT_APP_GRAPHQL_SERVER,
-    process.env.DEPLOY_ENV,
-    window.location.origin,
-  )
-
-  const link = authLink.concat(
-    createHttpLink({
-      uri: `${server}/graphql`,
-      credentials: "include",
-    }),
-  )
-
   React.useEffect(() => {
     const initializeCache = async () => {
-      await persistCache({
+      const persistor = new CachePersistor({
         cache,
         storage: new LocalStorageWrapper(localForage),
-        key: "dsc-apollo-cache-persist",
+        key: DSC_CACHE_KEY,
       })
+      const currentVersion = await localForage.getItem(SCHEMA_VERSION_KEY)
+      if (currentVersion === SCHEMA_VERSION) {
+        // If the current version matches the latest version,
+        // we're good to go and can restore the cache.
+        await persistor.restore()
+      } else {
+        // Otherwise, we'll want to purge the outdated persisted cache
+        // and mark ourselves as having updated to the latest version.
+        await persistor.purge()
+        await localForage.setItem(SCHEMA_VERSION_KEY, SCHEMA_VERSION)
+      }
       setCacheInitializing(false)
     }
 
