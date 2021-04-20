2381c8953808c455260146a2415ec185234839d0
wildlifehexagon <hartline.eric@gmail.com>
2021-04-20 09:52:09 -0500

    feat: clear cache on logout #37

--- src/features/Authentication/Logout.tsx
+++ src/features/Authentication/Logout.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from "react"
-import { useMutation } from "@apollo/client"
+import { useMutation, useApolloClient } from "@apollo/client"
 import { Redirect } from "react-router-dom"
 import { useAuthStore, ActionType } from "features/Authentication/AuthStore"
 import { LOGOUT } from "common/graphql/mutations"
 
 /**
- * Allows the user to logout
+ * Logout handles the user logout process.
  */
-
 const Logout = () => {
+  const client = useApolloClient()
   const [{ token }, dispatch] = useAuthStore()
   const [logout] = useMutation(LOGOUT, {
     context: {
@@ -23,7 +23,8 @@ const Logout = () => {
     dispatch({
       type: ActionType.LOGOUT,
     })
-  }, [dispatch, logout])
+    client.resetStore()
+  }, [dispatch, logout, client])
 
   return <Redirect to="/" />
 }
