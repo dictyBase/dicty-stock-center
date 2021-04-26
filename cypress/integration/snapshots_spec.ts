6861c395343617210c140b68ac56508bfd159718
wildlifehexagon <hartline.eric@gmail.com>
2021-04-26 11:30:49 -0500

    test: use findByText method for snapshots

--- cypress/integration/snapshots_spec.ts
+++ cypress/integration/snapshots_spec.ts
@@ -3,50 +3,30 @@ const screenWidths = [375, 768, 1280]
 describe("DSC snapshots", () => {
   it("visits the DSC homepage", () => {
     cy.visit("/")
-    cy.wait(800)
-    cy.contains("Genomic library pools")
+    cy.findByText(/Genomic library pools/)
     cy.percySnapshot("DSC homepage", { widths: screenWidths })
   })
   it("visits the order information page", () => {
     cy.visit("/information/order")
-    cy.contains("Ordering Information")
+    cy.findByText("Ordering Information")
     cy.percySnapshot("order information page", {
       widths: screenWidths,
     })
   })
-  // it("visits the strain catalog", () => {
-  //   cy.visit("/strains")
-  //   cy.contains("HL206")
-  //   cy.percySnapshot("strain catalog", {
-  //     widths: screenWidths,
-  //   })
-  // })
   it("visits a strain details page", () => {
     cy.visit("/strains/DBS0351367")
-    cy.contains("Extrachromosomal")
+    cy.findByText("Extrachromosomal")
     cy.percySnapshot("strain details", {
       widths: screenWidths,
     })
   })
-  // it("visits the plasmid catalog", () => {
-  //   cy.visit("/plasmids")
-  //   cy.contains("pSigK")
-  //   cy.percySnapshot("plasmid catalog", {
-  //     widths: screenWidths,
-  //   })
-  // })
   it("visits a plasmid details page", () => {
     cy.visit("/plasmids/DBP0001070")
-    cy.contains("overexpression")
+    cy.findByText(
+      "overexpression of fAR1 with YFP tag; parental vector: pDV-CYFP, dictyBase genes: far1, plasmid resistance: amp",
+    )
     cy.percySnapshot("plasmid details", {
       widths: screenWidths,
     })
   })
-  // it("visits a phenotype search results page", () => {
-  //   cy.visit("/phenotypes/abolished+protein+phosphorylation")
-  //   cy.contains("gskA-")
-  //   cy.percySnapshot("phenotype search results", {
-  //     widths: screenWidths,
-  //   })
-  // })
 })
