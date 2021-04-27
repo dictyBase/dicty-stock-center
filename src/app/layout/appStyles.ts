f579f888d091aafb50a149e63324fb96e941c867
wildlifehexagon <hartline.eric@gmail.com>
2021-04-27 11:18:09 -0500

    refactor: add font weight to headers

--- src/app/layout/appStyles.ts
+++ src/app/layout/appStyles.ts
@@ -5,6 +5,14 @@ const useStyles = makeStyles((theme: Theme) => ({
     fontSize: "16px",
     color: "#333",
     backgroundColor: "#fff",
+    "& h1, h2, h3, h4, h5, h6": {
+      fontWeight: 500,
+      lineHeight: 1.1,
+    },
+    "& h4, h5, h6": {
+      marginTop: theme.spacing(1.2),
+      marginBottom: theme.spacing(1.2),
+    },
   },
   main: {
     marginTop: theme.spacing(2),
