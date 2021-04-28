04cc4aaf5a61b8d1ccb83a03ac860917c2eeeac7
wildlifehexagon <hartline.eric@gmail.com>
2021-04-28 14:49:03 -0500

    chore: increase default timeout for cypress

--- cypress/integration/browse_catalog_spec.ts
+++ cypress/integration/browse_catalog_spec.ts
@@ -2,12 +2,12 @@ describe("browsing catalogs", () => {
   it("loads all strains", () => {
     cy.visit("/strains")
     cy.location("pathname").should("eq", "/stockcenter/strains")
-    cy.contains("DBS0351367")
+    cy.findByText(/DBS0351367/)
   })
 
   it("loads all plasmids", () => {
     cy.visit("/plasmids")
     cy.location("pathname").should("eq", "/stockcenter/plasmids")
-    cy.contains("DBP0001070")
+    cy.findByText(/DBP0001070/)
   })
 })
--- cypress/integration/snapshots_spec.ts
+++ cypress/integration/snapshots_spec.ts
@@ -3,7 +3,7 @@ const screenWidths = [375, 768, 1280]
 describe("DSC snapshots", () => {
   it("visits the DSC homepage", () => {
     cy.visit("/")
-    cy.contains("Genomic library pools", { timeout: 8000 })
+    cy.findByText(/Genomic library pools/)
     cy.percySnapshot("DSC homepage", { widths: screenWidths })
   })
   it("visits the order information page", () => {
