6ad4b32ba286ff9f56b8a32615ea257653252c2a
wildlifehexagon <hartline.eric@gmail.com>
2021-04-29 14:25:03 -0500

    test: add other catalog loading tests back in

--- cypress/integration/browse_catalog_spec.ts
+++ cypress/integration/browse_catalog_spec.ts
@@ -1,11 +1,11 @@
 describe("browsing catalogs", () => {
-  xit("loads all strains", () => {
+  it("loads all strains", () => {
     cy.visit("/strains")
     cy.location("pathname").should("eq", "/stockcenter/strains")
     cy.findByText(/DBS0351367/)
   })
 
-  xit("loads all plasmids", () => {
+  it("loads all plasmids", () => {
     cy.visit("/plasmids")
     cy.location("pathname").should("eq", "/stockcenter/plasmids")
     cy.findByText(/DBP0001070/)
