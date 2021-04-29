7fdd8399031531f88ae5973b6d8537e38cb5b7c0
wildlifehexagon <hartline.eric@gmail.com>
2021-04-29 14:33:53 -0500

    test: add snapshots to ordering process

--- cypress/integration/order_stocks_spec.ts
+++ cypress/integration/order_stocks_spec.ts
@@ -1,25 +1,31 @@
 describe("ordering stocks", () => {
   it("should add strains to cart, fill out order form and successfully submit", () => {
     cy.visit("/")
+    cy.findByText(/Genomic library pools/)
+    cy.percySnapshot("DSC homepage", { widths: screenWidths })
 
     cy.log("Navigate to strain catalog")
     // have to force click because of MUI list element "not visible"
     cy.findByRole("link", { name: /Strain Catalog/ }).click({ force: true })
     cy.location("pathname").should("eq", "/stockcenter/strains")
     cy.findByText(/DBS0351367/)
+    cy.percySnapshot("Strain catalog", { widths: screenWidths })
 
     cy.log("Navigate to details page")
     cy.findByRole("link", { name: /HL501\/X55/ }).click()
     cy.location("pathname").should("eq", "/stockcenter/strains/DBS0351365")
+    cy.percySnapshot("Strain details page", { widths: screenWidths })
 
     cy.log("Add two of this item to cart")
     cy.findByRole("button", { name: /Qty 1/i }).click()
     cy.findByRole("option", { name: "2" }).click()
     cy.findByRole("button", { name: /Add to Cart/i }).click()
+    cy.percySnapshot("Add to cart popup", { widths: screenWidths })
 
     cy.log("Go to cart from popup")
     cy.findByText(/Added to Cart/)
     cy.findByRole("button", { name: /View cart/i }).click()
+    cy.percySnapshot("Shopping cart page", { widths: screenWidths })
 
     cy.log("Review cart and navigate to checkout page")
     cy.location("pathname").should("eq", "/stockcenter/cart")
@@ -44,11 +50,17 @@ describe("ordering stocks", () => {
 
     cy.log("Select shipping method")
     cy.findByRole("radio", { name: /Send prepaid shipping label/ }).click()
+    cy.percySnapshot("Order form shipping details page", {
+      widths: screenWidths,
+    })
     cy.findByRole("button", { name: /Continue/i }).click()
 
     cy.log("Fill out payment information")
     cy.findByRole("checkbox").click()
     cy.findByRole("radio", { name: /Credit Card/i }).click()
+    cy.percySnapshot("Order form payment details page", {
+      widths: screenWidths,
+    })
     cy.findByRole("button", { name: /Continue/i }).click()
 
     cy.log("Verify and submit order")
@@ -56,11 +68,17 @@ describe("ordering stocks", () => {
     cy.findAllByText(/Art Vandelay/)
     cy.findByText(/Sending prepaid shipping label/i)
     cy.findByText(/Credit/i)
+    cy.percySnapshot("Order form submit page", {
+      widths: screenWidths,
+    })
     cy.findByRole("button", { name: /Submit/i }).click()
 
     cy.log("Verify order summary")
     cy.location("pathname").should("eq", "/stockcenter/order/submitted")
     cy.findByText(/Thank you for your order/)
     cy.findByText(/Order ID: 8888888/)
+    cy.percySnapshot("Order form summary page", {
+      widths: screenWidths,
+    })
   })
 })
