adecdc4323ed7ebf319d18f8aaa4c73442891a27
wildlifehexagon <hartline.eric@gmail.com>
2021-06-14 10:30:46 -0500

    fix: add basename to analytics tracking

--- src/common/hooks/useGoogleAnalytics.tsx
+++ src/common/hooks/useGoogleAnalytics.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { useLocation } from "react-router-dom"
 
 // useGoogleAnalytics is a hook to initialize GA tracking
-// currently uses universal analytics tag
+// currently using a universal analytics tag
 const useGoogleAnalytics = () => {
   const location = useLocation()
 
@@ -10,9 +10,12 @@ const useGoogleAnalytics = () => {
     const setGoogleAnalytics = async () => {
       try {
         const module = await import("react-ga")
-        const page = location.pathname + location.search
+        const trackingID = process.env.REACT_APP_GA_TRACKING_ID
+        const basename = process.env.REACT_APP_BASENAME
+        const page = basename + location.pathname + location.search
         let ReactGA = module.default
-        ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID)
+
+        ReactGA.initialize(trackingID)
         ReactGA.set({ page: page, anonymizeIp: true })
         ReactGA.pageview(page)
 
