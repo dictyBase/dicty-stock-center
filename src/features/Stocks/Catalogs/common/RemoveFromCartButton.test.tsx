c360b2c1b0b9cf8331a10e0867dc35aac849ddd9
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-20 17:37:59 -0500

    test: add tests for RemoveFromCartButton component

---
+++ src/features/Stocks/Catalogs/common/RemoveFromCartButton.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen, waitFor } from "@testing-library/react"
+import RemoveFromCartButton from "./RemoveFromCartButton"
+import userEvent from "@testing-library/user-event"
+
+describe("Stocks/Catalog//common/AppBar/AppBarSearch", () => {
+  const MockComponent = () => <RemoveFromCartButton handleClick={() => {}} />
+  const buttonTitle = "Remove from cart"
+
+  describe("initial render", () => {
+    it("should render chip holder", () => {
+      render(<MockComponent />)
+      expect(screen.getByTitle(buttonTitle)).toBeInTheDocument()
+    })
+
+    it("should fire onClick", async () => {
+      render(<MockComponent />)
+      const button = screen.getByTitle(buttonTitle)
+      await waitFor(() => userEvent.click(button))
+    })
+
+    it("should render on small window", () => {
+      render(<MockComponent />)
+      global.innerWidth = 300
+      global.dispatchEvent(new Event("resize"))
+      const button = screen.getByTitle(buttonTitle)
+      expect(button).toBeInTheDocument()
+      expect(button.firstElementChild).toBeInTheDocument()
+    })
+  })
+})
