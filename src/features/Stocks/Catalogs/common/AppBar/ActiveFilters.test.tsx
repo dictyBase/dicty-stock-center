2fe7ae618be8999fd227109bb74e865039a2b717
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-21 07:58:51 -0500

    test: test cases for remove active filters

--- src/features/Stocks/Catalogs/common/AppBar/ActiveFilters.test.tsx
+++ src/features/Stocks/Catalogs/common/AppBar/ActiveFilters.test.tsx
@@ -1,56 +1,12 @@
 import { render, screen, waitFor } from "@testing-library/react"
-import { useHistory } from "react-router-dom"
-import useCatalogStore from "features/Stocks/Catalogs/context/useCatalogStore"
-import useCatalogDispatch from "features/Stocks/Catalogs/context/useCatalogDispatch"
 import ActiveFilters from "./ActiveFilters"
-import { CatalogProvider } from "../../context/CatalogContext"
 import userEvent from "@testing-library/user-event"
-
-const mockHistoryPush = jest.fn()
-
-jest.mock("react-router-dom", () => {
-  const originalModule = jest.requireActual("react-router-dom")
-  return {
-    ...originalModule,
-    useLocation: () => ({
-      search: "?filter=regular",
-    }),
-    useHistory: jest.fn(),
-  }
-})
-const mockSetQueryVariables = jest.fn()
-const mockSetActiveFilters = jest.fn()
-
-jest.mock("features/Stocks/Catalogs/context/useCatalogDispatch")
-const mockedUseCatalogDispatch = useCatalogDispatch as jest.Mock
-mockedUseCatalogDispatch.mockReturnValue({
-  setQueryVariables: mockSetQueryVariables,
-  setActiveFilters: mockSetActiveFilters,
-})
-
-jest.mock("features/Stocks/Catalogs/context/useCatalogStore")
-const mockedUseCatalogStore = useCatalogStore as jest.Mock
-mockedUseCatalogStore.mockReturnValue({
-  state: {
-    searchValue: "GWDI",
-    leftDropdownValue: "regular",
-    activeFilters: ["Regular"],
-  },
-})
 
 describe("Stocks/Catalog//common/AppBar/AppBarSearch", () => {
   let i = 0
   const MockComponent = () => (
-    <CatalogProvider stockType="strain">
-      <ActiveFilters removeFilter={() => i++} />
-    </CatalogProvider>
+    <ActiveFilters filters={["Regular"]} removeFilter={() => i++} />
   )
-
-  beforeEach(() => {
-    ;(useHistory as jest.Mock).mockReturnValue({
-      push: mockHistoryPush,
-    })
-  })
 
   afterEach(() => {
     i = 0
