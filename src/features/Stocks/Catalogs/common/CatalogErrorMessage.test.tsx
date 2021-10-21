7ffd13e3e8dbe05edc586c0e6b78c45ae2365a40
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-20 19:34:47 -0500

    test: cover CatalogErrorMessage

---
+++ src/features/Stocks/Catalogs/common/CatalogErrorMessage.test.tsx
@@ -0,0 +1,33 @@
+import CatalogErrorMessage from "./CatalogErrorMessage"
+
+describe("Stocks/Catalog/common/CatalogErrorMessage", () => {
+  describe("initial render", () => {
+    it("should use function", () => {
+      expect(
+        CatalogErrorMessage({
+          error: {
+            graphQLErrors: [],
+            name: "",
+            extraInfo: undefined,
+            networkError: null,
+            message: "GraphQL error: You must be logged in",
+          },
+        }),
+      ).not.toBeNull()
+
+      expect(
+        CatalogErrorMessage({
+          error: {
+            graphQLErrors: [],
+            name: "",
+            extraInfo: undefined,
+            networkError: new Error("Hello"),
+            message: "GraphQL error: You must be logged in",
+          },
+        }),
+      ).not.toBeNull()
+
+      expect(CatalogErrorMessage({})).toBeNull()
+    })
+  })
+})
