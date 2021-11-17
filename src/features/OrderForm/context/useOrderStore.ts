78c1f140933785e772d86e4b1b5ca0cc524e3f1a
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-16 18:58:23 -0600

    refactor: code climate similar blocks issue

--- src/features/OrderForm/context/useOrderStore.ts
+++ src/features/OrderForm/context/useOrderStore.ts
@@ -3,12 +3,10 @@ import { OrderContext } from "./OrderContext"
 
 const useOrderStore = () => {
   const orderContext = React.useContext(OrderContext)
-  if (!orderContext) {
-    throw new Error(
-      "useOrderStore must only be used inside an OrderProvider component",
-    )
-  }
-  return orderContext
+  
+  if (orderContext) return orderContext
+  // Throw error if hook is used outside OrderProvider
+  throw new Error("useOrderStore must only be used inside OrderProvider")
 }
 
 export default useOrderStore
