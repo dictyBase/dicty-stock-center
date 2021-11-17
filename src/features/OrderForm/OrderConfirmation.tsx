6b4362db47a9f9b4131a80e3de35475e3d3f84ba
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-17 14:21:24 -0600

    refactor: smaller chunks for codeclimate

--- src/features/OrderForm/OrderConfirmation.tsx
+++ src/features/OrderForm/OrderConfirmation.tsx
@@ -27,6 +27,50 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }))
 
+const BackToHomepage = ({ button }: { button: string }) => (
+  <Grid item>
+    <Box margin={2}>
+      <Button
+        component={Link}
+        to="/"
+        color="primary"
+        variant="contained"
+        size="large"
+        className={button}
+        startIcon={<FontAwesomeIcon icon="home" />}>
+        Back to DSC homepage
+      </Button>
+    </Box>
+  </Grid>
+)
+
+const OrderHeader = ({
+  confirmation,
+  orderID,
+}: {
+  confirmation: string
+  orderID: string
+}) => (
+  <Box mt={3} mb={3}>
+    <Typography gutterBottom component="p" className={confirmation}>
+      <strong>Order ID: {orderID}</strong>
+    </Typography>
+  </Box>
+)
+
+const OrderDescription = () => (
+  <Box mb={3}>
+    <Typography gutterBottom component="p">
+      We have sent you a confirmation email.
+    </Typography>
+    <Typography gutterBottom component="p">
+      The <strong>Payer</strong> will soon receive emails through the{" "}
+      <strong>NU Core</strong> (Northwestern University) system to complete
+      payment.
+    </Typography>
+  </Box>
+)
+
 /**
  * Displays notification that the user's order was submitted successfully.
  */
@@ -46,24 +90,11 @@ const OrderConfirmation = () => {
           <Typography gutterBottom variant="h1">
             <FontAwesomeIcon icon="check-circle" /> Thank you for your order
           </Typography>
-          <Box mt={3} mb={3}>
-            <Typography
-              gutterBottom
-              component="p"
-              className={classes.confirmation}>
-              <strong>Order ID: {state.orderID}</strong>
-            </Typography>
-          </Box>
-          <Box mb={3}>
-            <Typography gutterBottom component="p">
-              We have sent you a confirmation email.
-            </Typography>
-            <Typography gutterBottom component="p">
-              The <strong>Payer</strong> will soon receive emails through the{" "}
-              <strong>NU Core</strong> (Northwestern University) system to
-              complete payment.
-            </Typography>
-          </Box>
+          <OrderHeader
+            confirmation={classes.confirmation}
+            orderID={state.orderID}
+          />
+          <OrderDescription />
           <PDFViewer width={800} height={600}>
             <OrderSummaryPDF
               cartItems={state.cartItems}
@@ -74,20 +105,7 @@ const OrderConfirmation = () => {
           </PDFViewer>
         </Box>
       </Grid>
-      <Grid item>
-        <Box margin={2}>
-          <Button
-            component={Link}
-            to="/"
-            color="primary"
-            variant="contained"
-            size="large"
-            className={classes.button}
-            startIcon={<FontAwesomeIcon icon="home" />}>
-            Back to DSC homepage
-          </Button>
-        </Box>
-      </Grid>
+      <BackToHomepage button={classes.button} />
     </Grid>
   )
 }
